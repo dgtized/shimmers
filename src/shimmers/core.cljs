507b80426765e63cbf823c68da6078d270de8a1b
Charles Comstock <dgtized@gmail.com>
2020-08-25 13:57:15 -0500

    undulating "circle" like shape

--- src/shimmers/core.cljs
+++ src/shimmers/core.cljs
@@ -3,24 +3,30 @@
             [quil.middleware :as m]))
 
 (defn setup []
-  (q/frame-rate 30)
-  (q/color-mode :hsb)
-  {:color 0
-   :angle 0})
+  (q/frame-rate 30))
 
 (defn update-state [state]
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  state)
 
-(defn draw-state [state]
-  (q/background 240)
-  (q/fill (:color state) 255 255)
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      (q/ellipse x y 100 100))))
+(defn draw-state [{:keys [dt]}]
+  (q/background 0)
+  (q/stroke 255)
+  (q/no-fill)
+  (q/with-translation [(/ (q/width) 2)
+                       (/ (q/height) 2)]
+    (q/begin-shape)
+    (loop [angle 0.0]
+      (if (<= angle (* 2 Math/PI))
+        (do
+          (let [dt (/ (q/frame-count) 100)
+                xoff (+ (q/cos angle) 1)
+                yoff (+ (q/sin angle) 1)
+                r (q/map-range (q/noise xoff yoff dt) 0 1 100 200)
+                x (* r (q/cos angle))
+                y (* r (q/sin angle))]
+            (q/vertex x y))
+          (recur (+ angle 0.05)))))
+    (q/end-shape :close)))
 
 ;; this function is called in index.html
 (defn ^:export run-sketch []
