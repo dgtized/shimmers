194013fee290a3ffdf517828b996d0665651d938
Charles Comstock <dgtized@gmail.com>
2020-08-25 16:56:30 -0500

    construct translated vertices and then draw vertices separately

--- src/shimmers/core.cljs
+++ src/shimmers/core.cljs
@@ -9,22 +9,23 @@
 (defn update-state [state]
   (update state :theta (fn [theta] (rem (+ theta 0.05) (* 2 Math/PI)))))
 
-(defn circle-blob [pos rmin rmax]
-  (let [dtheta (/ (* 2 Math/PI) 10)
-        vertices (for [angle (take-while (fn [x] (<= x (* 2 Math/PI)))
-                                         (iterate (partial + dtheta) 0.0))]
-                   (let [dt (/ (q/frame-count) 50)
-                         xoff (+ (q/cos angle) 1)
-                         yoff (+ (q/sin angle) 1)
-                         r (q/map-range (q/noise xoff yoff dt) 0 1 rmin rmax)
-                         x (* r (q/cos angle))
-                         y (* r (q/sin angle))]
-                     [x y]))]
-    (q/with-translation pos
-      (q/begin-shape)
-      (doseq [[x y] vertices]
-        (q/vertex x y))
-      (q/end-shape :close))))
+(defn circle-blob [[cx cy] rmin rmax]
+  (let [dtheta (/ (* 2 Math/PI) 10)]
+    (for [angle (take-while (fn [x] (<= x (* 2 Math/PI)))
+                            (iterate (partial + dtheta) 0.0))]
+      (let [dt (/ (q/frame-count) 50)
+            xoff (+ (q/cos angle) 1)
+            yoff (+ (q/sin angle) 1)
+            r (q/map-range (q/noise xoff yoff dt) 0 1 rmin rmax)
+            x (+ cx (* r (q/cos angle)))
+            y (+ cy (* r (q/sin angle)))]
+        [x y]))))
+
+(defn draw-shape [vertices]
+  (q/begin-shape)
+  (doseq [[x y] vertices]
+    (q/vertex x y))
+  (q/end-shape :close))
 
 (defn polar-coord [theta radius x y]
   [(+ x (* radius (q/cos theta)))
@@ -34,10 +35,10 @@
   (q/background 0)
   (q/stroke 255)
   (q/no-fill)
-  (circle-blob (polar-coord theta 50 100 100)
-               25 50)
-  (circle-blob (polar-coord (+ theta 2) 25 400 400)
-               25 50))
+  (draw-shape (circle-blob (polar-coord theta 50 100 100)
+                           25 50))
+  (draw-shape (circle-blob (polar-coord (+ theta 2) 25 400 400)
+                           25 50)))
 
 ;; this function is called in index.html
 (defn ^:export run-sketch []
