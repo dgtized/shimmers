bb4c667c2056c784f5aed33a6e9b79aa83eecc69
Charles Comstock <dgtized@gmail.com>
2020-10-21 00:06:17 -0500

    Color each particle randomly

--- src/shimmers/particles.cljs
+++ src/shimmers/particles.cljs
@@ -3,12 +3,18 @@
             [quil.middleware :as m]
             [shimmers.vector :as v]))
 
+(def colors [[128 32]
+             [128 0 0 32]
+             [0 0 128 32]
+             [0 128 0 32]])
+
 (defn make-particle []
   (let [initial-pos [(q/random (q/width)) (q/random (q/height))]]
     {:last-pos initial-pos
      :position initial-pos
      :velocity (q/random-2d)
-     :acceleration (q/random-2d)}))
+     :acceleration (q/random-2d)
+     :color (rand-nth colors)}))
 
 (defn wrap-value [x lower upper]
   (if (< x lower) upper
@@ -24,26 +30,27 @@
    (q/constrain y lower upper)])
 
 (defn update-particle
-  [{:keys [position velocity acceleration]} particle]
+  [{:keys [position velocity acceleration] :as particle}]
   (let [new-velocity (-> (v/add velocity acceleration) (constrain2d -1.5 1.5))
         new-position (v/add position new-velocity)
         wrapped-position (wrap-around new-position)]
-    {:last-pos (if (= wrapped-position new-position) position wrapped-position)
-     :position wrapped-position
-     :velocity new-velocity
-     :acceleration (v/scale (q/random-2d) 0.5)}))
+    (assoc particle
+           :last-pos (if (= wrapped-position new-position) position wrapped-position)
+           :position wrapped-position
+           :velocity new-velocity
+           :acceleration (v/scale (q/random-2d) 0.5))))
 
 (defn setup []
   (q/background "white")
-  {:particles (repeatedly 100 make-particle)})
+  {:particles (repeatedly 50 make-particle)})
 
 (defn update-state [state]
   (update-in state [:particles] (partial map update-particle)))
 
 (defn draw [{:keys [particles]}]
   ;; (q/background 256 16)
-  (q/stroke 128 32)
-  (doseq [{:keys [position last-pos]} particles]
+  (doseq [{:keys [position last-pos color]} particles]
+    (apply q/stroke color)
     (let [[lx ly] last-pos
           [x y] position]
       (q/line lx ly x y))))
