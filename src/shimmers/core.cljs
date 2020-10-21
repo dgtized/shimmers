cd203d4fc9149329f0f58a47dceded69ef9b22f9
Charles Comstock <dgtized@gmail.com>
2020-10-20 21:00:13 -0500

    experiment in hard-coding sketch swapping

--- src/shimmers/core.cljs
+++ src/shimmers/core.cljs
@@ -5,5 +5,16 @@
 (enable-console-print!)
 
 ;; initialize sketch on first-load
-(defonce start-up (ray-marching/run-sketch))
+
+(defn ^:export run-sketch []
+  (q/defsketch particles
+    :host "quil-host"
+    :size [500 500]
+    :setup (fn [] (q/background "white"))
+    :draw (fn [] (q/point (q/random (q/width))
+                         (q/random (q/height))))))
+
+(defonce start-up (ray-marching/run-sketch)
+  #_(run-sketch))
+
 
