c633d369f1c26413d3d54ae3743bc319280e5267
Charles Comstock <dgtized@gmail.com>
2020-10-28 18:50:03 -0500

    Introduce color/random-gradient with a conversion function

--- src/shimmers/color.cljs
+++ src/shimmers/color.cljs
@@ -1,5 +1,7 @@
 (ns shimmers.color
-  (:require [quil.core :as q :include-macros true]))
+  (:require [quil.core :as q :include-macros true]
+            [thi.ng.color.core :as col]
+            [thi.ng.color.gradients :as grad]))
 
 (defn random []
   (let [colors [[128 192 128 32]
@@ -12,3 +14,14 @@
 (defn random-lerp [from to]
   (let [color (q/lerp-color from to (rand))]
     [(q/red color) (q/green color) (q/blue color) 32]))
+
+(defn as-vector [color]
+  (map (fn [x] (float (* x 255)))
+       [(col/red color) (col/green color) (col/blue color) (col/alpha color)]))
+
+(defn random-gradient [scheme]
+  (let [color (->> scheme
+                   grad/cosine-schemes
+                   (grad/cosine-gradient 32)
+                   rand-nth)]
+    (as-vector (col/adjust-alpha color -0.95))))
--- src/shimmers/particles.cljs
+++ src/shimmers/particles.cljs
@@ -22,7 +22,7 @@
      :acceleration (v/vec2 0 0)
      :mass (apply q/random mass-range)
      ;; :color (color/random)
-     :color (color/random-lerp (q/color "cyan") (q/color "blue"))}))
+     :color (color/random-gradient :green-cyan)}))
 
 (defn stokes-drag [velocity]
   "Viscous resistance is a negative force proportional to velocity.
