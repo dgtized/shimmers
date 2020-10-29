46c0bbe105decb5adbdf34602ce1acfddd49594a
Charles Comstock <dgtized@gmail.com>
2020-10-29 01:02:35 -0500

    Fix framerate function to require goog.string.format without name mangling

--- src/shimmers/framerate.cljs
+++ src/shimmers/framerate.cljs
@@ -1,10 +1,14 @@
 (ns shimmers.framerate
-  (:require [goog.string :as gs]
+  ;; see https://martinklepsch.org/posts/requiring-closure-namespaces.html for
+  ;; weirdness around requiring goog.string.format to force it to create format
+  ;; in goog.string. Requiring goog.string will not add format to the namespace,
+  ;; but works until compiling with :optimization :advanced.
+  (:require [goog.string.format]
             [goog.dom :as dom]))
 
 ;; TODO: move to quil middleware
 (defn display [value]
   (let [rate (if (= value "")
                ""
-               (gs/format "(framerate: %.1f)" value))]
+               (goog.string/format "(framerate: %.1f)" value))]
     (dom/setTextContent (dom/getElement "framerate") rate)))
