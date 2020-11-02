f5f51cbb5e40dc7046a909bd523fef49f83b1eba
Charles Comstock <dgtized@gmail.com>
2020-11-02 03:32:07 -0600

    Change to a rectangle function and add some spin

--- src/shimmers/cube.cljs
+++ src/shimmers/cube.cljs
@@ -4,22 +4,29 @@
             [shimmers.math.vector :as v]
             [shimmers.framerate :as framerate]))
 
+(defn rectangle [[x y] width height]
+  [(v/vec2 x y) (v/vec2 (+ x width) y)
+   (v/vec2 (+ x width) (+ y height))
+   (v/vec2 x (+ y height))])
+
 (defn setup []
-  (q/background "white")
-  {:vertices [(v/vec2 100 100) (v/vec2 200 100)
-              (v/vec2 200 200) (v/vec2 100 200)]})
+  {:vertices (rectangle [0 0] 50 50)})
 
 (defn update-state [state]
   state)
 
 (defn draw [{:keys [vertices]}]
+  (q/background "white")
   (q/stroke "black")
   (q/stroke-weight 1)
-  (q/begin-shape)
-  (doseq [vertex vertices]
-    (apply q/vertex vertex))
-  (apply q/vertex (first vertices))
-  (q/end-shape)
+  (q/point 150 150)
+  (q/with-translation [200 200]
+    (q/with-rotation [(mod (/ (q/frame-count) 100) 360)]
+      (q/begin-shape)
+      (doseq [vertex vertices]
+        (apply q/vertex vertex))
+      (apply q/vertex (first vertices))
+      (q/end-shape)))
   (framerate/display (q/current-frame-rate)))
 
 (defn ^:export run-sketch []
