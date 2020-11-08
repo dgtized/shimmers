2cb926ce2bd4d095e6d48d4cd70ffef388a44fb7
Charles Comstock <dgtized@gmail.com>
2020-11-08 06:37:11 -0600

    Model flame at a point as fuel/heat

--- src/shimmers/fire.cljs
+++ src/shimmers/fire.cljs
@@ -12,15 +12,29 @@
     0.0))
 
 (defn setup []
-  (q/background "black")
-  (let [size 10
+  (q/frame-rate 5)
+  (let [size 20
         w    (/ (q/width) size)
         h    (/ (q/height) size)]
     {:size size
      :fire (nd/ndarray :float64 (repeatedly (* w h) #(fire-prob)) [w h])
      :fuel (nd/ndarray :float64 (repeatedly (* w h) #(rand)) [w h])}))
 
-(defn update-state [state]
+(defn update-state [{:keys [fire fuel size] :as state}]
+  (let [[xdim ydim] (nd/shape fire)]
+    (loop/c-for [x 0 (< x xdim) (inc x)
+                 y 0 (< y ydim) (inc y)]
+      (let [heat (nd/get-at fire x y)
+            wood (nd/get-at fuel x y)]
+        (if (> heat 0.1)
+          (if (> wood 0.2)
+            (do (nd/set-at fire x y (min (* heat 1.05) 1))
+                (nd/set-at fuel x y (* wood 0.96)))
+            (do (nd/set-at fire x y (min (* heat 0.75) 1))
+                (nd/set-at fuel x y (* wood 0.99))))
+          (when (and (> heat 0.6) (> (rand) 0.6))
+            ;; expand to neighbor
+            )))))
   state)
 
 (defn paint [grid size color]
@@ -35,6 +49,7 @@
           (q/ellipse (* x size) (* y size) (q/lerp 1 size v) (q/lerp 1 size v)))))))
 
 (defn draw [{:keys [fire fuel size]}]
+  (q/background "black")
   (paint fuel size [0 255 0 255])
   (paint fire size [255 0 0 255])
   (framerate/display (q/current-frame-rate)))
