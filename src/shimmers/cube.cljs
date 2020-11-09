d07b2d0c1f12dff93bcb1ad9504aa64aec74881a
Charles Comstock <dgtized@gmail.com>
2020-11-09 13:37:41 -0600

    Remove rotation/translation and center at 175,175,0

--- src/shimmers/cube.cljs
+++ src/shimmers/cube.cljs
@@ -4,13 +4,13 @@
             [shimmers.math.vector :as v]
             [shimmers.framerate :as framerate]))
 
-(defn rectangle [[x y] width height]
+(defn rectangle [[x y z] width height]
   [(v/vec2 x y) (v/vec2 (+ x width) y)
    (v/vec2 (+ x width) (+ y height))
    (v/vec2 x (+ y height))])
 
 (defn setup []
-  {:vertices (rectangle [0 0] 50 50)})
+  {:vertices (rectangle [175 175 0] 50 50)})
 
 (defn update-state [state]
   state)
@@ -19,14 +19,11 @@
   (q/background "white")
   (q/stroke "black")
   (q/stroke-weight 1)
-  (q/point 150 150)
-  (q/with-translation [200 200]
-    (q/with-rotation [(mod (/ (q/frame-count) 100) 360)]
-      (q/begin-shape)
-      (doseq [vertex vertices]
-        (apply q/vertex vertex))
-      (apply q/vertex (first vertices))
-      (q/end-shape)))
+  (q/begin-shape)
+  (doseq [vertex vertices]
+    (apply q/vertex vertex))
+  (apply q/vertex (first vertices))
+  (q/end-shape)
   (framerate/display (q/current-frame-rate)))
 
 (defn ^:export run-sketch []
