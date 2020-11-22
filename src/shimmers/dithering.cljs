539201a442f2c30cc2e2fe33a1beed79a2e759a8
Charles Comstock <dgtized@gmail.com>
2020-11-21 20:24:44 -0600

    Make width/height properties of state from setup and simplify accordingly

--- src/shimmers/dithering.cljs
+++ src/shimmers/dithering.cljs
@@ -4,15 +4,16 @@
             [shimmers.macros.loop :as loop]
             [shimmers.framerate :as framerate]))
 
-(def width 320)
-(def height 240)
-
 (defn setup []
-  (let [applet (quil.sketch/current-applet)
+  (let [width 320
+        height 240
+        applet (quil.sketch/current-applet)
         capture (.createCapture applet "video")]
     (.size capture width height)
     (.hide capture)
-    {:capture capture}))
+    {:width width
+     :height height
+     :capture capture}))
 
 (defn closest-color [color]
   (* 256 (q/round (/ color 256))))
@@ -20,41 +21,41 @@
 (defn idx [x y width]
   (* 4 (+ x (* y width))))
 
-(defn read [pixels x y]
-  (aget pixels (idx x y width)))
+(defn write [pixels i v]
+  (aset pixels (+ i 0) v)
+  (aset pixels (+ i 1) v)
+  (aset pixels (+ i 2) v)
+  (aset pixels (+ i 3) 255))
 
-(defn write [pixels x y v]
-  (let [i (idx x y width)]
-    (aset pixels (+ i 0) v)
-    (aset pixels (+ i 1) v)
-    (aset pixels (+ i 2) v)
-    (aset pixels (+ i 3) 255)))
-
-(defn propagate [source x y error]
-  (let [i (idx x y width)]
-    (aset source i (+ error (aget source i)))))
+(defn propagate [source i error]
+  (aset source i (+ error (aget source i))))
 
 ;; https://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
-(defn dither [capture]
+(defn dither [capture width height]
   (let [image (q/create-image width height)
         source(q/pixels capture)
         target (q/pixels image)]
     (dotimes [y height]
       (dotimes [x width]
-        (let [old (read source x y)
+        (let [i (idx x y width)
+              old (aget source i)
               new (closest-color old)
               error (- old new)]
-          (write target x y new)
-          (propagate source (inc x) y (* error (/ 7 16)))
-          (propagate source (dec x) (inc y) (* error (/ 3 16)))
-          (propagate source x (inc y) (* error (/ 5 16)))
-          (propagate source (inc x) (inc y) (* error (/ 1 16))))))
+          (write target i new)
+          (propagate source (idx (inc x) y width)
+                     (* error (/ 7 16)))
+          (propagate source (idx (dec x) (inc y) width)
+                     (* error (/ 3 16)))
+          (propagate source (idx x (inc y) width)
+                     (* error (/ 5 16)))
+          (propagate source (idx (inc x) (inc y) width)
+                     (* error (/ 1 16))))))
     (q/update-pixels image)
     image))
 
-(defn draw [{:keys [capture]}]
+(defn draw [{:keys [capture width height]}]
   (q/background 255)
-  (q/image (dither capture) 0 0 (* width 2) (* height 2))
+  (q/image (dither capture width height) 0 0 (* width 2) (* height 2))
   ;; (q/image capture (+ 10 width) 0)
   (framerate/display (q/current-frame-rate)))
 
