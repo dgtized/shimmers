dd885c519bd3cf92453ea5f2ff97d0d2862ad67f
Charles Comstock <dgtized@gmail.com>
2020-11-23 02:20:06 -0600

    Document where color gradients are from

--- src/shimmers/math/color.cljs
+++ src/shimmers/math/color.cljs
@@ -1,4 +1,7 @@
 (ns shimmers.math.color
+  "Useful tools for generating or working with colors
+
+  Gradients borrowed from https://github.com/thi-ng/color/blob/master/src/gradients.org"
   (:require [quil.core :as q :include-macros true]
             [thi.ng.color.core :as col]
             [thi.ng.color.gradients :as grad]))
