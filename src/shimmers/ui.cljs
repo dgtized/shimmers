7d30376054e27b1da9729a7b1893bcf2a0b5b11e
Charles Comstock <dgtized@gmail.com>
2020-12-01 16:26:26 -0600

    Reference global gtag

--- src/shimmers/ui.cljs
+++ src/shimmers/ui.cljs
@@ -1,7 +1,15 @@
 (ns shimmers.ui)
 
+;; https://coderwall.com/p/s3j4va/google-analytics-tracking-in-clojurescript
+;; Bypasses externs by grabbing gtag function from window directly and then
+;; converting arguments to js array
+(defn gtag [& args]
+  (when js/gtag
+    (.. (aget js/window "gtag")
+        (apply nil (clj->js args)))))
+
 (defn screen-view [sketch-name]
-  (.gtag js/window "event" "screen_view" {:screen_name sketch-name}))
+  (gtag "event" "screen_view" {:screen_name sketch-name}))
 
 (defn cycle-next [lst current]
   (->> lst
