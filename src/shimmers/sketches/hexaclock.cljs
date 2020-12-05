83775fb3c5df36fa9afda81d3d7f37e88a50f037
Charles Comstock <dgtized@gmail.com>
2020-12-04 22:15:13 -0600

    basics of the clock, following along the outer perimeter for each 6 segment

--- src/shimmers/sketches/hexaclock.cljs
+++ src/shimmers/sketches/hexaclock.cljs
@@ -4,26 +4,40 @@
             [shimmers.framerate :as framerate]
             [shimmers.math.vector :as v]))
 
-(defn angles []
+(defn spur-angles []
   (for [spur (range 0 6)]
-    (/ (- (* spur 60 Math/PI) 90) 180)))
+    (- (/ (* spur 60 Math/PI) 180)
+       (/ Math/PI 2))))
 
 (defn spur [radius angle]
   [(* radius (q/cos angle))
    (* radius (q/sin angle))])
 
+(defn hexagon [radius amt]
+  (let [angles (spur-angles)]
+    (doseq [idx (range 0 6)
+            :while (< idx amt)]
+      (q/line (spur radius (nth angles idx))
+              (spur radius (nth angles (mod (inc idx) 6)))))))
+
 (defn draw []
   (q/background 255 32)
   (q/stroke 32 16)
-  (q/stroke-weight 1)
-  (let [s (q/seconds)
+  (let [sec (q/map-range (q/seconds) 0 60 0 6)
+        min (q/map-range (q/minute) 0 60 0 6)
+        hour (q/map-range (q/hour) 0 24 0 6)
         cx (/ (q/width) 2)
         cy (/ (q/height) 2)
-        radius (/ (q/width) (Math/sqrt 6))]
+        rH (/ (q/width) (Math/sqrt 5))
+        rM (/ (q/width) (Math/sqrt 6))
+        rS (/ (q/width) (Math/sqrt 7))]
     (q/translate cx cy)
-    (doseq [idx (range 0 6)]
-      (q/line (spur radius (nth (angles) (mod idx 6)))
-              (spur radius (nth (angles) (mod (inc idx) 6)))))))
+    (q/stroke-weight 1)
+    (hexagon rS sec)
+    (q/stroke-weight 2)
+    (hexagon rM min)
+    (q/stroke-weight 4)
+    (hexagon rH hour)))
 
 (defn ^:export run-sketch []
   (q/defsketch hexaclock
