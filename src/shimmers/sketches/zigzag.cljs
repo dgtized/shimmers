f835a8a3975e8f875d81668533e795f4efc7be03
Charles Comstock <dgtized@gmail.com>
2020-12-04 19:52:46 -0600

    Add polar noise loop to the stair pattern

--- src/shimmers/sketches/zigzag.cljs
+++ src/shimmers/sketches/zigzag.cljs
@@ -3,26 +3,35 @@
             [quil.middleware :as m]
             [shimmers.framerate :as framerate]))
 
+(defn polar-noise [angle]
+  (q/noise (+ (q/cos angle) 1)
+           (+ (q/sin angle) 1)))
+
 (defn setup []
-  (q/frame-rate 10)
+  (q/frame-rate 30)
   {})
 
-(defn zig [[x y]]
-  (q/stroke-weight (+ (* 0.8 (rand)) 0.2))
+(defn zig [[x y] t]
+  ;; (q/stroke-weight (+ (* 0.8 (rand)) 0.2))
   (if (and (> x 0) (< y (q/height)))
-    (let [nx (- x 10 (rand))
-          ny (+ y 10 (rand))]
+    (let [c 8
+          h 12
+          a (+ (+ x y) t)
+          jitter (* h (polar-noise a))
+          nx (- x c jitter)
+          ny (+ y c jitter)]
       (q/line x y nx y)
       (q/line nx y nx ny)
-      (recur [nx ny]))))
+      (recur [nx ny] t))))
 
 (defn draw [state]
-  (q/background 255)
-  (q/stroke 0 100)
+  (q/background 255 20)
+  (q/stroke 0 10)
   (let [m 50
-        o (/ (q/width) (/ m 2))]
+        o (/ (q/width) (/ m 2))
+        t (/ (q/frame-count) 300)]
     (dotimes [i m]
-      (zig [(* i o) 0]))))
+      (zig [(* i o) 0] t))))
 
 (defn ^:export run-sketch []
   (q/defsketch zigzag
