2061607d6ba89312c0b9b8ea06b6563603a507e5
Charles Comstock <dgtized@gmail.com>
2020-12-04 19:56:54 -0600

    Make step-size and jitter amount proportional to gap size from screen size

--- src/shimmers/sketches/zigzag.cljs
+++ src/shimmers/sketches/zigzag.cljs
@@ -11,27 +11,27 @@
   (q/frame-rate 30)
   {})
 
-(defn zig [[x y] t]
+(defn zig [gap [x y] t]
   ;; (q/stroke-weight (+ (* 0.8 (rand)) 0.2))
   (if (and (> x 0) (< y (q/height)))
-    (let [c 8
-          h 12
+    (let [step-size (* gap (/ 3 8))
+          h (- gap step-size)
           a (+ (+ x y) t)
           jitter (* h (polar-noise a))
-          nx (- x c jitter)
-          ny (+ y c jitter)]
+          nx (- x step-size jitter)
+          ny (+ y step-size jitter)]
       (q/line x y nx y)
       (q/line nx y nx ny)
-      (recur [nx ny] t))))
+      (recur gap [nx ny] t))))
 
 (defn draw [state]
   (q/background 255 20)
   (q/stroke 0 10)
   (let [m 50
-        o (/ (q/width) (/ m 2))
+        gap (/ (q/width) (/ m 2))
         t (/ (q/frame-count) 300)]
     (dotimes [i m]
-      (zig [(* i o) 0] t))))
+      (zig gap [(* i gap) 0] t))))
 
 (defn ^:export run-sketch []
   (q/defsketch zigzag
