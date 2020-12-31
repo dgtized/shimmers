e71d4d14632ef9a3aaa3b2955c38085d66fb8bd1
Charles Comstock <dgtized@gmail.com>
2020-12-31 05:18:25 -0600

    Muck with the wave parameters

--- src/shimmers/sketches/waves.cljs
+++ src/shimmers/sketches/waves.cljs
@@ -10,15 +10,15 @@
     (nd/ndarray :float32 zeros [width height])))
 
 (defn setup []
-  (let [width (/ (q/width) 2)
-        height (/ (q/height) 2)]
+  (let [width (/ (q/width) 3)
+        height (/ (q/height) 3)]
     {:width width
      :height height
      :buffer (make-buffer width height)
      :previous (make-buffer width height)}))
 
 (defn update-state [{:keys [width height buffer previous] :as state}]
-  (when (= 0 (mod (q/frame-count) 10))
+  (when (= 0 (mod (q/frame-count) 30))
     (let [i (int (q/random width))
           j (int (q/random height))]
       (println (str "droplet at " i " " j))
@@ -26,7 +26,7 @@
   (loop/c-for [j 1 (< j (- height 1)) (inc j)
                i 1 (< i (- width 1)) (inc i)]
     (nd/set-at buffer i j
-               (* 0.6
+               (* 0.9
                   (- (/ (+ (nd/get-at previous (inc i) j)
                            (nd/get-at previous (dec i) j)
                            (nd/get-at previous i (inc j))
@@ -38,14 +38,14 @@
          :previous buffer))
 
 (defn draw [{:keys [width height buffer]}]
-  (q/background 255 16)
+  (q/background 255 32)
   (q/no-stroke)
   (loop/c-for [j 0 (< j height) (inc j)
                i 0 (< i width) (inc i)]
     (let [amount (nd/get-at buffer i j)]
       (when (> amount 0)
-        (q/fill (* amount 192) 16)
-        (q/rect (* 2 i) (* 2 j) 2 2)))))
+        (q/fill 128 128 (+ (* amount 64) 128) 16)
+        (q/rect (* 3 i) (* 3 j) 3 3)))))
 
 (defn ^:export run-sketch []
   (q/defsketch waves
