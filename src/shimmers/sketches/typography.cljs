6903232981e2f5885fe82dc430c881d5720e4a07
Charles Comstock <dgtized@gmail.com>
2021-01-11 06:58:26 -0600

    more tuning

--- src/shimmers/sketches/typography.cljs
+++ src/shimmers/sketches/typography.cljs
@@ -5,19 +5,20 @@
             [clojure.string :as str]))
 
 (defn setup []
-  (q/frame-rate 4)
+  (q/frame-rate 6)
   {:text ["This is how the world ends, not with a bang, but with a whimper."
           "This is often how the world ends, not with a bang, but with a whimper."
           "This is slowly how the world ends, not with a bang, but with a whimper."
           "This is how the world ends, this is how the world ends, not with a bang, but with a whimper."
           "With a whimper."
-          "With a bang."]})
+          "With a bang."
+          ""]})
 
 (defn update-state [state]
   state)
 
 (defn draw [{:keys [text]}]
-  (q/background 255 (rand-nth [16 16 16 255]))
+  (q/background 255 (rand-nth [16 64 128 255 200]))
   (q/fill 0 255)
   ;; (q/text-font (rand-nth fonts))
   (let [x (atom 20)
@@ -25,20 +26,22 @@
     (doseq [word (str/split (rand-nth text) #"\s+")]
       (let [style (rand-nth [:normal :italic :bold :bolditalic])
             size (rand-nth [10 15 20 25 30])]
+        (q/rotate (rand-nth [0 0 0 0 0 0 0 0 0.02 -0.02 -0.05]))
         (q/text-style style)
         (q/text-size size)
         (q/text word @x @y)
-        (let [yset (rand-nth [0 0 0 0 20 40])]
+        (let [yset (rand-nth [0 0 0 0 0 20 40])
+              text-width (q/text-width (str word " "))]
           (if (or (and (> yset 0) (>= @x 50) (>= @x 150)))
             (swap! x - (rand-nth [0 30 50 50]))
-            (swap! x + (/ (* size (count word)) 2) 5))
+            (swap! x + (rand-nth [-10 50 text-width text-width])))
           (swap! y + yset))))))
 
 (defn ^:export run-sketch []
-(q/defsketch typography
-  :host "quil-host"
-  :size [600 400]
-  :setup setup
-  :update update-state
-  :draw draw
-  :middleware [m/fun-mode framerate/mode]))
+  (q/defsketch typography
+    :host "quil-host"
+    :size [600 400]
+    :setup setup
+    :update update-state
+    :draw draw
+    :middleware [m/fun-mode framerate/mode]))
