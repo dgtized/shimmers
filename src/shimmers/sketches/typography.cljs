acefafe40ddeba06b09bdc614b461cf365c264f2
Charles Comstock <dgtized@gmail.com>
2021-01-11 06:34:18 -0600

    Add some randomness and sometimes persist between frames

--- src/shimmers/sketches/typography.cljs
+++ src/shimmers/sketches/typography.cljs
@@ -6,27 +6,31 @@
 
 (defn setup []
   (q/frame-rate 4)
-  {:text "This is how the world ends, not with a bang, but with a whimper."})
+  {:text ["This is how the world ends, not with a bang, but with a whimper."
+          "This is often how the world ends, not with a bang, but with a whimper."
+          "This is slowly how the world ends, not with a bang, but with a whimper."
+          "This is how the world ends, this is how the world ends, not with a bang, but with a whimper."
+          "With a whimper."
+          "With a bang."]})
 
 (defn update-state [state]
   state)
 
 (defn draw [{:keys [text]}]
-  (q/background 255)
-  (q/fill 0)
+  (q/background 255 (rand-nth [16 16 16 255]))
+  (q/fill 0 255)
   ;; (q/text-font (rand-nth fonts))
   (let [x (atom 20)
         y (atom 50)]
-    (doseq [word (str/split text #"\s+")]
+    (doseq [word (str/split (rand-nth text) #"\s+")]
       (let [style (rand-nth [:normal :italic :bold :bolditalic])
             size (rand-nth [10 15 20 25 30])]
         (q/text-style style)
         (q/text-size size)
         (q/text word @x @y)
-        (println [@x @y])
         (let [yset (rand-nth [0 0 0 0 20 40])]
-          (if (and (> yset 0) (>= @x 50))
-            (swap! x - (rand-nth [0 30 50]))
+          (if (or (and (> yset 0) (>= @x 50) (>= @x 150)))
+            (swap! x - (rand-nth [0 30 50 50]))
             (swap! x + (/ (* size (count word)) 2) 5))
           (swap! y + yset))))))
 
