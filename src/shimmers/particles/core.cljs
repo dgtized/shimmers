2dee3d770a6d25d35370d3ed6d4a5cda5a8a2e00
Charles Comstock <dgtized@gmail.com>
2021-01-14 19:14:15 -0600

    Make color optional for particles/draw

--- src/shimmers/particles/core.cljs
+++ src/shimmers/particles/core.cljs
@@ -3,9 +3,10 @@
 
 (defn draw [particles & {:keys [weight]}]
   (doseq [{:keys [position last-pos color] :as particle} particles]
-    (apply q/stroke color)
     (let [[lx ly] last-pos
           [x y] position]
+      (when color
+        (apply q/stroke color))
       (when weight
         (q/stroke-weight (weight particle)))
       (q/line lx ly x y))))
