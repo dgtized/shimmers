70fb2f25ddd11ef7f9fa3dbcb8e3f958e9844599
Charles Comstock <dgtized@gmail.com>
2021-01-16 23:35:15 -0600

    Reduce the speed of the fade

--- src/shimmers/sketches/radar.cljs
+++ src/shimmers/sketches/radar.cljs
@@ -6,14 +6,14 @@
             [thi.ng.geom.core :as tg]
             [thi.ng.math.core :as tm]))
 
-(def green [0 230 0 200])
+(defn green [x] [0 230 0 x])
 
 (defn setup []
   (let [hw (/ (q/width) 2)
         hh (/ (q/height) 2)
         radius (* 0.45 (q/height))]
-    (apply q/stroke green)
-    (apply q/fill green)
+    (apply q/stroke (green 255))
+    (apply q/fill (green 32))
     (q/ellipse-mode :radius)
     (q/ellipse hw hh radius radius)
     {:theta 0.0
@@ -28,8 +28,9 @@
       (update :point (fn [pos] (v/wrap2d pos (q/width) (q/height))))))
 
 (defn draw [{:keys [theta center radius point]}]
-  (q/background 0 3)
-  (apply q/stroke green)
+  (when (= 0 (mod (q/frame-count) 8))
+    (q/background 0 12))
+  (apply q/stroke (green 255))
   (apply q/translate center)
   (let [tpoint (tm/- point center)
         delta (- (tg/heading tpoint) (mod theta (* 2 Math/PI)))]
@@ -39,7 +40,8 @@
       (apply q/point tpoint)))
   (q/stroke-weight 2)
   (q/point 0 0)
-  (q/stroke-weight 0.8)
+  (apply q/stroke (green 255))
+  (q/stroke-weight 1)
   (q/line 0 0 (* radius (q/cos theta)) (* radius (q/sin theta))))
 
 (defn ^:export run-sketch []
