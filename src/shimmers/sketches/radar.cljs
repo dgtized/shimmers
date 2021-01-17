197376931bf0ef4361d3acd10fc40351381d8a79
Charles Comstock <dgtized@gmail.com>
2021-01-17 15:35:31 -0600

    Switch to a basic particle system with a couple of particles floating around

--- src/shimmers/sketches/radar.cljs
+++ src/shimmers/sketches/radar.cljs
@@ -6,6 +6,13 @@
             [thi.ng.geom.core :as tg]
             [thi.ng.math.core :as tm]))
 
+(defrecord Particle [position velocity])
+
+(defn step [{:keys [position velocity] :as particle}]
+  (-> particle
+      (assoc :position (v/add position velocity))
+      (update :position (fn [pos] (v/wrap2d pos (q/width) (q/height))))))
+
 (defn green [x] [0 230 0 x])
 
 (defn setup []
@@ -19,13 +26,14 @@
     {:theta 0.0
      :center (v/vec2 hw hh)
      :radius radius
-     :point (v/vec2 0 60)}))
+     :particles [(->Particle (v/vec2 0 60) (v/vec2 0.15 0.02))
+                 (->Particle (v/vec2 (- (q/width)) 60) (v/vec2 -0.2 0.02))
+                 (->Particle (v/vec2 120 0) (v/vec2 -0.05 0.15))]}))
 
 (defn update-state [state]
   (-> state
       (update :theta + 0.012)
-      (update :point v/add (v/vec2 0.15 0.02))
-      (update :point (fn [pos] (v/wrap2d pos (q/width) (q/height))))))
+      (update :particles (partial map step))))
 
 (defn draw-point-hit [theta radius center point]
   (let [tpoint (tm/- point center)
@@ -35,15 +43,16 @@
       (println [(tg/heading tpoint) (mod theta (* 2 Math/PI)) delta tpoint])
       (apply q/point tpoint))))
 
-(defn draw [{:keys [theta center radius point]}]
+(defn draw [{:keys [theta center radius particles]}]
   (when (= 0 (mod (q/frame-count) 6))
     (q/background 0 8))
   (apply q/stroke (green 255))
   (apply q/translate center)
   (q/stroke-weight 2)
-  (draw-point-hit theta radius center point)
+  (doseq [{:keys [position]} particles]
+    (draw-point-hit theta radius center position))
   (q/point 0 0)
-  (apply q/stroke (green 255))
+  (apply q/stroke (green 192))
   (q/stroke-weight 1)
   (q/line 0 0 (* radius (q/cos theta)) (* radius (q/sin theta))))
 
