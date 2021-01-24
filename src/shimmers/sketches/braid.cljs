22be044fa89aa7ac780cf92c48e1073827e1ead9
Charles Comstock <dgtized@gmail.com>
2021-01-24 01:01:46 -0600

    Change color to be provided by strand-change function

--- src/shimmers/sketches/braid.cljs
+++ src/shimmers/sketches/braid.cljs
@@ -21,23 +21,23 @@
 (defn braid-row [braid-fn row]
   (first (nth (iterate braid-fn [[0 1 2] 0]) row)))
 
+(defn color [value]
+  (let [low 64 high 192]
+    (condp = value
+      0 [high low low]
+      1 [low high low]
+      2 [low low high])))
+
 (defn strand-changes [row]
   (let [strands (braid-row next-row row)
         next-strands (braid-row next-row (inc row))]
     (for [position [1 0 2]
           :let [value (nth strands position)]]
-      [value position (index-of next-strands value)])))
+      [(color value) position (index-of next-strands value)])))
 
 (defn setup []
   {:rate 10.0
    :braids (repeat 6 strand-changes)})
-
-(defn color [value]
-  (let [low 64 high 192]
-    (condp = value
-      0 (q/stroke high low low)
-      1 (q/stroke low high low)
-      2 (q/stroke low low high))))
 
 (defn draw [{:keys [rate braids]}]
   (let [rh 10 ;; row height
@@ -54,8 +54,8 @@
     (doseq [strand-changes braids]
       (q/translate even-split 0)
       (q/with-translation [0 (* row rh)]
-        (doseq [[value from to] (strand-changes row)]
-          (color value)
+        (doseq [[color from to] (strand-changes row)]
+          (apply q/stroke color)
           (cq/lerp-line [(* from cw) 0] [(* to cw) rh] percent))))))
 
 (defn ^:export run-sketch []
