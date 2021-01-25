336e75e29a225fecc022ee4105be8a066862ce90
Charles Comstock <dgtized@gmail.com>
2021-01-25 00:50:04 -0600

    Make the wings flap a little

--- src/shimmers/sketches/butterfly.cljs
+++ src/shimmers/sketches/butterfly.cljs
@@ -3,11 +3,13 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]))
 
-(defn wing []
+(defn wing [angle]
+  (q/push-matrix)
+  (q/rotate-y angle)
   (q/begin-shape)
   (q/vertex 5 -10 0)
   (q/vertex 5 -40 0)
-  (q/vertex 75 -70)
+  (q/vertex 75 -70 0)
   (q/vertex 100 -60 0)
   (q/vertex 90 -20 0)
   (q/vertex 70 0 0)
@@ -16,17 +18,18 @@
   (q/vertex 5 40 0)
   (q/vertex 5 -5 0)
   (q/vertex 50 0 0)
+  (q/vertex 60 10 0)
   (q/vertex 40 40 0)
-  (q/end-shape :close))
+  (q/end-shape :close)
+  (q/pop-matrix))
 
 (defn draw [_]
   (q/background 255)
 
-  (q/ellipsoid 5 50 5)
-  (wing)
-  (q/rotate-y Math/PI)
-  (wing)
-  )
+  (let [theta (/ (q/frame-count) 10)]
+    (q/ellipsoid 5 50 5)
+    (wing (* 0.9 (q/cos theta)))
+    (wing (- Math/PI (* 0.9 (q/cos theta))))))
 
 (defn ^:export run-sketch []
   (q/defsketch butterfly
