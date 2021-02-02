96e229ba890bc4bb7832fbce11c656cb184a736f
Charles Comstock <dgtized@gmail.com>
2021-02-02 03:37:04 -0600

    Use thi.ng.geom.circle directly for bubbles

--- src/shimmers/sketches/bubbles.cljs
+++ src/shimmers/sketches/bubbles.cljs
@@ -3,51 +3,48 @@
             [quil.core :as q :include-macros true]
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
-            [shimmers.math.vector :as v]))
+            [shimmers.math.vector :as v]
+            [thi.ng.geom.circle :as tc]
+            [thi.ng.geom.core :as geom]))
 
 (defn make-bubble []
-  {:position (v/vec2 (q/random (q/width)) (q/height))
-   :size (+ 1 (rand-int 6))})
+  (tc/circle (v/vec2 (q/random (q/width)) (q/height))
+             (+ 1 (rand-int 6))))
 
 (defn setup []
   {:bubbles []})
 
-(defn in-bounds? [[_ y]]
-  (<= 0 y (q/height)))
+(defn in-bounds? [c]
+  (<= 0 (get-in c [:p :y]) (q/height)))
 
-(defn update-bubble [{:keys [position size] :as bubble}]
-  (when (in-bounds? position)
-    (assoc bubble :position (v/sub position (v/vec2 0 (/ 0.3 size))))))
+(defn update-bubble [bubble]
+  (when (in-bounds? bubble)
+    (geom/translate bubble (v/vec2 0 (- (/ 0.3 (:r bubble)))))))
 
 (defn combine-bubble [a b]
-  (let [arad (:size a)
-        brad (:size b)]
-    {:position (if (> arad brad)
-                 (:position a)
-                 (:position b))
-     :size (Math/sqrt (+ (Math/pow arad 2)
-                         (Math/pow brad 2)))}))
-
-(defn intersects? [a b]
-  (when (< (apply v/distance (map :position [a b]))
-           (+ (:size a) (:size b)))
-    b))
+  (tc/circle (if (> (:r a) (:r b))
+               (:p a)
+               (:p b))
+             (Math/sqrt (+ (Math/pow (:r a) 2)
+                           (Math/pow (:r b) 2)))))
 
 (defn combine-intersecting [bubbles]
-  (loop [ordered (sort-by (comp :position :x) bubbles)
+  (loop [ordered (sort-by (comp :p :x) bubbles)
          results []]
     (match [ordered]
       [([] :seq)] results
       [([a] :seq)] (conj results a)
       [([a & xs] :seq)]
-      (if-let [b (some (partial intersects? a) xs)]
-        (recur (conj (remove #{b} xs) (combine-bubble a b)) results)
+      (if-let [b (some (fn [b] (when (geom/intersect-shape a b) b)) xs)]
+        (do
+          (println [a b (combine-bubble a b)])
+          (recur (conj (remove #{b} xs) (combine-bubble a b)) results))
         (recur xs (conj results a))))))
 
 (defn update-state [{:keys [bubbles] :as state}]
   (let [active (keep update-bubble (combine-intersecting bubbles))]
     (assoc state :bubbles
-           (if (and (< (rand) 0.04)
+           (if (and (< (rand) 0.03)
                     (< (count active) 512))
              (conj active (make-bubble))
              active))))
@@ -56,9 +53,9 @@
   (q/background 250 150 140 32)
   (q/no-fill)
   (q/stroke 0 192)
-  (doseq [{:keys [position size]} bubbles
-          :let [[x y] position]]
-    (q/ellipse x y size size)))
+  (doseq [{:keys [p r]} bubbles
+          :let [[x y] p]]
+    (q/ellipse x y r r)))
 
 (defn ^:export run-sketch []
   (q/defsketch bubbles
