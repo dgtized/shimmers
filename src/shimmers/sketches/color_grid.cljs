dafd9645705c4a8465cfc36dd8f56d2730fd3e94
Charles Comstock <dgtized@gmail.com>
2021-02-07 18:11:45 -0600

    Add some pinwheel rotation effects

--- src/shimmers/sketches/color_grid.cljs
+++ src/shimmers/sketches/color_grid.cljs
@@ -7,20 +7,47 @@
   [(/ (+ x y) 20) (+ 0.2 (/ x 20)) (+ 0.2 (/ y 12)) 1])
 
 (defn make-grid [cols rows]
-  {:dims [cols rows]
+  {:effects []
+   :dims [cols rows]
    :grid
    (apply merge
           (for [r (range rows)
                 c (range cols)]
             {[c r] (sample-color (inc c) (inc r))}))})
 
+(defn pinwheel [c r]
+  {:cells [[(dec c) (dec r)] [(dec c) r] [c (dec r)] [c r]]
+   :theta 0
+   :delta -0.1})
+
+(defn apply-step [effects]
+  (map (fn [effect] (update effect :theta + (:delta effect)))
+       effects))
+
+(defn draw-step [grid effect w h]
+  (let [cells (:cells effect)
+        p (last cells)
+        [c r] p]
+    (q/translate (* c w) (* r h))
+    (q/rotate (:theta effect))
+    (apply q/fill (get grid (nth cells 0)))
+    (q/rect (- w) (- h) w h)
+    (apply q/fill (get grid (nth cells 1)))
+    (q/rect (- w) 0 w h)
+    (apply q/fill (get grid (nth cells 2)))
+    (q/rect 0 (- h) w h)
+    (apply q/fill (get grid (nth cells 3)))
+    (q/rect 0 0 w h)))
+
 (defn setup []
   (make-grid 12 8))
 
-(defn update-state [state]
-  state)
+(defn update-state [{:keys [effects] :as state}]
+  (if (empty? effects)
+    (update state :effects into [(pinwheel 7 5) (pinwheel 3 2)])
+    (update state :effects apply-step)))
 
-(defn draw [{:keys [grid dims]}]
+(defn draw [{:keys [grid dims effects]}]
   (q/color-mode :hsl 1 1 1 1)
   (q/rect-mode :corner)
   (let [[cols rows] dims
@@ -28,7 +55,11 @@
         h (/ (q/height) rows)]
     (doseq [[[c r] color] grid]
       (apply q/fill color)
-      (q/rect (* c w) (* r h) w h))))
+      (q/rect (* c w) (* r h) w h))
+    (doseq [effect effects]
+      (q/push-matrix)
+      (draw-step grid effect w h)
+      (q/pop-matrix))))
 
 (defn ^:export run-sketch []
   (q/defsketch color-grid
