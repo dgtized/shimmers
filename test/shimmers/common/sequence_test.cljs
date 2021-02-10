052b2c6a4fea2520d4d0000fb7ee5e3c7cde4c80
Charles Comstock <dgtized@gmail.com>
2021-02-09 19:26:50 -0600

    Expand some test examples for sequence/rotate

--- test/shimmers/common/sequence_test.cljs
+++ test/shimmers/common/sequence_test.cljs
@@ -3,7 +3,10 @@
             [cljs.test :as t :include-macros true]))
 
 (t/deftest rotate
+  (t/is (= [1 2 3] (sut/rotate 0 [1 2 3])))
   (t/is (= [2 3 1] (sut/rotate 1 [1 2 3])))
-  (t/is (= [3 1 2] (sut/rotate -1 [1 2 3]))))
+  (t/is (= [3 1 2] (sut/rotate -1 [1 2 3])))
+  (t/is (= [3 4 1 2] (sut/rotate 2 [1 2 3 4])))
+  (t/is (= [2 3 4 1] (sut/rotate -3 [1 2 3 4]))))
 
 
