ca021f7c748ea0692341bfa041cd2ac82023f29c
Charles Comstock <dgtized@gmail.com>
2021-02-11 00:28:50 -0600

    Move rings forward until exit

--- src/shimmers/sketches/tunnel_flight.cljs
+++ src/shimmers/sketches/tunnel_flight.cljs
@@ -6,27 +6,27 @@
             [shimmers.math.polar :refer [angles]]
             [shimmers.math.vector :as v]))
 
-(defn blob [z r0 r1]
+(defn blob [z base r0 r1]
   (for [theta (angles 10)
-        :let [zmod (+ 0.5 (mod z 3))
-              xoff (+ 1 (q/cos theta))
+        :let [xoff (+ 1 (q/cos theta))
               yoff (+ 1 (q/sin theta))
-              r (q/map-range (q/noise xoff yoff z) 0 1
-                             (* zmod r0) (* zmod r1))]]
+              r (* (q/map-range (q/noise xoff yoff base) 0 1
+                                r0 r1)
+                   (+ 1 (* 12 z)))]]
     (v/scale (v/unit2-from-angle theta) r)))
 
 (defn setup []
   {:z 0.0})
 
 (defn update-state [state]
-  (update state :z + 0.005))
+  (update state :z + 0.0005))
 
 (defn draw [{:keys [z]}]
   (q/background 255)
   (q/no-fill)
   (q/translate (/ (q/width) 2) (/ (q/height) 2))
-  (doseq [base (range 0 100 10)]
-    (cq/draw-shape (blob (+ z base) (* base 10) (+ (* base 10))))))
+  (doseq [base (range 0 60 6)]
+    (cq/draw-shape (blob z base (* base 4) (* base 6)))))
 
 (defn ^:export run-sketch []
   (q/defsketch tunnel-flight
