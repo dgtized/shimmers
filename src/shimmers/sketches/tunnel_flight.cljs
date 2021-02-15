9d3814d86ff58caecfda151463facd79966a5a48
Charles Comstock <dgtized@gmail.com>
2021-02-15 06:21:32 -0600

    Add a little random rotation to make it more interesting

--- src/shimmers/sketches/tunnel_flight.cljs
+++ src/shimmers/sketches/tunnel_flight.cljs
@@ -45,6 +45,7 @@
   (q/background 255)
   (q/no-fill)
   (q/translate (/ (q/width) 2) (/ (q/height) 2))
+  (q/rotate (* 2 Math/PI (q/noise (* 0.06 z))))
   (doseq [ring rings]
     (q/push-matrix)
     (apply q/translate (v/scale (:position ring) (* 1.2 (- z (:base ring)))))
