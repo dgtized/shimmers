6a589369b508ddc239e14abfdeada98b755c610b
Charles Comstock <dgtized@gmail.com>
2021-02-17 18:59:11 -0600

    Fix the max radius for slow movers

--- src/shimmers/sketches/ascendance.cljs
+++ src/shimmers/sketches/ascendance.cljs
@@ -10,7 +10,8 @@
               (/ (rand) 10)
               (rand-nth [1.5 2.0 3.0 4.0])
               (rand-nth [(fn [_] 150)
-                         (fn [t] (- 150 t))])))
+                         (fn [t] (- 150 t))
+                         (fn [t] (/ 150 (+ t 1)))])))
 
 (defn update-particle
   [{:keys [t1 decay] :as p}]
