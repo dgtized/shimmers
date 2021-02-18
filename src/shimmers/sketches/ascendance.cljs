00abb5cd225af1573afb84abc925f894efadf3c7
Charles Comstock <dgtized@gmail.com>
2021-02-17 18:46:03 -0600

    Extend to multiple ascending particles following same function

--- src/shimmers/sketches/ascendance.cljs
+++ src/shimmers/sketches/ascendance.cljs
@@ -3,21 +3,44 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]))
 
+(defrecord Particle [t0 t1 lifespan])
+
+(defn make-particle [t]
+  (->Particle t t 100.0))
+
+(defn update-particle
+  [{:keys [t1] :as p}]
+  (assoc p :t0 t1 :t1 (+ t1 0.1)))
+
+(defn position [p t h]
+  (let [hh (/ h 2)]
+    [(* 150 (q/cos t))
+     (q/map-range t 0.0 100.0 hh (- hh))
+     (* 150 (q/sin t))]))
+
 (defn setup []
-  {})
+  (q/ortho)
+  {:particles []})
+
+(defn alive? [{:keys [t1 lifespan]}]
+  (< t1 lifespan))
+
+(defn add-particle [particles]
+  (let [alive (map update-particle
+                   (filter alive? particles))]
+    (if (and (< (count alive) 100) (< (rand) 0.01))
+      (conj alive (make-particle 0.0))
+      alive)))
 
 (defn update-state [state]
-  state)
+  (update state :particles add-particle))
 
-(defn draw [state]
-  (q/background 255 16)
-  (q/color 0 16)
-  (let [h (q/height)
-        t (/ (q/frame-count) 10)]
-    (println [h t (/ h (mod t h))])
-    (q/point (* 150 (q/cos t))
-             (q/map-range (mod (* 2 t) h) 0 h 200 -200)
-             (* 150 (q/sin t)))))
+(defn draw [{:keys [particles]}]
+  (q/background 255 1)
+  (q/color 0)
+  (let [h (q/height)]
+    (doseq [{:keys [t0 t1] :as p} particles]
+      (q/line (position p t0 h) (position p t1 h)))))
 
 (defn ^:export run-sketch []
   (q/defsketch template
