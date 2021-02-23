2906a84e115e66e39c6bf0ebe8f875b90ae1b680
Charles Comstock <dgtized@gmail.com>
2021-02-23 00:14:46 -0600

    Subdivide triangles recursively

--- src/shimmers/sketches/triangulating_subdivisions.cljs
+++ src/shimmers/sketches/triangulating_subdivisions.cljs
@@ -5,32 +5,36 @@
             [shimmers.math.vector :as v]
             [thi.ng.geom.core :as geom]))
 
+(defrecord Triangle [a b c])
 (defrecord Line [a b])
 
-(defn subdivide [line]
-  (let [{:keys [a b]} line
-        cutpoint (v/scale (v/sub a b) (rand))
-        midpoint (v/add b cutpoint)
-        normal (geom/scale (geom/normal cutpoint) 0.1)]
-    [(->Line a midpoint)
-     (->Line midpoint b)
-     (->Line (v/sub midpoint normal)
-             (v/add midpoint normal))]))
+(defn triangle->lines [{:keys [a b c]}]
+  [(->Line a b)
+   (->Line b c)
+   (->Line c a)])
+
+(defn subdivide-line [{:keys [a b]}]
+  (v/add b (v/scale (v/sub a b) (rand))))
+
+(defn subdivide-triangle [{:keys [a b c]}]
+  (let [[a b c] (shuffle [a b c])
+        m (subdivide-line (->Line a b))]
+    [(->Triangle a m c)
+     (->Triangle b m c)]))
 
 (defn setup []
+  (q/frame-rate 2)
   (let [top (v/vec2 (* 0.5 (q/width)) (* 0.1 (q/height)))
         left (v/vec2 (* 0.1 (q/width)) (* 0.9 (q/height)))
         right (v/vec2 (* 0.9 (q/width)) (* 0.9 (q/height)))]
-    {:lines (mapcat subdivide
-                    [(->Line top left)
-                     (->Line left right)
-                     (->Line right top)])}))
+    {:triangles [(->Triangle top left right)]}))
 
-(defn update-state [state]
-  state)
+(defn update-state [{:keys [triangles] :as state}]
+  (let [[s & r] (shuffle triangles)]
+    (assoc state :triangles (into (subdivide-triangle s) r))))
 
-(defn draw [{:keys [lines]}]
-  (doseq [line lines]
+(defn draw [{:keys [triangles]}]
+  (doseq [line (mapcat triangle->lines triangles)]
     (q/line (:a line) (:b line))))
 
 (defn ^:export run-sketch []
