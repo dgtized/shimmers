a337e19445a01b7e0e4ce33785a02d60f04fb3e1
Charles Comstock <dgtized@gmail.com>
2021-02-22 23:51:42 -0600

    Subdivide each face at a random interval

--- src/shimmers/sketches/triangulating_subdivisions.cljs
+++ src/shimmers/sketches/triangulating_subdivisions.cljs
@@ -2,17 +2,29 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
-            [shimmers.math.vector :as v]))
+            [shimmers.math.vector :as v]
+            [thi.ng.geom.core :as geom]))
 
 (defrecord Line [a b])
+
+(defn subdivide [line]
+  (let [{:keys [a b]} line
+        cutpoint (v/scale (v/sub a b) (rand))
+        midpoint (v/add b cutpoint)
+        normal (geom/scale (geom/normal cutpoint) 0.1)]
+    [(->Line a midpoint)
+     (->Line midpoint b)
+     (->Line (v/sub midpoint normal)
+             (v/add midpoint normal))]))
 
 (defn setup []
   (let [top (v/vec2 (* 0.5 (q/width)) (* 0.1 (q/height)))
         left (v/vec2 (* 0.1 (q/width)) (* 0.9 (q/height)))
         right (v/vec2 (* 0.9 (q/width)) (* 0.9 (q/height)))]
-    {:lines [(->Line top left)
-             (->Line left right)
-             (->Line right top)]}))
+    {:lines (mapcat subdivide
+                    [(->Line top left)
+                     (->Line left right)
+                     (->Line right top)])}))
 
 (defn update-state [state]
   state)
