f54a1f9263e171e2ea0c06b6701d1d2653ffcd91
Charles Comstock <dgtized@gmail.com>
2021-02-23 00:27:16 -0600

    Use thi.ng.geom.triangle for triangle object

--- src/shimmers/sketches/triangulating_subdivisions.cljs
+++ src/shimmers/sketches/triangulating_subdivisions.cljs
@@ -3,31 +3,29 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.math.vector :as v]
+            [thi.ng.geom.triangle :as gt]
             [thi.ng.geom.core :as geom]))
 
-(defrecord Triangle [a b c])
 (defrecord Line [a b])
 
-(defn triangle->lines [{:keys [a b c]}]
-  [(->Line a b)
-   (->Line b c)
-   (->Line c a)])
+(defn triangle->lines [t]
+  (map (fn [[a b]] (->Line a b)) (geom/edges t)))
 
 (defn subdivide-line [{:keys [a b]}]
   (v/add b (v/scale (v/sub a b) (q/random 0.25 0.75))))
 
-(defn subdivide-triangle [{:keys [a b c]}]
-  (let [[a b c] (shuffle [a b c])
+(defn subdivide-triangle [t]
+  (let [[a b c] (shuffle (:points t))
         m (subdivide-line (->Line a b))]
-    [(->Triangle a m c)
-     (->Triangle b m c)]))
+    [(gt/triangle2 a m c)
+     (gt/triangle2 b m c)]))
 
 (defn setup []
   (q/frame-rate 2)
   (let [top (v/vec2 (* 0.5 (q/width)) (* 0.1 (q/height)))
         left (v/vec2 (* 0.1 (q/width)) (* 0.9 (q/height)))
         right (v/vec2 (* 0.9 (q/width)) (* 0.9 (q/height)))]
-    {:triangles [(->Triangle top left right)]}))
+    {:triangles [(gt/triangle2 top left right)]}))
 
 (defn update-state [{:keys [triangles] :as state}]
   (let [[s & r] (shuffle triangles)]
