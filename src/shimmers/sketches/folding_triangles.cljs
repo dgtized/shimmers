c11bfab4eef322f7edf1ba77732cee16ed2f9d47
Charles Comstock <dgtized@gmail.com>
2021-03-01 00:07:08 -0600

    Start working on apply a texture to the triangle

--- src/shimmers/sketches/folding_triangles.cljs
+++ src/shimmers/sketches/folding_triangles.cljs
@@ -5,17 +5,28 @@
             [shimmers.common.quil :as quil]))
 
 (defn setup []
-  {})
+  {:texture (let [t (q/create-graphics 1 1)]
+              (q/with-graphics t
+                (q/no-stroke)
+                (q/fill 0)
+                (q/rect 0 0 0.5 1)
+                (q/fill 100)
+                (q/rect 0.5 0 0.5 1))
+              t)})
+
+(defn shape-texture []
+  )
 
 (defn update-state [state]
   state)
 
-(defn draw [state]
+(defn draw [{:keys [texture]}]
   (q/background 255)
   (q/rotate-y (/ (q/millis) 1000))
   (q/scale 50)
-  (quil/normal-material)
-  (q/begin-shape :triangles)
+  (q/texture texture)
+  (.textureMode (q/current-graphics) "normal")
+  (q/begin-shape)
   (q/vertex 0 0 0)
   (q/vertex 0 1 0)
   (q/vertex 1 0 0)
