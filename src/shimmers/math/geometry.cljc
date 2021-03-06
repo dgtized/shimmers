44a660658b48282d563e1a05f6962ed0ba7a7200
Charles Comstock <dgtized@gmail.com>
2021-03-06 14:08:15 -0600

    Extract random-point-inside impl into math.geometry

--- src/shimmers/math/geometry.cljc
+++ src/shimmers/math/geometry.cljc
@@ -1,5 +1,30 @@
 (ns shimmers.math.geometry
-  (:require [thi.ng.geom.core :as geom]))
+  (:require [shimmers.math.probability :as p]
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.triangle :as gt]
+            #?(:clj [thi.ng.geom.types] :cljs [thi.ng.geom.types :refer [Polygon2]])
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm])
+  #?(:clj (:import [thi.ng.geom.types Polygon2])))
+
+;; http://extremelearning.com.au/evenly-distributing-points-in-a-triangle/
+;; https://stackoverflow.com/questions/47410054/generate-random-locations-within-a-triangular-domain/47418580#47418580
+(defn random-point-in-triangle2 [{:keys [points]}]
+  (let [[s t] (sort [(rand) (rand)])
+        weighting (gv/vec3 s (- t s) (- 1 t))]
+    (gv/vec2 (tm/dot (apply gv/vec3 (map :x points)) weighting)
+             (tm/dot (apply gv/vec3 (map :y points)) weighting))))
+
+;; Uniformly sample points from tesselated triangles of polygon
+;; https://blogs.sas.com/content/iml/2020/10/21/random-points-in-polygon.html
+;; https://observablehq.com/@scarysize/finding-random-points-in-a-polygon
+(extend-type Polygon2
+  geom/ISample
+  (random-point-inside
+    [_] (->> (geom/tessellate _)
+             (map gt/triangle2)
+             (p/weighted-by geom/area)
+             random-point-in-triangle2)))
 
 (defn rotate-around-centroid [polygon t]
   (-> polygon
--- src/shimmers/sketches/noisy_shapes.cljs
+++ src/shimmers/sketches/noisy_shapes.cljs
@@ -2,33 +2,12 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
-            [shimmers.math.probability :as p]
-            [shimmers.math.geometry :refer [rotate-around-centroid]]
+            [shimmers.math.geometry :as geometry]
             [thi.ng.geom.circle :as tc]
             [thi.ng.geom.core :as geom]
-            thi.ng.geom.polygon
             [thi.ng.geom.rect :as rect]
             [thi.ng.geom.triangle :as gt]
-            [thi.ng.geom.types :refer [Polygon2]]
-            [thi.ng.geom.vector :as gv]
-            [thi.ng.math.core :as tm]))
-
-;; http://extremelearning.com.au/evenly-distributing-points-in-a-triangle/
-;; https://stackoverflow.com/questions/47410054/generate-random-locations-within-a-triangular-domain/47418580#47418580
-(defn random-point-in-triangle2 [{:keys [points]}]
-  (let [[s t] (sort [(rand) (rand)])
-        weighting (gv/vec3 s (- t s) (- 1 t))]
-    (gv/vec2 (tm/dot (apply gv/vec3 (map :x points)) weighting)
-             (tm/dot (apply gv/vec3 (map :y points)) weighting))))
-
-;; https://observablehq.com/@scarysize/finding-random-points-in-a-polygon
-(extend-type Polygon2
-  geom/ISample
-  (random-point-inside
-    [_] (->> (geom/tessellate _)
-             (map gt/triangle2)
-             (p/weighted-by geom/area)
-             random-point-in-triangle2)))
+            [thi.ng.geom.vector :as gv]))
 
 (defn setup []
   (q/frame-rate 1)
@@ -60,17 +39,9 @@
     ((rand-nth brushes))))
 
 (defn generate-strokes [brush random-position n]
-  (repeatedly n #(rotate-around-centroid
+  (repeatedly n #(geometry/rotate-around-centroid
                   (geom/translate brush (random-position))
                   (q/random 0 Math/PI))))
-
-;; FIXME: random-point-inside is not implemented for Polygon2
-;; See https://blogs.sas.com/content/iml/2020/10/21/random-points-in-polygon.html
-;; or https://observablehq.com/@scarysize/finding-random-points-in-a-polygon
-;; ie tesselate convex Polygon, and sample uniformly per triangle
-
-;; If this is implemented, rectangle can be rotated/translated and still use
-;; fuzzy shapes.
 
 (defn sample-shape [shape brush fill-density edge-density]
   (doseq [copy
@@ -90,8 +61,8 @@
   (let [w (q/width)
         h (q/height)
         shape1 (rect/rect (* 0.1 w) (* 0.25 h) (* 0.3 w) (* 0.4 h))
-        shape2 (rotate-around-centroid (rect/rect (* 0.55 w) (* 0.2 h) (* 0.3 w) (* 0.4 h))
-                                       0.2)
+        shape2 (-> (rect/rect (* 0.55 w) (* 0.2 h) (* 0.3 w) (* 0.4 h))
+                   (geometry/rotate-around-centroid 0.2))
         shape3 (rect/rect (* 0.35 w) (* 0.5 h) (* 0.3 w) (* 0.4 h))
         shapes [[shape3 [105 0.5 0.5 0.2]
                  (q/random 500 1300) 200]
