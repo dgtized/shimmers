3258b778a5accd18942abb73cb8c65debb85cb0f
Charles Comstock <dgtized@gmail.com>
2021-03-08 15:34:09 -0600

    Generate random brush strokes as triangles rotated

--- src/shimmers/sketches/superposition.cljs
+++ src/shimmers/sketches/superposition.cljs
@@ -4,7 +4,8 @@
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
             [thi.ng.geom.core :as geom]
-            [thi.ng.geom.rect :as rect]))
+            [thi.ng.geom.rect :as rect]
+            [thi.ng.geom.triangle :as gt]))
 
 (defn rel-h [p]
   (* (q/height) p))
@@ -15,16 +16,27 @@
 (defn draw-polygon [poly]
   (cq/draw-shape (geom/vertices poly)))
 
+(defn random-shape-at [position]
+  (-> (gt/triangle2 [0 0] [0 13] [17 0])
+      (geom/rotate (* 2 Math/PI (rand)))
+      (geom/translate position)))
+
 (defn setup []
-  {:shapes [(rect/rect (rel-w 0.15) (rel-h 0.15) (rel-w 0.3) (rel-h 0.3))
-            (rect/rect (rel-w 0.55) (rel-h 0.55) (rel-w 0.3) (rel-h 0.3))]})
+  (let [shapes [(rect/rect (rel-w 0.15) (rel-h 0.15) (rel-w 0.3) (rel-h 0.3))
+                (rect/rect (rel-w 0.55) (rel-h 0.55) (rel-w 0.3) (rel-h 0.3))]]
+    {:shapes shapes
+     :brushes (->> (fn [] (geom/random-point-inside (first shapes)))
+                   (repeatedly 100)
+                   (map random-shape-at))}))
 
 (defn update-state [state]
   state)
 
-(defn draw [{:keys [shapes]}]
+(defn draw [{:keys [shapes brushes]}]
   (doseq [shape shapes]
-    (draw-polygon shape)))
+    (draw-polygon shape))
+  (doseq [brush brushes]
+    (draw-polygon brush)))
 
 (defn ^:export run-sketch []
   ;; 20210308
