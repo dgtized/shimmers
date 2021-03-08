0e1dcc8e5f7e86b2d88447f0fa783925e25e4137
Charles Comstock <dgtized@gmail.com>
2021-03-08 15:41:50 -0600

    Draw triangles migrating between two sampling rectangles

--- src/shimmers/sketches/superposition.cljs
+++ src/shimmers/sketches/superposition.cljs
@@ -5,7 +5,8 @@
             [shimmers.common.quil :as cq]
             [thi.ng.geom.core :as geom]
             [thi.ng.geom.rect :as rect]
-            [thi.ng.geom.triangle :as gt]))
+            [thi.ng.geom.triangle :as gt]
+            [thi.ng.math.core :as tm]))
 
 (defn rel-h [p]
   (* (q/height) p))
@@ -16,16 +17,18 @@
 (defn draw-polygon [poly]
   (cq/draw-shape (geom/vertices poly)))
 
-(defn random-shape-at [position]
-  (-> (gt/triangle2 [0 0] [0 13] [17 0])
-      (geom/rotate (* 2 Math/PI (rand)))
-      (geom/translate position)))
+(defn random-shape-at [[p1 p2]]
+  (fn [t]
+    (-> (gt/triangle2 [0 0] [0 13] [17 0])
+        (geom/rotate (* 2 Math/PI (rand)))
+        (geom/translate (tm/mix p1 p2 t)))))
 
 (defn setup []
   (let [shapes [(rect/rect (rel-w 0.15) (rel-h 0.15) (rel-w 0.3) (rel-h 0.3))
                 (rect/rect (rel-w 0.55) (rel-h 0.55) (rel-w 0.3) (rel-h 0.3))]]
     {:shapes shapes
-     :brushes (->> (fn [] (geom/random-point-inside (first shapes)))
+     :brushes (->> (fn [] [(geom/random-point-inside (first shapes))
+                          (geom/random-point-inside (second shapes))])
                    (repeatedly 100)
                    (map random-shape-at))}))
 
@@ -33,10 +36,11 @@
   state)
 
 (defn draw [{:keys [shapes brushes]}]
-  (doseq [shape shapes]
-    (draw-polygon shape))
+  ;; (q/background 255)
+  ;; (doseq [shape shapes]
+  ;;   (draw-polygon shape))
   (doseq [brush brushes]
-    (draw-polygon brush)))
+    (draw-polygon (brush (Math/abs (q/cos (/ (q/frame-count) 1000)))))))
 
 (defn ^:export run-sketch []
   ;; 20210308
