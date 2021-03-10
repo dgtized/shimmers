b76763234031fc3350b8ec8f302bd7068de10e52
Charles Comstock <dgtized@gmail.com>
2021-03-10 00:35:30 -0600

    Add some background shapes and "trees"

--- src/shimmers/sketches/scribbles.cljs
+++ src/shimmers/sketches/scribbles.cljs
@@ -6,10 +6,8 @@
             [thi.ng.geom.vector :as gv]
             [thi.ng.math.core :as tm]))
 
-(defn scribble [points]
-  (q/stroke-weight 0.2)
-  (q/stroke 0 0 0 0.9)
-  (q/curve-tightness 4.0)
+(defn scribble [points t]
+  (q/curve-tightness t)
   (q/begin-shape)
   (doseq [p points]
     (q/curve-vertex (cq/rel-w (:x p)) (cq/rel-h (:y p))))
@@ -30,12 +28,31 @@
   (q/color-mode :hsl 1.0)
   {})
 
+(defn rel-v [[x y]]
+  (gv/vec2 (cq/rel-w x) (cq/rel-h y)))
+
 (defn draw [_]
   (q/background 255)
+  (q/stroke-weight 0.15)
+  (q/stroke 0 0.4 0.3 0.9)
   (doseq [y (range 0.01 0.9 0.008)]
-    (scribble (curly-line (gv/vec2 0 y) (gv/vec2 1.0 (+ 0.1 y)))))
+    (scribble (curly-line (gv/vec2 0 y) (gv/vec2 1.0 (+ 0.11 y))) 4.0))
+  (q/stroke 0.3 0.4 0.3 0.9)
   (doseq [y (range 0.01 0.9 0.02)]
-    (scribble (curly-line (gv/vec2 0 (+ 0.3 y)) (gv/vec2 1.0 (- y 0.1))))))
+    (scribble (curly-line (gv/vec2 0 (+ 0.3 y)) (gv/vec2 1.0 (- y 0.1))) 4.0))
+  (q/no-stroke)
+  (q/fill 0.0 0.8 0.8 0.2)
+  (q/rect (cq/rel-w 0.3) (cq/rel-h 0.15) (cq/rel-w 0.3) (cq/rel-h 0.1))
+  (q/rect (cq/rel-w 0.2) (cq/rel-h 0.35) (cq/rel-w 0.3) (cq/rel-h 0.15))
+  (q/rect (cq/rel-w 0.1) (cq/rel-h 0.6) (cq/rel-w 0.3) (cq/rel-h 0.1))
+  (q/fill 0.52 0.6 0.8 0.2)
+  (q/rect (cq/rel-w 0.6) (cq/rel-h 0.3) (cq/rel-w 0.3) (cq/rel-h 0.15))
+  (q/rect (cq/rel-w 0.5) (cq/rel-h 0.6) (cq/rel-w 0.25) (cq/rel-h 0.1))
+  (q/rect (cq/rel-w 0.4) (cq/rel-h 0.8) (cq/rel-w 0.2) (cq/rel-h 0.15))
+  (q/stroke 0 0 0 0.8)
+  (q/stroke-weight 1)
+  (doseq [point (repeatedly 100 random-vertex)]
+    (q/line (rel-v point) (rel-v (tm/+ point (gv/vec2 0 (- (* 0.015 (rand)))))))))
 
 (defn ^:export run-sketch []
   ;; 20210309
