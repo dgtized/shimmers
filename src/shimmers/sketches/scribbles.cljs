28b69ab39b962a52ba9ef8f8d199f1af54ab95c9
Charles Comstock <dgtized@gmail.com>
2021-03-10 00:49:51 -0600

    explain components of image

--- src/shimmers/sketches/scribbles.cljs
+++ src/shimmers/sketches/scribbles.cljs
@@ -32,23 +32,27 @@
   (gv/vec2 (cq/rel-w x) (cq/rel-h y)))
 
 (defn draw [_]
-  (q/background 255)
+  (q/background 1.0)
   (q/stroke-weight 0.15)
   (q/stroke 0 0.4 0.3 0.9)
+  ;; landscape
   (doseq [y (range 0.01 0.9 0.008)]
     (scribble (curly-line (gv/vec2 0 y) (gv/vec2 1.0 (+ 0.11 y))) 4.0))
   (q/stroke 0.3 0.4 0.3 0.9)
   (doseq [y (range 0.01 0.9 0.02)]
     (scribble (curly-line (gv/vec2 0 (+ 0.3 y)) (gv/vec2 1.0 (- y 0.1))) 4.0))
   (q/no-stroke)
+  ;; field panes
   (q/fill 0.35 0.6 0.8 0.2)
   (q/rect (cq/rel-w 0.3) (cq/rel-h 0.15) (cq/rel-w 0.25) (cq/rel-h 0.1))
   (q/rect (cq/rel-w 0.2) (cq/rel-h 0.35) (cq/rel-w 0.2) (cq/rel-h 0.15))
   (q/rect (cq/rel-w 0.1) (cq/rel-h 0.6) (cq/rel-w 0.15) (cq/rel-h 0.1))
+  ;; water panes
   (q/fill 0.52 0.6 0.8 0.2)
-  (q/rect (cq/rel-w 0.6) (cq/rel-h 0.3) (cq/rel-w 0.3) (cq/rel-h 0.15))
+  (q/rect (cq/rel-w 0.6) (cq/rel-h 0.4) (cq/rel-w 0.3) (cq/rel-h 0.15))
   (q/rect (cq/rel-w 0.5) (cq/rel-h 0.6) (cq/rel-w 0.25) (cq/rel-h 0.1))
   (q/rect (cq/rel-w 0.4) (cq/rel-h 0.8) (cq/rel-w 0.2) (cq/rel-h 0.15))
+  ;; "trees"
   (q/stroke 0.3 0.4 0.2 0.8)
   (q/stroke-weight 1)
   (doseq [point (repeatedly 128 random-vertex)]
