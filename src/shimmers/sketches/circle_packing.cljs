52e74505a4982edaa777f0543e7408cfb8c4c46b
Charles Comstock <dgtized@gmail.com>
2021-03-11 00:42:14 -0600

    lots of tweaks, but not working

--- src/shimmers/sketches/circle_packing.cljs
+++ src/shimmers/sketches/circle_packing.cljs
@@ -2,45 +2,60 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
-            [thi.ng.geom.spatialtree :as spatialtree]
-            [thi.ng.geom.core :as geom]
             [thi.ng.geom.circle :as gc]
-            [thi.ng.geom.vector :as gv]
-            [thi.ng.geom.rect :as rect]))
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.rect :as rect]
+            [thi.ng.geom.spatialtree :as spatialtree]
+            [thi.ng.geom.vector :as gv]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  (q/frame-rate 10)
+  (q/frame-rate 1)
   {:quadtree (spatialtree/quadtree 0 0 (q/width) (q/height))
    :boundary (rect/rect 0 0 (q/width) (q/height))
    :circles []})
 
-(defn contains-entity? [rect {:keys [p r]}]
-  (let [closest (geom/closest-point rect p)]
-    (< (* r r) (geom/dist-squared closest p))))
+(defn contains-entity? [boundary {:keys [p r]}]
+  (let [[x y] p]
+    (and (> (- x r) (rect/left boundary))
+         (< (+ x r) (rect/right boundary))
+         (< (+ y r) (rect/top boundary))
+         (> (- y r) (rect/bottom boundary)))))
+
+(defn intersects [c1 c2]
+  (when (geom/intersect-shape c1 c2) c2))
 
 (defn add-circle [quadtree boundary]
-  (let [center (gv/vec2 (q/random (q/width)) (q/random (q/height)))
-        r 2
+  (let [r 100
+        center (gv/vec2 (q/random r (- (q/width) r))
+                        (q/random  r (- (q/height) r)))
         circle (gc/circle center r)
         near (spatialtree/select-with-circle quadtree center (* r 10))]
-    (println circle)
-    (when (and (contains-entity? boundary circle)
-               (not (some (partial geom/intersect-shape circle) near)))
-      circle)))
+    (if (and (contains-entity? boundary circle)
+             (not (some (partial intersects circle) near)))
+      circle
+      nil)))
+
+(defn spatial-replace [tree {:keys [p] :as circle}]
+  (geom/add-point (geom/delete-point tree p) p circle))
 
 (defn grow [quadtree boundary circle]
-  (let [growth (geom/scale-size circle 1.025)
-        near (spatialtree/select-with-circle quadtree (:p growth) (* (:r growth) 10))]
-    (if (and (contains-entity? boundary growth)
-             (not (some (partial geom/intersect-shape growth) near)))
-      growth
-      circle)))
+  (if-not (:done circle)
+    (let [growth (geom/scale-size circle 1.1)
+          near (spatialtree/select-with-circle quadtree (:p growth) (* (:r growth) 10))]
+      (if (and (contains-entity? boundary growth)
+               (not (some (partial intersects growth) near)))
+        growth
+        (do (println {:growth near :near near})
+            (pr (some (partial intersects growth) near))
+            (assoc circle :done true))))
+    circle))
 
 (defn update-state [{:keys [boundary quadtree circles] :as state}]
-  (let [growth (map (partial grow quadtree boundary) circles)
-        s' (assoc state :circles growth)]
-    (if-let [circle (add-circle quadtree boundary)]
+  (let [circles' (map (partial grow quadtree boundary) circles)
+        quadtree' (reduce spatial-replace quadtree (remove :done circles'))
+        s' (assoc state :circles circles' :quadtree quadtree')]
+    (if-let [circle (add-circle quadtree' boundary)]
       (-> s'
           (update :circles conj circle)
           (update :quadtree geom/add-point (:p circle) circle))
@@ -49,7 +64,10 @@
 (defn draw [{:keys [circles]}]
   (q/background 1.0)
   (q/stroke-weight 0.5)
-  (doseq [{:keys [p r]} circles]
+  (doseq [{:keys [p r done]} circles]
+    (if done
+      (q/stroke 0 0.5 0.5 0.5)
+      (q/stroke 0 0 0 1.0))
     (q/ellipse (:x p) (:y p) r r)))
 
 (defn ^:export run-sketch []
