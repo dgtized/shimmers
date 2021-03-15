c91482a3c8a4db473826f8e160be5b5ca0c5e930
Charles Comstock <dgtized@gmail.com>
2021-03-15 18:00:18 -0500

    Extract kinematic chain/segments into their own namespace

---
+++ src/shimmers/algorithm/kinematic_chain.cljc
@@ -0,0 +1,40 @@
+(ns shimmers.algorithm.kinematic-chain
+  (:require [thi.ng.geom.core :as geom]
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm]))
+
+(defrecord KinematicChain [segments])
+
+(defrecord KinematicSegment [base angle length])
+
+(defn segment-follow [{:keys [base length]} target]
+  (let [direction (tm/- target base)
+        heading (geom/heading direction)]
+    (->KinematicSegment (tm/- target (geom/as-cartesian (gv/vec2 length heading)))
+                        heading
+                        length)))
+
+(defn segment-endpoint [{:keys [base angle length]}]
+  (tm/+ base (geom/as-cartesian (gv/vec2 length angle))))
+
+(defn make-chain [start n length]
+  (->KinematicChain (repeatedly n #(->KinematicSegment start 0 length))))
+
+(defn chain-follow [{:keys [segments] :as chain} target]
+  (loop [segments (reverse segments) target target new-chain []]
+    (if (empty? segments)
+      (assoc chain :segments (reverse new-chain))
+      (let [segment (segment-follow (first segments) target)]
+        (recur (rest segments) (:base segment) (conj new-chain segment))))))
+
+(defn chain-propagate [{:keys [segments] :as chain} base]
+  (loop [segments segments base base new-chain []]
+    (if (empty? segments)
+      (assoc chain :segments new-chain)
+      (let [s (assoc (first segments) :base base)]
+        (recur (rest segments) (segment-endpoint s) (conj new-chain s))))))
+
+(defn chain-update [chain base target]
+  (cond-> chain
+    target (chain-follow target)
+    base (chain-propagate base)))
--- src/shimmers/sketches/kinematic_chain.cljs
+++ src/shimmers/sketches/kinematic_chain.cljs
@@ -2,66 +2,30 @@
   "Inspired by https://www.youtube.com/watch?v=hbgDqyy8bIwa"
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
+            [shimmers.algorithm.kinematic-chain :as chain]
             [shimmers.common.framerate :as framerate]
-            [thi.ng.geom.core :as geom]
-            [thi.ng.geom.vector :as gv]
-            [thi.ng.math.core :as tm]
             [shimmers.common.quil :as cq]
-            [shimmers.math.probability :as p]))
-
-(defrecord KinematicChain [segments])
-
-(defrecord KinematicSegment [base angle length])
-
-(defn segment-follow [{:keys [base length]} target]
-  (let [direction (tm/- target base)
-        heading (geom/heading direction)]
-    (->KinematicSegment (tm/- target (geom/as-cartesian (gv/vec2 length heading)))
-                        heading
-                        length)))
-
-(defn segment-endpoint [{:keys [base angle length]}]
-  (tm/+ base (geom/as-cartesian (gv/vec2 length angle))))
-
-(defn make-chain [start n length]
-  (->KinematicChain (repeatedly n #(->KinematicSegment start 0 length))))
-
-(defn chain-follow [{:keys [segments] :as chain} target]
-  (loop [segments (reverse segments) target target new-chain []]
-    (if (empty? segments)
-      (assoc chain :segments (reverse new-chain))
-      (let [segment (segment-follow (first segments) target)]
-        (recur (rest segments) (:base segment) (conj new-chain segment))))))
-
-(defn chain-propagate [{:keys [segments] :as chain} base]
-  (loop [segments segments base base new-chain []]
-    (if (empty? segments)
-      (assoc chain :segments new-chain)
-      (let [s (assoc (first segments) :base base)]
-        (recur (rest segments) (segment-endpoint s) (conj new-chain s))))))
-
-(defn chain-update [chain base target]
-  (cond-> chain
-    target (chain-follow target)
-    base (chain-propagate base)))
+            [shimmers.math.probability :as p]
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.vector :as gv]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  {:chains [(assoc (make-chain (gv/vec2 (* (q/width) 0.5) 0)
-                               80 4)
+  {:chains [(assoc (chain/make-chain (gv/vec2 (* (q/width) 0.5) 0)
+                                     80 4)
                    :color [0.3 0.5 0.5 0.025])
-            (assoc (make-chain (gv/vec2 (* (q/width) 0.5) (* (q/height) 0.5))
-                               80 4)
+            (assoc (chain/make-chain (gv/vec2 (* (q/width) 0.5) (* (q/height) 0.5))
+                                     80 4)
                    :color [0.6 0.5 0.5 0.025])
-            (assoc (make-chain (gv/vec2 (* (q/width) 0.5) (q/height))
-                               80 4)
+            (assoc (chain/make-chain (gv/vec2 (* (q/width) 0.5) (q/height))
+                                     80 4)
                    :color [0.9 0.5 0.5 0.025])]})
 
 (defn draw-chain [{:keys [segments]}]
   (q/begin-shape)
   (doseq [s segments]
     (apply q/vertex (:base s)))
-  (apply q/vertex (segment-endpoint (last segments)))
+  (apply q/vertex (chain/segment-endpoint (last segments)))
   (q/end-shape))
 
 (defn screen-point [w h]
@@ -85,7 +49,7 @@
   (cond-> state
     true (assoc :chains
                 (map-indexed (fn [idx chain]
-                               (chain-update
+                               (chain/chain-update
                                 chain
                                 nil ;; (gv/vec2 (/ (q/width) 2) (q/height))
                                 (circle-target (* (inc idx) (cq/rel-h 0.15)))))
