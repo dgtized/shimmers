db9e329152d06de81525816dd8e5806680088a1f
Charles Comstock <dgtized@gmail.com>
2021-03-16 23:14:12 -0500

    Exploration of rotation around axis

--- src/shimmers/sketches/folding_triangles.cljs
+++ src/shimmers/sketches/folding_triangles.cljs
@@ -4,7 +4,10 @@
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
             [thi.ng.geom.core :as geom]
-            [thi.ng.geom.triangle :as gt]))
+            [thi.ng.geom.triangle :as gt]
+            [thi.ng.geom.line :as gl]
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm]))
 
 (defn setup []
   {})
@@ -13,8 +16,31 @@
   state)
 
 (defn draw [_]
+  (q/background 255)
+  (q/push-matrix)
+  (q/translate -100 -100)
+  (let [a (gv/vec3 [-80 -50 0])
+        b (gv/vec3 [-40 -55 0])
+        c (gv/vec3 [-60 -30 0])
+        close (gl/line3 c (geom/closest-point (gl/line3 a b) c))
+        reflect (geom/reflect close (gl/line3 a b))
+        reflect-c (first (:points reflect))]
+    (doseq [s [(gt/triangle3 a b c)
+               close
+               reflect
+               (gt/triangle3 a b reflect-c)]]
+      (cq/draw-shape (geom/vertices s))))
+  (q/push-matrix)
+  (q/translate 150 -50)
+  (let [a (gv/vec3 [0 0 0])
+        b (gv/vec3 [2 4 0])
+        c (gv/vec3 [4 1 0])]
+    (doseq [s [(geom/rotate-around-axis (gt/triangle3 a b c) (tm/- b a)
+                                        (/ (q/millis) 1000))]]
+      (cq/draw-shape (geom/vertices s))))
+  (q/pop-matrix)
+  (q/pop-matrix)
   (let [triangle (gt/triangle3 [0 -5 0] [0 10 0] [10 0 0])]
-    (q/background 255)
     (q/push-matrix)
     (q/rotate-y (/ (q/millis) 1000))
     (q/scale 10)
