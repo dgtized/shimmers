18b105431589ec7587feb6e617dd376817971d53
Charles Comstock <dgtized@gmail.com>
2021-03-19 15:22:28 -0500

    Add modes to kinematic-chains

--- src/shimmers/sketches/kinematic_chain.cljs
+++ src/shimmers/sketches/kinematic_chain.cljs
@@ -2,11 +2,15 @@
   "Inspired by https://www.youtube.com/watch?v=hbgDqyy8bIwa"
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
+            [reagent.core :as r]
             [shimmers.algorithm.kinematic-chain :as chain]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
+            [shimmers.common.ui.controls :as ctrl]
             [thi.ng.geom.vector :as gv]
             [thi.ng.math.core :as tm]))
+
+(defonce ui-state (r/atom {:mode :sin}))
 
 (defn rel-v [w h]
   (gv/vec2 (cq/rel-pos w h)))
@@ -15,19 +19,21 @@
   (gv/vec2 (q/mouse-x) (q/mouse-y)))
 
 (defn noise-target [rate bw bh]
-  (let [fc (q/frame-count)]
-    (rel-v (q/noise bw (/ fc rate))
-           (q/noise bh (/ fc rate)))))
+  (let [t (q/millis)]
+    (rel-v (q/noise bw (/ t rate))
+           (q/noise bh (/ t rate)))))
 
 (defn sin-target []
   (let [t (q/millis)]
     (rel-v (tm/map-interval (q/cos (+ Math/PI (/ t 10000))) [-1 1] [0.05 0.95])
            (tm/map-interval (q/sin (/ t 2000)) [-1 1] [0.1 0.9]))))
 
-(defn setup []
-  (q/color-mode :hsl 1.0)
-  {:chain (assoc (chain/make-chain (sin-target) 96 12)
-                 :color [0.35 0.6 0.6 0.05])})
+(def modes {:sin sin-target
+            :mouse mouse-target
+            :noise (partial noise-target 4500 100 200)})
+
+(defn follow []
+  ((get modes (:mode @ui-state))))
 
 (defn draw-chain [{:keys [segments]}]
   (q/begin-shape)
@@ -36,9 +42,14 @@
   (apply q/vertex (chain/segment-endpoint (last segments)))
   (q/end-shape))
 
+(defn setup []
+  (q/color-mode :hsl 1.0)
+  {:chain (assoc (chain/make-chain (follow) 96 16)
+                 :color [0.35 0.5 0.5 0.03])})
+
 (defn update-state [state]
   (-> state
-      (update :chain chain/chain-update nil (sin-target))
+      (update :chain chain/chain-update nil (follow))
       (update-in [:chain :color 0] (fn [c] (mod (+ 0.001 c) 1.0)))))
 
 (defn draw [{:keys [chain]}]
@@ -49,6 +60,7 @@
 
 (defn ^:export run-sketch []
   ;; 20210319
+  (ctrl/mount (partial ctrl/change-mode ui-state (keys modes)))
   (q/defsketch kinematic-chain
     :host "quil-host"
     :size [600 400]
