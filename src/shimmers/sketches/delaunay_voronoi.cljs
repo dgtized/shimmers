7f6034dea572baf0709e3c49050c305ed946499a
Charles Comstock <dgtized@gmail.com>
2021-03-22 03:45:42 -0500

    Show the bisecting lines for each triangle face

--- src/shimmers/sketches/delaunay_voronoi.cljs
+++ src/shimmers/sketches/delaunay_voronoi.cljs
@@ -3,14 +3,25 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.triangle :as gt]
             [thi.ng.geom.utils.delaunay :as delaunay]
-            [thi.ng.geom.vector :as gv]))
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm]))
 
 (defn generate-points [n dist]
   (repeatedly n #(gv/vec2 (dist) (dist))))
 
+(defn bisect
+  [[p q]]
+  (let [s (tm/- q p)]
+    (->> [(/ Math/PI 3)
+          (* 5 (/ Math/PI 3))]
+         (mapv (fn [t] (geom/translate p (geom/rotate s t)))))))
+
 (defn setup []
-  (let [points (generate-points 30 #(q/random 0.15 0.85))]
+  (q/no-loop)
+  (let [points (generate-points 4 #(q/random 0.15 0.85))]
     {:points points
      :triangles (delaunay/triangulate points)}))
 
@@ -22,8 +33,12 @@
 
   (q/stroke-weight 0.5)
   (q/no-fill)
-  (doseq [triangle triangles]
-    (apply q/triangle (mapcat cq/rel-pos triangle)))
+  (doseq [triangle triangles
+          :let [view-pts (map cq/rel-pos triangle)]]
+    (apply q/triangle (flatten view-pts))
+    (doseq [edge (geom/edges (apply gt/triangle2 view-pts))
+            :let [[a b] (bisect edge)]]
+      (q/line a b)))
 
   (q/ellipse-mode :radius)
   (q/fill 0 0 0)
