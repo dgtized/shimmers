09bc8b4c7c9fe9bdb20f55228b6f9576f1d582b2
Charles Comstock <dgtized@gmail.com>
2021-03-27 23:48:54 -0500

    Strange magnetic field ish vector fields

--- src/shimmers/sketches/verlet_brushes.cljs
+++ src/shimmers/sketches/verlet_brushes.cljs
@@ -7,7 +7,8 @@
             [thi.ng.geom.physics.core :as physics]
             [thi.ng.geom.rect :as rect]
             [thi.ng.geom.vector :as gv]
-            [thi.ng.math.core :as tm]))
+            [thi.ng.math.core :as tm]
+            [thi.ng.geom.circle :as gc]))
 
 (defn make-particle []
   ;; physics/particle sets initial previous to 0,0
@@ -20,9 +21,9 @@
 (defn position-noise []
   (fn [particle delta]
     (let [[x y] (physics/position particle)
-          factor 200
-          theta (q/noise (/ x factor) (/ y factor) (/ (q/frame-count) 2000))
-          force (geom/as-cartesian (gv/vec2 1.0 (* 4 Math/PI theta)))]
+          factor 100
+          theta (q/noise (/ x factor) (/ y factor) (/ (q/frame-count) 1000))
+          force (geom/as-cartesian (gv/vec2 0.01 (* 4 Math/PI theta)))]
       (physics/add-force particle (tm/* force delta)))))
 
 (defn boundary-push [boundary r strength]
@@ -30,33 +31,36 @@
     (fn [particle delta]
       (let [pos (physics/position particle)
             closest (geom/closest-point boundary pos)
-            d (tm/- pos closest)
+            v (physics/velocity particle)
+            d (tm/- closest pos)
+            b (tm/cross (gv/vec3 (:x d) (:y d) 0) (gv/vec3 0 0 1))
             l (+ (tm/mag-squared d) 1e-6)]
         (when (< l rsq)
-          (physics/add-force particle (tm/* d
+          (physics/add-force particle (tm/* (gv/vec2 (:x b) (:y b))
                                             (/ (* (- 1.0 (/ l rsq)) (* strength delta))
                                                (Math/sqrt l)))))))))
 
 (defn setup []
-  (let [screen-bounds (rect/rect 0 0 (q/width) (q/height))]
+  (let [ring (gc/circle (cq/rel-w 0.5) (cq/rel-h 0.5) (cq/rel-h 0.35))
+        screen-bounds (rect/rect 0 0 (q/width) (q/height))]
     {:physics
      (physics/physics
-      {:particles (repeatedly 256 make-particle)
+      {:particles (repeatedly 64 make-particle)
        :behaviors
-       {;; :position-noise (position-noise)
-        :boundary-push (boundary-push screen-bounds 150 0.5)}
+       {:position-noise (position-noise)
+        :boundary-push (boundary-push ring (cq/rel-h 0.25) 0.1)}
        :constraints
        {:screen-bounds (physics/shape-constraint-inside screen-bounds)}
        :drag 0.01})}))
 
 (defn update-state [state]
-  (update state :physics physics/timestep 16))
+  (update state :physics physics/timestep 10))
 
 (defn draw [{:keys [physics]}]
-  (q/stroke-weight 0.05)
+  (q/stroke-weight 1)
   (doseq [{:keys [inv-weight] :as particle} (:particles physics)]
     (let [[x y] (physics/position particle)
-          size (tm/map-interval inv-weight [0 1] [0.2 2])]
+          size (tm/map-interval inv-weight [0 1] [1 20])]
       (q/ellipse x y size size))))
 
 (defn ^:export run-sketch []
