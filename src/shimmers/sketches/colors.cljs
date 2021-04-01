8a34b16a96ccba84bae52d5a6971e2239ed69f36
Charles Comstock <dgtized@gmail.com>
2021-04-01 00:20:08 -0500

    Allow mixing saturation and lightness

--- src/shimmers/sketches/colors.cljs
+++ src/shimmers/sketches/colors.cljs
@@ -5,7 +5,8 @@
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
             [shimmers.common.ui.controls :as ctrl]
-            [shimmers.math.core :as sm]))
+            [shimmers.math.core :as sm]
+            [thi.ng.math.core :as tm]))
 
 (defn random-hues []
   {:hue1 (int (* 100 (rand)))
@@ -32,13 +33,28 @@
                      [:hue1] [0 100])
         (ctrl/slider ui (fn [v] (str "Hue2: " v))
                      [:hue2] [0 100])
-        [:input {:type "button" :value "Randomize"
-                 :on-click #(swap! ui merge (random-hues))}]])]))
+        [:input {:type "button" :value "Randomize Hues"
+                 :on-click #(swap! ui merge (random-hues))}]
+        (ctrl/slider ui (fn [v] (str "Saturation1: " v))
+                     [:saturation1] [0 100])
+        (ctrl/slider ui (fn [v] (str "Saturation2: " v))
+                     [:saturation2] [0 100])
+        (ctrl/slider ui (fn [v] (str "Lightness1: " v))
+                     [:lightness1] [0 100])
+        (ctrl/slider ui (fn [v] (str "Lightness2: " v))
+                     [:lightness2] [0 100])])]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  (let [ui (r/atom (merge {:mode :hsla :lightness 50 :alpha 100}
-                          (random-hues)))]
+  (let [defaults
+        {:mode :hsla
+         :lightness 50
+         :alpha 100
+         :saturation1 50
+         :saturation2 50
+         :lightness1 50
+         :lightness2 50}
+        ui (r/atom (merge defaults (random-hues)))]
     (ctrl/mount (controls ui))
     {:ui ui}))
 
@@ -61,11 +77,11 @@
 (defn draw-mix-mod [ui]
   (let [dx 0.01
         [h1 h2] (map #(/ % 100) (vals (select-keys @ui [:hue1 :hue2])))
-        s 0.5
-        l 0.5
+        [s1 s2] (map #(/ % 100) (vals (select-keys @ui [:saturation1 :saturation2])))
+        [l1 l2] (map #(/ % 100) (vals (select-keys @ui [:lightness1 :lightness2])))
         a 1.0]
     (doseq [h (range 0 1 dx)]
-      (q/fill (sm/mix-mod h1 h2 h) s l a)
+      (q/fill (sm/mix-mod h1 h2 h) (tm/mix* s1 s2 h) (tm/mix* l1 l2 h) a)
       (q/rect (cq/rel-w h) (cq/rel-h 0) (cq/rel-w dx) (cq/rel-h 1)))))
 
 (defn draw [{:keys [ui]}]
