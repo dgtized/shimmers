e316671ca2844c6a6103372c8f790402f6b59c6f
Charles Comstock <dgtized@gmail.com>
2021-04-01 00:09:01 -0500

    Show spectrum from a mix-mod pair

--- src/shimmers/sketches/colors.cljs
+++ src/shimmers/sketches/colors.cljs
@@ -1,18 +1,18 @@
 (ns shimmers.sketches.colors
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
+            [reagent.core :as r]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
-            [reagent.core :as r]
             [shimmers.common.ui.controls :as ctrl]
-            [shimmers.sketches.lattice-of-common-chords :refer [color-mix]]))
+            [shimmers.math.core :as sm]))
 
 ;; TODO: add hover color info for hsl values?
 ;; Limit range to show spectrum / palette somehow?
 (defn controls [ui]
   (fn []
     [:div
-     (ctrl/change-mode ui [:hsla :nearby])
+     (ctrl/change-mode ui [:hsla :mix-mod])
      (case (:mode @ui)
        :hsla [:div
               (ctrl/checkbox ui "Animate" [:animate])
@@ -20,11 +20,17 @@
                            [:lightness] [0 100])
               (ctrl/slider ui (fn [v] (str "Opacity: " (/ v 100)))
                            [:alpha] [0 100])]
-       :nearby [:div])]))
+       :mix-mod [:div
+                 (ctrl/slider ui (fn [v] (str "Hue1: " v))
+                              [:hue1] [0 100])
+                 (ctrl/slider ui (fn [v] (str "Hue2: " v))
+                              [:hue2] [0 100])])]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  (let [ui (r/atom {:mode :hsla :lightness 50 :alpha 100})]
+  (let [ui (r/atom {:mode :hsla :lightness 50 :alpha 100
+                    :hue1 (int (* 100 (rand)))
+                    :hue2 (int (* 100 (rand)))})]
     (ctrl/mount (controls ui))
     {:ui ui}))
 
@@ -44,13 +50,14 @@
                 s l a)
         (q/rect (cq/rel-w h) (cq/rel-h s) (cq/rel-w dx) (cq/rel-h dy))))))
 
-(defn draw-nearby [ui]
+(defn draw-mix-mod [ui]
   (let [dx 0.01
+        [h1 h2] (map #(/ % 100) (vals (select-keys @ui [:hue1 :hue2])))
         s 0.5
         l 0.5
         a 1.0]
     (doseq [h (range 0 1 dx)]
-      (q/fill h s l a)
+      (q/fill (sm/mix-mod h1 h2 h) s l a)
       (q/rect (cq/rel-w h) (cq/rel-h 0) (cq/rel-w dx) (cq/rel-h 1)))))
 
 (defn draw [{:keys [ui]}]
@@ -58,7 +65,7 @@
   (q/no-stroke)
   (case (:mode @ui)
     :hsla (draw-hsla ui)
-    :nearby (draw-nearby ui)))
+    :mix-mod (draw-mix-mod ui)))
 
 (defn ^:export run-sketch []
   (q/defsketch colors
