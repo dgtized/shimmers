63107373a7d0ea64c73f04ebb531a9f297141e26
Charles Comstock <dgtized@gmail.com>
2021-04-02 15:24:16 -0500

    Animate the scene by rotating points around a circle

--- src/shimmers/sketches/point_to_point.cljs
+++ src/shimmers/sketches/point_to_point.cljs
@@ -3,9 +3,11 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
-            [shimmers.math.geometry :as geometry]
             [shimmers.common.sequence :as cs]
-            [thi.ng.geom.core :as geom]))
+            [shimmers.math.geometry :as geometry]
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm]))
 
 ;; Some experiments with drawing lines derived from a set of random points
 
@@ -16,10 +18,18 @@
        (map second)))
 
 (defn setup []
-  {:points (geometry/generate-points 24 #(+ 0.42 (* 0.13 (q/random-gaussian))))})
+  {:circles (map #(assoc {}
+                         :p %
+                         :theta (* 2 Math/PI (rand))
+                         :radius (tm/clamp (+ 0.05 (* 0.02 (q/random-gaussian))) 0 0.5))
+                 (geometry/generate-points 24 #(+ 0.5 (* 0.13 (q/random-gaussian)))))})
 
-(defn update-state [state]
-  state)
+(defn update-state [{:keys [circles] :as state}]
+  (assoc state
+         :circles (map #(update % :theta + 0.01) circles)
+         :points
+         (for [{:keys [p theta radius]} circles]
+           (geom/translate p (geom/as-cartesian (gv/vec2 radius theta))))))
 
 (defn all-lines [points]
   (doseq [point points
@@ -58,7 +68,7 @@
     (q/ellipse x y 0.2 0.2))
 
   (q/stroke-weight 0.2)
-  (doseq [[p q] (take (* 0.8 (count points)) (short-pairs points))]
+  (doseq [[p q] (take (* 1.2 (count points)) (short-pairs points))]
     (q/line (cq/rel-pos p) (cq/rel-pos q)))
 
   (all-lines points)
