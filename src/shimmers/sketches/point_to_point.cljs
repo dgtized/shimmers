af0fb4eb83c83f1f811304da368c7d30f16c9538
Charles Comstock <dgtized@gmail.com>
2021-04-02 16:24:17 -0500

    Just use a helper to calculate the closest edge

--- src/shimmers/sketches/point_to_point.cljs
+++ src/shimmers/sketches/point_to_point.cljs
@@ -44,26 +44,24 @@
     (q/line 0 y (q/width) y)
     (q/line x 0 x (q/height))))
 
-(defn shortest-to-edge [points]
-  (for [[x y] points]
-    [[x y]
-     (cond (and (>= x 0.5) (>= y 0.5))
-           (if (> x y)
-             [1.0 y]
-             [x 1.0])
-           (and (< x 0.5) (< y 0.5))
-           (if (> x y)
-             [x 0.0]
-             [0.0 y])
-           (and (>= x 0.5) (< y 0.5))
-           (if (> (- 1.0 x) y)
-             [x 0.0]
-             [1.0 y])
-           (and (< x 0.5) (>= y 0.5))
-           (if (> x (- 1.0 y))
-             [x 1.0]
-             [0.0 y])
-           )]))
+(defn closest-edge-point
+  [[x y]]
+  (cond (and (>= x 0.5) (>= y 0.5))
+        (if (> x y)
+          [1.0 y]
+          [x 1.0])
+        (and (< x 0.5) (< y 0.5))
+        (if (> x y)
+          [x 0.0]
+          [0.0 y])
+        (and (>= x 0.5) (< y 0.5))
+        (if (> (- 1.0 x) y)
+          [x 0.0]
+          [1.0 y])
+        (and (< x 0.5) (>= y 0.5))
+        (if (> x (- 1.0 y))
+          [x 1.0]
+          [0.0 y])))
 
 (defn draw [{:keys [points]}]
   (q/background 1.0 1.0)
@@ -85,9 +83,9 @@
 
   (q/stroke 0.45 0.3 0.5)
   (q/stroke-weight 1.0)
-  (doseq [[p q] (shortest-to-edge points)]
-    (q/line (cq/rel-pos p) (cq/rel-pos q)))
-  )
+  (doseq [p points
+          :let [q (closest-edge-point p)]]
+    (q/line (cq/rel-pos p) (cq/rel-pos q))))
 
 (defn ^:export run-sketch []
   (q/defsketch point-to-point
