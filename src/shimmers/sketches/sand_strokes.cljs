387477bb76ac67fdb59667b4f97c0f553032d559
Charles Comstock <dgtized@gmail.com>
2021-04-05 23:38:24 -0500

    Draw more columns per frame

--- src/shimmers/sketches/sand_strokes.cljs
+++ src/shimmers/sketches/sand_strokes.cljs
@@ -11,10 +11,10 @@
             [thi.ng.geom.line :as gl]))
 
 (defn displacement-noise [t v]
-  (cq/rel-h (* 0.2 (q/noise (/ t 2) v))))
+  (cq/rel-h (* 0.3 (q/noise t (* 2 v)))))
 
 (defn rand-color []
-  [0 0 0 0.05])
+  [0 0 0 0.15])
 
 (defn setup []
   (q/color-mode :hsl 1.0)
@@ -23,15 +23,17 @@
   (q/ellipse-mode :radius)
   {:t 0
    :v 0
-   :density 256
+   :density 384
    :shape (gl/line2 (cq/rel-pos 0.05 0.5)
                     (cq/rel-pos 0.95 0.5))
    :displacement (displacement-noise 0 0)
    :angle 0
    :color (rand-color)})
 
+(def dt 0.02)
+
 (defn update-state [{:keys [t v color] :as state}]
-  (let [t' (mod (+ t (* 0.0002 (rand))) 1.0)
+  (let [t' (mod (+ t dt) 1.0)
         new-pass (< t' t)
         v' (if new-pass (inc v) v)
         color' (if new-pass (rand-color) color)]
@@ -39,21 +41,23 @@
            :t t'
            :v v'
            :color color'
-           :angle (ksd/draw (ksd/normal {:mu 0 :sd 2}))
-           :displacement (displacement-noise t' v'))))
+           :angle (ksd/draw (ksd/normal {:mu 0 :sd 1})))))
 
-(defn draw [{:keys [t density shape angle displacement color]}]
+(defn draw [{:keys [t v density shape angle displacement color]}]
   (q/stroke-weight 0.2)
   (q/no-fill)
   (apply q/stroke color)
-  (let [[x y] (geom/point-at shape t)
-        s-disp displacement
-        line (gl/line2 (+ x angle) (- y s-disp) (- x angle) (+ y (* 1.2 s-disp)))
+  (let [cols 40
         ;; normal (ksd/normal {:mu 0.5 :sd 0.05})
         uniform (ksd/uniform {:a 0.1 :b 0.9})]
-    (doseq [p (ksd/sample density uniform)
-            :let [[x y] (geom/point-at line p)]]
-      (q/ellipse x y 0.03 0.03))))
+    (dotimes [iter cols]
+      (let [t (+ t (* dt (+ (/ iter cols) (* (/ 1 cols) (rand)))))
+            s-disp (displacement-noise t v)
+            [x y] (geom/point-at shape t)
+            line (gl/line2 (+ x angle) (- y s-disp) (- x angle) (+ y (* 1.2 s-disp)))]
+        (doseq [p (ksd/sample density uniform)
+                :let [[x y] (geom/point-at line p)]]
+          (q/ellipse x y 0.03 0.03))))))
 
 (defn ^:export run-sketch []
   (q/defsketch sand-strokes
