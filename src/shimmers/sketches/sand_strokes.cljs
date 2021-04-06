54ceb42e180f6e64ff8fd4592913b9cb9187e690
Charles Comstock <dgtized@gmail.com>
2021-04-05 22:44:13 -0500

    Use lower opacity, black and white and move each line faster

--- src/shimmers/sketches/sand_strokes.cljs
+++ src/shimmers/sketches/sand_strokes.cljs
@@ -2,19 +2,19 @@
   "Attempting the sand-stroke technique from
   http://www.complexification.net/gallery/machines/sandstroke/ and further
   explained in https://inconvergent.net/2017/grains-of-sand/."
-  (:require [quil.core :as q :include-macros true]
+  (:require [kixi.stats.distribution :as ksd]
+            [quil.core :as q :include-macros true]
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
             [thi.ng.geom.core :as geom]
-            [thi.ng.geom.line :as gl]
-            [kixi.stats.distribution :as ksd]))
+            [thi.ng.geom.line :as gl]))
 
 (defn displacement-noise [t v]
-  (cq/rel-h (* 0.2 (q/noise (/ t 2) v))))
+  (cq/rel-h (* 0.3 (q/noise (/ t 2) v))))
 
 (defn rand-color []
-  [0 0 (* 0.4 (rand)) 0.005])
+  [0 0 0 0.002])
 
 (defn setup []
   (q/color-mode :hsl 1.0)
@@ -22,7 +22,7 @@
   (q/noise-detail 6 0.75)
   {:t 0
    :v 0
-   :density 1500
+   :density 750
    :shape (gl/line2 (cq/rel-pos 0.05 0.5)
                     (cq/rel-pos 0.95 0.5))
    :displacement (displacement-noise 0 0)
@@ -30,7 +30,7 @@
    :color (rand-color)})
 
 (defn update-state [{:keys [t v color] :as state}]
-  (let [t' (mod (+ t (* 0.001 (rand))) 1.0)
+  (let [t' (mod (+ t (* 0.003 (rand))) 1.0)
         new-pass (< t' t)
         v' (if new-pass (inc v) v)
         color' (if new-pass (rand-color) color)]
@@ -44,7 +44,8 @@
 (defn draw [{:keys [t density shape angle displacement color]}]
   (apply q/stroke color)
   (let [[x y] (geom/point-at shape t)
-        line (gl/line2 (+ x angle) (- y displacement) (- x angle) (+ y displacement))
+        s-disp displacement
+        line (gl/line2 (+ x angle) (- y s-disp) (- x angle) (+ y (* 1.2 s-disp)))
         ;; normal (ksd/normal {:mu 0.5 :sd 0.05})
         uniform (ksd/uniform {:a 0.1 :b 0.9})]
     (doseq [p (ksd/sample density uniform)]
