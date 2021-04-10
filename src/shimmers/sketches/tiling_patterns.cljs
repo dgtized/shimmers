e900283d6bb0a31ebef8e38be402328b81c3fed8
Charles Comstock <dgtized@gmail.com>
2021-04-10 16:12:07 -0500

    Separate svg generation from cell generation

--- src/shimmers/sketches/tiling_patterns.cljs
+++ src/shimmers/sketches/tiling_patterns.cljs
@@ -4,7 +4,8 @@
             [thi.ng.geom.rect :as rect]
             [thi.ng.geom.svg.adapter :as adapt]
             [thi.ng.geom.svg.core :as svg]
-            [thi.ng.geom.vector :as gv]))
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm]))
 
 (defn svg
   "Replaces svg/svg, and removes warnings about xlink & react keys"
@@ -18,18 +19,22 @@
 ;; palette from https://htmlcolors.com/palette/288/paleta-6
 (def paleta-6 ["rgb(7,31,65)" "rgb(0,75,90)" "rgb(246,199,111)", "rgb(237,69,52)" "rgb(188,52,44)"])
 
-(defn tiling-rect [n width palette]
-  (let [rect (rect/rect 0 0 width width)]
-    (for [i (range n)
-          j (range n)]
+(defn seed-rect [rows cols palette]
+  (for [i (range rows)
+        j (range cols)]
+    {:pos (gv/vec2 i j) :fill (rand-nth palette)}))
+
+(defn cells->svg-rect [cells size]
+  (let [rect (rect/rect 0 0 size size)]
+    (for [{:keys [pos fill]} cells]
       (-> rect
-          (geom/translate (gv/vec2 (* i width) (* j width)))
-          (with-meta {:fill (rand-nth palette)})))))
+          (geom/translate (tm/* pos (gv/vec2 size size)))
+          (with-meta {:fill fill})))))
 
 (defn scene []
   (let [a 0]
     (svg {:width 800 :height 600 :stroke "black"}
-         (tiling-rect 5 20 paleta-6))))
+         (cells->svg-rect (seed-rect 5 5 paleta-6) 20))))
 
 (defn page []
   (adapt/all-as-svg (scene)))
