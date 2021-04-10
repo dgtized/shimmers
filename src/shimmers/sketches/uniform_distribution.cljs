7ac5f3f11459a31afa33466250605629b7384933
Charles Comstock <dgtized@gmail.com>
2021-04-09 20:35:11 -0500

    More scrunching

--- src/shimmers/sketches/uniform_distribution.cljs
+++ src/shimmers/sketches/uniform_distribution.cljs
@@ -32,13 +32,13 @@
 
 (defn scene []
   (svg {:width 900 :height 600 :stroke "black"}
-       (example (gv/vec2 150 150) (gc/circle 0 0 50)
+       (example (gv/vec2 100 100) (gc/circle 0 0 50)
                 geom/random-point-inside
                 "g/random-point-inside circle")
-       (example (gv/vec2 450 150) (gc/circle 0 0 50)
+       (example (gv/vec2 350 100) (gc/circle 0 0 50)
                 (fn [_] (p/confusion-disk (gv/vec2 0 0) 50))
                 "g/sample-uniform-inside circle")
-       (example (gv/vec2 150 300) (rect/rect -50 -50 100 100)
+       (example (gv/vec2 100 250) (rect/rect -50 -50 100 100)
                 geom/random-point-inside
                 "g/random-point-inside rect")))
 
