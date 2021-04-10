c01f7731202f6275fd754abfe14b1638d3f8ec2c
Charles Comstock <dgtized@gmail.com>
2021-04-09 22:14:17 -0500

    Shrink the box to fit the contents

--- src/shimmers/sketches/uniform_distribution.cljs
+++ src/shimmers/sketches/uniform_distribution.cljs
@@ -47,7 +47,7 @@
         triangle (gt/triangle2 (gv/vec2 0 0) (gv/vec2 100 50) (gv/vec2 25 100))
         polygon (gp/polygon2 [0 0] [50 75] [100 100] [100 50] [75 25])
         ]
-    (svg {:width 900 :height 600 :stroke "black"}
+    (svg {:width 800 :height 600 :stroke "black"}
          (example (gv/vec2 100 100) circle
                   (sample-points circle geom/random-point-inside)
                   "g/random-point-inside circle")
