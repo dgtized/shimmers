5da73f3f5800112d496b7db0eca90491666a2a1b
Charles Comstock <dgtized@gmail.com>
2021-04-09 20:49:40 -0500

    Lift shape definitions into surrounding let

--- src/shimmers/sketches/uniform_distribution.cljs
+++ src/shimmers/sketches/uniform_distribution.cljs
@@ -21,34 +21,38 @@
         body))
 
 (defn example [pos shape sample-method description]
-  (svg/group {}
-             (svg/text (tm/+ pos (gv/vec2 0 -70))
-                       description
-                       {:text-anchor "middle"})
-             (svg/group {:fill "none"} (geom/translate shape pos))
-             (svg/group {:fill "black" :opacity 0.6}
-                        (for [i (range 500)
-                              :let [[x y] (sample-method shape)]]
-                          (with-meta (geom/translate (gc/circle x y 1) pos)
-                            {:key (str description "-" i)})))))
+  (let [shape-centered (geom/center shape)]
+    (svg/group {}
+               (svg/text (tm/+ pos (gv/vec2 0 -70))
+                         description
+                         {:text-anchor "middle"})
+               (svg/group {:fill "none"} (geom/translate shape-centered pos))
+               (svg/group {:fill "black" :opacity 0.6}
+                          (for [i (range 500)
+                                :let [[x y] (sample-method shape-centered)]]
+                            (with-meta (geom/translate (gc/circle x y 1) pos)
+                              {:key (str description "-" i)}))))))
 
 (defn scene []
-  (svg {:width 900 :height 600 :stroke "black"}
-       (example (gv/vec2 100 100) (gc/circle 0 0 50)
-                geom/random-point-inside
-                "g/random-point-inside circle")
-       (example (gv/vec2 350 100) (gc/circle 0 0 50)
-                (fn [_] (p/confusion-disk (gv/vec2 0 0) 50))
-                "g/sample-uniform-inside circle")
-       (example (gv/vec2 100 250) (geom/center (rect/rect 0 0 100 100))
-                geom/random-point-inside
-                "g/random-point-inside rect")
-       (example (gv/vec2 100 400) (geom/center (gt/triangle2 (gv/vec2 0 0) (gv/vec2 100 50) (gv/vec2 25 100)))
-                geom/random-point-inside
-                "g/random-point-inside triangle")
-       (example (gv/vec2 350 400) (geom/center (gt/triangle2 (gv/vec2 0 0) (gv/vec2 100 50) (gv/vec2 25 100)))
-                geometry/random-point-in-triangle2
-                "g/sample-uniform-inside triangle")))
+  (let [circle (gc/circle 0 0 50)
+        triangle (gt/triangle2 (gv/vec2 0 0) (gv/vec2 100 50) (gv/vec2 25 100))
+        rectangle (rect/rect 0 0 100 100)]
+    (svg {:width 900 :height 600 :stroke "black"}
+         (example (gv/vec2 100 100) circle
+                  geom/random-point-inside
+                  "g/random-point-inside circle")
+         (example (gv/vec2 350 100) circle
+                  (fn [_] (p/confusion-disk (gv/vec2 0 0) 50))
+                  "g/sample-uniform-inside circle")
+         (example (gv/vec2 100 250) rectangle
+                  geom/random-point-inside
+                  "g/random-point-inside rect")
+         (example (gv/vec2 100 400) triangle
+                  geom/random-point-inside
+                  "g/random-point-inside triangle")
+         (example (gv/vec2 350 400) triangle
+                  geometry/random-point-in-triangle2
+                  "g/sample-uniform-inside triangle"))))
 
 (defn page []
   (adapt/all-as-svg (scene)))
