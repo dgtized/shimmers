81e30c8c3d9519deeb857569148b44fabf105b01
Charles Comstock <dgtized@gmail.com>
2021-04-15 13:26:08 -0500

    Clarify x/y gap generation

--- src/shimmers/sketches/decaying_foundations.cljs
+++ src/shimmers/sketches/decaying_foundations.cljs
@@ -17,17 +17,18 @@
   (rect/rect 0 0 w h))
 
 (defn layer [y width height]
-  (for [x (range (* (rand) (- width)) (q/width) width)]
-    (geom/translate (brick (- width (tm/random (/ width 24) (/ width 12))) height)
-                    (gv/vec2 x y))))
+  (let [x-gap (tm/random (/ width 24) (/ width 12))]
+    (for [x (range (- (tm/random width)) (q/width) width)]
+      (geom/translate (brick (- width x-gap) height)
+                      (gv/vec2 x y)))))
 
 (defn wall [height]
-  (let [gap (tm/random (/ height 16) (/ height 8))]
+  (let [y-gap (tm/random (/ height 16) (/ height 8))]
     (mapcat identity
             (for [y (range (- (tm/random height)) (q/height) height)]
-              (layer (+ y (/ gap 2))
+              (layer (+ y (/ y-gap 2))
                      (rand-nth (map (partial * height) (range 1 3.5 0.5)))
-                     (- height gap))))))
+                     (- height y-gap))))))
 
 (defn update-state [state]
   state)
