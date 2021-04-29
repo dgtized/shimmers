416f3072acdbd5d06a79b93298dad6b2c8ea1b65
Charles Comstock <dgtized@gmail.com>
2021-04-29 04:35:36 -0500

    Generate 20 samples of the displace line algorithm for superposition

--- src/shimmers/sketches/path_distribution.cljs
+++ src/shimmers/sketches/path_distribution.cljs
@@ -1,11 +1,36 @@
 (ns shimmers.sketches.path-distribution
+  "Perturbations of displaced lines."
   (:require [shimmers.common.svg :as csvg]
             [shimmers.common.ui.controls :as ctrl]
-            [thi.ng.geom.svg.core :as svg]))
+            [thi.ng.geom.svg.core :as svg]
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.bezier :as bezier]
+            [shimmers.math.probability :as p]
+            [thi.ng.math.core :as tm]))
+
+(defn displace-line
+  "For a given line p to q, find a random spot in the middle third, and pick a
+  random point c in the circle around that spot and return a bezier curve
+  running from p to q through c. "
+  [p q d]
+  (let [curve (->> (* d 0.5 (geom/dist p q))
+                   (p/confusion-disk (tm/mix p q (tm/random 0.33 0.66)))
+                   gv/vec2)]
+    (geom/sample-uniform (bezier/auto-spline2 [p curve q]) 15 false)))
+
+(def width 800)
+(def height 600)
+(defn r [x y]
+  (gv/vec2 (* width x) (* height y)))
 
 (defn scene []
-  (csvg/svg {:width 800 :height 600}
-            (svg/path [[:M [50 50]] [:L [250 50]]] {:stroke "black"})))
+  (csvg/svg {:width width :height height}
+            (concat [(svg/polyline [(r 0.1 0.1) (r 0.9 0.1)] {:stroke "black" :key "p0"})
+                     (svg/polyline [(r 0.1 0.9) (r 0.9 0.9)] {:stroke "black" :key "p1"})]
+                    (for [i (range 20)]
+                      (svg/polyline (displace-line (r 0.1 0.5) (r 0.9 0.5) 0.5)
+                                    {:stroke "black" :stroke-width 0.2 :key (str "line" i)})))))
 
 (defn page []
   [:div (scene)])
