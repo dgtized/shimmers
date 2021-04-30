a1b90f3cf262cf2802b55d6af7d925b8b0642677
Charles Comstock <dgtized@gmail.com>
2021-04-30 16:18:50 -0500

    Expand the radius and standardize on using a line

--- src/shimmers/sketches/path_distribution.cljs
+++ src/shimmers/sketches/path_distribution.cljs
@@ -14,8 +14,9 @@
 (defn displace-line
   "Generate a set of points on a curved line between p and q by picking a confused
   midpoint."
-  [p q d]
-  (let [curve (geometry/confused-midpoint p q d)]
+  [line d]
+  (let [[p q] (geom/vertices line)
+        curve (geometry/confused-midpoint p q d)]
     (geom/sample-uniform (bezier/auto-spline2 [p curve q]) 15 true)))
 
 ;; https://stackoverflow.com/a/45701864/34450
@@ -27,8 +28,8 @@
     (geom/translate (gl/line2 (- y1) x1 (- y2) x2)
                     midpoint)))
 
-(defn displace-at-bisector [p q d]
-  (let [line (gl/line2 p q)
+(defn displace-at-bisector [line d]
+  (let [[p q] (geom/vertices line)
         bisector (scaled-bisector line d)
         curve (geom/random-point bisector)]
     (geom/sample-uniform (bezier/auto-spline2 [p curve q]) 15 true)))
@@ -39,8 +40,8 @@
   (gv/vec2 (* width x) (* height y)))
 
 (defn scene [d]
-  (let [line1 (gl/line2 (r 0.1 0.33) (r 0.9 0.33))
-        line2 (gl/line2 (r 0.1 0.66) (r 0.9 0.66))
+  (let [line1 (gl/line2 (r 0.1 0.25) (r 0.9 0.25))
+        line2 (gl/line2 (r 0.1 0.75) (r 0.9 0.75))
         bisector1 (scaled-bisector line1 d)
         bisector2 (scaled-bisector line2 d)]
     (csvg/svg {:width width :height height :stroke "black" :stroke-width 0.2}
@@ -50,16 +51,16 @@
                                              (* d 0.5 (apply geom/dist (geom/vertices line1))))
                          {:stroke "blue" :fill "none" :key "c1"})]
                       (for [i (range 20)]
-                        (svg/polyline (displace-line (r 0.1 0.33) (r 0.9 0.33) d)
+                        (svg/polyline (displace-line line1 d)
                                       {:key (str "line" i)}))
                       [(with-meta line2 {:stroke "blue" :key "a2"})
                        (with-meta bisector2 {:stroke "blue" :key "b2"})]
                       (for [i (range 20)]
-                        (svg/polyline (displace-at-bisector (r 0.1 0.66) (r 0.9 0.66) d)
+                        (svg/polyline (displace-at-bisector line2 d)
                                       {:key (str "bisector" i)}))))))
 
 (defn page []
-  (let [d 0.25]
+  (let [d 0.3]
     [:div (scene d)
      [:h4 "Two approaches for adding random curvature to a line"]
      [:p "The upper example is drawing a curved line through a random point in the circle."]
