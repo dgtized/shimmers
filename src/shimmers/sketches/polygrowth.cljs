f34faac7fdf09a1c72c7b981eb78e98316ec59e8
Charles Comstock <dgtized@gmail.com>
2021-05-02 18:06:00 -0500

    Extrude growth from center

--- src/shimmers/sketches/polygrowth.cljs
+++ src/shimmers/sketches/polygrowth.cljs
@@ -4,14 +4,25 @@
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
             [thi.ng.geom.circle :as gc]
-            [thi.ng.geom.core :as geom]))
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.polygon :as gp]
+            [thi.ng.geom.triangle :as gt]
+            [thi.ng.math.core :as tm]))
+
+(defn grow [polygon]
+  (let [center (geom/centroid polygon)]
+    (gp/polygon2
+     (for [v (geom/vertices polygon)]
+       (tm/+ center (tm/* (tm/- v center) 1.015))))))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  {:shapes [(geom/as-polygon (gc/circle (cq/rel-pos 0.5 0.5) 50))]})
+  {:shapes [(geom/as-polygon (gc/circle (cq/rel-pos 0.5 0.5) 30))
+            (geom/as-polygon (gc/circle (cq/rel-pos 0.2 0.3) 30))
+            (geom/as-polygon (gt/triangle2 (cq/rel-pos 0.3 0.8) (cq/rel-pos 0.4 0.9) (cq/rel-pos 0.45 0.8)))]})
 
 (defn update-state [state]
-  state)
+  (update state :shapes (partial map grow)))
 
 (defn draw [{:keys [shapes]}]
   (doseq [shape shapes
