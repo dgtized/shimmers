81cd73c9ba5da4c0cb110540a206662f1ea1b850
Charles Comstock <dgtized@gmail.com>
2021-05-18 20:36:41 -0500

    Change representation to a circle, but upscale to hexagon at draw

--- src/shimmers/sketches/six_of_one_half_dozen_of_the_other.cljs
+++ src/shimmers/sketches/six_of_one_half_dozen_of_the_other.cljs
@@ -3,10 +3,11 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
+            [thi.ng.geom.circle :as gc]
             [thi.ng.geom.core :as geom]
-            [thi.ng.math.core :as tm]
+            [thi.ng.geom.polygon :as gp]
             [thi.ng.geom.vector :as gv]
-            [thi.ng.geom.polygon :as gp]))
+            [thi.ng.math.core :as tm]))
 
 (defn polar [r theta]
   (geom/as-cartesian (gv/vec2 r theta)))
@@ -17,32 +18,36 @@
       (butlast r)
       r)))
 
-(defn hexagon [r]
-  (gp/polygon2
-   (for [theta (butlast (range 0 tm/TWO_PI (/ tm/TWO_PI 6)))]
-     (polar r theta))))
+(defn hexagon->polygon [{:keys [p r]}]
+  (-> (for [theta (butlast (range 0 tm/TWO_PI (/ tm/TWO_PI 6)))]
+        (polar r theta))
+      gp/polygon2
+      (geom/translate p)))
 
-(defn subdivide-hexagon3 [r]
+(defn hexagon [p r]
+  (gc/circle p r))
+
+(defn subdivide-hexagon3 [p r]
   (let [r' (/ r 3)
         pos (- r r')
-        hex (hexagon r')]
+        hex (hexagon p r')]
     (into [hex]
           (for [theta (hex-range 6 0)]
             (geom/translate hex (polar pos theta))))))
 
-(defn subdivide-hexagon4 [r]
+(defn subdivide-hexagon4 [p r]
   (let [r' (/ r 4)
         pos (- r r')
-        hex (hexagon r')]
+        hex (hexagon p r')]
     (concat [hex]
             (for [theta (hex-range 6 0)]
               (geom/translate hex (polar pos theta)))
             (for [theta (hex-range 6 (/ 1 12))]
               (geom/translate hex (polar (/ (* (Math/sqrt 3) r) 4) theta))))))
 
-(defn subdivide-hexagon5 [r]
+(defn subdivide-hexagon5 [p r]
   (let [r' (/ r 5)
-        hex (hexagon r')]
+        hex (hexagon p r')]
     (concat [hex]
             (for [theta (hex-range 6 0)]
               (geom/translate hex (polar (/ (* 3 r) 5) theta)))
@@ -51,9 +56,10 @@
             (for [theta (hex-range 6 (/ 1 12))]
               (geom/translate hex (polar (/ (* 2 (Math/sqrt 3) r) 5) theta))))))
 
-(defn subdivide-hexagon6 [r]
+;; FIXME: incorrect outer edges
+(defn subdivide-hexagon6 [p r]
   (let [r' (/ r 6)
-        hex (hexagon r')]
+        hex (hexagon p r')]
     (concat [hex]
             (for [theta (hex-range 6 0)]
               (geom/translate hex (polar (/ r 2) theta)))
@@ -69,9 +75,10 @@
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  (let [base (* 0.5 (q/height))]
-    {:shapes (concat [(hexagon base)]
-                     (subdivide-hexagon6 base))}))
+  (let [p (gv/vec2)
+        r (* 0.5 (q/height))]
+    {:shapes (concat [(hexagon p r)]
+                     (subdivide-hexagon5 p r))}))
 
 (defn update-state [state]
   state)
@@ -79,7 +86,7 @@
 (defn draw [{:keys [shapes]}]
   (q/with-translation (cq/rel-pos 0.5 0.5)
     (doseq [shape shapes]
-      (cq/draw-shape (geom/vertices shape)))))
+      (cq/draw-shape (geom/vertices (hexagon->polygon shape))))))
 
 (defn ^:export run-sketch []
   ;; 20210517
