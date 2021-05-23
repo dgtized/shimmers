fe49c5068cfee4d635bf694dea63247483eb114e
Charles Comstock <dgtized@gmail.com>
2021-05-23 16:55:36 -0500

    More changes to hex-divisions, probably renaming it?

--- src/shimmers/sketches/hex_divisions.cljs
+++ src/shimmers/sketches/hex_divisions.cljs
@@ -5,20 +5,29 @@
             [shimmers.common.quil :as cq]
             [shimmers.math.hexagon :as hex]
             [thi.ng.geom.core :as geom]
-            [thi.ng.geom.vector :as gv]))
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  {:divisions 1})
+  {})
 
 (defn update-state [state]
-  (update state :divisions #(mod (inc %) 24)))
+  state)
 
-(defn draw [{:keys [divisions]}]
-  (let [r (/ (* 0.25 (q/height)) divisions)]
-    (doseq [pos (hex/cube-spiral (gv/vec3) divisions)
-            :let [hex (hex/hexagon (hex/axial->hex r (hex/cube->axial pos)) r)]]
-      (cq/draw-shape (geom/vertices (geom/translate hex (cq/rel-pos 0.5 0.5)) 6)))))
+(defn draw [_]
+  (q/background 1.0 0.025)
+  (q/stroke-weight 0.5)
+  (q/stroke 0.0 1.0)
+  (q/fill 1.0 0.1)
+  (let [t (/ (q/millis) 1000)
+        divisions (int (tm/map-interval (q/sin t) [-1 1] [0 20]))
+        r (/ (* (/ (Math/sqrt 2) 5) (q/height)) (inc divisions))]
+    (q/with-translation (cq/rel-pos 0.5 0.5)
+      (q/with-rotation [(/ t 12)]
+        (doseq [pos (hex/cube-spiral (gv/vec3) divisions)
+                :let [hex (hex/hexagon (hex/axial->hex r (hex/cube->axial pos)) r)]]
+          (cq/draw-shape (geom/vertices hex 6)))))))
 
 (defn ^:export run-sketch []
   ;; 20210523
