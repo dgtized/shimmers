fa2e7f1cb5c18d58fab4e58e6ea5951cc8855bea
Charles Comstock <dgtized@gmail.com>
2021-05-25 00:53:10 -0500

    Replace calls to subdivide-hexagon by n with a generic inset function

--- src/shimmers/sketches/six_of_one_half_dozen_of_the_other.cljs
+++ src/shimmers/sketches/six_of_one_half_dozen_of_the_other.cljs
@@ -29,54 +29,32 @@
   (for [theta (hex-range 6 phase)]
     (geom/translate hex (polar radius theta))))
 
-(defn subdivide-hexagon3 [p r]
-  (let [r' (/ r 3)]
-    (map (comp (partial geom/translate (hex/hexagon p r'))
-               (partial hex/axial-flat->pixel r'))
-         (hex/axial-range 1))))
+(defn subdivide-hexagon-inset
+  "Returns a list of hexagons contained in or just touching the border of
+  containing hexagon subdivided by `n`, where `n` >= 3."
+  [{:keys [p r]} n]
+  (let [r' (/ r n)
+        hex (hexagon->polygon (hex/hexagon p (* r 0.999)))]
+    (->> (hex/axial-range (- n 2))
+         (map (comp (partial geom/translate (hex/hexagon p r'))
+                    (partial hex/axial-flat->pixel r')))
+         (filter (fn [s] (geom/contains-point? hex (:p s)))))))
 
-(defn subdivide-hexagon3-outside [p r]
+(defn subdivide-hexagon3-outside [{:keys [p r]}]
   (let [r' (/ r 3)
         hex (hexagon p r')]
     (into [hex]
           (surrounding-hexes hex 0 (* 3 r')))))
 
-(defn subdivide-hexagon4 [p r]
-  (let [r' (/ r 4)
-        hex (hexagon p r')]
-    (concat [hex]
-            (surrounding-hexes hex 0 (* 3 r'))
-            (surrounding-hexes hex (/ 1 12) (* (Math/sqrt 3) r')))))
-
-(defn subdivide-hexagon5 [p r]
-  (let [r' (/ r 5)
-        hex (hexagon p r')]
-    (concat [hex]
-            (surrounding-hexes hex 0 (* 3 r'))
-            (surrounding-hexes hex (/ 1 12) (* (Math/sqrt 3) r'))
-            (surrounding-hexes hex (/ 1 12) (* 2 (Math/sqrt 3) r')))))
-
-;; TODO: build up the coordinate surroundings instead of using surrounding-hexes
-(defn subdivide-hexagon6 [p r]
-  (let [r' (/ r 6)
-        hex (hexagon p r')]
-    (concat [hex]
-            (surrounding-hexes hex 0 (* 3 r'))
-            (surrounding-hexes hex (/ 1 12) (* (Math/sqrt 3) r'))
-            (surrounding-hexes hex (/ 1 12) (* 2 (Math/sqrt 3) r'))
-            (for [coord [[2 1] [1 2] [-1 3] [-2 3] [-3 2] [-3 1]
-                         [-2 -1] [-1 -2] [1 -3] [2 -3] [3 -2] [3 -1]]]
-              (geom/translate hex (hex/axial-flat->pixel r' coord))))))
-
 (defn maybe-subdivide [shape]
-  (let [subdiv (p/weighted {subdivide-hexagon3 32
+  (let [subdiv (p/weighted {(fn [s] (subdivide-hexagon-inset s 3)) 32
                             subdivide-hexagon3-outside 1
-                            subdivide-hexagon4 16
-                            subdivide-hexagon5 8
-                            subdivide-hexagon6 4})]
+                            (fn [s] (subdivide-hexagon-inset s 4)) 16
+                            (fn [s] (subdivide-hexagon-inset s 5)) 8
+                            (fn [s] (subdivide-hexagon-inset s 6)) 4})]
     (if-not (:divided shape)
       (into [(assoc shape :divided true)]
-            (random-sample (/ 35 36) (subdiv (:p shape) (:r shape))))
+            (random-sample (/ 35 36) (subdiv shape)))
       [shape])))
 
 (defn setup []
@@ -85,12 +63,10 @@
         ;; height is 1/sqrt(3) to fit exactly, so scale it down by a hair
         r (* (/ 0.99 (Math/sqrt 3)) (q/height))]
     ;; Chance of *two* root hexagons, so patterns can fill in from underneath
-    {:shapes (into [(hexagon p r)]
-                   (p/weighted {[] 5
-                                (subdivide-hexagon3 p r) 1
-                                (subdivide-hexagon4 p r) 1
-                                (subdivide-hexagon5 p r) 1
-                                (subdivide-hexagon6 p r) 1}))}))
+    (let [start (hexagon p r)]
+      {:shapes (into [start]
+                     (p/weighted {[] 5
+                                  (subdivide-hexagon-inset start (+ 3 (rand-int 4))) 4}))})))
 
 (defn update-state [state]
   (if (< (count (:shapes state)) 1200)
