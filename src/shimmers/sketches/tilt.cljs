5a5dba61c4747bfe6dda116da843549c8bfaff80
Charles Comstock <dgtized@gmail.com>
2021-05-30 11:15:15 -0500

    Add a declining slope to the stalactite height

--- src/shimmers/sketches/tilt.cljs
+++ src/shimmers/sketches/tilt.cljs
@@ -2,33 +2,31 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
-            [thi.ng.geom.triangle :as gt]
             [shimmers.common.quil :as cq]
-            [thi.ng.geom.core :as geom]))
+            [thi.ng.geom.core :as geom]
+            [thi.ng.geom.triangle :as gt]))
 
 (defn stalactite []
-  (let [x1 (rand)
+  (let [x1 (- (* 1.1 (rand)) 0.1)
         x2 (+ x1 (* 0.09 (q/random-gaussian)))
-        x3 (+ x1 (* 0.2 (rand)))
-        y3 (* 0.6 (rand))]
+        x3 (+ x1 (* 0.3 (rand)))
+        y3 (+ (* 0.4 x3) 0.2 (* 0.03 (q/random-gaussian)))]
     (gt/triangle2 (cq/rel-pos x1 1.0)
                   (cq/rel-pos x2 1.0)
                   (cq/rel-pos x3 y3))))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  {:shapes (repeatedly 64 stalactite)})
+  {:shapes (repeatedly 96 stalactite)})
 
 (defn update-state [state]
   state)
 
-;; Overlay a downward slope of limiting the height of each stalactite and then
-;; blocking out the base.
-;; Also consider adding spheres of confusion somehow, ie circles or hexagon bokeh?
+;; Consider adding spheres of confusion somehow, ie circles or hexagon bokeh?
 (defn draw [{:keys [shapes]}]
   (q/background 1.0)
   (q/no-stroke)
-  (q/fill 0.0 0.4)
+  (q/fill 0.0 0.3)
   (doseq [shape shapes]
     (cq/draw-shape (geom/vertices shape))))
 
