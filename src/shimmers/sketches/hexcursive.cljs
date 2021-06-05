b2836418899796d74bc135164827e5e79f580831
Charles Comstock <dgtized@gmail.com>
2021-06-05 14:25:42 -0500

    Colorize hexcursive

--- src/shimmers/sketches/hexcursive.cljs
+++ src/shimmers/sketches/hexcursive.cljs
@@ -5,7 +5,14 @@
             [shimmers.common.sequence :as cs]
             [shimmers.math.hexagon :as hex]
             [thi.ng.geom.core :as geom]
-            [thi.ng.geom.vector :as gv]))
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm]))
+
+(defn colors [i]
+  [(if (odd? i) 0.45 0.55)
+   0.65
+   (tm/map-interval (/ i 7) 0.0 1.0 0.45 0.85)
+   1.0])
 
 (defn subdivide [depth {:keys [p r] :as h}]
   (if (> depth 8)
@@ -13,25 +20,29 @@
     (let [children
           (->> (hex/cube-spiral (gv/vec3) 1)
                (cs/rotate 1)
-               (mapv (fn [x] (-> x
-                                (hex/cube-hexagon (/ r 3))
-                                (geom/translate p)))))]
+               (map-indexed (fn [i x] (-> x
+                                         (hex/cube-hexagon (/ r 3))
+                                         (geom/translate p)
+                                         (assoc :color (colors (mod (+ i depth) 7)))))))]
       (concat [h]
               (take depth children)
-              (mapcat subdivide (map #(+ depth 1 %) (range 0 8))
+              (mapcat subdivide (map #(+ depth 1 %) (range 0 7))
                       (drop depth children))))))
 
 (defn setup []
   (q/no-loop)
   (q/color-mode :hsl 1.0)
   (let [r (* (/ 0.99 (Math/sqrt 3)) (q/height))]
-    {:shapes (subdivide 0 (hex/hexagon (gv/vec2) r))}))
+    {:shapes (subdivide 0
+                        (assoc (hex/hexagon (gv/vec2) r)
+                               :color (colors 6)))}))
 
 (defn draw [{:keys [shapes]}]
-  (q/stroke-weight 0.4)
+  (q/stroke-weight 0.3)
   (q/no-fill)
   (q/translate (cq/rel-pos 0.5 0.5))
   (doseq [a-hex shapes]
+    (apply q/fill (:color a-hex))
     (-> a-hex
         (geom/vertices 6)
         cq/draw-shape)))
