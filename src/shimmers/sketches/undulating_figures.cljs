7792cc48dbe28da082c3164ef8dff1d438b388a1
Charles Comstock <dgtized@gmail.com>
2021-06-06 15:20:30 -0500

    Push bleedover onto opposing sides and add opposing stroke colors

--- src/shimmers/sketches/undulating_figures.cljs
+++ src/shimmers/sketches/undulating_figures.cljs
@@ -18,26 +18,28 @@
 (defn draw [state]
   (q/background 1.0)
   (q/no-stroke)
-  (q/fill 0.0 1.0)
-  (let [[ma mb] (sort [(q/noise (/ (q/frame-count) 100) 50 50)
-                       (q/noise (/ (q/frame-count) 100) 90 100)])
+  (q/no-fill)
+  (let [[ma mb] [(q/noise (/ (q/frame-count) 100) 50 50)
+                 (q/noise (/ (q/frame-count) 120) 90 100)]
         [ma mb] (if (<= ma mb) [ma mb] [mb ma])]
     (loop [y 0]
       (when (<= y 1.0)
-        (let [a (min ma (rand))
+        (let [a (max ma (rand))
               b (max mb (rand))
               thickness (* 0.01 (rand))
               padding (* 0.01 (rand))]
-          (q/rect (cq/rel-w (+ 0.2 (* 0.8
+          (q/stroke 0 0.5 0.5 1.0)
+          (q/rect (cq/rel-w (- 0.9 (* 0.4
                                       (q/noise (/ (q/frame-count) 200) y))))
                   (cq/rel-h y)
-                  (cq/rel-w (- a))
-                  (cq/rel-h thickness))
-          (q/rect (cq/rel-w (* 0.5
-                               (q/noise (/ (q/frame-count) 180) y)))
+                  (cq/rel-w (- b))
+                  (cq/rel-h (* 0.7 thickness)))
+          (q/stroke 0.55 0.5 0.5 1.0)
+          (q/rect (cq/rel-w (+ 0.1 (* 0.4
+                                      (q/noise (/ (q/frame-count) 180) y))))
                   (cq/rel-h y)
-                  (cq/rel-w b)
-                  (cq/rel-h (* 0.5 thickness)))
+                  (cq/rel-w a)
+                  (cq/rel-h (* 0.3 thickness)))
           (recur (+ y thickness padding)))))))
 
 (defn ^:export run-sketch []
