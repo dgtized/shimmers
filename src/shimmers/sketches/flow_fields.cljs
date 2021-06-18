15f05439df6e937215579e0292c5f177438cab42
Charles Comstock <dgtized@gmail.com>
2021-06-17 21:59:18 -0500

    Basic static flow-field image

--- src/shimmers/sketches/flow_fields.cljs
+++ src/shimmers/sketches/flow_fields.cljs
@@ -4,7 +4,14 @@
             [shimmers.common.framerate :as framerate]
             [thi.ng.geom.vector :as gv]
             [shimmers.math.vector :as v]
-            [thi.ng.math.core :as tm]))
+            [thi.ng.math.core :as tm]
+            [shimmers.common.quil :as cq]
+            [shimmers.math.deterministic-random :as dr]))
+
+(defn dir-at
+  ([[x y]] (dir-at x y))
+  ([x y]
+   (* tm/TWO_PI (q/noise (/ x 60) (/ y 60)))))
 
 (defn draw-grid [size]
   (let [w (/ (q/width) size)
@@ -12,19 +19,38 @@
     (doseq [[p dir]
             (for [x (range (* -2 size) (* (+ 3 w) size) size)
                   y (range (* -2 size) (* (+ 3 h) size) size)]
-              [(gv/vec2 x y) (* tm/TWO_PI (q/noise (/ x 60) (/ y 60)))])]
+              [(gv/vec2 x y) (dir-at x y)])]
       (q/line p (v/add p (v/polar (* 0.5 size) dir))))))
+
+(defn next-flow-point [p r]
+  (tm/+ p (v/polar r (dir-at p))))
+
+(defn flow-points [p r n]
+  (reductions (fn [p] (next-flow-point p r)) p (range n)))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
+  (q/no-loop)
   {})
 
 (defn update-state [state]
   state)
 
-(defn draw [state]
+(defn draw [_]
+  (q/noise-seed (dr/random 1000000))
   (q/background 1.0)
-  (draw-grid 10))
+  ;; (q/stroke-weight 0.1)
+  ;; (q/stroke 0.0 0.0 0.0 1.0)
+  ;; (draw-grid 10)
+  (q/stroke-weight 0.8)
+  (q/no-fill)
+  (q/stroke 0.0 0.5 0.8 1.0)
+  (time
+   (dotimes [_ 3000]
+     (q/begin-shape)
+     (doseq [[x y] (flow-points (gv/vec2 (cq/rel-pos (dr/random) (dr/random))) 2 30)]
+       (q/curve-vertex x y))
+     (q/end-shape))))
 
 (defn ^:export run-sketch []
   ;; 2021
