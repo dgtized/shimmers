22eb3fc2aa6faacf25d56b7198d3fc6486883742
Charles Comstock <dgtized@gmail.com>
2021-07-06 15:33:27 -0500

    Add vendored sparkles library to allow space to put js interop
    Not clear what's going on with the sparkles/package.json main. If that is
    included it fails to load cljs-oss/module-deps, but by excluding the "main"
    field it's happy and works.
    
    Possibly related to https://github.com/bhauman/figwheel-main/issues/271?

---
+++ src/shimmers/scratch/sparkles.cljs
@@ -0,0 +1,6 @@
+(ns shimmers.scratch.sparkles
+  (:require [sparkles]))
+
+(comment
+  (sparkles/trace {:a 1 :b [2 3]}))
+
---
+++ vendor/sparkles/src/sparkles.js
@@ -0,0 +1,7 @@
+var sparkles = {
+  trace: function trace(v) {
+    console.log("Hello From Sparkles: ");
+    console.trace(v);
+    return ["js data"];
+  }
+};
