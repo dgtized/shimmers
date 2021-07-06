3a6846db3de59b8d4c534e81d95ac25127fcb6ce
Charles Comstock <dgtized@gmail.com>
2021-07-06 18:56:14 -0500

    Add chance of streaking for sea as well

--- src/shimmers/sketches/sea_and_sky.cljs
+++ src/shimmers/sketches/sea_and_sky.cljs
@@ -6,6 +6,7 @@
             [shimmers.common.quil :as cq]
             [shimmers.math.geometry :as geometry]
             [shimmers.math.probability :as p]
+            [shimmers.math.vector :as v]
             [shimmers.sketch :as sketch :include-macros true]
             [thi.ng.geom.core :as geom]
             [thi.ng.geom.rect :as rect]
@@ -31,23 +32,34 @@
   (q/no-stroke)
   (q/ellipse-mode :radius)
   (q/fill (+ 0.6 (* 0.04 (q/random-gaussian))) (tm/random 0.4 0.6) (tm/random 0.4 0.75) 0.05)
-  (let [sea' (geometry/rotate-around-centroid sea (* 0.05 (q/random-gaussian)))]
+  (let [angle (* 0.05 (q/random-gaussian))
+        sea' (geometry/rotate-around-centroid sea angle)]
     (dotimes [i 96]
       (let [pos (geom/random-point-inside sea')]
-        (-> pos
-            (random-triangle-at (tm/random tm/TWO_PI) (tm/random 6 32))
-            geom/vertices
-            cq/draw-shape))))
+        (if (p/chance 0.1)
+          (let [theta (tm/random tm/TWO_PI)
+                size (tm/random 6 16)
+                d (rand-nth [-0.2 0.2])]
+            (dotimes [j (int (tm/random 8 32))]
+              (-> (tm/+ pos (v/polar (* j d size) angle))
+                  (random-triangle-at theta size)
+                  geom/vertices
+                  cq/draw-shape)))
+          (-> pos
+              (random-triangle-at (tm/random tm/TWO_PI) (tm/random 6 32))
+              geom/vertices
+              cq/draw-shape)))))
   (q/fill (+ 0.02 (* 0.05 (q/random-gaussian)))
           (tm/random 0.4 0.6)
           (tm/random 0.4 0.75)
           0.05)
+
   (dotimes [i 32]
     (let [pos (geom/random-point-inside sky)]
       (if (p/chance 0.1)
         (let [r (tm/random 1.0 10.0)
               d (rand-nth [-0.5 0.5])]
-          (dotimes [j (int (tm/random 8 24))]
+          (dotimes [j (int (tm/random 8 32))]
             (cq/circle (tm/+ pos (gv/vec2 (* d r j) 0)) r)))
         (cq/circle pos (tm/random 1.0 10.0))))))
 
