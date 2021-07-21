55eeedc11e9a2154604e103b8bb5f94ee68e3c00
Charles Comstock <dgtized@gmail.com>
2021-07-21 17:42:57 -0500

    mess around drawing a curve through the midpoint

--- src/shimmers/sketches/stem_and_leaf.cljs
+++ src/shimmers/sketches/stem_and_leaf.cljs
@@ -3,6 +3,7 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
+            [shimmers.math.vector :as v]
             [shimmers.sketch :as sketch :include-macros true]
             [thi.ng.geom.circle :as gc]
             [thi.ng.geom.vector :as gv]
@@ -17,16 +18,30 @@
 (defn update-state [state]
   state)
 
+(defn curve-by [points]
+  (q/begin-shape)
+  (doseq [[x y] points]
+    (q/curve-vertex x y))
+  (q/end-shape))
+
 (defn draw [{:keys [circles]}]
   (q/background 1.0)
   (q/ellipse-mode :radius)
+  (q/no-fill)
   (doseq [{:keys [p r]} circles]
     (cq/circle p r))
   (doseq [{:keys [p r parent]} circles
           :when parent
           :let [{p' :p r' :r} (nth circles parent)]]
     (q/line (tm/+ p (gv/vec2 0 r)) (tm/+ p' (gv/vec2 0 r')))
-    (q/line (tm/- p (gv/vec2 0 r)) (tm/- p' (gv/vec2 0 r')))))
+    (q/line (tm/- p (gv/vec2 0 r)) (tm/- p' (gv/vec2 0 r')))
+
+    (curve-by [(tm/+ p (v/polar r (* 1.5 Math/PI)))
+               (tm/+ p (v/polar r (* 1.6 Math/PI)))
+               (tm/+ p (v/polar r (* 1.7 Math/PI)))
+               (tm/+ p' (tm/* (tm/- p p') 0.5)) ;; midpoint
+               (tm/+ p' (v/polar r' (* 0.50 Math/PI)))
+               (tm/+ p' (v/polar r' (* 0.33 Math/PI)))])))
 
 (sketch/defquil stem-and-leaf
   :created-at "2021-07-21"
