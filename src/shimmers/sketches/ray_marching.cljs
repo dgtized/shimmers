c9d6a44ffabbcc222b104418159c3ec8e0bac6a5
Charles Comstock <dgtized@gmail.com>
2021-07-26 15:03:41 -0500

    switch to Math/abs to avoid dependency on quil

--- src/shimmers/sketches/ray_marching.cljs
+++ src/shimmers/sketches/ray_marching.cljs
@@ -15,7 +15,7 @@
   (let [epsilon 0.000000001
         denominator (- (* (- x1 x2) (- y3 y4))
                        (* (- y1 y2) (- x3 x4)))]
-    (when (>= (q/abs denominator) epsilon)
+    (when (>= (Math/abs denominator) epsilon)
       (let [t (/ (- (* (- x1 x3) (- y3 y4))
                     (* (- y1 y3) (- x3 x4)))
                  denominator)
