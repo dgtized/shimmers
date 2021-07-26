256f05a00b9cfea1db16ab4e0d74e4c848276914
Charles Comstock <dgtized@gmail.com>
2021-07-26 15:29:09 -0500

    Represent lines as bags of segments

--- src/shimmers/sketches/additive_displacement.cljs
+++ src/shimmers/sketches/additive_displacement.cljs
@@ -11,36 +11,34 @@
 
 ;; Draw line vertical lines from segments, but ensure none of them overlap
 
-(defn make-segment [line a b]
-  (assoc (gl/line2 a b) :line line))
+(defn make-segment [a b]
+  (gl/line2 a b))
 
-(defn make-line [line & points]
+(defn make-line [& points]
   (for [[a b] (partition 2 1 points)]
-    (make-segment line a b)))
+    (make-segment a b)))
 
-(defn add-line [line segments]
-  (loop [base-pos (gv/vec2 (+ 0.01 (* line 0.05)) 0)
+(defn add-line [segments]
+  (loop [base-pos (tm/+ (-> segments first :points first) (gv/vec2 (* 0.02 (rand)) 0))
          addition []]
-    (let [next-pos (tm/+ base-pos (* 0.01 (q/random-gaussian)) (* 0.1 (rand)))
-          prov-line (make-segment line base-pos next-pos)]
+    (let [next-pos (tm/+ base-pos (* 0.003 (q/random-gaussian)) (* 0.08 (rand)))
+          prov-line (make-segment base-pos next-pos)]
       (cond (some (fn [s] (geometry/line-intersect s prov-line)) segments)
             (recur base-pos addition)
             (>= (:y next-pos) 1.0)
-            (conj addition (make-segment line base-pos
+            (conj addition (make-segment base-pos
                                          (gv/vec2 (:x next-pos) (min (:y next-pos) 1.0))))
             :else
             (recur next-pos (conj addition prov-line))))))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  {:lines [(add-line 0 [])]})
+  {:lines [(add-line [(make-segment (gv/vec2 0 0) (gv/vec2 0 1))])]})
 
 (defn update-state [{:keys [lines] :as state}]
-  (let [line-count (count lines)]
-    (if (< line-count 20)
-      (-> state
-          (update :lines conj (add-line line-count (last lines))))
-      state)))
+  (if (< (count lines) 50)
+    (update state :lines conj (add-line (last lines)))
+    state))
 
 (defn draw [{:keys [lines]}]
   (q/background 1.0)
