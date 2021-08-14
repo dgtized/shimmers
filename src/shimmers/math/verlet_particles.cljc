c0b53dbfcf3bc05c4e92ee7b07d12b1eb177f42f
Charles Comstock <dgtized@gmail.com>
2021-08-14 11:15:35 -0500

    Extract verlet-particles out of fireworks sketch

---
+++ src/shimmers/math/verlet_particles.cljc
@@ -0,0 +1,64 @@
+(ns shimmers.math.verlet-particles
+  "Simplified version of thi.ng.geom.physics.core.VerletPhysics with slightly
+  more functional approach."
+  (:require [thi.ng.math.core :as tm]
+            [thi.ng.geom.core :as geom]))
+
+(defprotocol IParticle
+  (pstep [_ drag force delta]))
+
+(defprotocol ISystem
+  (add-particles [_ new-particles])
+  (update-particles [_ delta])
+  (apply-constraints [_ delta])
+  (timestep [_ iter]))
+
+(defrecord Particle [^:mutable pos
+                     ^:mutable prev
+                     ^:mutable age
+                     ^:mutable mass]
+  IParticle
+  (pstep [_ drag force delta]
+    (let [pos' (tm/madd force
+                        (* (/ 1.0 mass) (* delta delta))
+                        (tm/msub pos 2.0 prev))]
+      (set! prev (tm/mix pos pos' drag))
+      (set! pos pos')
+      (set! age (+ age delta)))
+    _))
+
+(defrecord System [^:mutable particles
+                   mechanics
+                   constraints
+                   drag]
+  ISystem
+  (add-particles [_ new-particles]
+    (set! particles (into particles new-particles))
+    _)
+  (update-particles [_ delta]
+    (let [drag' (* delta drag)]
+      (doseq [particle (seq particles)]
+        (let [force (reduce (fn [force mechanic]
+                              (tm/+ force (mechanic particle delta)))
+                            (geom/clear* (:pos particle)) ;; 2d or 3d
+                            mechanics)]
+          (pstep particle drag' force delta))))
+    _)
+  (apply-constraints [_ delta]
+    (set! particles
+          (filter (fn [particle] (every? #(% particle delta) constraints))
+                  (seq particles)))
+    _)
+  (timestep [_ iter]
+    (let [delta (/ 1.0 iter)]
+      (dotimes [_i iter]
+        (-> (update-particles _ delta)
+            (apply-constraints delta))))
+    _))
+
+(defn make-particle [pos prev mass]
+  (Particle. pos prev 0 mass))
+
+(defn make-system [{:keys [particles drag mechanics constraints]
+                    :or {particles [] mechanics [] constraints [] drag 0.0}}]
+  (System. (vec particles) mechanics constraints drag))
--- src/shimmers/sketches/fireworks.cljs
+++ src/shimmers/sketches/fireworks.cljs
@@ -6,70 +6,9 @@
             [shimmers.math.probability :as p]
             [shimmers.math.vector :as v]
             [shimmers.sketch :as sketch :include-macros true]
-            [thi.ng.geom.core :as geom]
+            [shimmers.math.verlet-particles :as vp]
             [thi.ng.geom.vector :as gv]
             [thi.ng.math.core :as tm]))
-
-;; Simplified version of thi.ng.geom.physics.core.VerletPhysics with slightly
-;; more functional approach
-(defprotocol IParticle
-  (pstep [_ drag force delta]))
-
-(defprotocol ISystem
-  (add-particles [_ new-particles])
-  (update-particles [_ delta])
-  (apply-constraints [_ delta])
-  (timestep [_ iter]))
-
-(defrecord Particle [^:mutable pos
-                     ^:mutable prev
-                     ^:mutable age
-                     ^:mutable mass]
-  IParticle
-  (pstep [_ drag force delta]
-    (let [pos' (tm/madd force
-                        (* (/ 1.0 mass) (* delta delta))
-                        (tm/msub pos 2.0 prev))]
-      (set! prev (tm/mix pos pos' drag))
-      (set! pos pos')
-      (set! age (+ age delta)))
-    _))
-
-(defrecord System [^:mutable particles
-                   mechanics
-                   constraints
-                   drag]
-  ISystem
-  (add-particles [_ new-particles]
-    (set! particles (into particles new-particles))
-    _)
-  (update-particles [_ delta]
-    (let [drag' (* delta drag)]
-      (doseq [particle (seq particles)]
-        (let [force (reduce (fn [force mechanic]
-                              (tm/+ force (mechanic particle delta)))
-                            (geom/clear* (:pos particle)) ;; 2d or 3d
-                            mechanics)]
-          (pstep particle drag' force delta))))
-    _)
-  (apply-constraints [_ delta]
-    (set! particles
-          (filter (fn [particle] (every? #(% particle delta) constraints))
-                  (seq particles)))
-    _)
-  (timestep [_ iter]
-    (let [delta (/ 1.0 iter)]
-      (dotimes [_i iter]
-        (-> (update-particles _ delta)
-            (apply-constraints delta))))
-    _))
-
-(defn make-particle [pos prev mass]
-  (Particle. pos prev 0 mass))
-
-(defn make-system [{:keys [particles drag mechanics constraints]
-                    :or {particles [] mechanics [] constraints [] drag 0.0}}]
-  (System. (vec particles) mechanics constraints drag))
 
 ;; Mechanics specific for this sketch
 
@@ -102,7 +41,7 @@
 (defn make-rocket [loc]
   (let [emitter (gv/vec2 loc)
         velocity (gv/vec2 (* 0.015 (q/random-gaussian)) (tm/random 0.9 1.2))]
-    (assoc (make-particle emitter (tm/+ emitter velocity) 100.0)
+    (assoc (vp/make-particle emitter (tm/+ emitter velocity) 100.0)
            :type :rocket
            :hue (popper-colors)
            :max-age 600)))
@@ -111,7 +50,7 @@
 (defn make-payload [{:keys [pos prev hue]} type {:keys [quantity force max-age]}]
   (let [mass (/ 50 quantity)]
     (repeatedly quantity
-                #(assoc (make-particle (tm/+ pos (v/jitter (force))) prev mass)
+                #(assoc (vp/make-particle (tm/+ pos (v/jitter (force))) prev mass)
                         :type type
                         :hue hue
                         :max-age max-age))))
@@ -127,7 +66,7 @@
     (repeatedly quantity
                 #(let [f (apply tm/random force)
                        theta (tm/random (- Math/PI tm/QUARTER_PI) (+ tm/TWO_PI tm/QUARTER_PI))]
-                   (assoc (make-particle (tm/+ pos (v/polar f theta)) prev weight)
+                   (assoc (vp/make-particle (tm/+ pos (v/polar f theta)) prev weight)
                           :type :rain
                           :hue hue
                           :max-age (int (tm/random 55 90)))))))
@@ -177,19 +116,20 @@
   (q/color-mode :hsl 1.0)
   (let [fps 60]
     {:system
-     (make-system {:mechanics [(gravity (gv/vec2 0 (/ 9.8 fps)))
-                               (solid-fuel-thruster (* 2 fps) 30.0 (/ 24.0 fps))]
-                   :constraints [(max-age) (above-ground)]
-                   :drag (/ 0.1 fps)})
+     (vp/make-system {:mechanics [(gravity (gv/vec2 0 (/ 9.8 fps)))
+                                  (solid-fuel-thruster (* 2 fps) 30.0 (/ 24.0 fps))]
+                      :constraints [(max-age) (above-ground)]
+                      :drag (/ 0.1 fps)})
      :explode (exploder 0.30 0.80)}))
 
 (defn update-state [{:keys [system explode] :as state}]
   (when (and (< (count (:particles system)) 64)
              (p/chance (+ 0.01 (* 0.05 (q/noise 50 (q/frame-count))))))
     (let [loc (cq/rel-pos (rand-nth [0.25 0.4 0.5 0.6 0.75]) 1.0)]
-      (add-particles system (repeatedly (rand-int 3) (partial make-rocket loc)))))
+      (vp/add-particles system (repeatedly (rand-int 3) (partial make-rocket loc)))))
+  ;; TODO: add this as a mechanics behavior to avoid touching internal state?
   (set! (.-particles system) (mapcat explode (:particles system)))
-  (timestep system 2)
+  (vp/timestep system 2)
   state)
 
 (defn draw-particle [{:keys [age pos hue type]}]
