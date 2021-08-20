8040aac2163f235137153c15b6babec6b152680d
Charles Comstock <dgtized@gmail.com>
2021-08-20 06:36:49 -0500

    reformat and push upper-left bounds further out to clip the corner

--- src/shimmers/sketches/slashes.cljs
+++ src/shimmers/sketches/slashes.cljs
@@ -5,6 +5,7 @@
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
             [shimmers.sketch :as sketch :include-macros true]
+            [thi.ng.geom.core :as geom]
             [thi.ng.geom.rect :as rect]
             [thi.ng.geom.vector :as gv]
             [thi.ng.math.core :as tm]))
@@ -35,10 +36,14 @@
   (q/color-mode :hsl 1.0)
   (let [bounds (rect/rect (cq/rel-pos 0 0) (cq/rel-pos 1 1))]
     {:slashes
-     (concat (slash-region bounds (tm/random 5.0 6.0) 0 (int (tm/random 6 16)) (constantly 10) (constantly 1))
-             (slash-region bounds (tm/random 5.0 6.0) (cq/rel-w 0.8) 10 #(tm/random 5 15) #(tm/random 0.8 6))
-             (slash-region bounds (tm/random 3.5 4.5) (cq/rel-w 0.4) 10 #(tm/random 5 15) #(tm/random 0.5 4))
-             (slash-region bounds (tm/random 3.5 4.5) (cq/rel-w 1.0) 8 #(tm/random 5 15) #(tm/random 0.5 2)))}))
+     (concat (slash-region bounds (tm/random 5.0 6.0) 0
+                           (int (tm/random 6 16)) (constantly 10) (constantly 1))
+             (slash-region bounds (tm/random 5.0 6.0) (cq/rel-w 0.8)
+                           10 #(tm/random 5 15) #(tm/random 0.8 6))
+             (slash-region bounds (tm/random 3.5 4.5) (cq/rel-w 0.4)
+                           (int (tm/random 4 16)) #(tm/random 5 15) #(tm/random 0.5 4))
+             (slash-region bounds (tm/random 3.5 4.5) (* (geom/width bounds) 1.3)
+                           8 #(tm/random 5 15) #(tm/random 0.5 2)))}))
 
 (defn update-state [state]
   state)
