61c3b96fed43b64779e76b013f31a91e04231675
Charles Comstock <dgtized@gmail.com>
2021-08-25 02:23:25 -0500

    center the original hashmark and then paint around relative origin

--- src/shimmers/sketches/variations.cljs
+++ src/shimmers/sketches/variations.cljs
@@ -3,15 +3,17 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.sketch :as sketch :include-macros true]
+            [thi.ng.geom.core :as geom]
             [thi.ng.geom.line :as gl]
             [thi.ng.geom.vector :as gv]
             [thi.ng.math.core :as tm]))
 
 (defn hashmark []
-  [(gl/line2 0.4 0.1 0.4 0.9)
-   (gl/line2 0.6 0.1 0.6 0.9)
-   (gl/line2 0.1 0.4 0.9 0.4)
-   (gl/line2 0.1 0.6 0.9 0.6)])
+  (map (fn [l] (geom/translate l (gv/vec2 -0.5 -0.5)))
+       [(gl/line2 0.4 0.1 0.4 0.9)
+        (gl/line2 0.6 0.1 0.6 0.9)
+        (gl/line2 0.1 0.4 0.9 0.4)
+        (gl/line2 0.1 0.6 0.9 0.6)]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
@@ -33,8 +35,8 @@
         delta (tm/* (gv/vec2 (q/width) (q/height)) (gv/vec2 (/ 1 I) (/ 1 J)))]
     (doseq [i (range I)]
       (doseq [j (range J)]
-        (draw-mark (tm/* (gv/vec2 (+ i 0.05) (+ j 0.05)) delta)
-                   (* 0.9 (/ (q/width) I)))))))
+        (draw-mark (tm/* (gv/vec2 (+ i 0.5) (+ j 0.5)) delta)
+                   (/ (q/width) I))))))
 
 (sketch/defquil variations
   :created-at "2021-08-25"
