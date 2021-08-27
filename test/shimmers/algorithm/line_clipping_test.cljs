c05c11bc03d44ae46eab80b6445ae70f20a8754e
Charles Comstock <dgtized@gmail.com>
2021-08-27 18:08:41 -0500

    Fix tests to assert lines are equal with tolerance of 1e-4

--- test/shimmers/algorithm/line_clipping_test.cljs
+++ test/shimmers/algorithm/line_clipping_test.cljs
@@ -3,7 +3,17 @@
             [shimmers.algorithm.line-clipping :as sut]
             [thi.ng.geom.line :as gl]
             [thi.ng.geom.rect :as rect]
-            [thi.ng.geom.vector :as gv]))
+            [thi.ng.geom.vector :as gv]
+            [thi.ng.math.core :as tm]))
+
+;; Test helper
+
+(defn line-delta=
+  ([l1 l2] (line-delta= l1 l2 1e-4))
+  ([{[p1 q1] :points} {[p2 q2] :points} eps]
+   (and (tm/delta= p1 p2 eps) (tm/delta= q1 q2 eps))))
+
+;; Body of tests
 
 (def r (rect/rect 1 1 2 2))
 
@@ -19,13 +29,13 @@
   (is (nil? (sut/clip-line r (gv/vec2 4 4) (gv/vec2 5 4))))
   (is (nil? (sut/clip-line r (gv/vec2 4 4) (gv/vec2 4 2))))
   (is (nil? (sut/clip-line r (gv/vec2 0 0) (gv/vec2 10 0))))
-  (is (= (gl/line2 1 1.5 2 2) (sut/clip-line r (gv/vec2 0 1) (gv/vec2 2 2))))
-  (is (= (gl/line2 2 1 3 1.5) (sut/clip-line r (gv/vec2 0 0) (gv/vec2 10 5))))
-  (is (= (gl/line2 1 1 2 2) (sut/clip-line r (gv/vec2 0 0) (gv/vec2 2 2))))
-  (is (= (gl/line2 1 1 3 3) (sut/clip-line r (gv/vec2 0 0) (gv/vec2 4 4))))
-  (is (= (gl/line2 1 2 3 2) (sut/clip-line r (gv/vec2 0 2) (gv/vec2 4 2)))
+  (is (line-delta= (gl/line2 1 1.5 2 2) (sut/clip-line r (gv/vec2 0 1) (gv/vec2 2 2))))
+  (is (line-delta= (gl/line2 2 1 3 1.5) (sut/clip-line r (gv/vec2 0 0) (gv/vec2 10 5))))
+  (is (line-delta= (gl/line2 1 1 2 2) (sut/clip-line r (gv/vec2 0 0) (gv/vec2 2 2))))
+  (is (line-delta= (gl/line2 1 1 3 3) (sut/clip-line r (gv/vec2 0 0) (gv/vec2 4 4))))
+  (is (line-delta= (gl/line2 1 2 3 2) (sut/clip-line r (gv/vec2 0 2) (gv/vec2 4 2)))
       "horizontal line clipping")
-  (is (= (gl/line2 2 1 2 3) (sut/clip-line r (gv/vec2 2 0) (gv/vec2 2 4)))
+  (is (line-delta= (gl/line2 2 1 2 3) (sut/clip-line r (gv/vec2 2 0) (gv/vec2 2 4)))
       "vertical line clipping"))
 
 (deftest hatching
