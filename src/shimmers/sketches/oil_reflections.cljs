afe2f79cd7481bff427ae13b07a7e30633d851c2
Charles Comstock <dgtized@gmail.com>
2021-09-05 11:01:20 -0500

    add parameter to ensure spacing between circles

--- src/shimmers/sketches/oil_reflections.cljs
+++ src/shimmers/sketches/oil_reflections.cljs
@@ -3,28 +3,32 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
-            [shimmers.math.geometry :as geometry]
+            [shimmers.math.equations :as eq]
             [shimmers.sketch :as sketch :include-macros true]
             [thi.ng.geom.circle :as gc]
             [thi.ng.geom.core :as geom]
             [thi.ng.geom.rect :as rect]))
 
-(defn intersects [c1 c2]
-  (when (geometry/circles-overlap? c1 c2)
-    c2))
+(defn intersects
+  [spacing
+   {p1 :p r1 :r}
+   {p2 :p r2 :r :as c2}]
+  (let [dist-sqr (eq/sq (+ r1 r2 spacing))]
+    (when (< (geom/dist-squared p1 p2) dist-sqr)
+      c2)))
 
-(defn add-circle [circles bounds r]
+(defn add-circle [circles bounds r spacing]
   (let [p (geom/random-point-inside bounds)
         near circles ;; todo optimize
         candidate (gc/circle p r)]
-    (when-not (some (partial intersects candidate) near)
+    (when-not (some (partial intersects spacing candidate) near)
       candidate)))
 
-(defn circle-pack [circles bounds radius n-candidates]
+(defn circle-pack [circles bounds radius spacing n-candidates]
   (loop [i 0 circles circles]
     (if (>= i n-candidates)
       circles
-      (if-let [circle (add-circle circles bounds radius)]
+      (if-let [circle (add-circle circles bounds radius spacing)]
         (recur (inc i)
                (conj circles circle))
         (recur (inc i) circles)))))
@@ -40,10 +44,11 @@
                      (<= n 16.0) 32.0
                      (<= n 24) 24.0
                      (<= n 48) 12.0
-                     :else 8.0)]
-    (if (>= n 64)
+                     (<= n 64) 8.0
+                     :else 6.0)]
+    (if (>= n 128)
       state
-      (update state :circles circle-pack bounds radius 10))))
+      (update state :circles circle-pack bounds radius radius 10))))
 
 (defn draw [{:keys [circles]}]
   (q/background 1.0)
