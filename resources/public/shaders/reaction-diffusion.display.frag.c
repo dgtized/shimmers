3919b49e0069c2c597999629b02cd3e38b0ec5db
Charles Comstock <dgtized@gmail.com>
2021-09-15 19:16:45 -0500

    display using absolute difference in concentrations

--- resources/public/shaders/reaction-diffusion.display.frag.c
+++ resources/public/shaders/reaction-diffusion.display.frag.c
@@ -8,8 +8,8 @@ uniform sampler2D image;
 
 void main() {
   vec2 pos = vTexCoord.xy;
-  vec4 current = texture2D(image, pos);
-  float grey = current.y;
+  vec4 col = texture2D(image, pos);
+  float grey = abs(col.y-col.x);
 
   gl_FragColor = vec4(grey,grey,grey,1.0);
 }
