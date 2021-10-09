166f77f98238022f9ec909893aa4dbf30736323e
Charles Comstock <dgtized@gmail.com>
2021-10-09 11:52:19 -0500

    Support re-building tree with different max-children settings

--- src/shimmers/sketches/rtree.cljs
+++ src/shimmers/sketches/rtree.cljs
@@ -5,17 +5,19 @@
    [shimmers.algorithm.rtree :as rtree]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.common.ui.controls :as ctrl]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.circle :as gc]))
 
+(defonce ui-state (ctrl/state {:max-children 10}))
+
 (defn setup []
   (q/color-mode :hsl 1.0)
-  (let [circles (repeatedly 1000 #(gc/circle (cq/rel-vec (rand) (rand)) 3.0))]
-    {:circles circles
-     :rtree (rtree/create circles)}))
+  (let [circles (repeatedly 1000 #(gc/circle (cq/rel-vec (rand) (rand)) 2.5))]
+    {:circles circles}))
 
-(defn update-state [state]
-  state)
+(defn update-state [{:keys [circles] :as state}]
+  (assoc state :rtree (rtree/create (select-keys @ui-state [:max-children]) circles)))
 
 (defn draw [{:keys [rtree]}]
   (doseq [{:keys [bounds data depth]}
@@ -28,8 +30,12 @@
       (q/stroke 0.0 0.6 0.6 1.0)
       (cq/circle p r))))
 
+(defn ui-controls []
+  [:div (ctrl/slider ui-state (fn [v] (str "Max Children " v)) [:max-children] [2 32 1])])
+
 (sketch/defquil rtree
   :created-at "2021-10-09"
+  :on-mount (fn [] (ctrl/mount ui-controls))
   :size [800 600]
   :setup setup
   :update update-state
