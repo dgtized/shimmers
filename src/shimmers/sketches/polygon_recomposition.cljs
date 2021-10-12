f89e3fe91feef74eb2c2bb9d968cccd1c2c9678b
Charles Comstock <dgtized@gmail.com>
2021-10-12 13:19:03 -0500

    animate by displacing or randomly swapping in new points

--- src/shimmers/sketches/polygon_recomposition.cljs
+++ src/shimmers/sketches/polygon_recomposition.cljs
@@ -4,22 +4,34 @@
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.math.probability :as p]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.core :as geom]
    [thi.ng.geom.polygon :as gp]
    [thi.ng.geom.rect :as rect]))
 
+(defn modify-points [points shape]
+  (let [p (rand-nth points)
+        p' (p/weighted {(geom/random-point-inside shape) 1
+                        (p/confusion-disk p 1.0) 8
+                        (p/confusion-disk p (rand)) 4})]
+    (replace {p p'} points)))
+
 (defn setup []
   (q/color-mode :hsl 1.0)
-  (let [shape (rect/rect (cq/rel-vec 0.1 0.1) (cq/rel-vec 0.9 0.9))
-        points (repeatedly 64 #(geom/random-point-inside shape))
-        polygon (gp/polygon2 (gp/convex-hull* points))]
-    {:points points
-     :hull polygon
-     :triangles (gp/tessellate* polygon)}))
+  (let [shape (rect/rect (cq/rel-vec 0.1 0.1) (cq/rel-vec 0.9 0.9))]
+    {:shape shape
+     :points (repeatedly 64 #(geom/random-point-inside shape))
+     :hull nil
+     :triangles []}))
 
-(defn update-state [state]
-  state)
+(defn update-state [{:keys [points shape] :as state}]
+  (let [new-points (modify-points points shape)
+        polygon (gp/polygon2 (gp/convex-hull* new-points))]
+    (assoc state
+           :points new-points
+           :hull polygon
+           :triangles (gp/tessellate* polygon))))
 
 (defn draw [{:keys [points hull triangles]}]
   (q/background 1.0)
@@ -30,10 +42,11 @@
     (cq/circle p 2.0))
 
   (q/stroke-weight 0.5)
-  (q/begin-shape)
-  (doseq [segment (geom/vertices hull)]
-    (apply q/vertex segment))
-  (q/end-shape :close)
+  (when hull
+    (q/begin-shape)
+    (doseq [segment (geom/vertices hull)]
+      (apply q/vertex segment))
+    (q/end-shape :close))
 
   (q/stroke 0.0 0.5 0.5)
   (doseq [t triangles]
