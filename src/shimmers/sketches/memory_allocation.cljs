30a971c9a3b8e50aaca6812a78c51004beaede7a
Charles Comstock <dgtized@gmail.com>
2021-10-15 12:03:38 -0500

    adjust probability of allocation based on available free memory

--- src/shimmers/sketches/memory_allocation.cljs
+++ src/shimmers/sketches/memory_allocation.cljs
@@ -19,26 +19,27 @@
   (let [pages (Math/pow 2 12)]
     (mem/initialize pages)))
 
-(defn update-state [{:keys [allocations free] :as state}]
-  (cond (p/chance 0.5)
-        state
+(defn update-state [{:keys [pages free allocations] :as state}]
+  (let [p-alloc (tm/smoothstep* 0.01 0.66 (/ (float free) (float pages)))
+        state' (assoc state :p-alloc p-alloc)]
+    (cond (p/chance p-alloc)
+          (mem/malloc state' (int (tm/random 4 128)))
 
-        (and (not-empty allocations) (p/chance 0.3))
-        (mem/free state (rand-nth (mem/allocation-ids allocations)))
+          (and (not-empty allocations) (p/chance 0.3))
+          (mem/free state' (rand-nth (mem/allocation-ids allocations)))
 
-        (and (p/chance 0.5)
-             (> (/ (count allocations) (inc (count (mem/allocation-ids allocations)))) 5))
-        (mem/defrag state)
+          (and (p/chance 0.5)
+               (> (/ (count allocations) (inc (count (mem/allocation-ids allocations)))) 5))
+          (mem/defrag state')
 
-        :else
-        (mem/malloc state (int (tm/random 4 128)))))
+          :else state')))
 
 ;; https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
 (def phi (/ (+ 1 (Math/sqrt 5)) 2))
 (defn color [id]
   [(mod (* id phi) 1.0) 0.75 0.55 1.0])
 
-(defn draw [{:keys [free pages allocations]}]
+(defn draw [{:keys [free pages p-alloc allocations]}]
   (q/background 1.0)
   (q/no-fill)
   (let [aspect (/ (q/width) (q/height))
@@ -49,6 +50,7 @@
         alloc-ids (count (mem/allocation-ids allocations))]
 
     (reset! defo {:free [free pages (tm/roundto (/ free pages) 0.01)]
+                  :p-alloc p-alloc
                   :allocations [allocs alloc-ids (tm/roundto (/ (float allocs) (inc alloc-ids)) 0.01)]})
 
     (doseq [[id group] (group-by :id allocations)]
