c2a4f8cb8bf9c36b63d4a3706037133aa327f330
Charles Comstock <dgtized@gmail.com>
2021-10-18 09:21:47 -0500

    Extract split-panes into algorithm.square-packing

--- src/shimmers/sketches/square_packing.cljs
+++ src/shimmers/sketches/square_packing.cljs
@@ -2,6 +2,7 @@
   (:require
    [quil.core :as q :include-macros true]
    [quil.middleware :as m]
+   [shimmers.algorithm.square-packing :as square]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.math.probability :as p]
@@ -12,36 +13,6 @@
 
 ;; Further Experiments: pack resulting squares with patterns of their own?
 ;; Colors and shapes, even tilted or "hand drawn" squares?
-
-;; Note that px,py are not clamped to 0,1 so some funky but interesting results
-;; are possible if using values outside of the range.
-(defn split-panes
-  "Split a rectangle into a square and the 4 surrounding rectangles. The square is
-  of `size`, with `px,py` indicating percent positioning within the
-  larger rectangle. row-major indicates if the panes should split by rows and
-  then fill in the gaps east and west of the square, or by columns and fill in
-  the gaps north or south of the square."
-  ([{[w h] :size :as rectangle} size percent]
-   (split-panes rectangle size percent (> h w)))
-  ([{p :p [width height] :size} size [px py] row-major]
-   (let [offset-x (* px (- width size))
-         offset-y (* py (- height size))
-         sq (rect/rect (tm/+ p [offset-x offset-y]) size size)]
-     (->> (if row-major
-            [(rect/rect p width offset-y) ;; south row
-             (rect/rect (tm/+ p [0 (+ size offset-y)]) width (- height size offset-y)) ;; north row
-             (rect/rect (tm/+ p [0 offset-y]) offset-x size) ;; east chunk
-             (rect/rect (tm/+ p [(+ offset-x size) offset-y]) (- width size offset-x) size) ;; west chunk
-             ]
-            [(rect/rect (tm/+ p [offset-x 0]) size offset-y) ;; south chunk
-             (rect/rect (tm/+ p [offset-x (+ size offset-y)]) size (- height size offset-y)) ;; north chunk
-             (rect/rect p offset-x height) ;; east column
-             (rect/rect (tm/+ p [(+ size offset-x) 0]) (- width size offset-x) height) ;; west column
-             ])
-          (into [sq])))))
-
-(defn has-area? [{:keys [size]}]
-  (every? pos? size))
 
 (defn middle-out
   "Alternate distribution for px,py"
@@ -55,7 +26,7 @@
         [w h] size
         square (* (min w h) ratio)
         [px py] (repeatedly 2 (fn [] (mod (* tm/PHI (rand)) 1.0)))]
-    (filter has-area? (split-panes rectangle square [px py]))))
+    (filter square/has-area? (square/split-panes rectangle square [px py]))))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
