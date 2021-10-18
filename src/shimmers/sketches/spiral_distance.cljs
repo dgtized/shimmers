8d8b1594a320ffc25d889293e3fed7b4dd5698a5
Charles Comstock <dgtized@gmail.com>
2021-10-18 12:55:20 -0500

    Add connecting lines and displace into a small noise map

--- src/shimmers/sketches/spiral_distance.cljs
+++ src/shimmers/sketches/spiral_distance.cljs
@@ -2,15 +2,21 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
+            [shimmers.common.quil :as cq]
+            [shimmers.math.vector :as v]
             [shimmers.sketch :as sketch :include-macros true]
             [thi.ng.geom.vector :as gv]
-            [shimmers.common.quil :as cq]
             [thi.ng.math.core :as tm]))
 
 (defn log-spiral [alpha k theta]
   (let [nat (* alpha (Math/exp (* k theta)))]
     (gv/vec2 (* nat (Math/cos theta))
              (* nat (Math/sin theta)))))
+
+(defn noise-displace [factor r t p]
+  (let [[x y] (tm/* p factor)
+        n (q/noise x y t)]
+    (tm/+ p (v/polar r (* tm/TWO_PI n)))))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
@@ -24,10 +30,13 @@
   (q/no-fill)
   (q/translate (cq/rel-vec 0.66 0.33))
   (let [t (/ (q/frame-count) 200)
-        points (for [theta (range 0 (* Math/PI 12) 0.2)]
-                 (log-spiral 0.1 0.25 (- theta (tm/fract t))))]
+        points (for [theta (range 0 (* Math/PI 13) 0.2)]
+                 (log-spiral 0.1 0.25 (+ theta (tm/fract t))))
+        points (mapv (partial noise-displace (/ 1 400) 10 t) points)]
     (q/begin-shape)
-    (doseq [p points]
+    (doseq [[i p] (map-indexed vector points)]
+      (when-let [q (nth points (- i 28) nil)]
+        (q/line p q))
       (apply q/vertex p))
     (q/end-shape)))
 
