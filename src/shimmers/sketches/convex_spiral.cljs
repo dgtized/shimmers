c16dbb98a68cd6838aa80c1128d7a0d354b2a911
Charles Comstock <dgtized@gmail.com>
2021-10-20 00:03:21 -0500

    Add a little explanation blurb for convex-spiral as an experiment

--- src/shimmers/sketches/convex_spiral.cljs
+++ src/shimmers/sketches/convex_spiral.cljs
@@ -3,13 +3,22 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
+            [shimmers.common.ui.controls :as ctrl]
             [shimmers.math.geometry :as geometry]
             [shimmers.sketch :as sketch :include-macros true]
             [thi.ng.geom.polygon :as gp]))
 
-;; Concept:
-;; Draw a convex hull, remove points on hull, recurse until 3 points, connecting
-;; end of outer with start of inner.
+(defn explanation []
+  [:div
+   [:h4 "Concept"]
+   [:p
+    "Draw a clockwise spiral through a set of random points in a plane."
+    [:ol
+     [:li "Generate a set of random points in a plane, and an empty path."]
+     [:li "Draw a convex hull around those points."]
+     [:li "Remove the points on the hull from the set in the plane and add them to the path in clockwise order."]
+     [:li "Repeat from step 2 on the remaining points until only 2 points remain."]
+     [:li "Display the path through each of the convex hulls connected end to end."]]]])
 
 ;; FIXME: sometimes there is overlap of an outer path and an inner?
 ;; Might just be at transition from outer to inner hull?
@@ -26,9 +35,6 @@
   (q/no-loop)
   {:points (geometry/generate-points 64 #(q/random 0.15 0.85))})
 
-(defn update-state [state]
-  state)
-
 (defn draw [{:keys [points]}]
   (q/background 255)
   (q/stroke-weight 0.5)
@@ -43,8 +49,8 @@
 (sketch/defquil convex-spiral-sketch
   :created-at "2021-03-22"
   :tags #{:static}
-  :size [600 400]
+  :on-mount #(ctrl/mount explanation)
+  :size [800 600]
   :setup setup
-  :update update-state
   :draw draw
   :middleware [m/fun-mode framerate/mode])
