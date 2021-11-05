98b1e4bc03107b62d8543817854703ad3ccb6631
Charles Comstock <dgtized@gmail.com>
2021-11-05 01:52:03 -0500

    Implement basic algorithm as described

--- src/shimmers/sketches/squiggle_line.cljs
+++ src/shimmers/sketches/squiggle_line.cljs
@@ -4,13 +4,53 @@
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
-   [thi.ng.geom.line :as gl]))
+   [thi.ng.geom.core :as g]
+   [thi.ng.geom.line :as gl]
+   [thi.ng.geom.vector :as gv]
+   [thi.ng.math.core :as tm]))
+
+(defn random-point-in-circle [r]
+  (-> (gv/vec2
+       (* r (Math/sqrt (dr/random-double)))
+       (* tm/TWO_PI (dr/random-double)))
+      g/as-cartesian))
 
 ;; Implementing squiggly line drawing as described in
 ;; https://rmarcus.info/blog/2017/10/23/humanlines.html
 ;; https://rmarcus.info/blog/assets/humanLines/Meraj08.pdf
 ;; https://github.com/RyanMarcus/humanLines/blob/master/index.js
+
+(defn squiggle-poly [[px py] [qx qy] t]
+  (let [tau (/ t 2.0)
+        term (- (* 15 (Math/pow tau 4))
+                (* 6 (Math/pow tau 5))
+                (* 10 (Math/pow tau 3)))]
+    (gv/vec2 (+ px (* (- px qx) term))
+             (+ py (* (- py qy) term)))))
+
+(defn squiggle-points [p q]
+  (let [dist (g/dist p q)
+        dt (cond (< dist 200) 0.5
+                 (< dist 400) 0.3
+                 :else 0.2)]
+    (map (partial squiggle-poly p q) (range 0 2 dt))))
+
+(defn squiggle-control [p q]
+  (let [midpoint (tm/mix p q 0.5)
+        displacement (random-point-in-circle 5)] ;; this should be biased toward normal at point?
+    (tm/+ midpoint displacement)))
+
+(defn squiggle [p q]
+  (q/begin-shape)
+  (apply q/vertex p)
+  (doseq [[prev current] (partition 2 1 (squiggle-points p q))
+          :let [[x y] current
+                [cx cy] (squiggle-control prev current)]]
+    (q/quadratic-vertex cx cy x y)
+    )
+  (q/end-shape))
 
 (defn rel-line [{[p q] :points}]
   (gl/line2 (cq/rel-vec p) (cq/rel-vec q)))
@@ -28,8 +68,9 @@
   state)
 
 (defn draw [{:keys [lines]}]
+  (q/background 1.0)
   (doseq [{[p q] :points} lines]
-    (q/line p q)))
+    (squiggle p q)))
 
 (sketch/defquil squiggle-line
   :created-at "2021-"
