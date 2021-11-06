0add8dcdd862c6dbf59f65c0c16c5211b18c2588
Charles Comstock <dgtized@gmail.com>
2021-11-06 14:29:20 -0500

    Add error handling and better logging for shimmers-visit-sketch

--- shimmers.el
+++ shimmers.el
@@ -4,12 +4,20 @@
 (require 'cider-eval)
 (require 'cider-mode)
 
+;; TODO: add function to force reload or reset atoms?
+;; also consider adding support for visiting with same seed
 (defun shimmers-visit-sketch ()
   (interactive)
-  (when-let ((ns (cider-current-ns)))
-    (when (string-match-p "shimmers.sketches.*" ns)
-      (cider-interactive-eval
-       (format "(shimmers.core/visit! '%s)" ns)))))
+  (let ((ns (cider-current-ns)))
+    (if (and ns (string-match-p "shimmers\\.sketches\\..*" ns))
+        (progn
+          (message (format "Visiting sketch %s" ns))
+          (cider-interactive-eval
+           (format "(if-let [page (:parameters (shimmers.core/visit! '%s))]
+                  [(keyword (get-in page [:path :name]))
+                   (get-in page [:query :seed])]
+                  \"Unknown sketch %s!\")" ns ns)))
+      (error (format "Namespace %s is not a sketch." ns)))))
 
 (define-key cider-mode-map (kbd "<f8>") #'shimmers-visit-sketch)
 
