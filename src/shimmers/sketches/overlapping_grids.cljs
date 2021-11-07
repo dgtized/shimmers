cae04a4333a28e99fe2478ec913d1fd382ab4b7c
Charles Comstock <dgtized@gmail.com>
2021-11-07 14:46:49 -0600

    Make the grid more deterministic

--- src/shimmers/sketches/overlapping_grids.cljs
+++ src/shimmers/sketches/overlapping_grids.cljs
@@ -5,41 +5,45 @@
    [shimmers.algorithm.line-clipping :as clip]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.core :as g]
-   [thi.ng.math.core :as tm]
-   [thi.ng.math.noise :as noise]))
+   [thi.ng.math.core :as tm]))
 
 (defn setup []
+  (q/noise-seed (dr/random-int 100000))
   (q/color-mode :hsl 1.0)
-  {})
-
-(defn update-state [state]
-  (let [region (cq/screen-rect 0.95)]
-    {:grids [{:grid (g/subdivide region {:rows 10 :cols 13})
+  (q/no-loop)
+  (let [region (cq/screen-rect 0.95)
+        theta (dr/random 0.6 1.2)]
+    {:grids [{:grid (dr/random-sample 0.85 (g/subdivide region {:rows 10 :cols 13}))
               :stroke-weight 1.0
-              :cell-color [0.0 0.5 0.5 0.5]
-              :noise-threshold 0.0
+              :cell-color [0.99 0.5 0.5 0.5]
+              :noise-threshold 0.5
               :noise-scale 0.05
-              :theta 0.5
+              :theta theta
               :spacing 8}
-             {:grid (g/subdivide region {:rows 6 :cols 7})
+             {:grid (dr/random-sample 0.90 (g/subdivide region {:rows 6 :cols 7}))
               :stroke-weight 0.7
               :cell-color [0.6 0.8 0.5 1.0]
-              :noise-threshold 0.1
-              :noise-scale 0.02
-              :theta 1.0
+              :noise-threshold 0.6
+              :noise-scale 0.03
+              :theta (+ theta (dr/random 0.5 1.0))
               :spacing 12}
-             {:grid (g/subdivide region {:rows 3 :cols 4})
+             {:grid (dr/random-sample 0.95 (g/subdivide region {:rows 3 :cols 4}))
               :stroke-weight 3.0
-              :cell-color [0.09 0.5 0.5 0.5]
-              :noise-threshold 0.2
-              :noise-scale 0.05
-              :theta 2.0
+              :cell-color [0.05 0.6 0.5 0.5]
+              :noise-threshold 0.75
+              :noise-scale 0.02
+              :theta (+ theta (dr/random 1.0 2.0))
               :spacing 12}]}))
 
+(defn update-state [state]
+  state)
+
 (defn scaled-noise [pos scale]
-  (apply noise/noise2 (tm/* pos scale)))
+  (let [[x y] (tm/* pos scale)]
+    (q/noise x y)))
 
 (defn draw [{:keys [grids]}]
   (q/background 1.0)
