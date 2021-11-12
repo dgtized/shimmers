a9e08ef418d60508230e3a4b869e52f00d3e9076
Charles Comstock <dgtized@gmail.com>
2021-11-12 13:20:20 -0600

    Show clipped regions of intersection

--- src/shimmers/sketches/overlapping_polygons.cljs
+++ src/shimmers/sketches/overlapping_polygons.cljs
@@ -8,20 +8,29 @@
    [shimmers.math.geometry.group :as gg]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.circle :as gc]
-   [thi.ng.geom.rect :as rect]))
+   [thi.ng.geom.core :as g]
+   [thi.ng.geom.rect :as rect]
+   [thi.ng.geom.vector :as gv]
+   [thi.ng.geom.triangle :as gt]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  {:pairs (->> [[(rect/rect) (rect/rect 0.1 0.1 1)]
-                [(rect/rect) (gc/circle 0.1 0.1 0.5)]]
-               (mapv gg/group))})
+  {:pairs [[(rect/rect) (rect/rect 0.1 0.1 1)]
+           [(rect/rect) (gc/circle 0.1 0.1 0.5)]
+           [(rect/rect) (gt/triangle2 [0.1 0.1] [0.2 1.2] [0.9 0.8])]]})
 
 (defn update-state [{:keys [pairs] :as state}]
-  (assoc state :shapes
-         (gg/tile-grid (cq/screen-rect 0.9) pairs)))
+  (->> pairs
+       (mapv (fn [[a b]]
+               (let [c (g/clip-with (g/as-polygon a) (g/as-polygon b))]
+                 [a b (g/translate c (gv/vec2 1.33 0))])))
+       (mapv gg/group)
+       (gg/tile-grid (cq/screen-rect 0.9))
+       (assoc state :shapes)))
 
 (defn draw [{:keys [shapes]}]
   (q/ellipse-mode :radius)
+  (q/no-fill)
   (q/background 1.0)
   (when shapes
     (qdg/draw shapes)))
