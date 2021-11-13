30676147b3c9f380e25be2d23d33a35bb1b84bc8
Charles Comstock <dgtized@gmail.com>
2021-11-13 01:06:45 -0600

    Verified that line averaging is working

--- src/shimmers/sketches/displacements_inbetween.cljs
+++ src/shimmers/sketches/displacements_inbetween.cljs
@@ -29,28 +29,33 @@
         gl/linestrip2)))
 
 (defn mix-line [path-a path-b factor]
-  (let [samples (max (count (:points path-a))
-                     (count (:points path-b)))]
-    (for [t (tm/norm-range (dec samples))]
-      (tm/mix (g/point-at path-a t) (g/point-at path-b t) factor))))
+  (gl/linestrip2
+   (let [samples (max (count (:points path-a))
+                      (count (:points path-b)))]
+     (for [t (tm/norm-range (dec samples))]
+       (tm/mix (g/point-at path-a t) (g/point-at path-b t) factor)))))
 
 (defn scene []
   (let [a (make-line (r 0.1 0.1) (r 0.1 0.9) 2 (* 0.05 width))
         b (make-line (r 0.5 0.0) (r 0.5 1.0) 3 (* 0.1 width))
         c (make-line (r 0.9 0.1) (r 0.9 0.9) 2 (* 0.05 width))]
-    (csvg/svg {:width width :height height}
-              (svg/polyline (:points a)
-                            {:key "a"
-                             :stroke "black"
-                             :stroke-width 2.0})
-              (svg/polyline (:points b)
-                            {:key "b"
-                             :stroke "black"
-                             :stroke-width 2.0})
-              (svg/polyline (:points c)
-                            {:key "c"
-                             :stroke "black"
-                             :stroke-width 2.0}))))
+    (csvg/svg {:width width :height height :stroke "black" :stroke-width 1.0}
+              (concat [(svg/polyline (:points a)
+                                     {:key "a"
+                                      :stroke "black"
+                                      :stroke-width 2.0})]
+                      (for [t (tm/norm-range 7)]
+                        (svg/polyline (:points (mix-line a b t))
+                                      {:key (str "ab" t)}))
+                      [(svg/polyline (:points b)
+                                     {:key "b"
+                                      :stroke-width 2.0})]
+                      (for [t (tm/norm-range 11)]
+                        (svg/polyline (:points (mix-line b c t))
+                                      {:key (str "bc" t)}))
+                      [(svg/polyline (:points c)
+                                     {:key "c"
+                                      :stroke-width 2.0})]))))
 
 (defn page []
   [:div (scene)])
