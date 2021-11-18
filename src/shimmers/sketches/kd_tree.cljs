0f9f8043e6f0707bd3f744068f3cd44b9ad837f6
Charles Comstock <dgtized@gmail.com>
2021-11-18 16:41:39 -0600

    Include setup for using debug state on kd-tree

--- src/shimmers/sketches/kd_tree.cljs
+++ src/shimmers/sketches/kd_tree.cljs
@@ -1,14 +1,24 @@
 (ns shimmers.sketches.kd-tree
-  (:require [quil.core :as q :include-macros true]
-            [quil.middleware :as m]
-            [shimmers.common.framerate :as framerate]
-            [shimmers.common.quil :as cq]
-            [shimmers.math.deterministic-random :as dr]
-            [shimmers.math.geometry :as geometry]
-            [shimmers.sketch :as sketch :include-macros true]
-            [thi.ng.geom.vector :as gv]))
+  (:require
+   [fipp.ednize :refer [IEdn]]
+   [quil.core :as q :include-macros true]
+   [quil.middleware :as m]
+   [shimmers.common.framerate :as framerate]
+   [shimmers.common.quil :as cq]
+   [shimmers.common.ui.debug :as debug]
+   [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.geometry :as geometry]
+   [shimmers.sketch :as sketch :include-macros true]
+   [thi.ng.geom.vector :as gv]))
+
+(defonce defo (debug/state))
 
 (defrecord Node [location axis lesser greater])
+
+(extend-protocol IEdn
+  Node
+  (-edn [{:keys [location axis lesser greater]}]
+    (tagged-literal 'Node [location axis lesser greater])))
 
 (defn children [{:keys [lesser greater]}]
   [lesser greater])
@@ -82,11 +92,13 @@
       )
     (q/stroke "grey")
     (q/stroke-weight 0.5)
+    ;; (swap! defo assoc :tree tree)
     (draw-tree tree [[0 (q/height)] [0 (q/width)]])))
 
 (sketch/defquil kd-tree-sketch
   :created-at "2020-11-28"
   :tags #{:datastructures}
+  ;; :on-mount #(debug/mount defo)
   :size [800 600]
   :setup setup
   :update update-state
