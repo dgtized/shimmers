6a2afcc24b17a539d17bed7bda65d02518123fbb
Charles Comstock <dgtized@gmail.com>
2021-11-19 15:25:52 -0600

    extract canvas-frame and draw-frame in canvas-test

--- src/shimmers/sketches/canvas_test.cljs
+++ src/shimmers/sketches/canvas_test.cljs
@@ -4,17 +4,22 @@
             [shimmers.sketch :as sketch :include-macros true]
             [shimmers.math.deterministic-random :as dr]))
 
+(defn canvas-frame [width height render-frame-fn]
+  (ctrl/canvas {:class "canvas-frame"
+                :width width :height height
+                :style {:width (str width "px")
+                        :height (str height "px")}}
+               render-frame-fn))
+
+(defn draw-frame [_ canvas]
+  (let [ctx (cv/high-dpi (.getContext canvas "2d"))]
+    (-> ctx
+        (cv/color-fill "black")
+        (cv/rect-fill (dr/random-int 50 550) (dr/random-int 50 350)
+                      200 200))))
+
 (defn page []
-  [:div
-   [ctrl/canvas {:class "canvas-frame"
-                 :width 800 :height 600
-                 :style {:width "800px" :height "600px"}}
-    (fn [_ canvas]
-      (let [ctx (cv/high-dpi (.getContext canvas "2d"))]
-        (-> ctx
-            (cv/color-fill "black")
-            (cv/rect-fill (dr/random-int 50 550) (dr/random-int 50 350)
-                          200 200))))]])
+  [:div [canvas-frame 800 600 draw-frame]])
 
 (sketch/definition canvas-test
   {:created-at "2021-11-18"
