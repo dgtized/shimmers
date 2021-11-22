1ab9589a1407343236d85a1f918ed26af46aa3c4
Charles Comstock <dgtized@gmail.com>
2021-11-21 21:12:31 -0600

    Add a bunch more tagged types from geom.types to IEdn debug output

--- src/shimmers/common/ui/debug.cljs
+++ src/shimmers/common/ui/debug.cljs
@@ -3,15 +3,42 @@
    [fipp.edn :as fedn]
    [fipp.ednize :refer [IEdn]]
    [shimmers.common.ui.controls :as ctrl]
-   [thi.ng.geom.types :refer [Rect2]]
+   [thi.ng.geom.types :refer [Circle2 Line2 LineStrip2 Polygon2 Rect2 Triangle2]]
    [thi.ng.geom.vector :refer [Vec2]]
    [thi.ng.math.core :as tm]))
 
+;; identity operation for a map record that removes type info so tagged-literal
+;; will not be called recursively with the original type. However this should
+;; pickup extra keys if something else has been assoced in beyond the record keys.
+;; TODO: worth including meta info?
+(defn untyped [s]
+  (zipmap (keys s) (vals s)))
+
 ;; Simplify IEdn output for pretty printing
 (extend-protocol IEdn
+  Circle2
+  (-edn [s]
+    (tagged-literal 'Circle2 (untyped s)))
+
+  Line2
+  (-edn [s]
+    (tagged-literal 'Line2 (untyped s)))
+
+  LineStrip2
+  (-edn [s]
+    (tagged-literal 'LineStrip2 (untyped s)))
+
+  Polygon2
+  (-edn [s]
+    (tagged-literal 'Polygon2 (untyped s)))
+
   Rect2
-  (-edn [{:keys [p size]}]
-    (tagged-literal 'Rect2 {:p p :size size}))
+  (-edn [s]
+    (tagged-literal 'Rect2 (untyped s)))
+
+  Triangle2
+  (-edn [s]
+    (tagged-literal 'Triangle2 (untyped s)))
 
   Vec2
   (-edn [s]
