d33010290b5a83ee951ed36d1ea20b6657653c53
Charles Comstock <dgtized@gmail.com>
2021-11-22 16:31:45 -0600

    Remove humane test output and just use fedn/pprint

--- test/shimmers/test_runner.cljs
+++ test/shimmers/test_runner.cljs
@@ -14,41 +14,23 @@
             [shimmers.math.geometry.group-test]
             [shimmers.math.hexagon-test]
             [cljs-test-display.core :as td]
-            [cljs.pprint :as pp]
             [cljs.test :as t]
-            [clojure.string :as str]
-            [goog.dom :as gdom]
-            [goog.dom.classlist :as classlist]
-            [pjstadig.print :as p]
-            [pjstadig.util :as util]))
+            [fipp.edn :as fedn]))
 
 (enable-console-print!)
 
-;; borrowed from https://github.com/bhauman/cljs-test-display/issues/5#issuecomment-619090019
-(def dummy-env (t/empty-env))
+(defn prettier [content]
+  (td/n :pre {}
+        (td/n :code {} (with-out-str (fedn/pprint content)))))
 
-;; reporting with diffs
-(set! td/add-fail-node!
-      (fn add-fail-node! [m]
-        (let [out (binding [cljs.test/*current-env* dummy-env] ;; we don't want `humane-test-output` to modify the env
-                    (with-out-str
-                      (util/report- (p/convert-event m))))
-              clean-out (->> (str/split out #"\n")
-                             (drop-while #(not (str/starts-with? % "expected")))
-                             (str/join "\n")
-                             (str (with-out-str (pp/pprint (:expected m))) "\n"))
-              node (td/div :test-fail
-                           (td/contexts-node)
-                           (td/div :fail-body
-                                   (when-let [message (:message m)]
-                                     (td/div :test-message message))
-                                   (td/n :pre {}
-                                         (td/n :code {} clean-out))))
-              curr-node (td/current-node)]
-          (js/console.log clean-out)
-          (classlist/add curr-node "has-failures")
-          (classlist/add (td/current-node-parent) "has-failures")
-          (gdom/appendChild curr-node node))))
+;; modified from https://github.com/bhauman/cljs-test-display/issues/5#issuecomment-619090019
+;; pretty print expected vs actual with fedn/pprint
+(set! td/comparison
+      (fn comparison [{:keys [expected actual]}]
+        (td/div
+         (prettier expected)
+         (td/div :actual (td/div :arrow "▶")
+                 (prettier actual)))))
 
 ;; to view, visit http://localhost:9500/figwheel-extra-main/tests
 (defn test-run []
