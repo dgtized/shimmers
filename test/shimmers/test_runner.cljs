e1f3efe7eee6f5d758e92fa3026289a5cbec039f
Charles Comstock <dgtized@gmail.com>
2021-11-22 16:01:46 -0600

    use humane-test output for cljs-test-runner

--- test/shimmers/test_runner.cljs
+++ test/shimmers/test_runner.cljs
@@ -13,10 +13,42 @@
             [shimmers.math.geometry-test]
             [shimmers.math.geometry.group-test]
             [shimmers.math.hexagon-test]
-            [cljs.test]
-            [cljs-test-display.core]))
+            [cljs-test-display.core :as td]
+            [cljs.pprint :as pp]
+            [cljs.test :as t]
+            [clojure.string :as str]
+            [goog.dom :as gdom]
+            [goog.dom.classlist :as classlist]
+            [pjstadig.print :as p]
+            [pjstadig.util :as util]))
 
 (enable-console-print!)
+
+;; borrowed from https://github.com/bhauman/cljs-test-display/issues/5#issuecomment-619090019
+(def dummy-env (t/empty-env))
+
+;; reporting with diffs
+(set! td/add-fail-node!
+      (fn add-fail-node! [m]
+        (let [out (binding [cljs.test/*current-env* dummy-env] ;; we don't want `humane-test-output` to modify the env
+                    (with-out-str
+                      (util/report- (p/convert-event m))))
+              clean-out (->> (str/split out #"\n")
+                             (drop-while #(not (str/starts-with? % "expected")))
+                             (str/join "\n")
+                             (str (with-out-str (pp/pprint (:expected m))) "\n"))
+              node (td/div :test-fail
+                           (td/contexts-node)
+                           (td/div :fail-body
+                                   (when-let [message (:message m)]
+                                     (td/div :test-message message))
+                                   (td/n :pre {}
+                                         (td/n :code {} clean-out))))
+              curr-node (td/current-node)]
+          (js/console.log clean-out)
+          (classlist/add curr-node "has-failures")
+          (classlist/add (td/current-node-parent) "has-failures")
+          (gdom/appendChild curr-node node))))
 
 ;; to view, visit http://localhost:9500/figwheel-extra-main/tests
 (defn test-run []
