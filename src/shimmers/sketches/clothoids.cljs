53635e028903e770b798b73b35a19040b13a27f2
Charles Comstock <dgtized@gmail.com>
2021-11-23 01:08:54 -0600

    Add some basic animations on length and A value

--- src/shimmers/sketches/clothoids.cljs
+++ src/shimmers/sketches/clothoids.cljs
@@ -7,16 +7,17 @@
    [shimmers.common.ui.debug :as debug]
    [shimmers.math.equations :as eq]
    [shimmers.sketch :as sketch :include-macros true]
-   [thi.ng.geom.vector :as gv]))
+   [thi.ng.geom.vector :as gv]
+   [thi.ng.math.core :as tm]))
 
 (defonce defo (debug/state))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  {})
+  {:t 0.0})
 
 (defn update-state [state]
-  state)
+  (update state :t + 0.005))
 
 ;; TODO: use a Cornu or clothoid spiral
 ;; https://pwayblog.com/2016/07/03/the-clothoid/ origin is the midpoint where r
@@ -26,21 +27,34 @@
 (defn plot [points]
   (q/begin-shape)
   (doseq [[x y] points]
-    (cq/circle x y 0.2)
+    (cq/circle x y 0.1)
     (q/vertex x y))
   (q/end-shape))
 
-(defn draw [_]
+(defn pen-color [n]
+  (q/stroke (mod (* n tm/PHI) 1.0) 0.7 0.4))
+
+(defn draw [{:keys [t]}]
   (q/background 1.0)
   (q/ellipse-mode :radius)
   (q/no-fill)
-  (q/stroke-weight 0.05)
   (q/stroke 0)
+  (q/stroke-weight 0.04)
 
   (q/translate (cq/rel-vec 0.5 0.5))
-  (q/scale 10.0)
-  (plot (eq/clothoid 16.32 40 20 -1 0.0 (gv/vec2)))
-  (plot (eq/clothoid 10 40 50 -1 Math/PI (gv/vec2))))
+  (q/scale 11.0)
+  (let [length (+ 40 (* 20 (Math/sin t)))
+        A3 (+ 15 (* 5 (Math/cos t)))
+        A4 (+ 8 (* 2 (Math/cos t)))]
+    (pen-color 0)
+    (plot (eq/clothoid 18 length 20 -1 0.0 (gv/vec2)))
+    (pen-color 1)
+    (plot (eq/clothoid 12 length 50 -1 Math/PI (gv/vec2)))
+    (pen-color 2)
+    (plot (eq/clothoid-from A3 50 30 1 0 (gv/vec2 0.0 0.0)))
+    (pen-color 3)
+    (plot (eq/clothoid-from A4 30 30 1 Math/PI (gv/vec2 0.0 0.0)))
+    ))
 
 (sketch/defquil clothoids
   :created-at "2021-11-23"
