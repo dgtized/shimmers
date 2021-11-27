5ff316390c0dd0193316f5bdab9bd2f6dc490618
Charles Comstock <dgtized@gmail.com>
2021-11-27 00:24:16 -0600

    break clothoids into modes animation and editor

--- src/shimmers/sketches/clothoids.cljs
+++ src/shimmers/sketches/clothoids.cljs
@@ -13,7 +13,7 @@
    [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
 
-(defonce ui-state (ctrl/state {:animate true
+(defonce ui-state (ctrl/state {:mode :animation
                                :A 12
                                :L 60
                                :phi0 0.0
@@ -67,7 +67,42 @@
     [(tm/- tangent-point (v/polar R (+ tangent-angle (* (tm/sign L) (if clockwise 1 -1) 0.5 Math/PI))))
      R]))
 
-(defn draw [{:keys [t]}]
+(defn draw-animation [{:keys [t]}]
+  (let [length (+ 40 (* 20 (Math/sin t)))
+        r 0.1
+        A3 (+ 15 (* 5 (Math/cos t)))
+        A4 (+ 8 (* 2 (Math/cos t)))]
+    (q/scale 11.0)
+    (q/stroke-weight 0.04)
+    (pen-color 0)
+    (plot r (eq/clothoid 18 length 20 -1 0.0 (gv/vec2)))
+    (pen-color 1)
+    (plot r (eq/clothoid 12 length 50 -1 Math/PI (gv/vec2)))
+    (pen-color 2)
+    (plot r (eq/clothoid-from A3 50 30 1 0 (gv/vec2 0.0 0.0)))
+    (pen-color 3)
+    (plot r (eq/clothoid-from A4 30 30 1 Math/PI (gv/vec2 0.0 0.0)))))
+
+(defn draw-sandbox [from]
+  (let [points (clothoid->points @ui-state)
+        lp (if from (first points) (last points))
+        [p r] (clothoid-circle-at-end points)]
+    (q/translate 0 0)
+    (q/scale 1.0)
+    (q/stroke-weight 1.0)
+    (q/stroke 0)
+    (plot 1.0 points)
+    (q/stroke 0.6 0.5 0.5)
+    (cq/circle (first points) 2.5)
+    (q/stroke 0.0 0.5 0.5)
+    (cq/circle (last points) 2.5)
+    (q/stroke-weight 0.5)
+    (cq/circle p r)
+    (q/line p lp)
+    (swap! defo assoc
+           :points points)))
+
+(defn draw [state]
   (reset! defo {})
   (q/background 1.0)
   (q/ellipse-mode :radius)
@@ -75,63 +110,41 @@
   (q/stroke 0)
   (q/translate (cq/rel-vec 0.5 0.5))
 
-  (let [{:keys [animate from]} @ui-state]
-    (if animate
-      (let [length (+ 40 (* 20 (Math/sin t)))
-            r 0.1
-            A3 (+ 15 (* 5 (Math/cos t)))
-            A4 (+ 8 (* 2 (Math/cos t)))]
-        (q/scale 11.0)
-        (q/stroke-weight 0.04)
-        (pen-color 0)
-        (plot r (eq/clothoid 18 length 20 -1 0.0 (gv/vec2)))
-        (pen-color 1)
-        (plot r (eq/clothoid 12 length 50 -1 Math/PI (gv/vec2)))
-        (pen-color 2)
-        (plot r (eq/clothoid-from A3 50 30 1 0 (gv/vec2 0.0 0.0)))
-        (pen-color 3)
-        (plot r (eq/clothoid-from A4 30 30 1 Math/PI (gv/vec2 0.0 0.0))))
-      (let [points (clothoid->points @ui-state)
-            lp (if from (first points) (last points))
-            [p r] (clothoid-circle-at-end points)]
-        (q/translate 0 0)
-        (q/scale 1.0)
-        (q/stroke-weight 1.0)
-        (q/stroke 0)
-        (plot 1.0 points)
-        (q/stroke 0.6 0.5 0.5)
-        (cq/circle (first points) 2.5)
-        (q/stroke 0.0 0.5 0.5)
-        (cq/circle (last points) 2.5)
-        (q/stroke-weight 0.5)
-        (cq/circle p r)
-        (q/line p lp)
-        (swap! defo assoc
-               :points points)
-        ))))
+  (let [{:keys [mode from]} @ui-state]
+    (case mode
+      :animation (draw-animation state)
+      :sandbox (draw-sandbox from))))
+
+(defn sandbox-ctrls []
+  [:div
+   (ctrl/numeric ui-state "A" [:A] [0.0 30.0 0.01])
+   (ctrl/numeric ui-state "Length" [:L] [-100.0 100.0 0.1])
+   (ctrl/numeric ui-state "𝛷₀" [:phi0] [0.0 eq/TAU 0.01])
+   (ctrl/numeric ui-state "N Points" [:N] [8 512 1])
+   (ctrl/checkbox ui-state "Clockwise" [:clockwise])
+   (ctrl/checkbox ui-state "From/To" [:from])
+   (ctrl/numeric ui-state "Scale" [:scale] [1.0 50.0 0.1])])
+
+(defn sandbox-view []
+  [:div {:style {:font-size "0.8em"}}
+   (let [points (clothoid->points @ui-state)
+         [p r] (clothoid-circle-at-end points)]
+     [:pre>code
+      (interpose "\n" [(str (first points))
+                       (str (last points))
+                       (str "circle " p " " r)])])])
 
 (defn ui-controls []
-  (let [animate (:animate @ui-state)]
+  (let [mode (:mode @ui-state)]
     [:div.flexcols
      (ctrl/container ;; TODO: params
-      (ctrl/checkbox ui-state "Animate" [:animate])
-      (when-not animate
-        [:div
-         (ctrl/numeric ui-state "A" [:A] [0.0 30.0 0.01])
-         (ctrl/numeric ui-state "Length" [:L] [-100.0 100.0 0.1])
-         (ctrl/numeric ui-state "𝛷₀" [:phi0] [0.0 eq/TAU 0.01])
-         (ctrl/numeric ui-state "N Points" [:N] [8 100 1])
-         (ctrl/checkbox ui-state "Clockwise" [:clockwise])
-         (ctrl/checkbox ui-state "From/To" [:from])
-         (ctrl/numeric ui-state "Scale" [:scale] [1.0 50.0 0.1])]))
-     (when-not animate
-       [:div {:style {:font-size "0.8em"}}
-        (let [points (clothoid->points @ui-state)
-              [p r] (clothoid-circle-at-end points)]
-          [:pre>code
-           (interpose "\n" [(str (first points))
-                            (str (last points))
-                            (str "circle " p " " r)])])])]))
+      (ctrl/change-mode ui-state [:animation :sandbox])
+      (case mode
+        :animation nil
+        :sandbox [:div [sandbox-ctrls]]))
+     (case mode
+       :animation nil
+       :sandbox [:div [sandbox-view]])]))
 
 (sketch/defquil clothoids
   :created-at "2021-11-23"
