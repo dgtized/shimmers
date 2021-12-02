9ca469439795ee993d8d74d208c13a17d67395a7
Charles Comstock <dgtized@gmail.com>
2021-12-02 13:10:18 -0600

    convert line into a polygon

--- src/shimmers/sketches/morse_patterns.cljs
+++ src/shimmers/sketches/morse_patterns.cljs
@@ -4,10 +4,14 @@
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.equations :as eq]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
+   [thi.ng.geom.core :as g]
+   [thi.ng.geom.polygon :as gp]
    [thi.ng.geom.rect :as rect]
-   [thi.ng.geom.vector :as gv]))
+   [thi.ng.geom.vector :as gv]
+   [thi.ng.math.core :as tm]))
 
 (def width 800)
 (def height 600)
@@ -16,9 +20,17 @@
 
 (defn shapes []
   (let [bounds (rect/rect 0 0 width height)]
-    (clip/variable-hatching bounds 0.3 0 120
-                            (dr/cyclic [2 4 8 12])
-                            (dr/cyclic [0.5 2 3 2 1]))))
+    (mapcat (fn [line]
+              (let [{[p q] :points width :width} (g/scale-size line 1.01)
+                    edge (tm/normalize (tm/- p q) (/ width 2))
+                    perp (g/rotate edge (* 0.25 eq/TAU))]
+                [(gp/polygon2 (tm/+ p perp)
+                              (tm/+ q perp)
+                              (tm/- q perp)
+                              (tm/- p perp))]))
+            (clip/variable-hatching bounds 0.3 0 120
+                                    (dr/cyclic [4 6 8])
+                                    (dr/cyclic [4 8])))))
 
 ;; FIXME: handle large gaps and overlapping lines
 (defn scene []
