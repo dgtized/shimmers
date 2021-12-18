547aacbad1d82fef0af79b931ef5d3ad790e0415
Charles Comstock <dgtized@gmail.com>
2021-12-18 16:15:35 -0600

    extract sampling into a dedicated namespace

---
+++ src/shimmers/algorithm/poisson_disc_sampling.cljc
@@ -0,0 +1,53 @@
+(ns shimmers.algorithm.poisson-disc-sampling
+  (:require [thi.ng.geom.core :as g]
+            [thi.ng.math.core :as tm]
+            [shimmers.common.sequence :as cs]
+            [shimmers.math.vector :as v]))
+
+;; https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
+;; http://extremelearning.com.au/an-improved-version-of-bridsons-algorithm-n-for-poisson-disc-sampling/
+;; https://arxiv.org/abs/2004.06789 -- Fast Variable Density Poisson-Disc Sample Generation with Directional Variation
+(defn init [bounds r k n]
+  (let [w (/ r (Math/sqrt 2))
+        p (g/random-point-inside bounds)
+        [x y] p
+        row (Math/floor (/ x w))
+        col (Math/floor (/ y w))]
+    {:bounds bounds
+     :r r
+     :k k
+     :n n;; => nil
+     :w w
+     :grid {[row col] p}
+     :active [p]}))
+
+(defn neighbors [grid row col]
+  (for [i [-1 0 1]
+        j [-1 0 1]
+        :let [neighbor (get grid [(+ row i) (+ col j)])]
+        :when neighbor]
+    neighbor))
+
+(defn maybe-add-sample [considering {:keys [r w grid active bounds] :as state}]
+  (let [sample (v/add considering
+                      (v/polar (tm/random r (* 2 r))
+                               (tm/random tm/TWO_PI)))
+        [sx sy] sample
+        row (Math/floor (/ sx w))
+        col (Math/floor (/ sy w))]
+    (if (and (g/contains-point? bounds sample)
+             (every? (fn [neighbor] (>= (g/dist sample neighbor) r))
+                     (neighbors grid row col)))
+      (assoc state
+             :active (conj active sample)
+             :grid (assoc grid [row col] sample))
+      state)))
+
+(defn fill-step [{:keys [k active] :as state}]
+  (if (> (count active) 0)
+    (let [considering (rand-nth active)
+          state' (cs/iterate-cycles k (partial maybe-add-sample considering) state)]
+      (if (= state state')
+        (update state' :active (partial remove #(= considering %)))
+        state'))
+    state))
--- src/shimmers/sketches/poisson_disc_sampling.cljs
+++ src/shimmers/sketches/poisson_disc_sampling.cljs
@@ -4,60 +4,10 @@
             [quil.middleware :as m]
             [shimmers.common.framerate :as framerate]
             [shimmers.common.quil :as cq]
+            [shimmers.algorithm.poisson-disc-sampling :as pds]
             [shimmers.common.sequence :as cs]
             [shimmers.common.ui.controls :as ctrl]
-            [shimmers.math.vector :as v]
-            [shimmers.sketch :as sketch :include-macros true]
-            [thi.ng.geom.core :as g]
-            [thi.ng.math.core :as tm]))
-
-;; https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
-;; http://extremelearning.com.au/an-improved-version-of-bridsons-algorithm-n-for-poisson-disc-sampling/
-;; https://arxiv.org/abs/2004.06789 -- Fast Variable Density Poisson-Disc Sample Generation with Directional Variation
-(defn poisson-disc-init [bounds r k n]
-  (let [w (/ r (Math/sqrt 2))
-        p (g/random-point-inside bounds)
-        [x y] p
-        row (Math/floor (/ x w))
-        col (Math/floor (/ y w))]
-    {:bounds bounds
-     :r r
-     :k k
-     :n n
-     :w w
-     :grid {[row col] p}
-     :active [p]}))
-
-(defn neighbors [grid row col]
-  (for [i [-1 0 1]
-        j [-1 0 1]
-        :let [neighbor (get grid [(+ row i) (+ col j)])]
-        :when neighbor]
-    neighbor))
-
-(defn maybe-add-sample [considering {:keys [r w grid active bounds] :as state}]
-  (let [sample (v/add considering
-                      (v/polar (tm/random r (* 2 r))
-                               (tm/random tm/TWO_PI)))
-        [sx sy] sample
-        row (Math/floor (/ sx w))
-        col (Math/floor (/ sy w))]
-    (if (and (g/contains-point? bounds sample)
-             (every? (fn [neighbor] (>= (g/dist sample neighbor) r))
-                     (neighbors grid row col)))
-      (assoc state
-             :active (conj active sample)
-             :grid (assoc grid [row col] sample))
-      state)))
-
-(defn poisson-disc-fill [{:keys [k active] :as state}]
-  (if (> (count active) 0)
-    (let [considering (rand-nth active)
-          state' (cs/iterate-cycles k (partial maybe-add-sample considering) state)]
-      (if (= state state')
-        (update state' :active (partial remove #(= considering %)))
-        state'))
-    state))
+            [shimmers.sketch :as sketch :include-macros true]))
 
 (def ui-state
   (ctrl/state {:radius 8 :samples 10}))
@@ -65,10 +15,10 @@
 (defn setup []
   (q/color-mode :hsl 1.0)
   (let [{:keys [radius samples]} @ui-state]
-    (poisson-disc-init (cq/screen-rect 0.8) radius samples 10)))
+    (pds/init (cq/screen-rect 0.8) radius samples 10)))
 
 (defn update-state [{:keys [n] :as state}]
-  (cs/iterate-cycles n poisson-disc-fill state))
+  (cs/iterate-cycles n pds/fill-step state))
 
 (defn draw [{:keys [active grid]}]
   (q/background 255)
