44cf511c67a99d385881b25dd342699436e3a79b
Charles Comstock <dgtized@gmail.com>
2021-12-31 14:44:23 -0600

    double up the middle hexagon

--- src/shimmers/sketches/hexflare.cljs
+++ src/shimmers/sketches/hexflare.cljs
@@ -1,5 +1,6 @@
 (ns shimmers.sketches.hexflare
   (:require
+   [shimmers.common.sequence :as cs]
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
@@ -9,6 +10,7 @@
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
+   [thi.ng.geom.polygon :as gp]
    [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
 
@@ -23,11 +25,13 @@
 (defn shapes []
   (let [r (* height 0.05)
         line (gl/line2 (rv 0.0 0.9) (rv 1.0 0.05))
-        dist (tm/mag line)]
-    (into (for [t (dr/density-range (/ (* 1.1 (Math/sqrt 3) r) dist)
-                                    (/ (* 4 r) dist))]
-            (g/as-polygon (gc/circle (g/point-at line t) r) 6))
-          [line])))
+        dist (tm/mag line)
+        hexes (for [t (dr/density-range (/ (* 1.1 (Math/sqrt 3) r) dist)
+                                        (/ (* 4 r) dist))]
+                (g/as-polygon (gc/circle (g/point-at line t) r) 6))
+        center (apply gp/polygon2 (gp/inset-polygon (g/vertices (cs/middle hexes)) 5))
+        ]
+    (into hexes [center line])))
 
 (defn scene []
   (csvg/svg {:width width
