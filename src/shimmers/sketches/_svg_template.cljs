cc07905a9f93ddd5ca6b25e0d9c10b39e025e0fa
Charles Comstock <dgtized@gmail.com>
2022-01-02 15:44:43 -0600

    add basic template for svg sketch

---
+++ src/shimmers/sketches/_svg_template.cljs
@@ -0,0 +1,30 @@
+(ns shimmers.sketches.svg-template
+  (:require
+   [shimmers.common.svg :as csvg]
+   [shimmers.common.ui.controls :as ctrl]
+   [shimmers.sketch :as sketch :include-macros true]
+   [shimmers.view.sketch :as view-sketch]
+   [thi.ng.geom.vector :as gv]))
+
+(def width 800)
+(def height 600)
+(defn rv [x y]
+  (gv/vec2 (* width x) (* height y)))
+
+(defn shapes []
+  [])
+
+(defn scene []
+  (csvg/svg {:width width
+             :height height
+             :stroke "black"
+             :fill "white"
+             :stroke-width 0.5}
+            (shapes)))
+
+(sketch/definition svg-template
+  {:created-at "2022-"
+   :type :svg
+   :tags #{}}
+  (ctrl/mount (view-sketch/page-for scene :svg-template)
+              "sketch-host"))
