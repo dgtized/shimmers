cb56e1481cd2f2f9ea2a963939819a25754666a8
Charles Comstock <dgtized@gmail.com>
2022-01-04 19:33:25 -0600

    add some diagonal striping with modular x/y and a little rotation too

--- src/shimmers/sketches/deeper_squares.cljs
+++ src/shimmers/sketches/deeper_squares.cljs
@@ -3,6 +3,7 @@
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.geometry :as geometry]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.core :as g]
@@ -28,14 +29,19 @@
                                       (* (dr/random 0.2 0.8) (- height h)))
                              (tm/- op ip)))))
 
+(defn rotations [group]
+  (for [square group]
+    (geometry/rotate-around-centroid square (dr/random -0.04 0.04))))
+
 (defn shapes []
-  (let [w 100
-        h 100
-        o 5]
-    (for [x (range (/ width w))
-          y (range (/ height h))]
+  (let [w 72
+        h 72
+        o 8]
+    (for [x (range (int (/ width w)))
+          y (range (int (/ height h)))]
       (svg/group {:transform (str "translate(" (* x w) "," (* y h) ")")}
-                 (take 12 (iterate deepen (rect/rect o o (- w o) (- h o))))))))
+                 (rotations (take (+ 4 (* 8 (/ (mod (- x y) 8) 8)))
+                                  (iterate deepen (rect/rect o o (- w o) (- h o)))))))))
 
 (defn scene []
   (csvg/svg {:width width
