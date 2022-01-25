c30474e92ab136eec27bcdd1a7be79aa728cdb39
Charles Comstock <dgtized@gmail.com>
2022-01-25 00:24:52 -0600

    draw a set of perpindicular tick marks along the spiral

--- src/shimmers/sketches/hairy_spiral.cljs
+++ src/shimmers/sketches/hairy_spiral.cljs
@@ -2,12 +2,14 @@
   (:require
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.equations :as eq]
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
+   [thi.ng.geom.line :as gl]
+   [thi.ng.geom.svg.core :as svg]
    [thi.ng.geom.vector :as gv]
-   [thi.ng.math.core :as tm]
-   [shimmers.math.equations :as eq]))
+   [thi.ng.math.core :as tm]))
 
 (def width 800)
 (def height 600)
@@ -18,11 +20,20 @@
   (for [theta (range 0 (* steps dtheta) dtheta)]
     (tm/+ center (v/polar (* dr (/ theta eq/TAU)) theta))))
 
+(defn perpindiculars [center dr dtheta steps]
+  (for [theta (range eq/TAU (* (inc steps) dtheta) dtheta)
+        :let [r (* dr (/ theta eq/TAU))]]
+    (gl/line2 (tm/+ center (v/polar (- r (* dr 0.4)) theta))
+              (tm/+ center (v/polar (+ r (* dr 0.4)) theta)))))
+
 (defn shapes []
-  (let [spiral (spiral-points (rv 0.5 0.5) 24 0.3 256)]
-    (->> (for [p spiral] [:T p])
-         (into [[:M (first spiral)]])
-         (csvg/path))))
+  (let [spiral (spiral-points (rv 0.5 0.5) 24 0.25 384)]
+    (svg/group {}
+               (->> (for [p spiral] [:T p])
+                    (into [[:M (first spiral)]])
+                    (csvg/path))
+               (svg/group {}
+                          (perpindiculars (rv 0.5 0.5) 24 0.5 128)))))
 
 (defn scene []
   (csvg/svg {:width width
