08856931b587391daf5705f98ff73008ac51ed80
Charles Comstock <dgtized@gmail.com>
2022-01-27 14:35:28 -0600

    extract translated-panes helper and fix translation

--- src/shimmers/sketches/negative_overlap.cljs
+++ src/shimmers/sketches/negative_overlap.cljs
@@ -42,30 +42,28 @@
 (defn rect= [{:keys [p size]} {pb :p sizeb :size}]
   (and (tm/delta= p pb) (tm/delta= size sizeb)))
 
+(defn translated-panes [{pa :p :as a} b split]
+  (->> (square/surrounding-panes (g/translate a (tm/- pa))
+                                 (g/translate b (tm/- pa))
+                                 split)
+       (map #(g/translate % pa))
+       (filter (comp square/has-area? g/bounds))))
+
 ;; for now this is removing the clip each time
 (defn rect-exclusion [a b]
   (let [clip (g/clip-with (g/as-polygon a) (g/as-polygon b))]
     (if (empty? (:points clip)) ;; no intersection between pair
       [a]
-      (let [clip (g/bounds clip)
-            {point-a :p} a
-            {point-b :p} b
-            t-clip (g/translate clip (tm/- point-a))]
-        (->> (cond (rect= clip b) ;; b is contained by a
-                   (concat (assign-open (map #(g/translate % point-a) (square/surrounding-panes a b :row))
-                                        (:open a))
-                           (assign-open [clip] (xor-fill a b)))
-                   (rect= clip a) ;; a is contained by b
-                   (concat (assign-open (map #(g/translate % point-b) (square/surrounding-panes b a :row))
-                                        (:open b))
-                           (assign-open [clip] (xor-fill a b)))
-                   :else ;; partial overlap
-                   (let [panes (map #(g/translate % point-a)
-                                    (square/surrounding-panes (g/translate a (tm/- point-a)) t-clip
-                                                              :row))]
-                     (concat (assign-open panes (:open a))
-                             (assign-open [clip] (xor-fill a b)))))
-             (filter (comp square/has-area? g/bounds)))))))
+      (let [clip (g/bounds clip)]
+        (cond (rect= clip b) ;; b is contained by a
+              (concat (assign-open (translated-panes a b :row) (:open a))
+                      (assign-open [clip] (xor-fill a b)))
+              (rect= clip a) ;; a is contained by b
+              (concat (assign-open (translated-panes b a :row) (:open b))
+                      (assign-open [clip] (xor-fill a b)))
+              :else ;; partial overlap
+              (concat (assign-open (translated-panes a clip :row) (:open a))
+                      (assign-open [clip] (xor-fill a b))))))))
 
 (def example (assign-open [(rect/rect (rv 0.25 0.25) (rv 0.75 0.75))
                            (rect/rect (rv 0 0) (rv 0.5 0.5))] true))
@@ -105,7 +103,7 @@
        (take n)))
 
 (defn shapes []
-  (let [additions example]
+  (let [additions (random-additions 2)]
     [(svg/group {} (map fill-shape (reduce add-split-shapes [base-shape] additions)))
      (svg/group {:fill "#F00"}
                 (mapcat (fn [{:keys [points]}] (map #(svg/circle % 2) points)) additions))]))
