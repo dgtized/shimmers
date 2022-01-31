7466df3557a9533bc8c1864b217bf7ae6f6acb36
Charles Comstock <dgtized@gmail.com>
2022-01-31 05:17:38 -0600

    Add the density to the vertical lines and make them meet in the middle

--- src/shimmers/sketches/angle_of_ascent.cljs
+++ src/shimmers/sketches/angle_of_ascent.cljs
@@ -4,10 +4,10 @@
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
-   [thi.ng.geom.vector :as gv]
-   [thi.ng.geom.rect :as rect]
+   [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
-   [thi.ng.geom.core :as g]))
+   [thi.ng.geom.rect :as rect]
+   [thi.ng.geom.vector :as gv]))
 
 (def width 800)
 (def height 600)
@@ -15,9 +15,10 @@
   (gv/vec2 (* width x) (* height y)))
 
 (defn lines [{p :p [w h] :size} f n]
-  (for [x (range 0 n)]
-    (-> (gl/line2 (gv/vec2 (* w (f (/ x n))) 0)
-                  (gv/vec2 (* w (f (/ x n))) h))
+  (for [i (range 0 n)
+        :let [x (f (/ i n))]]
+    (-> (gl/line2 (gv/vec2 (* w x) 0)
+                  (gv/vec2 (* w x) h))
         (g/translate p))))
 
 (defn shapes []
@@ -25,9 +26,11 @@
         b (rect/rect (rv 0.325 0.425) (rv 0.675 0.575))
         c (rect/rect (rv 0.55 0.1) (rv 0.9 0.25))]
     (concat [a b c]
-            (lines a (fn [x] (- 1 (Math/pow x 2))) 25)
-            (lines b identity 25)
-            (lines c (fn [x] (Math/pow x 2)) 25))))
+            (lines a (fn [x] (- 1 (Math/pow x 3))) 25)
+            (lines b (fn [x] (if (< x 0.5)
+                              (- 0.5 (Math/pow x 1.5))
+                              (+ 0.5 (Math/pow (- x 0.5) 1.5)))) 50)
+            (lines c (fn [x] (Math/pow x 3)) 25))))
 
 (defn scene []
   (csvg/svg {:width width
