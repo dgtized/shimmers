2429e29c7603763507f573ac8c713df245ac6872
Charles Comstock <dgtized@gmail.com>
2022-02-19 11:13:35 -0600

    generate rows of triangles too

--- src/shimmers/sketches/shapes_and_patterns.cljs
+++ src/shimmers/sketches/shapes_and_patterns.cljs
@@ -6,6 +6,8 @@
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.circle :as gc]
+   [thi.ng.geom.core :as g]
+   [thi.ng.geom.triangle :as gt]
    [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
 
@@ -14,18 +16,27 @@
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
-(defn gen-row [v row-height]
+(defn circle-row [v row-height]
   (let [diameter row-height
         cols (/ width diameter)
         r (* 0.5 diameter)]
     (for [u (tm/norm-range cols)]
       (gc/circle (rv (+ u (/ r width)) (+ v (/ r height))) r))))
 
+(defn triangle-row [v row-height]
+  (let [base row-height
+        cols (/ width base)]
+    (for [u (tm/norm-range cols)]
+      (g/translate (gt/equilateral2 (gv/vec2 0.0 0.1) (gv/vec2 (* -0.9 base) 0.1))
+                   (rv u v)))))
+
 (defn shapes [rows]
   (let [ranges (dr/var-range rows)
         heights (map - (rest ranges) ranges)]
-    (for [[v gap] (map vector ranges heights)]
-      (gen-row v (* gap height)))))
+    (for [[v gap] (map vector ranges heights)
+          :let [row-height (* gap height)]]
+      ((dr/weighted {#(circle-row v row-height) 1
+                     #(triangle-row v row-height) 1})))))
 
 (defn scene []
   (csvg/svg {:width width
