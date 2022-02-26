57c89e63a0646e595788bf1d022346389c1d35fb
Charles Comstock <dgtized@gmail.com>
2022-02-26 00:59:42 -0600

    remove stale comment and add in a new idea

--- src/shimmers/sketches/differential_growth.cljs
+++ src/shimmers/sketches/differential_growth.cljs
@@ -82,7 +82,7 @@
 (defn bounds-check [bounds points]
   (map #(v/clamp-bounds bounds %) points))
 
-;; What would happen if updates only happen to a percentage of nodes?
+;; What would happen if each node was a circle with radius and that adjusted force calculations
 (defn path-update [{:keys [points]}]
   (let [bounds (cq/screen-rect 0.95)
         config {:force-scale 0.33
