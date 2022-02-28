c1edb65a5258049d42f15206fbcb2f69f6a1687a
Charles Comstock <dgtized@gmail.com>
2022-02-27 19:13:03 -0600

    sum up all the forces from dipoles at each point, and vary the number

--- src/shimmers/sketches/magnetic_fields.cljs
+++ src/shimmers/sketches/magnetic_fields.cljs
@@ -21,20 +21,27 @@
 (defn closest [dipoles pos]
   (apply min-key (fn [dipole] (g/dist-squared pos (:p dipole))) dipoles))
 
+(defn line-step [dipoles]
+  (fn [pos]
+    (let [forces (for [{:keys [p strength]} dipoles
+                       :let [[x y] (tm/- pos p)
+                             b (tm/cross (gv/vec3 x y 0) (gv/vec3 0 0 strength))
+                             a (gv/vec2 (:x b) (:y b))]]
+                   (tm/* a (/ 200 (+ (g/dist-squared pos p) 1e-5))))]
+      (reduce tm/+ pos forces))))
+
 (defn line [bounds dipoles]
-  (let [start (g/random-point-inside (g/scale-size bounds 0.9))
-        step (fn [pos] (let [{:keys [p strength]} (closest dipoles pos)
-                            [x y] (tm/- pos p)
-                            b (tm/cross (gv/vec3 x y 0) (gv/vec3 0 0 strength))
-                            a (gv/vec2 (:x b) (:y b))]
-                        (tm/+ pos (tm/* a 0.025))))]
-    (gl/linestrip2 (take 50 (iterate step start)))))
+  (let [start (g/random-point-inside (g/scale-size bounds 0.9))]
+    (gl/linestrip2 (take 50 (iterate (line-step dipoles) start)))))
 
 (defn shapes []
   (let [bounds (rect/rect 0 0 width height)
-        dipoles (repeatedly 3 (fn [] {:p (g/random-point-inside (g/scale-size bounds 0.5))
-                                     :strength (if (dr/chance 0.5) (dr/random 2 6) (- (dr/random 2 6)))}))]
-    [(svg/group {} (map (fn [{:keys [p strength]}] (gc/circle p (Math/abs strength))) dipoles))
+        dipoles (repeatedly (dr/random-int 2 5)
+                            (fn [] {:p (g/random-point-inside (g/scale-size bounds 0.5))
+                                   :strength (if (dr/chance 0.5) (dr/random 2 6) (- (dr/random 2 6)))}))]
+    [(svg/group {} (for [{:keys [p strength]} dipoles]
+                     (with-meta (gc/circle p (Math/abs strength))
+                       {:fill (if (> strength 0) "none" "black")})))
      (svg/group {} (repeatedly 50 #(line bounds dipoles)))]))
 
 (defn scene []
