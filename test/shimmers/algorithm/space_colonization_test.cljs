f351885e1b42fec8dbc9445465d1649f6c62e97c
Charles Comstock <dgtized@gmail.com>
2022-03-01 04:35:48 -0600

    fix broken space-colonization test

--- test/shimmers/algorithm/space_colonization_test.cljs
+++ test/shimmers/algorithm/space_colonization_test.cljs
@@ -19,7 +19,7 @@
                                  [(v/vec2 2 2) (v/vec2 2 2)])))
   (let [branch (sut/->Branch nil (v/vec2 100 195))
         attractors [(v/vec2 112.0 189.0) (v/vec2 85.2 182.0) (v/vec2 [91.9 173.5])]]
-    (is (tm/delta= (v/vec2 -0.254 -0.967)
+    (is (tm/delta= (v/vec2 -0.102 -0.994)
                    (sut/average-attraction branch attractors)
                    0.001))))
 
