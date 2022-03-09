e0ba2f7747ad5c5c9f56f58019141f588d66fbc5
Charles Comstock <dgtized@gmail.com>
2022-03-09 00:55:06 -0600

    Add property info for triangles/halfedges

--- resources/delaunator-externs.js
+++ resources/delaunator-externs.js
@@ -1,6 +1,20 @@
-var Delaunator = {
-  "from": function () {}
+/* Attempting to use
+ * https://developers.google.com/closure/compiler/docs/externs-and-exports to
+ * force Closure compiler to recognize triangles/halfedges as properties on
+ * Delaunator after advanced compilation.
+ */
+var Delaunator = class {
+  constructor() {
+    this.coords;
+    /** @type {Uint32Array} */
+    this._triangles;
+    /** @type {Uint32Array} */
+    this._halfedges;
+  }
+
+  from() {}
 };
+
 Delaunator.prototype = {
   "update": function() {},
   "_addTriangle": function () {},
