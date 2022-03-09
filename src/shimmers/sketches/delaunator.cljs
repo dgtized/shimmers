b9c855234fa3ac42c3bd775b9b95f01c814bea4b
Charles Comstock <dgtized@gmail.com>
2022-03-08 19:23:13 -0600

    attempt calculation of voronoi-edges but is circumcenter correct?

--- src/shimmers/sketches/delaunator.cljs
+++ src/shimmers/sketches/delaunator.cljs
@@ -41,13 +41,16 @@
 (comment
   (triangle-edges [[0 10] [0 5] [5 5] [4 2]]))
 
+(defn delaunay-triangle [points delaunay t]
+  (let [a (aget (.-triangles delaunay) (* 3 t))
+        b (aget (.-triangles delaunay) (+ (* 3 t) 1))
+        c (aget (.-triangles delaunay) (+ (* 3 t) 2))]
+    (gt/triangle2 (nth points a) (nth points b) (nth points c))))
+
 (defn triangles [points]
   (let [delaunay (js/Delaunator.from (clj->js points))]
     (for [t (range (/ (.-length (.-triangles delaunay)) 3))]
-      (let [a (aget (.-triangles delaunay) (* 3 t))
-            b (aget (.-triangles delaunay) (+ (* 3 t) 1))
-            c (aget (.-triangles delaunay) (+ (* 3 t) 2))]
-        (gt/triangle2 (nth points a) (nth points b) (nth points c))))))
+      (delaunay-triangle points delaunay t))))
 
 (comment (triangles [[0 10] [0 5] [5 5] [4 2]]))
 
@@ -58,31 +61,48 @@
         D (* 2 (+ (* ax (- by cy))
                   (* bx (- cy ay))
                   (* cx (- ay by))))]
-    (gv/vec2 (/ (+ (* ad (- by cy))
+    (gv/vec2 (* (/ 1 D)
+                (+ (* ad (- by cy))
                    (* bd (- cy ay))
-                   (* cd (- ay cy)))
-                D)
-             (/ (+ (* ad (- cx bx))
+                   (* cd (- ay cy))))
+             (* (/ 1 D)
+                (+ (* ad (- cx bx))
                    (* bd (- ax cx))
-                   (* cd (- bx ax)))
-                D))))
+                   (* cd (- bx ax)))))))
+
+(defn triangle-of-edge [e]
+  (Math/floor (/ e 3)))
+
+(defn triangle-center [points delaunay t]
+  (let [{[a b c] :points} (delaunay-triangle points delaunay t)]
+    (circumcenter a b c)))
+
+(defn voronoi-edges [points]
+  (let [delaunay (js/Delaunator.from (clj->js points))]
+    (for [e (range (.-length (.-triangles delaunay)))
+          :when (< e (aget (.-halfedges delaunay) e))]
+      (gl/line2 (triangle-center points delaunay (triangle-of-edge e))
+                (triangle-center points delaunay (triangle-of-edge (aget (.-halfedges delaunay) e)))))))
 
 (defn shapes []
   (let [points (repeatedly 8 gen-point)
         edges (triangle-edges points)
         triangles (triangles points)
         circumcenters (for [{[a b c] :points} triangles]
-                        (gc/circle (circumcenter a b c) 3))]
+                        (circumcenter a b c))
+        voronoi-edges (voronoi-edges points)]
     (swap! defo assoc
            :points points
-           :edges triangle-edges
-           :circumcenters circumcenters)
+           :edges edges
+           :circumcenters circumcenters
+           :voronoi-edges voronoi-edges)
     [(svg/group {:fill "black"}
                 (for [p points]
                   (gc/circle p 2)))
      #_(svg/group {} edges)
      (svg/group {:fill "none"} triangles)
-     (svg/group {:fill "red"} circumcenters)]))
+     (svg/group {:fill "red"} (map (fn [p] (gc/circle p 3)) circumcenters))
+     (svg/group {:stroke "blue"} voronoi-edges)]))
 
 (defn scene []
   (csvg/svg {:width width
