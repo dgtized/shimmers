50abcef809a829d7d97a4298ba752797613065cc
Charles Comstock <dgtized@gmail.com>
2022-03-11 16:57:34 -0600

    Add delaunay.convex-hull from points

--- src/shimmers/algorithm/delaunay.cljs
+++ src/shimmers/algorithm/delaunay.cljs
@@ -1,6 +1,7 @@
 (ns shimmers.algorithm.delaunay
   (:require d3-delaunay
-            [thi.ng.geom.polygon :as gp]))
+            [thi.ng.geom.polygon :as gp]
+            [thi.ng.math.core :as tm]))
 
 (set! *warn-on-infer* true)
 
@@ -11,3 +12,11 @@
 
 (comment (delaunay-triangles [[0 0] [0 10] [10 10] [0 10] [3 3] [7 7] [3 7]]))
 
+(defn convex-hull [points]
+  (let [delaunay (js/d3.Delaunay.from (clj->js points))
+        hull (js->clj (.hullPolygon delaunay))]
+    (gp/polygon2 (if (tm/delta= (first hull) (last hull))
+                   (butlast hull)
+                   hull))))
+
+(comment (hull [[0 0] [5 0] [5 10]]))
