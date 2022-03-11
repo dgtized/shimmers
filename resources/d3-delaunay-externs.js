a080a6c0ce4c110faf1e84b24fcf6aed13b5fbeb
Charles Comstock <dgtized@gmail.com>
2022-03-11 16:57:14 -0600

    Convert externs to use class style definitions for easier annotation

--- resources/d3-delaunay-externs.js
+++ resources/d3-delaunay-externs.js
@@ -6,49 +6,41 @@ var Delaunay = class {
     this.halfedges;
     this.hull;
   }
+
+  update() {}
+  /** @return {Voronoi} */
+  voronoi() {}
+  neighbors() {}
+  find() {}
+  render() {}
+  renderPoints() {}
+  renderHull() {}
+  hullPolygon() {}
+  renderTriangles() {}
+  trianglePolygons() {}
+  trianglePolygon() {}
 };
-/**
- * @return {Delaunay}
- */
+/** @return {Delaunay} */
 Delaunay.from = function() {};
-Delaunay.prototype = {
-  "update": function() {},
-  "voronoi": function() {},
-  "neighbors": function() {},
-  "find": function() {},
-  "render": function() {},
-  "renderPoints": function() {},
-  "renderHull": function() {},
-  "hullPolygon": function() {},
-  "renderTriangle": function() {},
-  "trianglePolygons": function() {},
-  "trianglePolygon": function() {},
-};
-/**
- * @return {Voronoi}
- */
-Delaunay.prototype.voronoi = function() {};
 
 var Path = class {
   constructor() {}
-};
-Path.prototype = {
-  "moveTo": function() {},
-  "closePath": function() {},
-  "lineTo": function() {},
-  "arc": function() {},
-  "rect": function() {},
-  "value": function() {},
+
+  moveTo() {}
+  closePath() {}
+  lineTo() {}
+  arc() {}
+  rect() {}
+  value() {}
 };
 
 var Polygon = class {
   constructor() {}
-};
-Polygon.prototype = {
-  "moveTo": function() {},
-  "closePath": function() {},
-  "lineTo": function() {},
-  "value": function() {},
+
+  moveTo() {}
+  closePath() {}
+  lineTo() {}
+  value() {}
 };
 
 var Voronoi = class {
@@ -59,14 +51,13 @@ var Voronoi = class {
     this.xmax;
     this.ymax;
   }
-};
-Voronoi.prototype = {
-  "update": function() {},
-  "render": function() {},
-  "renderBounds": function() {},
-  "renderCell": function() {},
-  "cellPolygons": function() {},
-  "cellPolygon": function() {},
-  "contains": function() {},
-  "neighbors": function() {},
+
+  update() {}
+  render() {}
+  renderBounds() {}
+  renderCell() {}
+  cellPolygons() {}
+  cellPolygon() {}
+  contains() {}
+  neighbors() {}
 };
