836e249d3a22528ac49dbf6d23f61c90092455a5
Charles Comstock <dgtized@gmail.com>
2022-03-11 17:10:02 -0600

    Calculate voronoi-cell polygons

--- src/shimmers/algorithm/delaunay.cljs
+++ src/shimmers/algorithm/delaunay.cljs
@@ -1,22 +1,39 @@
 (ns shimmers.algorithm.delaunay
   (:require d3-delaunay
             [thi.ng.geom.polygon :as gp]
+            [thi.ng.geom.rect :as rect]
             [thi.ng.math.core :as tm]))
 
 (set! *warn-on-infer* true)
 
+(defn trimmed-polygon [points]
+  (gp/polygon2 (if (tm/delta= (first points) (last points))
+                 (butlast points)
+                 points)))
+
 (defn delaunay-triangles [points]
-  (let [delaunay (js/d3.Delaunay.from (clj->js points))]
+  (let [^js/Delaunay delaunay (js/d3.Delaunay.from (clj->js points))]
     (for [poly (.trianglePolygons delaunay)]
       (gp/polygon2 (js->clj poly)))))
 
 (comment (delaunay-triangles [[0 0] [0 10] [10 10] [0 10] [3 3] [7 7] [3 7]]))
 
 (defn convex-hull [points]
-  (let [delaunay (js/d3.Delaunay.from (clj->js points))
+  (let [^js/Delaunay delaunay (js/d3.Delaunay.from (clj->js points))
         hull (js->clj (.hullPolygon delaunay))]
-    (gp/polygon2 (if (tm/delta= (first hull) (last hull))
-                   (butlast hull)
-                   hull))))
+    (trimmed-polygon hull)))
 
-(comment (hull [[0 0] [5 0] [5 10]]))
+(comment (convex-hull [[0 0] [5 0] [5 10]]))
+
+(defn bounds->ranges [{[xmin ymin] :p [w h] :size}]
+  [xmin ymin (+ xmin w) (+ ymin h)])
+
+(comment (bounds->ranges (rect/rect 3 4 6 8)))
+
+(defn voronoi-cells [points bounds]
+  (let [^js/Delaunay delaunay (js/d3.Delaunay.from (clj->js points))
+        ^js/Voronoi voronoi (.voronoi delaunay (clj->js (bounds->ranges bounds)))]
+    (for [cell (.cellPolygons voronoi)]
+      (trimmed-polygon (js->clj cell)))))
+
+(comment (voronoi-cells [[5 5] [8 8] [3 6] [7 4]] (rect/rect 0 0 10 10)))
