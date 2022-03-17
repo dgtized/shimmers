90d4743dd62f5e413febde8e0076695c9b44dd9e
Charles Comstock <dgtized@gmail.com>
2022-03-17 13:46:20 -0500

    Attempt to remove errors about mis-reference to d3

--- src/shimmers/algorithm/delaunay.cljs
+++ src/shimmers/algorithm/delaunay.cljs
@@ -10,8 +10,12 @@
 ;; d3-delaunay polygons and triangles API results include the initial point as
 ;; the closing point, so they need to be trimmed of the last point.
 
+(defn delaunay-from [points]
+  (.from (get (js->clj js/d3-delaunay) "Delaunay")
+         (clj->js points)))
+
 (defn delaunay-triangles [points]
-  (let [^js/Delaunay delaunay (js/d3.Delaunay.from (clj->js points))]
+  (let [^js/Delaunay delaunay (delaunay-from points)]
     (for [triangle (.trianglePolygons delaunay)
           :let [[a b c] (js->clj triangle)]]
       (gt/triangle2 a b c))))
@@ -19,7 +23,7 @@
 (comment (delaunay-triangles [[0 0] [0 10] [10 10] [0 10] [3 3] [7 7] [3 7]]))
 
 (defn convex-hull [points]
-  (let [^js/Delaunay delaunay (js/d3.Delaunay.from (clj->js points))
+  (let [^js/Delaunay delaunay (delaunay-from points)
         hull (js->clj (.hullPolygon delaunay))]
     (gp/polygon2 (butlast hull))))
 
@@ -31,7 +35,7 @@
 (comment (bounds->ranges (rect/rect 3 4 6 8)))
 
 (defn voronoi-cells [points bounds]
-  (let [^js/Delaunay delaunay (js/d3.Delaunay.from (clj->js points))
+  (let [^js/Delaunay delaunay (delaunay-from points)
         ^js/Voronoi voronoi (.voronoi delaunay (clj->js (bounds->ranges bounds)))]
     (for [cell (.cellPolygons voronoi)]
       (gp/polygon2 (butlast (js->clj cell))))))
@@ -39,7 +43,7 @@
 (comment (voronoi-cells [[5 5] [8 8] [3 6] [7 4]] (rect/rect 0 0 10 10)))
 
 (defn voronoi-circumcenters [points bounds]
-  (let [^js/Delaunay delaunay (js/d3.Delaunay.from (clj->js points))
+  (let [^js/Delaunay delaunay (delaunay-from points)
         ^js/Voronoi voronoi (.voronoi delaunay (clj->js (bounds->ranges bounds)))]
     (for [t (range 0 (alength (.-circumcenters voronoi)) 2)]
       (gv/vec2 (aget (.-circumcenters voronoi) t)
