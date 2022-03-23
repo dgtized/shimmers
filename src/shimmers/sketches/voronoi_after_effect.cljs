34aa0b1683cf4bef6c81cd4d4176d53274d77878
Charles Comstock <dgtized@gmail.com>
2022-03-23 04:01:25 -0500

    perturb the points but keep displaying the voronoi graph

--- src/shimmers/sketches/voronoi_after_effect.cljs
+++ src/shimmers/sketches/voronoi_after_effect.cljs
@@ -6,21 +6,37 @@
    [shimmers.algorithm.random-points :as rp]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.equations :as eq]
+   [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
-   [thi.ng.geom.core :as g]))
+   [thi.ng.geom.core :as g]
+   [thi.ng.math.core :as tm]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
+  (q/background 1.0 1.0)
   (let [bounds (cq/screen-rect 0.95)]
     {:bounds bounds
      :points (rp/poisson-disc-sampling bounds 128)}))
 
-(defn update-state [state]
-  state)
+(defn kick-point [t]
+  (fn [p]
+    (let [[vx vy] (tm/* p 0.01)]
+      (tm/+ p (v/polar (dr/gaussian 2.0 1) (* eq/TAU (q/noise vx vy t)))))))
+
+(defn perturb-points [points t]
+  (dr/map-random-sample (constantly 0.01) (kick-point t) points))
+
+(defn update-state [{:keys [t] :as state}]
+  (-> state
+      (update :points perturb-points t)
+      (update :t + 0.01)))
 
 (defn draw [{:keys [points]}]
-  (q/background 1.0 0.01)
-  (q/stroke 0.0 0.33)
+  (q/no-fill)
+  (q/stroke-weight 0.33)
+  (q/stroke 0.0 0.01)
   (let [polygons (delvor/voronoi-cells points (cq/screen-rect))]
     (doseq [shape polygons]
       (cq/draw-shape (g/vertices shape)))))
