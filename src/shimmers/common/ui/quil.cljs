a1612e0c268e4859f39ca00a3e8d933bde81a303
Charles Comstock <dgtized@gmail.com>
2022-03-29 16:58:31 -0500

    remove unnecessary a/go macro and comment on broken unmount

--- src/shimmers/common/ui/quil.cljs
+++ src/shimmers/common/ui/quil.cljs
@@ -1,6 +1,5 @@
 (ns shimmers.common.ui.quil
   (:require
-   [cljs.core.async :as a :include-macros true]
    [quil.core :as q :include-macros true]
    [reagent.core :as r]
    [reagent.dom :as rdom]))
@@ -8,15 +7,18 @@
 ;; Amalgamation of:
 ;; https://github.com/quil/quil/issues/320#issuecomment-534859573
 ;; https://github.com/simon-katz/nomisdraw/blob/for-quil-api-request/src/cljs/nomisdraw/utils/nomis_quil_on_reagent.cljs
+
+;; FIXME: will-unmount is not actually unmounting the corresponding component to
+;; test, view reagent-quil-component, and then return to index and adjust `draw`
+;; to print, and it will continue to run.
 (defn sketch-component [sketch-args]
   [r/create-class
    {:component-did-mount
     (fn [component]
       (println "did-mount" component)
-      (a/go
-        (let [node (rdom/dom-node component)]
-          (println "on-start " node)
-          (apply q/sketch (apply concat (assoc sketch-args :host node))))))
+      (let [node (rdom/dom-node component)]
+        (println "on-start " node)
+        (apply q/sketch (apply concat (assoc sketch-args :host node)))))
     :component-will-unmount
     (fn [component]
       (println (str "will-unmount " component))
