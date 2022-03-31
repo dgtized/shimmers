5f5001e24a6e9189278335ecd0d3810b6439fdf7
Charles Comstock <dgtized@gmail.com>
2022-03-31 13:19:19 -0500

    Move comment about goog.format to relevant ns

--- src/shimmers/common/framerate.cljs
+++ src/shimmers/common/framerate.cljs
@@ -1,8 +1,4 @@
 (ns shimmers.common.framerate
-  ;; see https://martinklepsch.org/posts/requiring-closure-namespaces.html for
-  ;; weirdness around requiring goog.string.format to force it to create format
-  ;; in goog.string. Requiring goog.string will not add format to the namespace,
-  ;; but works until compiling with :optimization :advanced.
   (:require
    [goog.dom :as dom]
    [quil.core :as q]
--- src/shimmers/common/string.cljs
+++ src/shimmers/common/string.cljs
@@ -2,6 +2,11 @@
   (:require [goog.string :as gstring]
             [goog.string.format]))
 
+;; see https://martinklepsch.org/posts/requiring-closure-namespaces.html for
+;; weirdness around requiring goog.string.format to force it to create format
+;; in goog.string. Requiring goog.string will not add format to the namespace,
+;; but works until compiling with :optimization :advanced.
+
 (defn format [fmt & args]
   (apply gstring/format fmt args))
 
