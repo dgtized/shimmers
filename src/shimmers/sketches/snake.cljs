d520ed3e31b58defdf681ddc15734ceafac4b8b0
Charles Comstock <dgtized@gmail.com>
2022-04-02 20:46:34 -0500

    Use seeking behavior instead of following noise circle

--- src/shimmers/sketches/snake.cljs
+++ src/shimmers/sketches/snake.cljs
@@ -3,15 +3,19 @@
    [quil.core :as q :include-macros true]
    [quil.middleware :as m]
    [shimmers.algorithm.kinematic-chain :as chain]
+   [shimmers.algorithm.random-points :as rp]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.math.equations :as eq]
-   [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
+   [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
-   [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
+
+(defn gen-target []
+  (gc/circle (cq/rel-vec (dr/random) (dr/random))
+             (dr/random (cq/rel-h 0.05) (cq/rel-h 0.2))))
 
 (defn gen-segment [{:keys [angle] :as segment}]
   (let [base (chain/segment-endpoint segment)]
@@ -21,26 +25,26 @@
 (defn setup []
   (q/color-mode :hsl 1.0)
   {:t 0.0
-   :chain (->> (chain/->KinematicSegment (gv/vec2) (dr/random eq/TAU) 8)
+   :target (gen-target)
+   :chain (->> (chain/->KinematicSegment (cq/rel-vec (dr/random) (dr/random))
+                                         (dr/random eq/TAU) 8)
                (iterate gen-segment)
                (take 32)
                chain/->KinematicChain)})
 
-(defn follow [pos t]
-  (let [[x y] (v/polar (cq/rel-h 0.4) t)
-        n (q/noise (/ x 100) (/ y 100) t)
-        target (v/polar (+ (cq/rel-h 0.05) (* (cq/rel-h 0.6) n)) t)]
-    (tm/mix pos target 0.1)))
+(defn follow [pos {:keys [r] :as target}]
+  (tm/mix pos (rp/inside-circle target dr/random) (/ r (* 2 (cq/rel-h 1.0)))))
 
-(defn update-state [{:keys [chain t] :as state}]
+(defn update-state [{:keys [chain target] :as state}]
   (let [tip (chain/segment-endpoint (last (:segments chain)))]
-    (-> state
-        (update :chain chain/chain-update nil (follow tip t))
+    (-> (if (g/contains-point? target tip)
+          (assoc state :target (gen-target))
+          state)
+        (update :chain chain/chain-update nil (follow tip target))
         (update :t + 0.01))))
 
 (defn draw [{:keys [chain]}]
   (q/background 1.0 0.001)
-  (apply q/translate (cq/rel-vec 0.5 0.5))
   (q/stroke 0.0 0.05)
   (q/no-fill)
   (let [vertices (g/vertices chain)]
