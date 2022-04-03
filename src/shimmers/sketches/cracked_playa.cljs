694baef998a54798821f8114c629acb09da24771
Charles Comstock <dgtized@gmail.com>
2022-04-03 16:14:45 -0500

    Convert to voronoi and vary the gaps between

--- src/shimmers/sketches/cracked_playa.cljs
+++ src/shimmers/sketches/cracked_playa.cljs
@@ -1,11 +1,13 @@
 (ns shimmers.sketches.cracked-playa
   (:require
+   [shimmers.algorithm.delaunay :as delvor]
    [shimmers.algorithm.poisson-disc-sampling :as pds]
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
-   [thi.ng.geom.circle :as gc]
+   [thi.ng.geom.polygon :as gp]
    [thi.ng.geom.rect :as rect]
    [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]
@@ -17,14 +19,15 @@
   (gv/vec2 (* width x) (* height y)))
 
 (defn noise-at-point [p]
-  (let [[x y] (tm/* p 0.005)]
+  (let [[x y] (tm/* p 0.01)]
     (tm/clamp01 (noise/noise2 x y))))
 
 (defn shapes []
   (let [bounds (rect/rect 0 0 width height)
-        points (pds/generate-dynamic bounds 10 [25 60] noise-at-point)]
-    (for [p points]
-      (gc/circle p 2))))
+        points (pds/generate-dynamic bounds 10 [18 256] noise-at-point)
+        cells (delvor/voronoi-cells points bounds)]
+    (for [cell cells]
+      (gp/polygon2 (gp/inset-polygon (:points cell) (dr/random -0.5 -4))))))
 
 (defn scene []
   (csvg/svg {:width width
