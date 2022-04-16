9b676bb71d9a8b62be326d0ec7e98b27a0bb5b4f
Charles Comstock <dgtized@gmail.com>
2022-04-16 00:23:50 -0500

    Fix triangle-flow to be deterministic

--- src/shimmers/sketches/triangle_flow.cljs
+++ src/shimmers/sketches/triangle_flow.cljs
@@ -2,18 +2,19 @@
   (:require
    [quil.core :as q :include-macros true]
    [quil.middleware :as m]
+   [shimmers.algorithm.random-points :as rp]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.core :as sm]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.math.equations :as eq]
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.triangle :as gt]
    [thi.ng.geom.vector :as gv]
-   [thi.ng.math.core :as tm]
-   [shimmers.math.deterministic-random :as dr]))
+   [thi.ng.math.core :as tm]))
 
 (defn noise-at-p [bounds p t]
   (let [[rx ry] (tm/* (g/map-point bounds p) 2)
@@ -32,7 +33,7 @@
   (let [n (noise-at-p bounds pos t)
         rf (+ (* (tm/smoothstep* 0.2 0.8 (eq/unit-sin (* 0.5 (+ t dt)))) 0.15) 0.10)
         accel (tm/+ (v/polar rf (* 2 eq/TAU n))
-                    (v/polar 0.05 (tm/random (* 2 eq/TAU))))
+                    (v/polar 0.05 (dr/random (* 2 eq/TAU))))
         velocity (tm/- pos last-pos)
         velocity' (tm/* (tm/+ velocity accel) 0.9)
         pos' (tm/+ pos velocity')]
@@ -51,8 +52,8 @@
   (let [bounds (cq/screen-rect)]
     {:bounds bounds
      :t 0
-     :particles (repeatedly 12 #(make-particle (g/random-point-inside (g/scale-size bounds 0.8))
-                                               (v/polar 3 (* eq/TAU (tm/random)))))}))
+     :particles (map (fn [p] (make-particle p (v/polar 3 (dr/random eq/TAU))))
+                     (rp/random-points (g/scale-size bounds 0.8) 12))}))
 
 (defn update-state [{:keys [bounds t] :as state}]
   (-> state
@@ -106,6 +107,7 @@
 (sketch/defquil triangle-flow
   :created-at "2022-04-13"
   :on-mount #(ctrl/mount ui-controls)
+  :tags #{:deterministic}
   :size [900 600]
   :setup setup
   :update update-state
