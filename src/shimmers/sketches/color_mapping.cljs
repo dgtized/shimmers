abbf6913ed88224a297fd9becbd2aa9a7c9a1c32
Charles Comstock <dgtized@gmail.com>
2022-05-02 17:41:09 -0500

    use gradient noise for coloration

--- src/shimmers/sketches/color_mapping.cljs
+++ src/shimmers/sketches/color_mapping.cljs
@@ -2,28 +2,35 @@
   (:require
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.color.core :as col]
-   [thi.ng.geom.core :as g]
    [thi.ng.geom.rect :as rect]
-   [thi.ng.math.core :as tm]))
+   [thi.ng.geom.vector :as gv]
+   [thi.ng.math.core :as tm]
+   [thi.ng.math.noise :as noise]))
 
 (def width 800)
 (def height 600)
 
-(defn color [i j]
-  (col/as-css (col/hsla (mod (* i (* 3 j) tm/PHI) 1.0)
+(defn noise-at-point [seed p]
+  (let [[x y] (tm/+ seed (tm/* p 0.008))]
+    (tm/clamp01 (+ 0.5 (noise/noise2 x y)))))
+
+(defn color [seed i j]
+  (col/as-css (col/hsla (mod (* (noise-at-point seed (gv/vec2 i j)) tm/PHI) 1.0)
                         0.5 0.45 1.0)))
 
 (defn shapes []
-  (let [[cols rows] [40 30]
+  (let [[cols rows] [80 60]
         w (/ width cols)
-        h (/ height rows)]
+        h (/ height rows)
+        seed (gv/vec2 (dr/random 100) (dr/random 100))]
     (for [j (range rows)
           i (range cols)]
-      (vary-meta (g/scale-size (rect/rect (* i w) (* j h) w h) 0.95)
-                 assoc :fill (color i j)))))
+      (vary-meta (rect/rect (* i w) (* j h) w h)
+                 assoc :fill (color seed i j)))))
 
 (defn scene []
   (csvg/svg {:width width
