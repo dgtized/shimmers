4c5109b9572879c6fefe9672936f5ecdb795d5ca
Charles Comstock <dgtized@gmail.com>
2022-05-16 12:05:52 -0500

    Decompose cells overlapping roads

--- src/shimmers/sketches/texas_fields.cljs
+++ src/shimmers/sketches/texas_fields.cljs
@@ -1,7 +1,9 @@
 (ns shimmers.sketches.texas-fields
   (:require
+   [shimmers.algorithm.lines :as lines]
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.color :as color]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
@@ -25,13 +27,22 @@
         i (tm/norm-range 20)]
     (rect/rect (rv i j) (tm/+ (rv i j) (gv/vec2 (/ width 20) (/ height 15))))))
 
+(defn decompose [cell lines]
+  (reduce (fn [cells line]
+            (mapcat (fn [cell] (lines/cut-polygon cell line)) cells))
+          [cell] lines))
+
 (defn landscape []
   (let [roads (make-roads)
         grid (make-grid)]
-    (concat (map (fn [cell] (if-let [_ (some (fn [line] (g/intersect-line cell line)) roads)]
-                             (with-meta cell {:fill "hsl(0,50%,50%,10%)"})
-                             cell))
-                 grid) roads)))
+    (concat (mapcat (fn [cell]
+                      (if (some (fn [line] (g/intersect-line cell line)) roads)
+                        (for [[i cell] (map-indexed vector (decompose cell roads))
+                              :let [fill (color/css-hsl (mod (* i tm/PHI) 1.0) 0.5 0.5 0.3)]]
+                          (with-meta cell {:fill fill}))
+                        [cell]))
+                    grid)
+            roads)))
 
 (defn scene []
   (csvg/svg {:width width
