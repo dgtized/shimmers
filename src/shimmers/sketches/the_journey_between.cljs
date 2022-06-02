a46d170fa027cacabafb30746279103fbe116c0c
Charles Comstock <dgtized@gmail.com>
2022-06-02 13:58:43 -0500

    stop showing debug path

--- src/shimmers/sketches/the_journey_between.cljs
+++ src/shimmers/sketches/the_journey_between.cljs
@@ -81,8 +81,7 @@
   (let [dest (to-grid-loc grid-size mouse)]
     (reset! defo {:destination dest
                   :grid-cell [(loc-grid grid-size grid dest)
-                              (g/centroid (loc-grid grid-size grid dest))]
-                  :path path})
+                              (g/centroid (loc-grid grid-size grid dest))]})
     (q/no-stroke)
     (doseq [{[x y] :p [w h] :size noise :noise} grid]
       (q/fill 0.0 0.0 noise 1.0)
