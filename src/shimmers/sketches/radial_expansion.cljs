5ecf2f3b2995bd10a92d5d0c8fd210f6be2ac86b
Charles Comstock <dgtized@gmail.com>
2022-06-21 01:14:45 -0500

    Only add connections to planets that are in-bounds

--- src/shimmers/sketches/radial_expansion.cljs
+++ src/shimmers/sketches/radial_expansion.cljs
@@ -2,16 +2,18 @@
   (:require
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.equations :as eq]
+   [shimmers.math.geometry :as geometry]
+   [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
-   [thi.ng.geom.vector :as gv]
    [thi.ng.geom.circle :as gc]
-   [shimmers.math.deterministic-random :as dr]
-   [thi.ng.math.core :as tm]
-   [shimmers.math.equations :as eq]
-   [shimmers.math.vector :as v]
+   [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
-   [thi.ng.geom.core :as g]))
+   [thi.ng.geom.rect :as rect]
+   [thi.ng.geom.vector :as gv]
+   [thi.ng.math.core :as tm]))
 
 (def width 800)
 (def height 600)
@@ -38,24 +40,27 @@
 (defn init []
   [(new-planet (rv 0.5 0.5) 64)])
 
-(defn evolve [shapes]
-  (let [expansions (->> shapes
-                        (filter #(> (:spokes %) 0))
-                        (mapcat
-                         (fn [{:keys [p r spokes] :as planet}]
-                           (let [distance (* r (dr/random 0.3 1.8))
-                                 t0 (dr/random 0 1.0)]
-                             (for [t (butlast (tm/norm-range spokes))
-                                   :let [theta (+ (* eq/TAU t) t0)
-                                         radius (* r (dr/random 0.3 0.8))]]
-                               {:planet (new-planet (tm/+ p (v/polar (+ r distance radius) theta)) radius)
-                                :spoke (spoke planet distance theta)})))))]
+(defn evolve [bounds shapes]
+  (let [expansions
+        (->> shapes
+             (filter #(> (:spokes %) 0))
+             (mapcat
+              (fn [{:keys [p r spokes] :as planet}]
+                (let [distance (* r (dr/random 0.3 1.8))
+                      t0 (dr/random 0 1.0)]
+                  (for [t (butlast (tm/norm-range spokes))
+                        :let [theta (+ (* eq/TAU t) t0)
+                              radius (* r (dr/random 0.3 0.8))
+                              new (new-planet (tm/+ p (v/polar (+ r distance radius) theta)) radius)]
+                        :when (geometry/contains-circle? bounds new)]
+                    {:planet new
+                     :spoke (spoke planet distance theta)})))))]
     (concat (map (fn [p] (dissoc p :spokes)) shapes)
             (map :spoke expansions)
             (map :planet expansions))))
 
-(defn shapes [depth]
-  (nth (iterate evolve (init)) depth))
+(defn shapes [bounds depth]
+  (nth (iterate (partial evolve bounds) (init)) depth))
 
 (defn scene []
   (csvg/svg {:width width
@@ -63,7 +68,8 @@
              :stroke "black"
              :fill "white"
              :stroke-width 0.5}
-            (shapes (dr/random-int 2 5))))
+            (shapes (rect/rect 0 0 width height)
+                    (dr/random-int 2 5))))
 
 (sketch/definition radial-expansion
   {:created-at "2022-06-21"
