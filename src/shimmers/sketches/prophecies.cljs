373f9c42da7947477b35f6c0b59043c1f2b6c15d
Charles Comstock <dgtized@gmail.com>
2022-07-08 19:08:48 -0500

    add little flyout triangles glancing from the meridian

--- src/shimmers/sketches/prophecies.cljs
+++ src/shimmers/sketches/prophecies.cljs
@@ -8,6 +8,7 @@
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
+   [thi.ng.geom.polygon :as gp]
    [thi.ng.geom.rect :as rect]
    [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
@@ -23,6 +24,13 @@
      (rect/rect (tm/+ connect (gv/vec2 (* -0.5 height) (* 1.0 height)))
                 (tm/+ connect (gv/vec2 (* +0.5 height) 0)))]))
 
+(defn flyout [base height size angle]
+  (let [connect (v/+polar base height angle)]
+    [(gl/line2 base connect)
+     (gp/polygon2 [connect
+                   (v/+polar connect size (+ angle 0.5))
+                   (v/+polar connect size (- angle 0.5))])]))
+
 (defn shapes []
   (let [c1 (gc/circle (rv 0.35 0.5) (* width 0.25))
         c2 (gc/circle (rv 0.75 0.5) (* width 0.15))
@@ -31,7 +39,13 @@
         heading (g/heading (tm/- q p))]
     (concat [c1 c2 meridian]
             (stem (g/point-at meridian 0.33) (* width 0.05) (+ tm/HALF_PI heading))
-            (stem (g/point-at meridian 0.20) (* width -0.05) (+ tm/HALF_PI heading)))))
+            (stem (g/point-at meridian 0.20) (* width -0.05) (+ tm/HALF_PI heading))
+            (flyout (g/point-at meridian 0.925) (* width 0.2)
+                    (* width 0.1)
+                    (+ heading tm/HALF_PI))
+            (flyout (g/point-at meridian 0.65) (* width 0.2)
+                    (* width 0.1)
+                    (- heading tm/HALF_PI)))))
 
 (defn scene []
   (csvg/svg {:width width
