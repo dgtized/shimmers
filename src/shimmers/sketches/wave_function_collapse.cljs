ad3d0ebfaae32d0d35c345add08e71fdd09d081d
Charles Comstock <dgtized@gmail.com>
2022-07-14 21:52:07 -0500

    Add support for clearing / resettting the pattern

--- src/shimmers/sketches/wave_function_collapse.cljs
+++ src/shimmers/sketches/wave_function_collapse.cljs
@@ -292,7 +292,7 @@
              [:span (count examples) ": "])
            (svg-adjacency (nth tiles tile-idx) dir (nth tiles other-idx))]))])])
 
-(defn display-patterns [state edit-click toggle-rules toggle-rotations]
+(defn display-patterns [state edit-click clear reset toggle-rules toggle-rotations]
   (let [{:keys [pattern tiles rules mode show-rules rotations]} state]
     [:div
      [:div.flexcols
@@ -302,6 +302,8 @@
       (when (= mode :tileset)
         [:div
          [:h4 "Settings"]
+         [:div [:button {:on-click clear} "Clear Pattern"]]
+         [:div [:button {:on-click reset} "Reset Pattern"]]
          [:div.label-set {:key "Include Rotations"}
           [:input {:type "checkbox" :checked rotations
                    :on-change toggle-rotations}]
@@ -335,11 +337,28 @@
           [:p.readable "Does not yet support backtracking."]
           [display-patterns pattern-set
            (fn [loc _]
+             (cancel-active! state)
              (swap! state update-in [:pattern loc] (partial cs/cycle-next ["A" "B" "C"]))
+             (reset state))
+           (fn []
+             (cancel-active! state)
+             (swap! state update :pattern
+                    (fn [{[cols rows] :dims :as pattern}]
+                      (merge pattern
+                             (into {}
+                                   (for [i (range cols)
+                                         j (range rows)]
+                                     {(gv/vec2 i j) "A"})))))
+             (reset state))
+           (fn []
+             (cancel-active! state)
+             (swap! state assoc :pattern
+                    (wfc/matrix->grid rule-e wfc/cardinal-directions))
              (reset state))
            (fn []
              (swap! state update :show-rules not))
            (fn []
+             (cancel-active! state)
              (swap! state update-in [:rotations] not)
              (reset state))]]]))))
 
