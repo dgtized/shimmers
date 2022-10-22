c01eafe7968417b1b03e6b2783bb4e1983237b70
Charles Comstock <dgtized@gmail.com>
2022-10-22 14:40:49 -0500

    move ring-chance into split-chance per ring

--- src/shimmers/sketches/tree_rings.cljs
+++ src/shimmers/sketches/tree_rings.cljs
@@ -22,31 +22,24 @@
    (concat [[:M (first points)]]
            (mapv (fn [p] [:L p]) (rest points)))))
 
-(defn path-ring [points]
-  (csvg/path
-   (concat [[:M (first points)]]
-           (mapv (fn [p] [:L p]) (rest points))
-           [[:Z]])))
-
 (defn ring [seed r n]
-  (let [points (for [t (range 0 eq/TAU (/ eq/TAU n))]
+  (let [split-chance (dr/random 0.5 1.0)
+        points (for [t (range 0 eq/TAU (/ eq/TAU n))]
                  (let [p (g/as-cartesian (gv/vec2 r t))
                        noise (dr/noise-at-point seed 0.005 p)]
                    (tm/+ p (g/as-cartesian (gv/vec2 (* r 0.05) (* eq/TAU noise))))))]
-    (if (dr/chance 0.4)
-      [(path-ring points)]
-      (->> points
-           (partition-by (fn [_] (dr/chance 0.8)))
-           (filter #(> (count %) 1))
-           (mapv path-segment)))))
+    (->> points
+         (partition-by (fn [_] (dr/chance split-chance)))
+         (filter #(> (count %) 1))
+         (mapv path-segment))))
 
 (defn shapes []
-  (let [radius (int (/ height 2.05))
+  (let [radius (int (/ height 2.1))
         seed (gv/vec2 (dr/random 100) (dr/random 100))]
     (mapcat (fn [r]
               (ring seed
                     (* r radius)
-                    (int (Math/pow 20 (+ 1 (* 0.75 r))))))
+                    (int (Math/pow 30 (+ 1 r)))))
             (dr/density-range 0.0075 0.05))))
 
 (defn scene []
