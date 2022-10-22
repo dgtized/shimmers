67d8926c57e17138f5615af52c00d8fe9f554495
Charles Comstock <dgtized@gmail.com>
2022-10-22 14:01:37 -0500

    Generate gaps in the lines around the center

--- src/shimmers/sketches/tree_rings.cljs
+++ src/shimmers/sketches/tree_rings.cljs
@@ -17,22 +17,35 @@
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
+(defn path-segment [points]
+  (csvg/path
+   (concat [[:M (first points)]]
+           (mapv (fn [p] [:L p]) (rest points)))))
+
+(defn path-ring [points]
+  (csvg/path
+   (concat [[:M (first points)]]
+           (mapv (fn [p] [:L p]) (rest points))
+           [[:Z]])))
+
 (defn ring [seed r n]
   (let [points (for [t (range 0 eq/TAU (/ eq/TAU n))]
                  (let [p (g/as-cartesian (gv/vec2 r t))
                        noise (dr/noise-at-point seed 0.005 p)]
                    (tm/+ p (g/as-cartesian (gv/vec2 (* r 0.05) (* eq/TAU noise))))))]
-    (csvg/path
-     (concat [[:M (first points)]]
-             (mapv (fn [p] [:L p]) (rest points))
-             [[:Z]]))))
+    (if (dr/chance 0.5)
+      [(path-ring points)]
+      (->> points
+           (partition-by (fn [_] (dr/chance 0.75)))
+           (filter #(> (count %) 1))
+           (mapv path-segment)))))
 
 (defn shapes []
   (let [radius (int (/ height 2.05))
         points 60
         seed (gv/vec2 (dr/random 100) (dr/random 100))]
-    (for [r (dr/density-range 0.0075 0.05)]
-      (ring seed (* r radius) points))))
+    (mapcat (fn [r] (ring seed (* r radius) points))
+            (dr/density-range 0.0075 0.05))))
 
 (defn scene []
   (csvg/svg {:width width
