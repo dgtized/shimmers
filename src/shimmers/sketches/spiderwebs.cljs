d3b0982e5d435d39012e1babd5316976d3b9f383
Charles Comstock <dgtized@gmail.com>
2022-11-04 10:53:10 -0500

    punch out the connected center

--- src/shimmers/sketches/spiderwebs.cljs
+++ src/shimmers/sketches/spiderwebs.cljs
@@ -2,13 +2,14 @@
   (:require
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
+   [thi.ng.geom.polygon :as gp]
    [thi.ng.geom.vector :as gv]
-   [shimmers.math.deterministic-random :as dr]
    [thi.ng.math.core :as tm]))
 
 (def width 800)
@@ -20,13 +21,15 @@
   (let [center (rv 0.5 0.5)
         radius (* 0.4 height)
         points (mapv (fn [p] (tm/+ p (dr/jitter (* 0.08 radius))))
-                     (g/vertices (gc/circle center radius) (dr/random-int 15 23)))]
+                     (g/vertices (gc/circle center radius) (dr/random-int 15 23)))
+        center-r 0.02]
     (conj (for [p points]
-            (gl/line2 center p))
+            (gl/line2 (tm/mix center p center-r) p))
+          (gp/polygon2 (map #(tm/mix center % center-r) points))
           (gl/linestrip2
            (map (fn [point r] (tm/mix center point r))
                 (cycle points)
-                (drop 16 (dr/density-range 0.0001 0.003)))))))
+                (drop-while #(< % center-r) (dr/density-range 0.0001 0.0025)))))))
 
 (defn scene []
   (csvg/svg {:width width
