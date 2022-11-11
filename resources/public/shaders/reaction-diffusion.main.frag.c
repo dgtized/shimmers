38597b9a07e68dffddbf3cdcd25694b429e9e6b4
Charles Comstock <dgtized@gmail.com>
2022-11-11 12:52:24 -0600

    switch to vec2 representation

--- resources/public/shaders/reaction-diffusion.main.frag.c
+++ resources/public/shaders/reaction-diffusion.main.frag.c
@@ -14,18 +14,18 @@ uniform float kill;
 uniform float deltaT;
 
 // Translated from https://ciphrd.com/2019/08/24/reaction-diffusion-on-shader/
-vec3 laplacian(sampler2D tex, vec2 pos, vec2 texelSize) {
-  vec3 ab = vec3(0.0,0.0,0.0);
+vec2 laplacian(sampler2D tex, vec2 pos, vec2 texelSize) {
+  vec2 ab = vec2(0.0,0.0);
 
-  ab += texture2D(tex, pos + vec2(-1.0,-1.0)*texelSize).rgb * 0.05;
-  ab += texture2D(tex, pos + vec2(-0.0,-1.0)*texelSize).rgb * 0.2;
-  ab += texture2D(tex, pos + vec2( 1.0,-1.0)*texelSize).rgb * 0.05;
-  ab += texture2D(tex, pos + vec2(-1.0, 0.0)*texelSize).rgb * 0.2;
-  ab += texture2D(tex, pos + vec2( 0.0, 0.0)*texelSize).rgb * -1.0;
-  ab += texture2D(tex, pos + vec2( 1.0, 0.0)*texelSize).rgb * 0.2;
-  ab += texture2D(tex, pos + vec2(-1.0, 1.0)*texelSize).rgb * 0.05;
-  ab += texture2D(tex, pos + vec2( 0.0, 1.0)*texelSize).rgb * 0.2;
-  ab += texture2D(tex, pos + vec2( 1.0, 1.0)*texelSize).rgb * 0.05;
+  ab += texture2D(tex, pos + vec2(-1.0,-1.0)*texelSize).xy * 0.05;
+  ab += texture2D(tex, pos + vec2( 0.0,-1.0)*texelSize).xy * 0.2;
+  ab += texture2D(tex, pos + vec2( 1.0,-1.0)*texelSize).xy * 0.05;
+  ab += texture2D(tex, pos + vec2(-1.0, 0.0)*texelSize).xy * 0.2;
+  ab += texture2D(tex, pos + vec2( 0.0, 0.0)*texelSize).xy * -1.0;
+  ab += texture2D(tex, pos + vec2( 1.0, 0.0)*texelSize).xy * 0.2;
+  ab += texture2D(tex, pos + vec2(-1.0, 1.0)*texelSize).xy * 0.05;
+  ab += texture2D(tex, pos + vec2( 0.0, 1.0)*texelSize).xy * 0.2;
+  ab += texture2D(tex, pos + vec2( 1.0, 1.0)*texelSize).xy * 0.05;
 
   return ab;
 }
@@ -35,12 +35,12 @@ void main() {
   vec2 texelSize = 1.0 / resolution;
 
   vec4 current = texture2D(concentrations, pos);
-  float a = current.r;
-  float b = current.g;
-  vec3 lp = laplacian(concentrations, pos, texelSize);
+  float a = current.x;
+  float b = current.y;
+  vec2 lp = laplacian(concentrations, pos, texelSize);
 
-  float a2 = clamp(a + (diffusionA*lp.r - a*b*b + feed*(1.0-a)) * deltaT, 0.0, 1.0);
-  float b2 = clamp(b + (diffusionB*lp.g + a*b*b - (kill + feed)*b) * deltaT, 0.0, 1.0);
+  float a2 = clamp(a + (diffusionA*lp.x - a*b*b + feed*(1.0-a)) * deltaT, 0.0, 1.0);
+  float b2 = clamp(b + (diffusionB*lp.y + a*b*b - (kill + feed)*b) * deltaT, 0.0, 1.0);
 
   gl_FragColor = vec4(a2,b2,0.0,1.0);
 }
