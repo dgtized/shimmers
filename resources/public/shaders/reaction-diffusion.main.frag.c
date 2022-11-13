05974225f1d2ddd07fc6d709ba30ed2c36457569
Charles Comstock <dgtized@gmail.com>
2022-11-13 13:48:42 -0600

    Just call texelSize a texel

--- resources/public/shaders/reaction-diffusion.main.frag.c
+++ resources/public/shaders/reaction-diffusion.main.frag.c
@@ -14,30 +14,30 @@ uniform float kill;
 uniform float deltaT;
 
 // Translated from https://ciphrd.com/2019/08/24/reaction-diffusion-on-shader/
-vec2 laplacian(sampler2D tex, vec2 pos, vec2 texelSize) {
+vec2 laplacian(sampler2D tex, vec2 pos, vec2 texel) {
   vec2 ab = vec2(0.0,0.0);
 
-  ab += texture2D(tex, pos + vec2(-1,-1)*texelSize).xy * 0.05;
-  ab += texture2D(tex, pos + vec2( 0,-1)*texelSize).xy * 0.2;
-  ab += texture2D(tex, pos + vec2( 1,-1)*texelSize).xy * 0.05;
-  ab += texture2D(tex, pos + vec2(-1, 0)*texelSize).xy * 0.2;
-  ab += texture2D(tex, pos + vec2( 0, 0)*texelSize).xy * -1.0;
-  ab += texture2D(tex, pos + vec2( 1, 0)*texelSize).xy * 0.2;
-  ab += texture2D(tex, pos + vec2(-1, 1)*texelSize).xy * 0.05;
-  ab += texture2D(tex, pos + vec2( 0, 1)*texelSize).xy * 0.2;
-  ab += texture2D(tex, pos + vec2( 1, 1)*texelSize).xy * 0.05;
+  ab += texture2D(tex, pos + vec2(-1,-1)*texel).xy * 0.05;
+  ab += texture2D(tex, pos + vec2( 0,-1)*texel).xy * 0.2;
+  ab += texture2D(tex, pos + vec2( 1,-1)*texel).xy * 0.05;
+  ab += texture2D(tex, pos + vec2(-1, 0)*texel).xy * 0.2;
+  ab += texture2D(tex, pos + vec2( 0, 0)*texel).xy * -1.0;
+  ab += texture2D(tex, pos + vec2( 1, 0)*texel).xy * 0.2;
+  ab += texture2D(tex, pos + vec2(-1, 1)*texel).xy * 0.05;
+  ab += texture2D(tex, pos + vec2( 0, 1)*texel).xy * 0.2;
+  ab += texture2D(tex, pos + vec2( 1, 1)*texel).xy * 0.05;
 
   return ab;
 }
 
-vec2 laplacianCartesian(sampler2D tex, vec2 pos, vec2 texelSize) {
+vec2 laplacianCartesian(sampler2D tex, vec2 pos, vec2 texel) {
   vec2 ab = vec2(0.0,0.0);
 
-  ab += texture2D(tex, pos + vec2( 0,-1)*texelSize).xy;
-  ab += texture2D(tex, pos + vec2(-1, 0)*texelSize).xy;
-  ab += texture2D(tex, pos + vec2( 1, 0)*texelSize).xy;
-  ab += texture2D(tex, pos + vec2( 0, 1)*texelSize).xy;
-  ab += texture2D(tex, pos + vec2( 0, 0)*texelSize).xy * -4.0;
+  ab += texture2D(tex, pos + vec2( 0,-1)*texel).xy;
+  ab += texture2D(tex, pos + vec2(-1, 0)*texel).xy;
+  ab += texture2D(tex, pos + vec2( 1, 0)*texel).xy;
+  ab += texture2D(tex, pos + vec2( 0, 1)*texel).xy;
+  ab += texture2D(tex, pos + vec2( 0, 0)*texel).xy * -4.0;
 
   return ab;
 }
@@ -45,12 +45,12 @@ vec2 laplacianCartesian(sampler2D tex, vec2 pos, vec2 texelSize) {
 // Why is this drifting from right to left?
 void main() {
   vec2 pos = vTexCoord.xy;
-  vec2 texelSize = 1.0 / resolution;
+  vec2 texel = 1.0 / resolution;
 
   vec4 current = texture2D(concentrations, pos);
   float a = current.x;
   float b = current.y;
-  vec2 lp = laplacian(concentrations, pos, texelSize);
+  vec2 lp = laplacian(concentrations, pos, texel);
 
   float da = diffusionA*lp.x - a*b*b + feed*(1.0-a);
   float db = diffusionB*lp.y + a*b*b - (kill + feed)*b;
