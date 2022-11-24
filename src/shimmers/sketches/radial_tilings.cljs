4bd4dbcee119c8f7b79bba59e5a9a0dbf13394da
Charles Comstock <dgtized@gmail.com>
2022-11-24 03:25:33 -0600

    Add an index label to each hexagon

--- src/shimmers/sketches/radial_tilings.cljs
+++ src/shimmers/sketches/radial_tilings.cljs
@@ -2,11 +2,11 @@
   (:require
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.hexagon :as hex]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
-   [thi.ng.geom.vector :as gv]
-   [shimmers.math.hexagon :as hex]
-   [thi.ng.geom.core :as g]))
+   [thi.ng.geom.svg.core :as svg]
+   [thi.ng.geom.vector :as gv]))
 
 (def width 900)
 (def height 600)
@@ -16,10 +16,20 @@
 (defn hexagons []
   (let [radius (* 0.95 height)
         revolutions 4]
-    (for [hex (hex/cube-spiral (gv/vec3) revolutions)]
-      (-> hex
-          (hex/cube-hexagon (/ radius (* 2 (+ revolutions 3.5))))
-          hex/flat-hexagon->polygon))))
+    (for [[idx hex] (map-indexed vector (hex/cube-spiral (gv/vec3) revolutions))]
+      (let [circle (-> hex
+                       (hex/cube-hexagon (/ radius (* 2 (+ revolutions 3.5)))))
+            poly (hex/flat-hexagon->polygon circle)]
+        (csvg/group {}
+          poly
+          (svg/text (:p circle)
+                    (str idx)
+                    {:font-weight "normal"
+                     :font-size "0.66em"
+                     :stroke "none"
+                     :fill "black"
+                     :alignment-baseline "middle"
+                     :text-anchor "middle"}))))))
 
 (defn scene []
   (csvg/timed
