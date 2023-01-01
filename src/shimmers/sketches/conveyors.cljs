7251a7183ca8ccc29296adbd0834fdbdcf5291fc
Charles Comstock <dgtized@gmail.com>
2022-12-31 21:49:32 -0600

    add inscribed-triangle as an option

--- src/shimmers/sketches/conveyors.cljs
+++ src/shimmers/sketches/conveyors.cljs
@@ -8,11 +8,13 @@
    [shimmers.math.equations :as eq]
    [shimmers.math.geometry :as geometry]
    [shimmers.math.geometry.collisions :as collide]
+   [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.polygon :as gp]
    [thi.ng.geom.rect :as rect]
+   [thi.ng.geom.triangle :as gt]
    [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
 
@@ -20,6 +22,11 @@
   (geometry/rotate-around-centroid
    (g/bounds (g/scale-size circle 0.4))
    angle))
+
+(defn inscribed-triangle [{:keys [p r]} angle]
+  (gt/triangle2 (v/+polar p r (- angle (/ eq/TAU 3)))
+                (v/+polar p r angle)
+                (v/+polar p r (+ angle (/ eq/TAU 3)))))
 
 (defn generate-shape [{[_ y] :p [w h] :size}]
   (let [x-entry (* w 0.95)
@@ -31,8 +38,8 @@
        (fn [] (g/scale-size circle 0.5)) 1
        (fn [] (square circle (dr/random eq/TAU))) 1
        (fn [] (gp/polygon2
-              (take 3 (:points (square circle (dr/random eq/TAU))))))
-       (fn [] ())1}))))
+              (take 3 (:points (square circle (dr/random eq/TAU)))))) 1
+       (fn [] (inscribed-triangle circle (dr/random eq/TAU))) 1}))))
 
 (defn convey [bounds dx shape]
   (let [shape' (g/translate shape dx)]
