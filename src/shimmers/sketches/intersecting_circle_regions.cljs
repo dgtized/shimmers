7643de54f91e81185191f952bd927c8e05cd016b
Charles Comstock <dgtized@gmail.com>
2023-01-05 18:39:33 -0600

    partially solve for generating the *half-moon* shapes

--- src/shimmers/sketches/intersecting_circle_regions.cljs
+++ src/shimmers/sketches/intersecting_circle_regions.cljs
@@ -4,19 +4,47 @@
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.common.quil-draws-geom :as qdg]
+   [shimmers.math.equations :as eq]
    [shimmers.math.geometry.collisions :as collide]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.circle :as gc]
-   [thi.ng.geom.types :refer [Circle2]]
-   [thi.ng.geom.core :as g]))
+   [thi.ng.geom.core :as g]
+   [thi.ng.geom.polygon :as gp]
+   [thi.ng.geom.utils.intersect :as isec]
+   [thi.ng.math.core :as tm]))
 
-(defn regions [{pa :p ra :r :as a} {pb :p rb :r :as b}]
+(defn arc [circle t0 t1 res]
+  (println t0 t1)
+  (for [t (range t0 (if (< t0 t1) t1 (+ t1 eq/TAU)) res)]
+    (g/point-at circle (/ t eq/TAU))))
+
+(defn half-moon [{p :p :as a} {q :p :as b} up down res]
+  (println a b)
+  (gp/polygon2
+   (concat (arc a
+                (g/heading (tm/- up p))
+                (g/heading (tm/- down p))
+                res)
+           (reverse (arc b
+                         (g/heading (tm/- up q))
+                         (g/heading (tm/- down q))
+                         res)))))
+
+(defn regions [{ra :r :as a} {rb :r :as b} res]
   (if (collide/overlaps? a b)
-    [a b]
+    (if-let [contacts (isec/intersect-circle-circle? a b)]
+      (let [[up down] contacts]
+        [(half-moon a b up down res)
+         (half-moon b a down up res)])
+      (if (< ra rb)
+        [a]
+        [b]))
     [a b]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
+  (q/no-loop)
   {:circles [(gc/circle (cq/rel-vec 0.35 0.5) (cq/rel-h 0.4))
              (gc/circle (cq/rel-vec 0.65 0.5) (cq/rel-h 0.4))]})
 
@@ -28,13 +56,14 @@
   (q/ellipse-mode :radius)
   (q/no-fill)
   (q/stroke 0.0)
+  (q/stroke-weight 1.0)
   (doseq [circle circles]
     (cq/circle circle))
 
   (q/stroke 0.5)
-  (doseq [region (regions (first circles) (second circles))]
-    (cond (instance? Circle2 region)
-          (cq/circle (g/scale-size region 0.9)))))
+  (doseq [[i region] (map-indexed vector (regions (first circles) (second circles) (/ eq/TAU 10)))]
+    (q/fill (mod (* tm/PHI i) 1.0) 0.5 0.5 0.1)
+    (qdg/draw region)))
 
 (sketch/defquil intersecting-circle-regions
   :created-at "2023-01-05"
