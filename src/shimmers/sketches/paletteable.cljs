3b49eff167db145c5cce1c574777cd4beb561abd
Charles Comstock <dgtized@gmail.com>
2023-01-07 23:20:37 -0600

    Use ref to render to canvas only on draw

--- src/shimmers/sketches/paletteable.cljs
+++ src/shimmers/sketches/paletteable.cljs
@@ -1,21 +1,8 @@
 (ns shimmers.sketches.paletteable
   (:require
    [goog.dom :as dom]
-   [helins.canvas :as cv]
-   [shimmers.common.ui.canvas :as canvas]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.sketch :as sketch :include-macros true]))
-
-(defn set-image-cb [ui-state _]
-  (let [el (dom/getElement "still")]
-    (when-let [file (first (.-files el))]
-      (.then (js/createImageBitmap file)
-             (fn [image]
-               (swap! ui-state update
-                      :image
-                      (fn [existing]
-                        (when existing (.close existing))
-                        image)))))))
 
 (defn scale-dpi [canvas [width height]]
   (let [ctx (.getContext canvas "2d")
@@ -27,39 +14,38 @@
     (.scale ctx dpr dpr)
     ctx))
 
-;; This is rendering at 60 fps, can we draw canvas on image load?
-(defn draw-canvas [_ canvas state]
-  (cv/on-frame
-   (fn [_]
-     (when-let [image (:image @state)]
-       (let [sw (.-width image)
-             sh (.-height image)
-             ratio (/ sh (float sw))
-             {:keys [width]} @state
-             w width
-             h (int (* w ratio))
-             ctx (scale-dpi canvas [w h])]
-         (.drawImage ctx image 0 0 sw sh 0 0 w h)))
-     true)))
+(defn draw-canvas [!canvas width image]
+  (when-let [canvas @!canvas]
+    (let [sw (.-width image)
+          sh (.-height image)
+          ratio (/ sh (float sw))
+          w width
+          h (int (* w ratio))
+          ctx (scale-dpi canvas [w h])]
+      (.drawImage ctx image 0 0 sw sh 0 0 w h))))
+
+(defn set-image-cb [!canvas _]
+  (let [el (dom/getElement "still")]
+    (when-let [file (first (.-files el))]
+      (.then (js/createImageBitmap file)
+             (fn [image]
+               (draw-canvas !canvas 800 image))))))
 
 (defn page []
-  (let [ui-state (ctrl/state {:width 800
-                              :height 600
-                              :image nil})]
+  (let [!canvas (atom nil)]
     (fn []
       [:div
        [:div
-        [canvas/canvas-frame
-         {:class "canvas-frame"}
-         ui-state
-         draw-canvas]]
+        [:canvas
+         {:class "canvas-frame"
+          :ref #(reset! !canvas %)}]]
        [:div.contained
         [:label {:for "still"} "Select an image: "]
         [:input {:type "file"
                  :id "still"
                  :name "still"
                  :accept "image/png, image/jpg"
-                 :on-change (partial set-image-cb ui-state)}]]])))
+                 :on-change (partial set-image-cb !canvas)}]]])))
 
 (sketch/definition paletteable
   {:created-at "2023-06-07"
