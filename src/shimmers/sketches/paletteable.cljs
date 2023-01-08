829a1f431f1a82ffb1a38135cd4976f5967c1937
Charles Comstock <dgtized@gmail.com>
2023-01-07 21:16:15 -0600

    start incorporating a canvas

--- src/shimmers/sketches/paletteable.cljs
+++ src/shimmers/sketches/paletteable.cljs
@@ -1,12 +1,11 @@
 (ns shimmers.sketches.paletteable
   (:require
    [goog.dom :as dom]
+   [shimmers.common.ui.canvas :as canvas]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.sketch :as sketch :include-macros true]))
 
-(defonce ui-state (ctrl/state {:image nil}))
-
-(defn set-image-cb [_]
+(defn set-image-cb [ui-state _]
   (let [el (dom/getElement "still")]
     (when-let [file (first (.-files el))]
       (let [reader (new js/FileReader)]
@@ -16,24 +15,35 @@
                   (swap! ui-state assoc :image result))))
         (.readAsDataURL reader file)))))
 
+(defn draw-canvas [_ canvas state])
+
 (defn page []
-  [:div
-   [:div.contained
-    [:label {:for "still"} "Select an image: "]
-    [:input {:type "file"
-             :id "still"
-             :name "still"
-             :accept "image/png, image/jpg"
-             :on-change set-image-cb}]]
-   [:div
-    [:img {:id "preview"
-           :src (if-let [image (:image @ui-state)]
-                  image
-                  "")
-           :height 200}]]])
+  (let [ui-state (ctrl/state {:width 800
+                              :height 600
+                              :image nil})]
+    (fn []
+      [:div
+       [:div
+        [canvas/canvas-frame
+         {:class "canvas-frame"}
+         ui-state
+         draw-canvas]]
+       [:div.contained
+        [:label {:for "still"} "Select an image: "]
+        [:input {:type "file"
+                 :id "still"
+                 :name "still"
+                 :accept "image/png, image/jpg"
+                 :on-change (partial set-image-cb ui-state)}]
+        [:div
+         [:img {:id "preview"
+                :src (if-let [image (:image @ui-state)]
+                       image
+                       "")
+                :height 200}]]]])))
 
 (sketch/definition paletteable
   {:created-at "2023-06-07"
    :type :canvas
    :tags #{:genuary2023}}
-  (ctrl/mount page "sketch-host"))
+  (ctrl/mount (page) "sketch-host"))
