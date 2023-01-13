5b58dc06d914f10455354ebd7f21d8d8d8edd42c
Charles Comstock <dgtized@gmail.com>
2023-01-13 05:05:17 -0600

    Add little oversample storms to fire off in groups

--- src/shimmers/sketches/ion_storm.cljs
+++ src/shimmers/sketches/ion_storm.cljs
@@ -2,49 +2,72 @@
   (:require
    [quil.core :as q :include-macros true]
    [quil.middleware :as m]
+   [shimmers.algorithm.random-points :as rp]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.equations :as eq]
+   [shimmers.math.geometry.triangle :as triangle]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.circle :as gc]
-   [thi.ng.geom.vector :as gv]
-   [shimmers.math.geometry.triangle :as triangle]
-   [shimmers.math.equations :as eq]))
+   [thi.ng.geom.vector :as gv]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  (q/frame-rate 12)
-  {:t 0})
+  (q/frame-rate 10)
+  {:t 0
+   :storms []})
 
-(defn update-state [state]
-  (update state :t + 0.01))
+(defn random-circle [r-param max-r]
+  (let [R (min (q/height) (q/width))
+        r (max (min r-param max-r) 0.0)
+        radius (* R r)
+        hr (* radius 0.5)]
+    (gc/circle (gv/vec2 (dr/random hr (- (q/width) hr))
+                        (dr/random hr (- (q/height) hr)))
+               radius)))
 
-(defn draw [{:keys [t]}]
-  (q/background 1.0 0.3)
+(defn flash-storm [t]
+  {:region (random-circle (dr/gaussian 0.2 0.1) 0.5)
+   :t0 t
+   :t1 (+ t (dr/random 0.5 1.5))})
+
+(defn update-state [{:keys [t] :as state}]
+  (-> state
+      (update :storms
+              (fn [storms]
+                (let [storms' (remove (fn [{:keys [t1]}] (>= t t1)) storms)]
+                  (if (dr/chance 0.1)
+                    (conj storms' (flash-storm t))
+                    storms'))))
+      (update :t + 0.1)))
+
+(defn draw [{:keys [storms]}]
+  (q/background 1.0 0.1)
   (q/ellipse-mode :radius)
   (q/stroke 0.0)
   (q/stroke-weight 3.0)
 
-  (let [R (min (q/height) (q/width))]
-    (q/fill 0.0 0.1)
-    (dotimes [_ 2]
-      (let [r (max (min (dr/gaussian 0.3 0.1) 0.5) 0.0)
-            radius (* R r)
-            hr (* radius 0.5)]
-        (cq/circle
-         (gc/circle (gv/vec2 (dr/random hr (- (q/width) hr))
-                             (dr/random hr (- (q/height) hr)))
-                    radius))))
+  (q/fill 0.0 0.25)
+  (dotimes [_ 3]
+    (cq/circle (random-circle (dr/gaussian 0.3 0.1) 0.5)))
 
-    (q/fill 1.0 0.2)
-    (dotimes [_ 6]
-      (let [r (max (min (dr/gaussian 0.05 0.1) 0.3) 0.0)
-            radius (* R r)
-            hr (* radius 0.5)
-            c (gc/circle (gv/vec2 (dr/random hr (- (q/width) hr))
-                                  (dr/random hr (- (q/height) hr)))
-                         radius)]
-        (cq/draw-triangle (:points (triangle/inscribed-equilateral c (dr/random eq/TAU))))))))
+  (q/fill 1.0 0.2)
+  (dotimes [_ 6]
+    (let [c (random-circle (dr/gaussian 0.05 0.1) 0.3)]
+      (cq/draw-triangle (:points (triangle/inscribed-equilateral c (dr/random eq/TAU))))))
+
+  (q/fill 0.6 0.1 0.9 0.08)
+  (q/no-stroke)
+  (doseq [{:keys [region]} storms]
+    (let [{:keys [p r]} region]
+      (dotimes [_ 64]
+        (let [radius (* (max (min (dr/gaussian 0.2 0.1) 0.95) 0.0) r)
+              c (rp/inside-circle (gc/circle p (- r radius)) dr/random)]
+          (cq/draw-triangle
+           (:points (triangle/inscribed-equilateral
+                     (gc/circle c radius)
+                     (dr/random eq/TAU)))))))))
 
 (sketch/defquil ion-storm
   :created-at "2023-01-13"
