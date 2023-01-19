9084404e5916d03c4e774a4b233ebbc9b642db63
Charles Comstock <dgtized@gmail.com>
2023-01-18 19:03:10 -0600

    expand on baubles example

--- src/shimmers/sketches/baubles.cljs
+++ src/shimmers/sketches/baubles.cljs
@@ -9,13 +9,14 @@
    [shimmers.common.sequence :as cs]
    [thi.ng.geom.polygon :as gp]
    [shimmers.math.vector :as v]
-   [shimmers.math.equations :as eq]))
+   [shimmers.math.equations :as eq]
+   [shimmers.common.ui.controls :as ctrl]))
 
 (defn build-shape [t]
-  (let [s (+ 3 (int (mod t 12)))]
+  (let [s (+ 3 (int (mod t 13)))]
     (gp/polygon2
      (vec (map (fn [vert]
-                 (v/+polar (cq/rel-vec 0.5 0.5) (* 0.3 (q/height))
+                 (v/+polar (cq/rel-vec 0.5 0.5) (* 0.2 (q/height))
                            (- (* vert (/ eq/TAU s)) (* eq/TAU 0.25))))
                (range s))))))
 
@@ -38,15 +39,28 @@
   (q/ellipse-mode :radius)
   (q/no-fill)
   (doseq [s shapes]
-    (let [verts1 (g/vertices (g/translate s (cq/rel-vec -0.25 0.0)))]
+    (let [verts1 (g/vertices (g/translate s (cq/rel-vec -0.25 -0.25)))]
       (draw-points verts1)
-      (cq/draw-curve-shape verts1))
-    (let [verts2 (cs/rotate 1 (g/vertices (g/translate s (cq/rel-vec 0.25 0.0))))]
+      (cq/draw-curve-shape verts1)
+      (cq/draw-polygon (g/translate s (cq/rel-vec -0.25 0.25))))
+    (let [verts2 (cs/rotate 1 (g/vertices (g/translate s (cq/rel-vec 0.25 -0.25))))]
       (draw-points verts2)
-      (cq/draw-curve-shape verts2))))
+      (cq/draw-curve-shape verts2)
+      (let [v2' (mapv #(g/translate % (cq/rel-vec 0.0 0.5)) verts2)
+            v-overlap (into v2' (take 2 v2'))]
+        (draw-points v-overlap)
+        (cq/draw-curve-shape v-overlap)))))
+
+(defn ui-controls []
+  [:div
+   [:p.readable-width
+    "Experimenting with curve vertex on polygons. The issue is that by
+   default curve-vertex will use every 2nd vertex as a control point, but it
+   leaves a straight line at the start."]])
 
 (sketch/defquil baubles
   :created-at "2023-01-18"
+  :on-mount (fn [] (ctrl/mount ui-controls))
   :tags #{}
   :size [800 600]
   :setup setup
