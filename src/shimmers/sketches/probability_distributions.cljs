058427b236c344511228d9ab5679bba568465375
Charles Comstock <dgtized@gmail.com>
2023-01-31 19:47:41 -0600

    Add a histogram and rename original to scatter-1d

--- src/shimmers/sketches/probability_distributions.cljs
+++ src/shimmers/sketches/probability_distributions.cljs
@@ -2,19 +2,20 @@
   (:require
    [shimmers.common.svg :as csvg :include-macros true]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.common.ui.debug :as debug]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.rect :as rect]
    [thi.ng.geom.vector :as gv]
-   [shimmers.common.ui.debug :as debug]))
+   [thi.ng.math.core :as tm]))
 
 (def width 250)
 (def height 40)
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
-(defn histogram [samples]
+(defn scatter-1d [samples]
   (csvg/svg {:width width
              :height height
              :stroke "none"
@@ -23,6 +24,26 @@
     (for [x samples]
       (rect/rect (- (* x width) 0.5) 0
                  1.0 height))))
+
+(defn histogram [samples]
+  (csvg/svg {:width width
+             :height height
+             :stroke "none"
+             :fill "navy"
+             :stroke-width 0.5}
+    (let [freqs (frequencies (map (comp int (partial * 10)) samples))
+          buckets (reduce-kv (fn [b k v]
+                               (update b
+                                       (cond (< k 0) -1
+                                             (> k 10) 11
+                                             :else k) (fnil + 0)
+                                       v))
+                             {} freqs)]
+      (for [[bucket value] (sort-by first buckets)]
+        (rect/rect (* width (/ (inc bucket) 12))
+                   (* height (- 1.0 (min 1.0 (/ (float value) 128))))
+                   (* width (/ 1 12))
+                   height)))))
 
 (defn approaches []
   [{:title "Uniform Random"
@@ -48,6 +69,12 @@
    {:title "Pareto"
     :params {:scale 0.01 :shape 2.0}
     :generator #(dr/pareto 0.01 2.0)}
+   {:title "Pareto (clamped)"
+    :params {:scale 0.01 :shape 0.5}
+    :generator #(tm/clamp01 (dr/pareto 0.01 0.5))}
+   {:title "Pareto (clamped + noise)"
+    :params {:scale 0.01 :shape 0.5}
+    :generator #(tm/clamp01 (+ (dr/pareto 0.01 0.5) (dr/gaussian 0.0 0.02)))}
    ])
 
 (defn page []
@@ -63,6 +90,7 @@
              [:pre [:code (str params)]]]
             [:td
              [:div.canvas-frame
+              [scatter-1d samples]
               [histogram samples]]]
             [:td
              (debug/pre-edn (dr/summary-stats samples) {:width 50})]]))
