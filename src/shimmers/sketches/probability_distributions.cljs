12345c8682540543aa8d51244dce76f919f18b64
Charles Comstock <dgtized@gmail.com>
2023-01-31 19:12:19 -0600

    Expand the visualization a little

--- src/shimmers/sketches/probability_distributions.cljs
+++ src/shimmers/sketches/probability_distributions.cljs
@@ -6,10 +6,11 @@
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.rect :as rect]
-   [thi.ng.geom.vector :as gv]))
+   [thi.ng.geom.vector :as gv]
+   [shimmers.common.ui.debug :as debug]))
 
-(def width 400)
-(def height 50)
+(def width 250)
+(def height 40)
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
@@ -26,19 +27,45 @@
 (defn approaches []
   [{:title "Uniform Random"
     :generator #(dr/random)}
-   {:title "Gaussian {:mu 0.5 :sd 0.15}"
+   {:title "Gaussian"
+    :params {:mu 0.5 :sd 0.3}
+    :generator #(dr/gaussian 0.5 0.3)}
+   {:title "Gaussian"
+    :params {:mu 0.5 :sd 0.15}
     :generator #(dr/gaussian 0.5 0.15)}
-   {:title "Gaussian {:mu 0.5 :sd 0.08}"
-    :generator #(dr/gaussian 0.5 0.08)}])
+   {:title "Gaussian"
+    :params {:mu 0.5 :sd 0.075}
+    :generator #(dr/gaussian 0.5 0.075)}
+   {:title "Pareto"
+    :params {:scale 0.01 :shape 0.8}
+    :generator #(dr/pareto 0.01 0.8)}
+   {:title "Pareto"
+    :params {:scale 0.01 :shape 1.05}
+    :generator #(dr/pareto 0.01 1.05)}
+   {:title "Pareto"
+    :params {:scale 0.01 :shape 1.5}
+    :generator #(dr/pareto 0.01 1.5)}
+   {:title "Pareto"
+    :params {:scale 0.01 :shape 2.0}
+    :generator #(dr/pareto 0.01 2.0)}
+   ])
 
 (defn page []
   [:div
-   (into [:div]
-         (for [{:keys [title generator]} (approaches)]
-           [:<>
-            [:h3.center title]
-            [:div.canvas-frame
-             [histogram (repeatedly 200 generator)]]]))
+   [:p]
+   [:h3.contained.center "Visualize Probability Distributions"]
+   (into [:table.contained]
+         (for [{:keys [title params generator]} (approaches)
+               :let [samples (repeatedly 256 generator)]]
+           [:tr
+            [:td title
+             [:br]
+             [:pre [:code (str params)]]]
+            [:td
+             [:div.canvas-frame
+              [histogram samples]]]
+            [:td
+             (debug/pre-edn (dr/summary-stats samples) {:width 50})]]))
    [:div.explanation.contained
     [:div.center
      [:div [view-sketch/generate :probability-distributions]]]]])
