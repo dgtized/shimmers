41607971cd1ce844b2e5e1ec258b67b851c6e48f
Charles Comstock <dgtized@gmail.com>
2023-02-02 05:44:41 -0600

    combine spiral-surrounding logic

--- src/shimmers/sketches/spiral_pack.cljs
+++ src/shimmers/sketches/spiral_pack.cljs
@@ -4,11 +4,13 @@
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.math.equations :as eq]
+   [shimmers.math.geometry.collisions :as collide]
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
+   [thi.ng.geom.rect :as rect]
    [thi.ng.geom.vector :as gv]))
 
 (def width 800)
@@ -16,21 +18,22 @@
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
-(defn spiral-step [{r :r dr :dr t :t}]
-  (let [dr' (* 1.01 dr)
-        dt (Math/atan2 (+ dr dr') (+ r dr))]
-    {:t (+ t dt)
-     :r (+ r (* dr' 0.1))
-     :dr (* 1.03 dr')
-     :circle (gc/circle (v/+polar (rv 0.5 0.5) (+ dr r) t)
-                        dr)}))
+(def screen (rect/rect 0 0 width height))
 
 (defn spiral-surrounding []
   (let [radius (* height 0.05)
         circle (gc/circle (rv 0.5 0.5) radius)]
     (->> {:circle circle :t 0 :r radius :dr (* 0.3 radius)}
-         (iterate spiral-step)
-         (take 60)
+         (iterate
+          (fn [{r :r dr :dr t :t}]
+            (let [dr' (* 1.005 dr)
+                  dt (Math/atan2 (+ dr dr') (+ r dr))]
+              {:t (+ t dt)
+               :r (+ r (* dr' 0.1))
+               :dr (* 1.03 dr')
+               :circle (gc/circle (v/+polar (rv 0.5 0.5) (+ dr r) t)
+                                  dr)})))
+         (take-while (fn [{:keys [circle]}] (collide/bounded? screen circle)))
          (map :circle))))
 
 (defn spiral-inside []
@@ -56,7 +59,7 @@
              :stroke "black"
              :fill "none"
              :stroke-width 0.5}
-    ((dr/weighted {spiral-inside 8
+    ((dr/weighted {spiral-inside 2
                    spiral-surrounding 1}))))
 
 (sketch/definition spiral-pack
