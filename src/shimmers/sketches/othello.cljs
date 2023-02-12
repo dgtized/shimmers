cc7505369fbc3437cb3c6bbb45914332920a6a89
Charles Comstock <dgtized@gmail.com>
2023-02-12 00:30:33 -0600

    Overlap 3 colors

--- src/shimmers/sketches/othello.cljs
+++ src/shimmers/sketches/othello.cljs
@@ -10,24 +10,40 @@
 (defn draw-frame [ctx width height t]
   (let [cols 16
         rows 12]
-    (-> ctx
-        (cv/clear 0 0 width height)
-        (cv/color-fill "#000"))
+    (cv/clear ctx 0 0 width height)
     (dotimes [i cols]
       (dotimes [j rows]
-        (let [a0 (* eq/TAU (eq/unit-cos (+  (/ j rows) (/ i cols) (* 0.45 t))))
+        (let [x (* (/ width cols) (+ i 0.5))
+              y (* (/ height rows) (+ j 0.5))
+              r (* (/ width cols) 0.45)
+              a0 (* eq/TAU (eq/unit-cos (+  (/ j rows) (/ i cols) (* 0.45 t))))
               a1 (+ a0
                     (* eq/TAU
                        (eq/unit-sin (+ (/ 1.0 (inc i))
-                                       (/ 1.0 (inc j))
-                                       (* 0.25 t)))))]
-          (cv/arc (cv/begin ctx)
-                  (* (/ width cols) (+ i 0.5))
-                  (* (/ height rows) (+ j 0.5))
-                  (* (/ width cols) 0.45)
-                  a0
-                  a1
-                  false)
+                                       (- 1.0 (/ 1.0 (inc j)))
+                                       (* 0.25 t)))))
+              b0 (* eq/TAU (eq/unit-cos (+  (/ x width) (/ y height) (* 0.4 t))))
+              b1 (+ b0
+                    (* eq/TAU
+                       (eq/unit-sin (+ (/ 1.0 x)
+                                       (/ 1.0 y)
+                                       (* 0.2 t)))))
+              c0 (* eq/TAU (eq/unit-cos (+  (- 1.0 (/ x width))
+                                       (- 1.0 (/ y height))
+                                       (* 0.66 t))))
+              c1 (+ c0
+                    (* eq/TAU
+                       (eq/unit-sin (+ (- 1.0 (/ 1.0 x))
+                                       (- 1.0 (/ 1.0 y))
+                                       (* 0.33 t)))))]
+          (cv/arc (cv/begin ctx) x y r a0 a1 false)
+          (cv/color-fill ctx "rgba(192,0,32,0.5)")
+          (cv/fill ctx)
+          (cv/arc (cv/begin ctx) x y r b0 b1 false)
+          (cv/color-fill ctx "rgba(0.0,64,200,0.5)")
+          (cv/fill ctx)
+          (cv/arc (cv/begin ctx) x y r c0 c1 false)
+          (cv/color-fill ctx "rgba(240,240,0,0.5)")
           (cv/fill ctx))))
     ctx))
 
