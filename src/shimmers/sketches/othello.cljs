f792922c2e48d63505061a939353d43221600d0c
Charles Comstock <dgtized@gmail.com>
2023-02-11 23:39:59 -0600

    remove pass through helper

--- src/shimmers/sketches/othello.cljs
+++ src/shimmers/sketches/othello.cljs
@@ -7,13 +7,6 @@
    [shimmers.math.equations :as eq]
    [shimmers.sketch :as sketch :include-macros true]))
 
-;; remember arc-to
-(defn arc [ctx x y radius angle0 angle1 anti-clockwise?]
-  (let [path (cv/begin ctx)]
-    (cv/arc path x y radius angle0 angle1 anti-clockwise?)
-    (cv/fill ctx)
-    ctx))
-
 (defn draw-frame [ctx width height t]
   (let [cols 16
         rows 12
@@ -25,13 +18,14 @@
       (dotimes [j rows]
         (let [a0 (* eq/TAU (eq/unit-cos (+  (/ j rows) (/ i cols) (* 0.45 t))))
               a1 (+ a0 gap (* (- eq/TAU (* 1.5 gap)) (eq/unit-sin (+ (/ 1.0 (inc i)) (/ 1.0 (inc j)) (* 0.25 t)))))]
-          (arc ctx
-               (+ (* (/ width cols) (+ i 0.5)))
-               (* (/ height rows) (+ j 0.5))
-               (* (/ width cols) 0.45)
-               a0
-               a1
-               false))))
+          (cv/arc (cv/begin ctx)
+                  (+ (* (/ width cols) (+ i 0.5)))
+                  (* (/ height rows) (+ j 0.5))
+                  (* (/ width cols) 0.45)
+                  a0
+                  a1
+                  false)
+          (cv/fill ctx))))
     ctx))
 
 (defn do-frame []
