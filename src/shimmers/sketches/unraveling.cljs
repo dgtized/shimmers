4c5760dd8f9ea3e395839ece948f113d9f8ce850
Charles Comstock <dgtized@gmail.com>
2023-02-20 16:29:30 -0600

    Convert unraveling to use canvas directly over quil

--- src/shimmers/sketches/unraveling.cljs
+++ src/shimmers/sketches/unraveling.cljs
@@ -1,53 +1,74 @@
 (ns shimmers.sketches.unraveling
   (:require
-   [quil.core :as q :include-macros true]
-   [quil.middleware :as m]
+   [helins.canvas :as cv]
+   [reagent.core :as r]
    [shimmers.common.framerate :as framerate]
-   [shimmers.common.quil :as cq]
+   [shimmers.common.ui.canvas :as canvas]
+   [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.equations :as eq]
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
+   [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
 
 ;; Reminiscent of Yoann Bourgeois "Progress is not Linear" dance
 
-(defn spiral-inside [t0 dr dt]
-  (let [radius (cq/rel-h 0.48)
-        center (cq/rel-vec 0.5 0.5)
-        circle (gc/circle center radius)]
-    (->> {:circle circle :t t0 :r radius}
-         (iterate
-          (fn [{:keys [circle t r]}]
-            (let [r' (* dr r)]
-              {:circle (gc/circle (v/+polar (g/point-at circle (/ t eq/TAU)) r' (+ t Math/PI)) r')
-               :t (+ t dt)
-               :r r'})))
-         (take-while (fn [{:keys [r]}] (> r 3.0)))
-         (map :circle))))
-
-(defn setup []
-  (q/color-mode :hsl 1.0)
-  {:t 0})
+(defn spiral-inside [circle t0 dr dt]
+  (->> {:circle circle :t t0 :r (:r circle)}
+       (iterate
+        (fn [{:keys [circle t r]}]
+          (let [r' (* dr r)]
+            {:circle (gc/circle (v/+polar (g/point-at circle (/ t eq/TAU)) r' (+ t Math/PI)) r')
+             :t (+ t dt)
+             :r r'})))
+       (take-while (fn [{:keys [r]}] (> r 3.0)))
+       (map :circle)))
 
 (defn update-state [{:keys [t] :as state}]
-  (update state :t + (+ 0.002 (* 0.018 (eq/unit-cos (+ tm/PHI (/ t tm/PHI)))))))
+  (update state :t + (+ 0.003 (* 0.017 (eq/unit-cos (+ tm/PHI (/ t tm/PHI)))))))
 
-(defn draw [{:keys [t]}]
-  (q/background 1.0)
-  (q/ellipse-mode :radius)
-  (q/stroke-weight (/ 1 tm/PHI))
-  (doseq [circle (spiral-inside (* tm/PHI t)
-                                (+ 0.79 (* 0.175 (eq/unit-cos t)))
-                                (+ 0.01 (* 0.5 (eq/unit-cos (* tm/PHI t)))))]
-    (cq/circle circle)))
+(defn circle [ctx {[x y] :p r :r}]
+  (-> ctx
+      cv/begin
+      (cv/arc x y r 0 eq/TAU)
+      (cv/stroke)))
 
-(sketch/defquil unraveling
-  :created-at "2023-02-02"
-  :tags #{}
-  :size [800 600]
-  :setup setup
-  :update update-state
-  :draw draw
-  :middleware [m/fun-mode framerate/mode])
+(defn draw-frame [ctx width height {:keys [t]}]
+  (-> ctx
+      (cv/clear 0 0 width height)
+      (cv/line-width (/ 1.5 tm/PHI)))
+  (doseq [c (spiral-inside (gc/circle (gv/vec2 (* 0.5 width) (* 0.5 height))
+                                      (* 0.48 height))
+                           (* tm/PHI t)
+                           (+ 0.79 (* 0.175 (eq/unit-cos t)))
+                           (+ 0.01 (* 0.5 (eq/unit-cos (* tm/PHI t)))))]
+    (circle ctx c))
+  ctx)
+
+(defn do-frame [frame-state canvas-el canvas-state]
+  (let [measure-frames! (framerate/sampler)]
+    (canvas/on-animated-frame
+     {:delay 0}
+     (fn [t]
+       (measure-frames! t)
+       (let [{:keys [width height]} @canvas-state
+             ctx (canvas/scale-dpi canvas-el [width height])]
+         (swap! frame-state update-state)
+         (draw-frame ctx width height @frame-state))))))
+
+(defn page []
+  (let [canvas-state (r/atom {:width 800 :height 600})
+        toggle-fs (fn [] (canvas/toggle-full-screen! canvas-state {:width-pct 0.6}))
+        attributes {:class "canvas-frame"
+                    :on-double-click toggle-fs}]
+    (fn []
+      [:div
+       [canvas/canvas-frame attributes canvas-state (partial do-frame (atom {:t 0}))]])))
+
+(sketch/definition unraveling
+  {:created-at "2023-02-02"
+   :type :canvas
+   :tags #{}}
+  (ctrl/mount (page) "sketch-host"))
