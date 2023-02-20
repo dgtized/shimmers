2b38235d0318c7b075f2bf4f91be742a6c7eb4ec
Charles Comstock <dgtized@gmail.com>
2023-02-20 06:08:12 -0600

    convert slashes to an svg sketch instead of using quil

--- src/shimmers/sketches/slashes.cljs
+++ src/shimmers/sketches/slashes.cljs
@@ -1,41 +1,45 @@
 (ns shimmers.sketches.slashes
   (:require
-   [quil.core :as q :include-macros true]
-   [quil.middleware :as m]
    [shimmers.algorithm.line-clipping :as clip]
-   [shimmers.common.framerate :as framerate]
-   [shimmers.common.quil :as cq]
+   [shimmers.common.svg :as csvg :include-macros true]
+   [shimmers.common.ui.controls :as ctrl]
    [shimmers.sketch :as sketch :include-macros true]
-   [thi.ng.geom.core :as g]
+   [shimmers.view.sketch :as view-sketch]
+   [thi.ng.geom.rect :as rect]
+   [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
 
-(defn setup []
-  (q/color-mode :hsl 1.0)
-  (let [bounds (cq/screen-rect)
+(def width 800)
+(def height 600)
+(defn rv [x y]
+  (gv/vec2 (* width x) (* height y)))
+
+(defn shapes []
+  (let [bounds (rect/rect 0 0 width height)
         hatch (partial clip/variable-hatching bounds)]
-    {:slashes
-     (concat (hatch (tm/random 5.0 6.0) 0
-                    (int (tm/random 6 16)) (constantly 10) (constantly 1))
-             (hatch (tm/random 5.0 6.0) (cq/rel-w 0.8)
-                    10 #(tm/random 5 15) #(tm/random 0.8 6))
-             (hatch (tm/random 3.5 4.5) (cq/rel-w 0.4)
-                    (int (tm/random 4 16)) #(tm/random 5 15) #(tm/random 0.5 4))
-             (hatch (tm/random 3.5 4.5) (* (g/width bounds) 1.3)
-                    8 #(tm/random 5 15) #(tm/random 0.5 2)))}))
+    (concat (hatch (tm/random 5.0 6.0) 0
+                   (int (tm/random 6 16)) (constantly 10) (constantly 1))
+            (hatch (tm/random 5.0 6.0) (* width 0.8)
+                   10 #(tm/random 5 15) #(tm/random 0.8 6))
+            (hatch (tm/random 3.5 4.5) (* width 0.4)
+                   (int (tm/random 4 16)) #(tm/random 5 15) #(tm/random 0.5 4))
+            (hatch (tm/random 3.5 4.5) (* width 1.3)
+                   8 #(tm/random 5 15) #(tm/random 0.5 2)))))
 
-(defn update-state [state]
-  state)
+(defn scene []
+  (csvg/timed
+   (csvg/svg {:width width
+              :height height
+              :stroke "black"
+              :fill "white"
+              :stroke-width 1.0}
+     (mapv (fn [{:keys [width] :as line}]
+             (vary-meta line assoc :stroke-width width))
+           (shapes)))))
 
-(defn draw [{:keys [slashes]}]
-  (q/background 1.0)
-  (doseq [{[p q] :points width :width} slashes]
-    (q/stroke-weight width)
-    (q/line p q)))
-
-(sketch/defquil slashes
-  :created-at "2021-08-20"
-  :size [800 600]
-  :setup setup
-  :update update-state
-  :draw draw
-  :middleware [m/fun-mode framerate/mode])
+(sketch/definition slashes
+  {:created-at "2021-08-20"
+   :type :svg
+   :tags #{}}
+  (ctrl/mount (view-sketch/page-for scene :slashes)
+              "sketch-host"))
