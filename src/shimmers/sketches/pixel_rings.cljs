75c15a83cb4b240ac1d35578445ca704fb9acd6b
Charles Comstock <dgtized@gmail.com>
2023-02-23 13:49:52 -0600

    extract theta from each v/+polar call

--- src/shimmers/sketches/pixel_rings.cljs
+++ src/shimmers/sketches/pixel_rings.cljs
@@ -16,19 +16,22 @@
   (let [center (cq/rel-vec 0.15 0.4)]
     (doseq [radius (range 0.05 1.0 0.08)]
       (dotimes [_ (* radius 12000)]
-        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.08)))]
-          (cq/circle (v/+polar center r (dr/gaussian 0.0 0.5)) 1.0)))))
+        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.08)))
+              theta (dr/gaussian 0.0 0.5)]
+          (cq/circle (v/+polar center r theta) 1.0)))))
   (let [center (cq/rel-vec 0.8 0.55)]
     (doseq [radius (range 0.05 0.9 0.05)]
       (dotimes [_ 8000]
-        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.15)))]
-          (cq/circle (v/+polar center r (dr/gaussian (* 0.45 eq/TAU) 0.8)) 1.0)))))
+        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.15)))
+              theta (dr/gaussian (* 0.45 eq/TAU) 0.8)]
+          (cq/circle (v/+polar center r theta) 1.0)))))
   (q/fill 0.0 0.7 0.35)
   (let [center (cq/rel-vec 0.55 0.85)]
     (doseq [radius (range 0.05 0.8 0.1)]
       (dotimes [_ (* radius 16000)]
-        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.1)))]
-          (cq/circle (v/+polar center r (dr/gaussian (* 0.75 eq/TAU) (* 0.35 (- 1.2 radius)))) 1.0)))))
+        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.1)))
+              theta (dr/gaussian (* 0.75 eq/TAU) (* 0.35 (- 1.2 radius)))]
+          (cq/circle (v/+polar center r theta) 1.0)))))
   (q/no-loop))
 
 (sketch/defquil pixel-rings
