e00583cdb124019c73be704c580f5246b2a3ff39
Charles Comstock <dgtized@gmail.com>
2023-02-23 13:55:30 -0600

    add draw-samples helper

--- src/shimmers/sketches/pixel_rings.cljs
+++ src/shimmers/sketches/pixel_rings.cljs
@@ -9,29 +9,38 @@
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]))
 
+(defn draw-samples [n pos-f]
+  (dotimes [_ n] (cq/circle (pos-f) 1.0)))
+
 (defn draw [_]
   (q/color-mode :hsl 1.0)
   (q/fill 0)
   (q/no-stroke)
   (let [center (cq/rel-vec 0.15 0.4)]
     (doseq [radius (range 0.05 1.0 0.08)]
-      (dotimes [_ (* radius 12000)]
-        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.08)))
-              theta (dr/gaussian 0.0 0.5)]
-          (cq/circle (v/+polar center r theta) 1.0)))))
+      (draw-samples
+       (* radius 12000)
+       (fn []
+         (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.08)))
+               theta (dr/gaussian 0.0 0.5)]
+           (v/+polar center r theta))))))
   (let [center (cq/rel-vec 0.8 0.55)]
     (doseq [radius (range 0.05 0.9 0.05)]
-      (dotimes [_ 8000]
-        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.15)))
-              theta (dr/gaussian (* 0.45 eq/TAU) 0.8)]
-          (cq/circle (v/+polar center r theta) 1.0)))))
+      (draw-samples
+       8000
+       (fn []
+         (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.15)))
+               theta (dr/gaussian (* 0.45 eq/TAU) 0.8)]
+           (v/+polar center r theta))))))
   (q/fill 0.0 0.7 0.35)
   (let [center (cq/rel-vec 0.55 0.85)]
     (doseq [radius (range 0.05 0.8 0.1)]
-      (dotimes [_ (* radius 16000)]
-        (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.1)))
-              theta (dr/gaussian (* 0.75 eq/TAU) (* 0.35 (- 1.2 radius)))]
-          (cq/circle (v/+polar center r theta) 1.0)))))
+      (draw-samples
+       (* radius 16000)
+       (fn []
+         (let [r (* (cq/rel-h radius) (Math/sqrt (dr/gaussian 0.9 0.1)))
+               theta (dr/gaussian (* 0.75 eq/TAU) (* 0.35 (- 1.2 radius)))]
+           (v/+polar center r theta))))))
   (q/no-loop))
 
 (sketch/defquil pixel-rings
