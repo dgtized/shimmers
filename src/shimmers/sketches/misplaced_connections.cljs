6c5606591c6501ea57770a17eeea6bef8e63c30c
Charles Comstock <dgtized@gmail.com>
2023-03-10 16:07:30 -0600

    introduce random cut offsets in horizontal lines

--- src/shimmers/sketches/misplaced_connections.cljs
+++ src/shimmers/sketches/misplaced_connections.cljs
@@ -13,11 +13,21 @@
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
+(defn find-cuts [offsets]
+  (let [a (dr/rand-nth (butlast offsets))]
+    [a (dr/rand-nth (drop-while #(<= % a) offsets))]))
+
 (defn shapes []
-  (let [h-offsets (dr/gaussian-range 0.15 0.08)
-        v-offsets (dr/gaussian-range 0.1 0.08)]
+  (let [h-offsets (dr/gaussian-range 0.1 0.08)
+        v-offsets (dr/gaussian-range 0.05 0.08)]
     (concat
-     (for [h h-offsets] (gl/line2 (rv 0.0 h) (rv 1.0 h)))
+     (mapcat concat
+             (for [h h-offsets]
+               (if (dr/chance 0.66)
+                 (let [[a b] (find-cuts v-offsets)]
+                   [(gl/line2 (rv 0.0 h) (rv a h))
+                    (gl/line2 (rv b h) (rv 1.0 h))])
+                 [(gl/line2 (rv 0.0 h) (rv 1.0 h))])))
      (for [v v-offsets] (gl/line2 (rv v 0.0) (rv v 1.0))))))
 
 (defn scene []
