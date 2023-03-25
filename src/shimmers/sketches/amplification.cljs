c30cc978b92110fe9bf7d76e2c157275ab8277a8
Charles Comstock <dgtized@gmail.com>
2023-03-25 15:27:15 -0500

    Add connecting skip lines that extend from center radially

--- src/shimmers/sketches/amplification.cljs
+++ src/shimmers/sketches/amplification.cljs
@@ -1,5 +1,6 @@
 (ns shimmers.sketches.amplification
   (:require
+   [shimmers.algorithm.lines :as lines]
    [shimmers.common.svg :as csvg :include-macros true]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
@@ -9,6 +10,7 @@
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
+   [thi.ng.geom.line :as gl]
    [thi.ng.geom.rect :as rect]
    [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
@@ -21,6 +23,12 @@
 (defn distance-to-edge [bounds p]
   (g/dist p (g/closest-point bounds p)))
 
+(defn skip-line [a b]
+  (->> (dr/random-int 12 48)
+       (lines/segmented (gl/line2 a b))
+       (drop 1)
+       (keep-indexed (fn [i x] (when (= 0 (mod i 3)) x)))))
+
 (defn sketch-circle [pos r]
   (let [n (Math/ceil (* 6 (dr/circular-random)))]
     (for [_ (range n)]
@@ -32,17 +40,26 @@
 
 (defn shapes [bounds]
   (let [center (rv (dr/random 0.25 0.75) (dr/random 0.35 0.65))
-        edge-dist (distance-to-edge bounds center)]
+        close-edge-point (g/closest-point bounds center)
+        edge-dist (g/dist center close-edge-point)]
     (concat (make-concentric center
                              (* 0.66 edge-dist)
                              (drop 1 (tm/norm-range 5)))
             (mapcat (fn [t]
-                      (let [proj (v/+polar center (* 0.3 height)
-                                           (+ (* 0.75 eq/TAU) (* eq/TAU t)))]
-                        (if (g/contains-point? bounds proj)
-                          (make-concentric proj
-                                           (* 0.33 edge-dist)
-                                           (drop 1 (tm/norm-range 3)))
+                      (let [direction (dr/gaussian (+ (* 0.75 eq/TAU) (* eq/TAU t)) 0.2)
+                            proj (v/+polar center
+                                           (+ (* (- 1 (eq/cos-similarity (tm/- close-edge-point center)
+                                                                         (v/polar 1 direction)))
+                                                 (* 0.5 edge-dist))
+                                              (* 0.6 edge-dist))
+                                           direction)
+                            proj-edge-dist (distance-to-edge bounds proj)]
+                        (if (and (g/contains-point? bounds proj)
+                                 (> proj-edge-dist 25))
+                          (concat (make-concentric proj
+                                                   (* 0.4 proj-edge-dist)
+                                                   (drop 1 (tm/norm-range 3)))
+                                  (skip-line center proj))
                           [])))
                     (drop 1 (tm/norm-range (dr/random-int 2 8)))))))
 
