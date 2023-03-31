2429fceef12f4914fc39dec970c0283695977ec3
Charles Comstock <dgtized@gmail.com>
2023-03-31 05:09:10 -0500

    Add a quil-template using nested components

---
+++ src/shimmers/sketches/_quil_template.cljs
@@ -0,0 +1,32 @@
+(ns shimmers.sketches._quil-template
+  (:require
+   [quil.core :as q :include-macros true]
+   [quil.middleware :as m]
+   [shimmers.common.framerate :as framerate]
+   [shimmers.sketch :as sketch :include-macros true]
+   [shimmers.common.ui.controls :as ctrl]))
+
+(defn setup []
+  (q/color-mode :hsl 1.0)
+  {})
+
+(defn update-state [state]
+  state)
+
+(defn draw [state]
+  state)
+
+(defn page []
+  [:div
+   (sketch/component
+    :size [800 600]
+    :setup setup
+    :update update-state
+    :draw draw
+    :middleware [m/fun-mode framerate/mode])])
+
+(sketch/definition quil-template
+  {:created-at "2023-"
+   :type :quil
+   :tags #{}}
+  (ctrl/mount page "sketch-host"))
