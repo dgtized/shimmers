a5ea93f1240ad8289e58d979ef38d59970b461e0
Charles Comstock <dgtized@gmail.com>
2023-04-12 15:44:42 -0500

    Adjust stroke rules so more likely to be white/black but opacity is lower

--- src/shimmers/sketches/superposition_mirrored.cljs
+++ src/shimmers/sketches/superposition_mirrored.cljs
@@ -216,13 +216,13 @@
         (doseq [{:keys [pos angle]} particles]
           (let [r (* 2 (Math/pow (/ (g/dist pos (cq/rel-vec 0.5 0.5)) diagonal) tm/PHI))
                 fill-opacity (- 1.0 (center-filter 0.0 (noise-at [t r] 0.006 [200.0 200.0])))
-                stroke-opacity (- 1.0 (center-filter 0.0 (noise-at [(+ r t) (+ r t)] 0.005 [300.0 300.0])))]
+                stroke-opacity (center-filter 0.03 (noise-at [(+ r t) (+ r t)] 0.01 [300.0 300.0]))]
             (when color
               (q/fill (mod (* 3 (noise-at [(* 0.75 t) (eq/sqr r)] 0.01 [0 0])) 1.0)
                       (+ 0.45 (* 0.55 (noise-at [(+ t r) r] 0.15 [50.0 100.0])))
                       (+ 0.5 (* 0.5 (noise-at [r t] 0.1 [100.0 50.0])))
                       (+ 0.001 (* 0.04 fill-opacity)))
-              (q/stroke (tm/smoothstep* 0.4 0.6 (noise-at [r (+ t r)] 0.1 [500.0 500.0]))
+              (q/stroke (tm/smoothstep* 0.45 0.55 (noise-at [r (+ t r)] 0.1 [500.0 500.0]))
                         (+ 0.001 (* 0.1 stroke-opacity))))
             (cq/draw-polygon (triangle/inscribed-equilateral {:p pos :r scale} angle)))))))
 
