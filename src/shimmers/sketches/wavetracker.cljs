a37b0675bf4b3dc9e0d22e826cdc50c4d1d91914
Charles Comstock <dgtized@gmail.com>
2023-04-17 20:02:27 -0500

    Add an adjustable width and a jitter motion

--- src/shimmers/sketches/wavetracker.cljs
+++ src/shimmers/sketches/wavetracker.cljs
@@ -13,6 +13,7 @@
 
 (defn setup []
   (q/color-mode :hsl 1.0)
+  (q/ellipse-mode :radius)
   {:t 0.0})
 
 (defn update-state [state]
@@ -27,16 +28,21 @@
   (let [samples 75
         rate (* 30 Math/PI (+ 0.5 (eq/unit-sin (* 0.9 tm/QUARTER_PI t))))
         amplitude (* (cq/rel-h 0.4) (+ 0.2 (* 0.8 (eq/unit-sin (* 0.66 t)))))
-        jitter (tm/smoothstep* 0.33 0.80 (eq/unit-sin (* 0.37 t)))]
+        jitter (tm/smoothstep* 0.33 0.80 (eq/unit-sin (* 0.37 t)))
+        max-scale (cq/rel-h 0.004)
+        max-jitter (* 2 max-scale)
+        width (+ (* 0.6 (eq/unit-sin (* 0.1 (* 0.6 t))))
+                 (* 0.3 (eq/unit-sin (+ 0.2 (* 1.1 t))))
+                 (* 0.1 (eq/unit-sin (+ 0.3 (* 1.4 t)))))]
     (dotimes [j 10]
-      (let [time-factor (+ t (* 0.2 j))]
+      (let [time-factor (+ t (* 0.2 (inc j) width))]
         (dotimes [i samples]
           (let [x (* (mod (/ (float i) samples) 1.0) (q/width))
                 y (* (Math/cos (+ time-factor (/ x rate))))
                 scale (+ 0.25 (* 0.75 (eq/unit-sin (+ (* 2.5 t) (* j 0.5)))))]
             (cq/circle (tm/+ (gv/vec2 x (* amplitude y))
-                             (dr/jitter (* 8 jitter (- 1.0 scale))))
-                       (abs (* scale 4.0)))))))))
+                             (dr/jitter (* max-jitter jitter (- 1.0 scale))))
+                       (abs (* scale max-scale)))))))))
 
 (defn page []
   [:div
