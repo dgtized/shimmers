d3b3f79e649d8bd35161c6cd357baee91caae193
Charles Comstock <dgtized@gmail.com>
2023-04-22 23:28:51 -0500

    sort the order of requires

--- src/shimmers/common/ui/canvas.cljs
+++ src/shimmers/common/ui/canvas.cljs
@@ -2,8 +2,8 @@
   (:require
    [goog.dom :as dom]
    [reagent.core :as r]
-   [shimmers.common.ui.canvas-attributes :as ca :refer-macros [defattr]]
    [shimmers.common.framerate :as framerate]
+   [shimmers.common.ui.canvas-attributes :as ca :refer-macros [defattr]]
    [shimmers.math.equations :as eq]))
 
 ;; See also https://github.com/reagent-project/reagent-cookbook/tree/master/recipes/canvas-fills-div
