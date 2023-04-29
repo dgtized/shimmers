c0e9e4e034cbed768d0fdb7a8de3883283563095
Charles Comstock <dgtized@gmail.com>
2023-04-29 14:36:10 -0500

    Convert intertwined into a sketch/component

--- src/shimmers/sketches/intertwined.cljs
+++ src/shimmers/sketches/intertwined.cljs
@@ -260,12 +260,19 @@
      [:div (view-sketch/generate :intertwined)
       (debug/display defo)]]))
 
-(sketch/defquil intertwined
-  :created-at "2021-10-23"
-  :tags #{:deterministic}
-  :on-mount (fn [] (ctrl/mount ui-controls))
-  :size [800 600]
-  :setup setup
-  :update update-state
-  :draw draw
-  :middleware [m/fun-mode framerate/mode])
+(defn page []
+  [:div
+   (sketch/component
+    :size [800 600]
+    :setup setup
+    :update update-state
+    :draw draw
+    :middleware [m/fun-mode framerate/mode])
+   [:div.contained.explanation
+    [ui-controls]]])
+
+(sketch/definition intertwined
+  {:created-at "2021-10-23"
+   :tags #{:deterministic}
+   :type :quil}
+  (ctrl/mount page "sketch-host"))
