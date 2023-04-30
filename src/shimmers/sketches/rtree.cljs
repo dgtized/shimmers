1e59b9243b0e18556c2f359281cdb9ba013df80c
Charles Comstock <dgtized@gmail.com>
2023-04-29 22:22:32 -0500

    Convert rtree to sketch/component

--- src/shimmers/sketches/rtree.cljs
+++ src/shimmers/sketches/rtree.cljs
@@ -80,12 +80,19 @@
      ;; Debug output on hit path and mouse location
      (debug/display defo))))
 
-(sketch/defquil rtree
-  :created-at "2021-10-09"
-  :tags #{:datastructures}
-  :on-mount (fn [] (ctrl/mount ui-controls))
-  :size [800 600]
-  :setup setup
-  :update update-state
-  :draw draw
-  :middleware [m/fun-mode framerate/mode])
+(defn page []
+  [:div
+   (sketch/component
+    :size [800 600]
+    :setup setup
+    :update update-state
+    :draw draw
+    :middleware [m/fun-mode framerate/mode])
+   [:div.contained.explanation
+    [ui-controls]]])
+
+(sketch/definition rtree
+  {:created-at "2021-10-09"
+   :tags #{:datastructures}
+   :type :quil}
+  (ctrl/mount page "sketch-host"))
