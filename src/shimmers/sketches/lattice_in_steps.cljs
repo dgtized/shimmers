9d71431ae49642cccf60d24e028855211e78c922
Charles Comstock <dgtized@gmail.com>
2023-05-07 10:55:20 -0500

    convert lattice-in-steps to sketch/component

--- src/shimmers/sketches/lattice_in_steps.cljs
+++ src/shimmers/sketches/lattice_in_steps.cljs
@@ -4,6 +4,7 @@
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.geometry :as geometry]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.core :as g]
@@ -45,10 +46,16 @@
   (doseq [[p q] edges]
     (q/line p q)))
 
-(sketch/defquil lattice-in-steps
-  :created-at "2021-05-29"
-  :size [800 600]
-  :setup setup
-  :update update-state
-  :draw draw
-  :middleware [m/fun-mode framerate/mode])
+(defn page []
+  (sketch/component
+   :size [800 600]
+   :setup setup
+   :update update-state
+   :draw draw
+   :middleware [m/fun-mode framerate/mode]))
+
+(sketch/definition lattice-in-steps
+  {:created-at "2021-05-29"
+   :tags #{}
+   :type :quil}
+  (ctrl/mount page))
