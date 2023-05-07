4ca65e082a6a33aaaa00a3254caf6327b4f739a8
Charles Comstock <dgtized@gmail.com>
2023-05-07 10:38:19 -0500

    Convert substrate to sketch/component

--- src/shimmers/sketches/substrate.cljs
+++ src/shimmers/sketches/substrate.cljs
@@ -9,6 +9,7 @@
    [shimmers.algorithm.polygon-detection :as poly-detect]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]))
@@ -78,11 +79,16 @@
   (doseq [{:keys [start position]} cracks]
     (q/line start position)))
 
-(sketch/defquil substrate
-  :created-at "2021-01-21"
-  :tags #{:deterministic}
-  :size [800 600]
-  :setup setup
-  :update update-state
-  :draw draw
-  :middleware [m/fun-mode framerate/mode])
+(defn page []
+  (sketch/component
+   :size [800 600]
+   :setup setup
+   :update update-state
+   :draw draw
+   :middleware [m/fun-mode framerate/mode]))
+
+(sketch/definition substrate
+  {:created-at "2021-01-21"
+   :tags #{:deterministic}
+   :type :quil}
+  (ctrl/mount page))
