9a4df9e78c838cca156a00ace7e2dbf2c75fd1c5
Charles Comstock <dgtized@gmail.com>
2023-05-07 19:42:58 -0500

    Convert bubbles to sketch/component

--- src/shimmers/sketches/bubbles.cljs
+++ src/shimmers/sketches/bubbles.cljs
@@ -4,6 +4,7 @@
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.probability :as p]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.circle :as tc]
@@ -67,10 +68,16 @@
   (doseq [bubble bubbles]
     (cq/circle bubble)))
 
-(sketch/defquil bubbles
-  :created-at "2021-02-02"
-  :size [800 600]
-  :setup setup
-  :update update-state
-  :draw draw
-  :middleware [m/fun-mode framerate/mode])
+(defn page []
+  (sketch/component
+   :size [800 600]
+   :setup setup
+   :update update-state
+   :draw draw
+   :middleware [m/fun-mode framerate/mode]))
+
+(sketch/definition bubbles
+  {:created-at "2021-02-02"
+   :tags #{}
+   :type :quil}
+  (ctrl/mount page))
