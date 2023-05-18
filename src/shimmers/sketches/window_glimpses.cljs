4a8d44c33e0b6b5b65efcc1198fe222e764e7880
Charles Comstock <dgtized@gmail.com>
2023-05-18 02:04:01 -0500

    generate circles and rectangles

--- src/shimmers/sketches/window_glimpses.cljs
+++ src/shimmers/sketches/window_glimpses.cljs
@@ -6,16 +6,17 @@
    [shimmers.math.geometry.collisions :as collide]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
+   [thi.ng.geom.circle :as gc]
+   [thi.ng.geom.core :as g]
    [thi.ng.geom.rect :as rect]
-   [thi.ng.geom.vector :as gv]
-   [thi.ng.geom.core :as g]))
+   [thi.ng.geom.vector :as gv]))
 
 (def width 800)
 (def height 600)
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
-(defn generate-box [{[width height] :size} existing]
+(defn gen-box [{[width height] :size} existing]
   (let [w (dr/random-int (* 0.04 width) (int (* 0.44 width)))
         h (dr/random-int (* 0.04 height) (int (* 0.44 height)))
         box (rect/rect (dr/random-int 0 (- width w))
@@ -25,11 +26,25 @@
       existing
       (conj existing box))))
 
-(defn shapes [bounds n]
+(defn gen-circle [{[width height] :size} existing]
+  (let [r (dr/random-int (int (* 0.04 (min width height)))
+                         (int (* 0.44 (min width height))))
+        circle (gc/circle (dr/random-int r (- width r))
+                          (dr/random-int r (- height r))
+                          r)]
+    (if (some (fn [s] (collide/overlaps? (g/scale-size circle 1.05) s)) existing)
+      existing
+      (conj existing circle))))
+
+(defn generate [bounds f n]
   (->> []
-       (iterate (partial generate-box bounds))
+       (iterate (partial f bounds))
        (take-while (fn [s] (< (count s) n)))
        last))
+
+(defn shapes [bounds n]
+  (concat (generate bounds gen-box 16)
+          (generate bounds gen-circle 16)))
 
 (defn scene []
   (csvg/svg-timed {:width width
