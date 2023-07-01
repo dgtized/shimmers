23e307c1eafd85b279e767794b53c27cb20dc8da
Charles Comstock <dgtized@gmail.com>
2023-07-01 18:33:26 -0500

    Construct connecting lines from points not as center out strips

--- src/shimmers/sketches/spiderwebs.cljs
+++ src/shimmers/sketches/spiderwebs.cljs
@@ -20,19 +20,28 @@
 (defn shapes []
   (let [center (rv 0.5 0.5)
         radius (* 0.4 height)
+        n-points (dr/random-int 15 23)
         points (mapv (fn [p] (tm/+ p (dr/jitter (* 0.08 radius))))
-                     (g/vertices (gc/circle center radius) (dr/random-int 15 23)))
+                     (g/vertices (gc/circle center radius) n-points))
         center-r 0.02
-        radial-lines (map (fn [p] (gl/line2 (tm/mix center p center-r) p)) points)
         center-circle (gp/polygon2 (map #(tm/mix center % center-r) points))
         spiral (->> (dr/gaussian-range 0.0015 0.0015)
                     (drop-while #(< % center-r))
                     (map (fn [point r] (tm/mix center point r))
-                         (cycle points))
-                    gl/linestrip2)]
+                         (cycle points)))
+
+        radial-lines
+        (->> spiral
+             (map-indexed vector)
+             (reduce (fn [lines [i p]]
+                       (update lines (mod i n-points)
+                               (fnil (fn [pts] (conj pts p)) [])))
+                     {})
+             vals
+             (map gl/linestrip2))]
     (conj radial-lines
           center-circle
-          spiral)))
+          (gl/linestrip2 spiral))))
 
 (defn scene []
   (csvg/svg-timed {:width width
