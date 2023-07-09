73d444ad53518279868b939a6cea39bf6f8aff2e
Charles Comstock <dgtized@gmail.com>
2023-07-09 16:45:58 -0500

    increase chance of rectangles on top

--- src/shimmers/sketches/window_glimpses.cljs
+++ src/shimmers/sketches/window_glimpses.cljs
@@ -239,7 +239,7 @@
 
         boxes (generate bounds (partial gen-box {:affine (dr/chance 0.33)}) 20)
         circles (swap-triangles (generate bounds gen-circle 18) (dr/random-int 5))
-        [windows shapes] (if (dr/chance 0.8)
+        [windows shapes] (if (dr/chance 0.85)
                            [boxes circles]
                            [circles boxes])
         shapes
