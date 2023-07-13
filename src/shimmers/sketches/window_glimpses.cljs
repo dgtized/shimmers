a73aee4a8982856173862a4a47f0f33b5ddbb7f8
Charles Comstock <dgtized@gmail.com>
2023-07-13 14:37:37 -0500

    slightly increase margin to reduce chance of overlapping shadows

--- src/shimmers/sketches/window_glimpses.cljs
+++ src/shimmers/sketches/window_glimpses.cljs
@@ -74,7 +74,7 @@
               (geometry/rotate-around-centroid poly (dr/gaussian 0.0 0.08))
               poly)]
     (if (collide/bounded? bounds box)
-      (distinct-shape 1.12 existing box)
+      (distinct-shape 1.15 existing box)
       existing)))
 
 (defn gen-circle [{[width height] :size} existing]
@@ -82,7 +82,7 @@
         circle (gc/circle (dr/random-int r (- width r))
                           (dr/random-int r (- height r))
                           r)]
-    (distinct-shape 1.08 existing circle)))
+    (distinct-shape 1.1 existing circle)))
 
 (defn generate [bounds f n]
   (->> []
