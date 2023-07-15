b63ef82cf8b28715f87c628e27729ef6d1f285d1
Charles Comstock <dgtized@gmail.com>
2023-07-15 16:17:40 -0500

    Add jitter biased to perpendicular to the line and adjust ragged edges

--- src/shimmers/sketches/three_lines.cljs
+++ src/shimmers/sketches/three_lines.cljs
@@ -29,6 +29,13 @@
                     1.0)]
     (g/scale-size line scale)))
 
+(defn perpindicular-jitter
+  [{[p q] :points :as line} angle amount]
+  (let [offset (-> (tm/- q p)
+                   (g/rotate (+ tm/HALF_PI angle))
+                   (tm/normalize amount))]
+    (g/translate line offset)))
+
 (defn lines []
   (let [top (gl/line2 (rv (dr/random -0.2 0.4) (dr/random -0.2 0.1))
                       (rv (dr/random 0.2 0.6) (dr/random 0.6 0.9)))
@@ -50,13 +57,13 @@
 
 (defn draw-line [{[p q] :points}]
   (for [_ (range 60)]
-    (-> (gl/line2 (tm/mix p q (dr/gaussian 0.02 0.05))
-                  (tm/mix p q (dr/gaussian 0.98 0.05)))
+    (-> (gl/line2 (tm/mix p q (dr/gaussian 0.02 0.03))
+                  (tm/mix p q (dr/gaussian 0.98 0.03)))
+        (perpindicular-jitter (dr/gaussian 0.0 0.2) (dr/gaussian 0.0 14.0))
         (rotate-around-point (dr/gaussian 0.5 0.2) (dr/gaussian 0.0 0.02))
-        (g/translate (dr/jitter (dr/gaussian 10.0 6.0)))
         (vary-meta assoc
                    :stroke-linecap (dr/weighted {"round" 1.0 "square" 1.5})
-                   :stroke-width (tm/clamp (dr/gaussian 4.0 6.0) 0.6 12.0)
+                   :stroke-width (tm/clamp (dr/gaussian 2.5 7.0) 0.6 12.0)
                    :stroke (grey (tm/clamp01 (dr/gaussian 0.02 0.125)))))))
 
 (defn shapes []
