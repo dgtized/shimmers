3325ebf095d4192c5d422ea00409750bd351eb9d
Charles Comstock <dgtized@gmail.com>
2023-08-31 22:38:06 -0500

    Add some more elipticals and slow up the background rewrite

--- src/shimmers/sketches/eliptics.cljs
+++ src/shimmers/sketches/eliptics.cljs
@@ -17,14 +17,22 @@
   (assoc state :t (* 0.001 (q/millis))))
 
 (defn draw [{:keys [t]}]
-  (q/background 1.0)
+  (q/background 1.0 0.1)
   (q/no-fill)
   (let [p1 (v/+polar (cq/rel-vec 0.5 0.5) (cq/rel-h 0.1) (* t 1))
         p2 (v/+polar p1 (cq/rel-h 0.1) (* t (/ 5 3)))
-        p3 (v/+polar p2 (cq/rel-h 0.025) (* t (/ 7 2)))]
+        p3 (v/+polar p2 (cq/rel-h 0.025) (* t (/ 7 2)))
+        p2' (v/+polar p1
+                      (* (cq/rel-h 0.1) (Math/sin (* t (/ 5 7))))
+                      (* t (/ 11 3)))
+        p3' (v/+polar p2'
+                      (* (cq/rel-h 0.05) (Math/sin (* t (/ 3 5))))
+                      (* t (/ 13 7)))]
     (cq/circle p1 2.0)
     (cq/circle p2 3.0)
-    (cq/circle p3 (cq/rel-h 0.1))))
+    (cq/circle p3 (cq/rel-h 0.1))
+    (cq/circle p2' 4.0)
+    (cq/circle p3' (cq/rel-h 0.08))))
 
 (defn page []
   [:div
