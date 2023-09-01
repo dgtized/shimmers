8bbb7a16af35c2bec8da2cf6dfdd0171bd4f8b37
Charles Comstock <dgtized@gmail.com>
2023-08-31 22:44:43 -0500

    sometimes add some jitter on the big circles

--- src/shimmers/sketches/eliptics.cljs
+++ src/shimmers/sketches/eliptics.cljs
@@ -5,8 +5,11 @@
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.equations :as eq]
    [shimmers.math.vector :as v]
-   [shimmers.sketch :as sketch :include-macros true]))
+   [shimmers.sketch :as sketch :include-macros true]
+   [thi.ng.math.core :as tm]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
@@ -17,7 +20,7 @@
   (assoc state :t (* 0.001 (q/millis))))
 
 (defn draw [{:keys [t]}]
-  (q/background 1.0 0.1)
+  (q/background 1.0 0.15)
   (q/no-fill)
   (let [p1 (v/+polar (cq/rel-vec 0.5 0.5) (cq/rel-h 0.1) (* t 1))
         p2 (v/+polar p1 (cq/rel-h 0.1) (* t (/ 5 3)))
@@ -27,12 +30,14 @@
                       (* t (/ 11 3)))
         p3' (v/+polar p2'
                       (* (cq/rel-h 0.05) (Math/sin (* t (/ 3 5))))
-                      (* t (/ 13 7)))]
+                      (* t (/ 13 7)))
+        j3  (dr/jitter (* 5 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (* t (/ 4 7))))))
+        j3' (dr/jitter (* 4 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (* t (/ 5 11))))))]
     (cq/circle p1 2.0)
     (cq/circle p2 3.0)
-    (cq/circle p3 (cq/rel-h 0.1))
+    (cq/circle (tm/+ p3 j3) (cq/rel-h 0.1))
     (cq/circle p2' 4.0)
-    (cq/circle p3' (cq/rel-h 0.08))))
+    (cq/circle (tm/+ p3' j3') (cq/rel-h 0.08))))
 
 (defn page []
   [:div
