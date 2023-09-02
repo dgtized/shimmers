f1ffcdff19cb06447aa020265802e48980c8ba11
Charles Comstock <dgtized@gmail.com>
2023-09-02 18:00:57 -0500

    randomize the rate for each eliptic rotation

--- src/shimmers/sketches/eliptics.cljs
+++ src/shimmers/sketches/eliptics.cljs
@@ -5,34 +5,47 @@
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.core :as sm]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.math.equations :as eq]
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.math.core :as tm]))
 
+(defn generate-time-factors [n]
+  (let [primes (sm/primes-between 2 50)]
+    (->> (repeatedly (fn []
+                       (let [num (dr/rand-nth primes)]
+                         (/ num (dr/rand-nth (remove #{num} primes))))))
+         (filter (fn [x] (<= 0.1 x 2.5)))
+         (take n)
+         vec)))
+
+(comment (generate-time-factors 8))
+
 (defn setup []
   (q/color-mode :hsl 1.0)
   (q/ellipse-mode :radius)
-  {:t 0.0})
+  {:t 0.0
+   :rates (generate-time-factors 9)})
 
 (defn update-state [state]
   (assoc state :t (* 0.001 (q/millis))))
 
-(defn draw [{:keys [t]}]
+(defn draw [{:keys [t rates]}]
   (q/background 1.0 0.15)
   (q/no-fill)
-  (let [p1 (v/+polar (cq/rel-vec 0.5 0.5) (cq/rel-h 0.1) (* t 1))
-        p2 (v/+polar p1 (cq/rel-h 0.1) (* t (/ 5 3)))
-        p3 (v/+polar p2 (cq/rel-h 0.025) (* t (/ 7 2)))
+  (let [p1 (v/+polar (cq/rel-vec 0.5 0.5) (cq/rel-h 0.1) (nth rates 0))
+        p2 (v/+polar p1 (cq/rel-h 0.1) (* t (nth rates 1)))
+        p3 (v/+polar p2 (cq/rel-h 0.025) (* t (nth rates 2)))
         p2' (v/+polar p1
-                      (* (cq/rel-h 0.1) (Math/sin (* t (/ 5 7))))
-                      (* t (/ 11 3)))
+                      (* (cq/rel-h 0.1) (Math/sin (* t (nth rates 3))))
+                      (* t (nth rates 4)))
         p3' (v/+polar p2'
-                      (* (cq/rel-h 0.05) (Math/sin (* t (/ 3 5))))
-                      (* t (/ 13 7)))
-        j3  (dr/jitter (* 5 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (* t (/ 4 7))))))
-        j3' (dr/jitter (* 4 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (* t (/ 5 11))))))]
+                      (* (cq/rel-h 0.05) (Math/sin (* t (nth rates 5))))
+                      (* t (nth rates 6)))
+        j3  (dr/jitter (* 5 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (* t (nth rates 7))))))
+        j3' (dr/jitter (* 4 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (* t (nth rates 8))))))]
     (cq/circle p1 2.0)
     (cq/circle p2 3.0)
     (cq/circle (tm/+ p3 j3) (cq/rel-h 0.1))
