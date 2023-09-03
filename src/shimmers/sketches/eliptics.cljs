d373636ceb0d4e2e576ee1ff17e091d3bd5e64bd
Charles Comstock <dgtized@gmail.com>
2023-09-03 03:41:30 -0500

    add some size adjustments and change base-rate for jitter and expand/contract

--- src/shimmers/sketches/eliptics.cljs
+++ src/shimmers/sketches/eliptics.cljs
@@ -26,8 +26,9 @@
 (defn setup []
   (q/color-mode :hsl 1.0)
   (q/ellipse-mode :radius)
-  (let [rates (generate-time-factors 9)
-        basis (mapv * (generate-time-factors 9) (repeat eq/TAU))
+  (let [n 11
+        rates (generate-time-factors n)
+        basis (mapv * (generate-time-factors n) (repeat eq/TAU))
         rate-base (fn [i t] (+ (nth basis i) (* t (nth rates i))))]
     {:t 0.0
      :rates rates
@@ -49,13 +50,15 @@
         p3' (v/+polar p2'
                       (* (cq/rel-h 0.05) (Math/sin (rate-base 5 t)))
                       (rate-base 6 t))
-        j3  (dr/jitter (* 5 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (rate-base 7 t)))))
-        j3' (dr/jitter (* 4 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (rate-base 8 t)))))]
+        j3  (dr/jitter (* 5 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (rate-base 7 (* (/ 2 3) t))))))
+        j3' (dr/jitter (* 4 (tm/smoothstep* 0.35 0.9 (eq/unit-cos (rate-base 8 (* (/ 2 3) t))))))]
     (cq/circle p1 2.0)
     (cq/circle p2 3.0)
-    (cq/circle (tm/+ p3 j3) (cq/rel-h 0.1))
+    (cq/circle (tm/+ p3 j3)
+               (tm/mix* (cq/rel-h 0.05) (cq/rel-h 0.12) (eq/unit-sin (rate-base 9 (* (/ 1 3) t)))))
     (cq/circle p2' 4.0)
-    (cq/circle (tm/+ p3' j3') (cq/rel-h 0.08))))
+    (cq/circle (tm/+ p3' j3')
+               (tm/mix* (cq/rel-h 0.09) (cq/rel-h 0.14) (eq/unit-sin (rate-base 10 (* (/ 1 3) t)))))))
 
 (defn page []
   [:div
