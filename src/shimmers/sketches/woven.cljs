1bfeb47bd298117d2fe2d1551e09a716890b1b70
Charles Comstock <dgtized@gmail.com>
2023-10-25 11:16:36 -0500

    tweak offset change, seed movement and radius

--- src/shimmers/sketches/woven.cljs
+++ src/shimmers/sketches/woven.cljs
@@ -15,7 +15,7 @@
 
 (defn gen-threads [n pass]
   (for [t (range n)]
-    (let [o (+ (/ (float (inc t)) (inc n)) (dr/gaussian 0.0 (/ 0.33 (inc n))))]
+    (let [o (+ (/ (float (inc t)) (inc n)) (dr/gaussian 0.0 (/ 0.4 (inc n))))]
       (if (even? pass)
         [(cq/rel-vec -0.1 o) 0.0 v/right]
         [(cq/rel-vec o -0.1) 0.0 v/up]))))
@@ -39,7 +39,7 @@
 
 (defn update-pos [t dt [pos rot dir]]
   [(tm/+ pos (tm/* dir (* 0.1 dt)))
-   (+ rot (* (* 0.005 (Math/sin (* 0.001 t))) dt))
+   (+ rot (* (* 0.005 (Math/sin (* 0.0015 t))) dt))
    dir])
 
 (defn update-state [{:keys [triangles screen pass t] :as state}]
@@ -60,7 +60,7 @@
     (let [n (apply q/noise (tm/* (tm/+ seed pos) 0.005))]
       (q/fill 0.0 (+ 0.0005 (* n 0.005)))
       (q/stroke 0.0 (+ 0.005 (* n 0.08))))
-    (let [triangle (triangle/inscribed-equilateral pos (cq/rel-h (/ 0.5 (inc n))) rot)]
+    (let [triangle (triangle/inscribed-equilateral pos (cq/rel-h (/ 0.33 (inc n))) rot)]
       (cq/draw-triangle (g/vertices triangle)))))
 
 (defn page []
