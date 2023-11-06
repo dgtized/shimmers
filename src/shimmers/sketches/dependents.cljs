1392bcc0bda7ee9c16d2c6b6a6f4b2fab8bf3385
Charles Comstock <dgtized@gmail.com>
2023-11-05 23:44:23 -0600

    switch v0 velocity to a gaussian distribution

--- src/shimmers/sketches/dependents.cljs
+++ src/shimmers/sketches/dependents.cljs
@@ -25,7 +25,7 @@
 (defn gen-particles [n bounds]
   (for [pos (rp/random-points bounds n)]
     {:pos pos
-     :vel (v/polar (dr/random 0.05 0.25) (dr/random-tau))}))
+     :vel (v/polar (tm/clamp (dr/gaussian 0.2 0.05) 0.05 0.5) (dr/random-tau))}))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
