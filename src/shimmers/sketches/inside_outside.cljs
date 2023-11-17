2b45edb111078ea095170a3ca5761cb9b5dd2ed0
Charles Comstock <dgtized@gmail.com>
2023-11-17 04:14:41 -0600

    add some spirals to the mixture

--- src/shimmers/sketches/inside_outside.cljs
+++ src/shimmers/sketches/inside_outside.cljs
@@ -4,6 +4,8 @@
    [shimmers.common.svg :as csvg :include-macros true]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.equations :as eq]
+   [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.circle :as gc]
@@ -16,22 +18,45 @@
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
+(defn generate-circles [bounds R]
+  (reduce
+   (fn [circles pct]
+     (let [radius (* R pct)]
+       (pack/circle-pack
+        circles
+        {:bounds bounds
+         :candidates (min (int (/ 20 pct)) 800)
+         :gen-circle
+         (let [r (dr/random-int (* 0.75 radius) (* 1.25 radius))]
+           (fn [] (gc/circle (g/random-point-inside bounds) r)))
+         :spacing (max (* 0.005 R) (* 0.1 radius))})))
+   []
+   [0.2 0.12 0.1 0.08 0.06 0.04 0.02 0.01]))
+
+(defn spiral [circle dr dt min-r]
+  (->> {:circle circle :t (dr/random-tau) :r (:r circle)}
+       (iterate
+        (fn [{:keys [circle t r]}]
+          (let [r' (* dr r)
+                radial-pt (g/point-at circle (/ t eq/TAU))]
+            {:circle (gc/circle (v/+polar radial-pt r' (- t Math/PI)) r')
+             :t (+ t dt)
+             :r r'})))
+       (take-while (fn [{:keys [r]}] (> r min-r)))
+       (map :circle)))
+
 (defn shapes []
   (let [bounds (g/scale-size (rect/rect 0 0 width height) 0.98)
-        R (min (g/width bounds) (g/height bounds))]
-    (reduce
-     (fn [circles pct]
-       (let [radius (* R pct)]
-         (pack/circle-pack
-          circles
-          {:bounds bounds
-           :candidates (min (int (/ 20 pct)) 800)
-           :gen-circle
-           (let [r (dr/random-int (* 0.75 radius) (* 1.25 radius))]
-             (fn [] (gc/circle (g/random-point-inside bounds) r)))
-           :spacing (max (* 0.005 R) (* 0.1 radius))})))
-     []
-     [0.2 0.12 0.1 0.08 0.06 0.04 0.02 0.01])))
+        R (min (g/width bounds) (g/height bounds))
+        circles (sort-by :r > (generate-circles bounds R))]
+    (into [circles]
+          (mapcat (fn [c]
+                    (spiral c
+                            (dr/random 0.9 0.95)
+                            (* (dr/rand-nth [-1 1])
+                               (dr/random 0.2 0.8))
+                            (* 0.002 R)))
+                  (take 23 (dr/shuffle (take 45 circles)))))))
 
 (defn scene []
   (csvg/svg-timed {:width width
