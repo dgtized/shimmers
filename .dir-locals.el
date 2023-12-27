1b04ef4d66417f679686f3b925f61da7a6374e99
Charles Comstock <dgtized@gmail.com>
2023-12-26 23:28:31 -0600

    disable showing help banner at start

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,7 @@
   (cider-clojure-cli-aliases . "dev")
   (cider-default-cljs-repl . figwheel-main)
   (cider-figwheel-main-default-options . ":dev")
+  (cider-repl-display-help-banner . nil)
   (eval . ((lambda () (when (not (featurep 'shimmers))
                    (let ((shimmers-file (expand-file-name "shimmers.el" default-directory)))
                      (when (file-exists-p shimmers-file)
