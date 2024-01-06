76a79159a9dc7fcb0a4d982a8952c593c4624e84
Charles Comstock <dgtized@gmail.com>
2024-01-06 12:27:48 -0600

    improve graph generation using shortest all-pairs set

--- src/shimmers/sketches/graph_traces.cljs
+++ src/shimmers/sketches/graph_traces.cljs
@@ -5,19 +5,21 @@
    [shimmers.algorithm.random-points :as rp]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.common.sequence :as cs]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.sketch :as sketch :include-macros true]
-   [shimmers.math.deterministic-random :as dr]))
+   [thi.ng.geom.core :as g]))
 
 (defn new-graph []
   (let [nodes (zipmap (map (comp keyword char) (range 65 (+ 65 26)))
                       (repeatedly 11 #(rp/sample-point-inside (cq/screen-rect 0.85))))]
     {:nodes nodes
-     :edges (repeatedly 17
-                        (fn []
-                          (let [p (dr/rand-nth (keys nodes))]
-                            {:p p
-                             :q (dr/rand-nth (seq (disj (set (keys nodes)) p)))})))}))
+     :edges (for [[p q] (->> nodes
+                             keys
+                             cs/all-pairs
+                             (sort-by (fn [[p q]] (g/dist (get nodes p) (get nodes q))))
+                             (take 33))]
+              {:p p :q q})}))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
