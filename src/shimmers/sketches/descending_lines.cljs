5380716cfc9d3225b18e0d8cf55c050e0a43d9b1
Charles Comstock <dgtized@gmail.com>
2024-01-10 23:07:04 -0600

    modulate top and bottom height

--- src/shimmers/sketches/descending_lines.cljs
+++ src/shimmers/sketches/descending_lines.cljs
@@ -6,6 +6,7 @@
    [shimmers.common.quil :as cq]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.equations :as eq]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.math.core :as tm]))
 
@@ -21,18 +22,21 @@
         (update :t + dt))))
 
 (defn draw [{:keys [t dt]}]
-  (q/stroke (/ (mod t 7.0) 7.0)
+  (q/stroke (dr/gaussian (/ (mod t 7.0) 7.0) 0.01)
             (dr/gaussian 0.5 0.1)
             (dr/gaussian 0.5 0.1)
-            0.75)
-  (q/stroke-weight (tm/clamp (dr/gaussian 1.5 0.5) 0.2 100.0))
-  (let [x (cq/rel-w (/ (mod t 5.0) 5.0))]
-    (q/line (+ x (dr/gaussian 0.0 4.0)) 0
-            (+ x (dr/gaussian 0.0 4.0)) (q/height)))
-  (q/stroke 1.0)
-  (let [x (cq/rel-w (/ (mod (- t) 11.0) 11.0))]
-    (q/line (+ x (dr/gaussian 0.0 4.0)) 0
-            (+ x (dr/gaussian 0.0 4.0)) (q/height))))
+            1.0)
+  (q/stroke-weight (tm/clamp (dr/gaussian 1.5 0.66) 0.2 100.0))
+  (let [top (cq/rel-h (+ (* 0.1 (eq/unit-sin (* tm/PHI t)))
+                         (* 0.1 (eq/unit-cos (/ t tm/PHI)))))
+        bottom (- (q/height) top)]
+    (let [x (cq/rel-w (/ (mod t 5.0) 5.0))]
+      (q/line (+ x (dr/gaussian 0.0 4.0)) top
+              (+ x (dr/gaussian 0.0 4.0)) bottom))
+    (q/stroke (tm/smoothstep* 0.25 0.55 (eq/unit-cos (* 0.65 tm/PHI t))))
+    (let [x (cq/rel-w (/ (mod (- t) 11.0) 11.0))]
+      (q/line (+ x (dr/gaussian 0.0 2.0)) top
+              (+ x (dr/gaussian 0.0 2.0)) bottom))))
 
 (defn page []
   [:div
