d45fe17b3f990f205164e288901d5e1e6bb5d712
Charles Comstock <dgtized@gmail.com>
2024-01-16 00:24:46 -0600

    adjust algorithm so it more closely matches description

--- src/shimmers/sketches/splitting_polygons.cljs
+++ src/shimmers/sketches/splitting_polygons.cljs
@@ -1,51 +1,45 @@
 (ns shimmers.sketches.splitting-polygons
   (:require
+   [shimmers.algorithm.lines :as lines]
    [shimmers.common.svg :as csvg :include-macros true]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
    [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.core :as g]
+   [thi.ng.geom.line :as gl]
    [thi.ng.geom.rect :as rect]
    [thi.ng.geom.vector :as gv]
-   [shimmers.algorithm.lines :as lines]
-   [thi.ng.geom.line :as gl]
-   [thi.ng.geom.utils :as gu]))
+   [thi.ng.math.core :as tm]))
 
 (def width 800)
 (def height 600)
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
-(gu/arc-length-index [(gv/vec2 0 0) (gv/vec2 1 5) (gv/vec2 3 1)])
-
 (defn split-polygon [polygon]
-  (let [{:keys [a b]} (meta polygon)
-        [ca cb] (sort [a b])
-        dt (dr/random 0.1)]
-    (for [poly (lines/cut-polygon polygon (gl/line2 (g/point-at polygon ca)
-                                                    (g/point-at polygon cb)))]
-      (vary-meta poly assoc
-                 :a (mod (+ ca dt) 1.0)
-                 :b (mod (+ cb dt) 1.0)))))
+  (let [py (/ (:y (g/centroid polygon)) height)
+        t (if (< py 0.5)
+            (dr/gaussian 0.2 0.02)
+            (dr/gaussian 0.5 0.02))
+        line (apply min-key tm/mag-squared
+                    (repeatedly 8 (fn []
+                                    (let [a (dr/random)]
+                                      (gl/line2 (g/point-at polygon a)
+                                                (g/point-at polygon (mod (+ a t) 1.0)))))))]
+    (lines/cut-polygon polygon line)))
 
 (defn recursive-split [polygons]
-  (iterate (fn [polys]
-             (dr/mapcat-random-sample
-              (fn [polygon]
-                (if (> (gu/arc-length (g/vertices polygon)) 20)
-                  1.0 0.0))
-              split-polygon
-              polys))
+  (iterate (fn [polygons]
+             (let [xs (sort-by (fn [polygon] (get (meta polygon) :arc-length 0)) polygons)]
+               (into (butlast xs)
+                     (for [child (split-polygon (last xs))]
+                       (vary-meta child assoc :arc-length (g/circumference child))))))
            polygons))
 
 (defn shapes []
-  (let [t (dr/random)]
-    (nth (recursive-split
-          [(vary-meta (g/scale-size (rect/rect 0 0 width height) 0.9)
-                      assoc
-                      :a t
-                      :b (mod (+ t (dr/gaussian 0.5 0.1)) 1.0))]) 20)))
+  (nth (recursive-split [(g/scale-size (rect/rect 0 0 width height) 0.9)])
+       4000))
 
 (defn scene []
   (csvg/svg-timed {:width width
