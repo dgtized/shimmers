f770c7e4953b4163843fdfd590121f15d4695b1e
Charles Comstock <dgtized@gmail.com>
2024-01-22 23:41:20 -0600

    extract skip-draw

--- src/shimmers/sketches/harmonograph.cljs
+++ src/shimmers/sketches/harmonograph.cljs
@@ -17,18 +17,21 @@
   {:t 0})
 
 (defn update-state [state]
-  (update state :t inc))
+  (update state :t + 1))
+
+(defn skip-draw [k t]
+  (when (> (Math/sin (+ (* (/ 7 5) t) (* 3 (Math/sin (* (/ 1 5) t))))) 0)
+    (q/stroke-weight (+ 1.2 (* 0.6 (Math/sin (* 2 t)))))
+    (apply q/point (v/polar (* 0.3 (q/height) k) (* (/ 1 6) t)))))
 
 (defn draw [{:keys [t]}]
   (dotimes [i 1000]
     (let [t (+ (* 4.0 t) (/ i 200))
           k (dampen 0.09 (* 0.01 t))]
       (q/with-translation
-          [(tm/+ (cq/rel-vec 0.5 0.5)
-                 (v/polar (* 0.15 (q/height) k) (* (/ 1 4) t)))]
-        (when (> (Math/sin (+ (* (/ 7 5) t) (* 3 (Math/sin (* (/ 1 5) t))))) 0)
-          (q/stroke-weight (+ 1.2 (* 0.6 (Math/sin (* 2 t)))))
-          (apply q/point (v/polar (* 0.3 (q/height) k) (* (/ 1 6) t))))))))
+        [(tm/+ (cq/rel-vec 0.5 0.5)
+               (v/polar (* 0.15 (q/height) k) (* (/ 1 4) t)))]
+        (skip-draw k t)))))
 
 (defn page []
   [:div
