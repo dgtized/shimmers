84441cb827bd1bc2999a448152b0d35993d2244b
Charles Comstock <dgtized@gmail.com>
2024-02-08 22:12:54 -0600

    modernize brush-sweep

--- src/shimmers/sketches/brush_sweep.cljs
+++ src/shimmers/sketches/brush_sweep.cljs
@@ -2,9 +2,11 @@
   (:require
    [quil.core :as q :include-macros true]
    [quil.middleware :as m]
+   [shimmers.algorithm.random-points :as rp]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
@@ -17,7 +19,7 @@
   {:t 0})
 
 (defn update-state [state]
-  (update state :t (fn [t] (mod (+ t (tm/random 0.05)) 100.0))))
+  (update state :t (fn [t] (mod (+ t (dr/random 0.05)) 100.0))))
 
 (defn hairs [line]
   (let [hair (-> (gt/triangle2 [0 0] [3 7] [7 5])
@@ -25,28 +27,28 @@
                  (g/scale-size 5))]
     (->> (if true
            (g/sample-uniform line 20 true)
-           (repeatedly 64 #(g/random-point line)))
+           (repeatedly 64 #(rp/sample-point-inside line)))
          (map (fn [p] (-> hair
-                         (g/rotate (* tm/TWO_PI (tm/random)))
+                         (g/rotate (dr/random-tau))
                          (g/translate p)))))))
 
 (defn draw [{:keys [t]}]
   (q/stroke-weight 0.5)
   (q/stroke 0 0.01)
   (let [t' (/ t 100)]
-    (q/fill (tm/mix* 0.5 0.9 (+ t' (tm/random -0.05 0.05)))
+    (q/fill (tm/mix* 0.5 0.9 (+ t' (dr/random -0.05 0.05)))
 
             (tm/mix-circular 0.5 0.8 t')
-            (tm/random 0.5 0.7)
+            (dr/random 0.5 0.7)
             0.02)
     (let [pos (cq/rel-vec (+ -0.1
                              (/ (tm/smoothstep* 0.3 0.4 t') 30)
                              (/ (- (tm/step* 0.6 t')) 30))
                           (* 0.01 t))
-          slope (* (q/random 0.1) t')
+          slope (* (dr/random 0.1) t')
           line (gl/line2 pos
                          (gv/vec2 (q/width) (+ (* slope (q/width)) (:y pos))))]
-      (doseq [hair (shuffle (hairs line))]
+      (doseq [hair (dr/shuffle (hairs line))]
         (cq/draw-polygon hair)))))
 
 (defn page []
