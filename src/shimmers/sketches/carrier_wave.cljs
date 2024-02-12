3e7e3245be5fededa8288f1cd81a8b9895a8d2dd
Charles Comstock <dgtized@gmail.com>
2024-02-12 14:14:23 -0600

    one loop but extract draw-path

--- src/shimmers/sketches/carrier_wave.cljs
+++ src/shimmers/sketches/carrier_wave.cljs
@@ -12,7 +12,8 @@
 (defn setup [_cv]
   {:a (dr/random-int 1 4)
    :b (dr/random-int 1 4)
-   :c (dr/random-int 1 4)})
+   :c (dr/random-int 1 4)
+   :k (dr/random)})
 
 (defn update-state [state _dims _ms]
   state)
@@ -20,24 +21,31 @@
 (defn R [f p a s]
   (v/polar a (* eq/TAU (+ (* s f) p))))
 
-(defn generate-points [[a b c] center height t]
+(defn generate-points [[a b c k] center radius t]
   (for [s (tm/norm-range 512)]
     (-> (tm/+ (R (+ a (eq/unit-cos (* 0.2 t))) (eq/unit-cos (* 0.5 t)) 1.2 s)
               (R (+ b (eq/unit-sin (* -0.2 t))) (eq/unit-sin (* -0.5 t)) 1.1 s)
-              (R (+ c (Math/tan (* 0.15 t))) (dr/gaussian 0.0 0.005) 1.0 s))
-        (tm/* (* 0.1 height))
-        (tm/+ center ))))
+              (R (+ c (Math/tan (+ k (* 0.15 t)))) (dr/gaussian 0.0 0.005) 1.0 s))
+        (tm/* radius)
+        (tm/+ center))))
 
-(defn draw [{:keys [a b c]} ctx [width height] ms]
+(defn draw-path [ctx points]
+  (.beginPath ctx)
+  (canvas/move-to ctx (first points))
+  (doseq [p (rest points)]
+    (canvas/line-to ctx p))
+  (canvas/stroke ctx)
+  ctx)
+
+(defn draw [{:keys [a b c k]} ctx [width height] ms]
   (let [t (* 0.001 ms)
-        center (gv/vec2 (* 0.5 width) (* 0.5 height))
-        points (generate-points [a b c] center height t)]
-    (canvas/line-width ctx 2.0)
-    (.beginPath ctx)
-    (canvas/move-to ctx (first points))
-    (doseq [p (rest points)]
-      (canvas/line-to ctx p))
-    (canvas/stroke ctx)
+        center (gv/vec2 (* 0.5 width) (* 0.5 height))]
+    (canvas/line-width ctx 1.0)
+    (draw-path ctx (generate-points [a b c (* 1 k)] center (* 0.133 height) t))
+    ;; (canvas/line-width ctx 1.0)
+    ;; (draw-path ctx (generate-points [c a b (* 2 k)] center (* 0.099 height) t))
+    ;; (canvas/line-width ctx 2.0)
+    ;; (draw-path ctx (generate-points [b c a (* 3 k)] center (* 0.033 height) t))
     ctx))
 
 (defn page []
