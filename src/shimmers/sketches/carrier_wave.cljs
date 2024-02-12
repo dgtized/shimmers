31acb8123f65c3335599361c69107081fba12830
Charles Comstock <dgtized@gmail.com>
2024-02-12 14:54:19 -0600

    add an extra term and show the params

--- src/shimmers/sketches/carrier_wave.cljs
+++ src/shimmers/sketches/carrier_wave.cljs
@@ -10,10 +10,10 @@
    [thi.ng.math.core :as tm]))
 
 (defn setup [_cv]
-  {:a (dr/random-int 1 4)
-   :b (dr/random-int 1 4)
-   :c (dr/random-int 1 4)
-   :k (dr/random)})
+  (let [s {:params (repeatedly 4 #(dr/random-int -3 3))
+           :k (dr/random)}]
+    (println s)
+    s))
 
 (defn update-state [state _dims _ms]
   state)
@@ -21,11 +21,12 @@
 (defn R [f p a s]
   (v/polar a (* eq/TAU (+ (* s f) p))))
 
-(defn generate-points [[a b c k] center radius t]
+(defn generate-points [[a b c d k] center radius t]
   (for [s (tm/norm-range 512)]
-    (-> (tm/+ (R (+ a (eq/unit-cos (* 0.2 t))) (eq/unit-cos (* 0.5 t)) 1.2 s)
-              (R (+ b (eq/unit-sin (* -0.2 t))) (eq/unit-sin (* -0.5 t)) 1.1 s)
-              (R (+ c (Math/tan (+ k (* 0.15 t)))) (dr/gaussian 0.0 0.005) 1.0 s))
+    (-> (tm/+ (R (+ a (eq/unit-cos (* 0.2 t))) (Math/cos (* 0.5 t)) 1.2 s)
+              (R (+ b (eq/unit-sin (* -0.2 t))) (Math/sin (* -0.5 t)) 1.1 s)
+              (tm/+ (R (+ c (eq/unit-sin (* 0.8 t))) (* 2 k (Math/sin (* 0.25 t))) 0.5 s)
+                    (R (+ d (Math/tan (+ k (* 0.15 t)))) (dr/gaussian 0.0 0.005) 1.0 s)))
         (tm/* radius)
         (tm/+ center))))
 
@@ -37,11 +38,12 @@
   (canvas/stroke ctx)
   ctx)
 
-(defn draw [{:keys [a b c k]} ctx [width height] ms]
+(defn draw [{:keys [params k]} ctx [width height] ms]
   (let [t (* 0.001 ms)
-        center (gv/vec2 (* 0.5 width) (* 0.5 height))]
+        center (gv/vec2 (* 0.5 width) (* 0.5 height))
+        [a b c d] params]
     (canvas/line-width ctx 1.0)
-    (draw-path ctx (generate-points [a b c (* 1 k)] center (* 0.133 height) t))
+    (draw-path ctx (generate-points [a b c d (* 1 k)] center (* 0.133 height) t))
     ;; (canvas/line-width ctx 1.0)
     ;; (draw-path ctx (generate-points [c a b (* 2 k)] center (* 0.099 height) t))
     ;; (canvas/line-width ctx 2.0)
