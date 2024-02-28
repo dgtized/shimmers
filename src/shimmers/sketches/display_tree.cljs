728e7258a06baa189cc41a2ace8d24fc53a687e3
Charles Comstock <dgtized@gmail.com>
2024-02-28 00:03:33 -0600

    add a hex animation to the mix

--- src/shimmers/sketches/display_tree.cljs
+++ src/shimmers/sketches/display_tree.cljs
@@ -425,6 +425,27 @@
         (q/end-shape)
         (q/stroke-weight 1.0)))))
 
+(defn make-hex [bounds]
+  (let [scale (* 0.45 (Math/pow 0.98 (dr/random-int 5)))
+        radius (* scale (min (g/width bounds) (g/height bounds)))
+        spin-phase (dr/random-tau)
+        o-phase (dr/random-tau)]
+    (fn [pos rotation t f]
+      (let [center (rotated-centroid bounds pos rotation)
+            outline (pos? (Math/sin (+ (* 0.4 t) o-phase (* 1.5 (Math/sin (* 0.6 t))))))
+            r radius
+            spin (* eq/TAU (Math/sin (+ (* 0.13 t) spin-phase (Math/sin (* 0.17 t)))))]
+        (if outline
+          (do (q/no-stroke)
+              (q/fill (- 1.0 f)))
+          (do (q/stroke (- 1.0 f))
+              (q/no-fill)
+              (q/stroke-weight 5.0)))
+        (cq/draw-shape
+         (for [s (butlast (tm/norm-range 6))]
+           (v/+polar center r (+ spin (* s eq/TAU)))))
+        (q/stroke-weight 1.0)))))
+
 (defn add-animation
   [{:keys [children display] :as screen} t]
   (cond (seq children)
@@ -443,6 +464,7 @@
                                     [make-helix 2.0]
                                     [make-tunnel 1.0]
                                     ;; [make-circle-blob 1.0]
+                                    [make-hex 1.0]
                                     ])]
           (assoc screen :animation (mk-anim display t)))))
 
