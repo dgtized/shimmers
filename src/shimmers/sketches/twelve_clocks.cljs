3a02a132fc7e1b34cd33ccdd021c3aa6f2270d77
Charles Comstock <dgtized@gmail.com>
2024-04-20 00:14:31 -0500

    vary each arm by it's step

--- src/shimmers/sketches/twelve_clocks.cljs
+++ src/shimmers/sketches/twelve_clocks.cljs
@@ -17,16 +17,19 @@
 (defn update-state [state]
   state)
 
-(defn draw-hand [angle radius time]
+(defn draw-hand [step angle radius time]
   (let [a (v/+polar (cq/rel-vec 0.5 0.5) radius angle)
         b (v/+polar a (/ radius 6)
                     (+ angle time
+                       (* 0.1 step)
                        (math/sin (- angle (* 0.1 time)))))
         c (v/+polar b (/ radius 9)
                     (- angle (* 1.5 time)
+                       (* 0.2 step (eq/unit-sin (+ angle time)))
                        (Math/sin (+ angle (* 0.3 time)))))
         d (v/+polar c (/ radius 21)
                     (+ angle (* 2.5 time)
+                       (* 0.3 step (eq/unit-sin time))
                        (Math/sin (+ angle (* 0.5 time)))))]
     (q/stroke-weight (+ 1.5 (math/sin (+ angle (* 0.1 time)))))
     (q/line a b)
@@ -38,9 +41,10 @@
   (let [time (/ (q/millis) 1000.0)
         radius (cq/rel-h 0.35)
         dist (* 20 (Math/sin (* 0.2 time)))]
-    (doseq [step (range 5)
+    (doseq [step (range 7)
             hand (range 12)]
-      (draw-hand (* eq/TAU (+ (/ hand 12.0) (math/sin (* 0.025 time))))
+      (draw-hand step
+                 (* eq/TAU (+ (/ hand 12.0) (math/sin (* 0.025 time))))
                  (- radius (* dist step)) time))))
 
 (defn page []
