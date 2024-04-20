9d18371d6b60010b20414bd6dc0d32bd393f0cf1
Charles Comstock <dgtized@gmail.com>
2024-04-20 03:11:18 -0500

    more modulation of undulation

--- src/shimmers/sketches/twelve_clocks.cljs
+++ src/shimmers/sketches/twelve_clocks.cljs
@@ -8,7 +8,8 @@
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.equations :as eq]
    [shimmers.math.vector :as v]
-   [shimmers.sketch :as sketch :include-macros true]))
+   [shimmers.sketch :as sketch :include-macros true]
+   [thi.ng.math.core :as tm]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
@@ -18,18 +19,19 @@
   state)
 
 (defn draw-hand [step angle radius time]
-  (let [a (v/+polar (cq/rel-vec 0.5 0.5)
+  (let [r (/ radius (+ 6 (* 2 (math/sin (+ angle (* 0.25 time))))))
+        a (v/+polar (cq/rel-vec 0.5 0.5)
                     radius
                     (+ angle (* 0.1 (eq/unit-sin (+ (* 0.25 step) (* 0.075 time))))))
-        b (v/+polar a (/ radius 6)
+        b (v/+polar a r
                     (+ angle time
                        (* 0.1 step)
                        (math/sin (- angle (* 0.1 time)))))
-        c (v/+polar b (/ radius 9)
+        c (v/+polar b (/ r tm/PHI)
                     (- angle (* 1.5 time)
                        (* 0.2 step (eq/unit-sin (+ angle time)))
                        (Math/sin (+ angle (* 0.3 time)))))
-        d (v/+polar c (/ radius 21)
+        d (v/+polar c (/ r (math/pow tm/PHI 2))
                     (+ angle (* 2.5 time)
                        (* 0.3 step (eq/unit-sin time))
                        (Math/sin (+ angle (* 0.5 time)))))]
@@ -39,12 +41,12 @@
     (q/line c d)))
 
 (defn draw [_state]
-  (q/background 1.0)
+  (q/background 1.0 0.66)
   (let [time (/ (q/millis) 1000.0)
         radius (cq/rel-h 0.35)
         dist (* 20 (Math/sin (* 0.2 time)))]
-    (doseq [step (range 7)
-            hand (range 12)]
+    (doseq [hand (range 12)
+            step (range 7)]
       (draw-hand step
                  (* eq/TAU (+ (/ hand 12.0) (math/sin (* 0.025 time))))
                  (- radius (* dist step)) time))))
