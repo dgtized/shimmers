cd4f0d512f0e7b61ccdec374b5021272570e8ba8
Charles Comstock <dgtized@gmail.com>
2024-04-28 11:42:43 -0500

    parameter tweaks and pass through n-points in params map

--- src/shimmers/sketches/helix.cljs
+++ src/shimmers/sketches/helix.cljs
@@ -32,8 +32,8 @@
      (dr/gaussian 0.0 0.05)))
 
 ;; TODO use lcm to find single loop?
-(defn harmonic-loop [center radius {:keys [a a-osc b b-osc c c-osc]}]
-  (for [s (tm/norm-range 1024)]
+(defn harmonic-loop [center radius {:keys [n-points a a-osc b b-osc c c-osc]}]
+  (for [s (tm/norm-range n-points)]
     (-> (gv/vec2)
         (tm/+ (R a (* 0.2 (O a-osc 0 s)) 0.65 s))
         (tm/+ (R b (* 0.6 (O b-osc 0 (- 1.0 s))) 0.30 s))
@@ -41,12 +41,12 @@
         (tm/* radius)
         (tm/+ center))))
 
-(defn shapes [{:keys [angle-osc size-osc] :as params}]
+(defn shapes [{:keys [n-points angle-osc size-osc] :as params}]
   (let [pts (harmonic-loop (rv 0.5 0.5) (* 0.48 height) params)
         r (* 0.0033 height)]
     (for [[[p q] s] (mapv vector
                           (partition 2 1 pts)
-                          (tm/norm-range 1024))]
+                          (tm/norm-range n-points))]
       (let [z (tm/- q p)
             angle (* Math/PI (O angle-osc 0 s))
             delta (tm/normalize (g/rotate z (+ angle (/ eq/TAU 4)))
@@ -72,13 +72,13 @@
   (let [a (dr/weighted-by #(- 11 %) (range 1 11))
         b (dr/weighted-by #(- 13 %) (range 1 13))
         params
-        {:a a
-         :b b
-         :c (* (min (abs a) (abs b))
-               (dr/random-int 1 5))
-         :a-osc (dr/random-int -3 3)
-         :b-osc (dr/random-int -5 5)
-         :c-osc (dr/random-int -11 11)
+        {:n-points 2048
+         :a (* (dr/weighted {-1 1 1 6}) a)
+         :b (* (dr/weighted {-1 6 1 1}) b)
+         :c (* (min a b) (dr/random-int 1 6))
+         :a-osc (* (dr/random-sign) (dr/random-int 4))
+         :b-osc (* (dr/random-sign) (dr/random-int 6))
+         :c-osc (* (dr/random-sign) (dr/random-int 12))
          :angle-osc (dr/random-int 6 24)
          :size-osc (dr/random-int 4 12)}]
     (fn []
