6b3a86b8b8bc8f69973590498a4415830ab42b37
Charles Comstock <dgtized@gmail.com>
2024-05-07 20:01:50 -0500

    consolidate parameters into two arrays

--- src/shimmers/sketches/remaining.cljs
+++ src/shimmers/sketches/remaining.cljs
@@ -43,26 +43,28 @@
   (let [[a b c] (abc)]
     {:params
      {:n-points 200
-      :a a
-      :b b
-      :c c
-      :a-osc (* (dr/random-sign) (dr/random-int 5))
-      :b-osc (* (dr/random-sign) (dr/random-int 7))
-      :c-osc (* (dr/random-sign) (dr/random-int 13))
+      :freqs
+      [a b c]
+      :freqs-osc
+      [(* (dr/random-sign) (dr/random-int 5))
+       (* (dr/random-sign) (dr/random-int 7))
+       (* (dr/random-sign) (dr/random-int 13))]
       :phase 0
       :angle-osc (dr/random-int 6 32)
       :size-osc (dr/random-int 6 32)
       :radius-osc (dr/random-int 6 32)}}))
 
-(defn harmonic-loop [center radius {:keys [n-points a a-osc b b-osc c c-osc phase]}]
-  (for [s (tm/norm-range n-points)]
-    (let [s (+ s phase)]
-      (-> (gv/vec2)
-          (tm/+ (R a (* 0.2 (O a-osc 0 s)) 0.65 s))
-          (tm/+ (R b (* 0.6 (O b-osc 0 (- 1.0 s))) 0.30 s))
-          (tm/+ (R c (* 0.8 (O c-osc 0 s)) 0.05 s))
-          (tm/* radius)
-          (tm/+ center)))))
+(defn harmonic-loop [center radius {:keys [n-points phase freqs freqs-osc]}]
+  (let [[a b c] freqs
+        [a-osc b-osc c-osc] freqs-osc]
+    (for [s (tm/norm-range n-points)]
+      (let [s (+ s phase)]
+        (-> (gv/vec2)
+            (tm/+ (R a (* 0.2 (O a-osc 0 s)) 0.65 s))
+            (tm/+ (R b (* 0.6 (O b-osc 0 (- 1.0 s))) 0.30 s))
+            (tm/+ (R c (* 0.8 (O c-osc 0 s)) 0.05 s))
+            (tm/* radius)
+            (tm/+ center))))))
 
 (defn update-state [state]
   (update-in state [:params :phase] +
