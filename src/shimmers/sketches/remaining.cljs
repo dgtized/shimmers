d09e5402839c3c8b0375a9a444374cabc514c5db
Charles Comstock <dgtized@gmail.com>
2024-05-07 20:02:59 -0500

    Reduce likelyhood of 1.0 multiple for c

--- src/shimmers/sketches/remaining.cljs
+++ src/shimmers/sketches/remaining.cljs
@@ -30,10 +30,13 @@
 (defn abc []
   (let [a (dr/weighted-by #(Math/pow 0.9 %) (range 1 11))
         b (dr/weighted-by #(sm/lcm % a) (range 1 13))
+        lcm (sm/lcm a b)
+        gcd (sm/gcd a b)
         c (dr/weighted
            [[(* (min a b) (dr/random-int 1 6)) 2.0]
-            [(sm/lcm a b) 1.0]
-            [(sm/gcd a b) 1.0]])]
+            [lcm (if (> lcm 1.0) 1.0 0.0)]
+            [gcd (if (> gcd 1.0) 1.0 0.0)]
+            [(dr/random-int 1 10) 1.0]])]
     (tm/* (gv/vec3 a b c)
           (gv/vec3 (dr/rand-nth (mc/selections [-1 1] 3))))))
 
