dc7bbdd901f8cfacb9a0734e16039f4ba1683fc2
Charles Comstock <dgtized@gmail.com>
2024-05-09 22:06:30 -0500

    transition sphere to use clojure.math

--- src/shimmers/sketches/sphere.cljs
+++ src/shimmers/sketches/sphere.cljs
@@ -1,5 +1,6 @@
 (ns shimmers.sketches.sphere
   (:require
+   [clojure.math :as math]
    [quil.core :as q :include-macros true]
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
@@ -10,11 +11,11 @@
 (defn sphere-points [vertice-count]
   (for [j (range vertice-count)
         i (range vertice-count)
-        :let [longitude (tm/map-interval i 0 vertice-count (- Math/PI) Math/PI)
-              latitude (tm/map-interval j 0 vertice-count (- (/ Math/PI 2)) (/ Math/PI 2))]]
-    [(* (Math/sin longitude) (Math/cos latitude))
-     (* (Math/sin longitude) (Math/sin latitude))
-     (Math/cos longitude)]))
+        :let [longitude (tm/map-interval i 0 vertice-count (- math/PI) math/PI)
+              latitude (tm/map-interval j 0 vertice-count (- (/ math/PI 2)) (/ math/PI 2))]]
+    [(* (math/sin longitude) (math/cos latitude))
+     (* (math/sin longitude) (math/sin latitude))
+     (math/cos longitude)]))
 
 (def ordering-fns
   [shuffle
