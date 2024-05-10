d69219ef13d8e29fdabce751eebd0fff8b05b8de
Charles Comstock <dgtized@gmail.com>
2024-05-09 22:17:47 -0500

    convert reflections to use clojure.math

--- src/shimmers/sketches/reflections.cljs
+++ src/shimmers/sketches/reflections.cljs
@@ -1,5 +1,6 @@
 (ns shimmers.sketches.reflections
   (:require
+   [clojure.math :as math]
    [quil.core :as q :include-macros true]
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
@@ -29,18 +30,18 @@
 
 (defn shapes [t]
   (let [dt 0.01]
-    [(gc/circle (+ 0.5 (* 0.2 (Math/sin (* 3 t)))) 0.4 0.2)
-     (gc/circle (+ 0.5 (* 0.2 (Math/sin (* 3 (+ t dt))))) 0.4 0.2)
+    [(gc/circle (+ 0.5 (* 0.2 (math/sin (* 3 t)))) 0.4 0.2)
+     (gc/circle (+ 0.5 (* 0.2 (math/sin (* 3 (+ t dt))))) 0.4 0.2)
      (g/translate (gc/circle 0.5 0.5 0.12) (v/polar 0.3 (* 4 (- t dt))))
      (g/translate (gc/circle 0.5 0.5 0.11) (v/polar 0.3 (* 4 t)))
      (g/translate (gc/circle 0.5 0.5 0.10) (v/polar 0.3 (* 4 (+ t dt))))
-     (gl/line2 (gv/vec2 0.1 0.1) (gv/vec2 0.9 (+ 0.3 (* 0.2 (Math/sin (* 2.5 t))))))
-     (gl/line2 (gv/vec2 0.1 0.1) (gv/vec2 0.9 (+ 0.3 (* 0.2 (Math/sin (* 2.5 (+ t dt)))))))
+     (gl/line2 (gv/vec2 0.1 0.1) (gv/vec2 0.9 (+ 0.3 (* 0.2 (math/sin (* 2.5 t))))))
+     (gl/line2 (gv/vec2 0.1 0.1) (gv/vec2 0.9 (+ 0.3 (* 0.2 (math/sin (* 2.5 (+ t dt)))))))
      (gl/line2 (gv/vec2 0.1 0.5) (gv/vec2 0.9 0.9))
-     (gl/line2 (gv/vec2 (+ 0.5 (* 0.35 (Math/sin (* 0.3 t)))) 0.1)
-               (gv/vec2 (+ 0.5 (* 0.3 (Math/sin (* 0.4 t)))) 0.9))
-     (gl/line2 (gv/vec2 (+ 0.5 (* 0.35 (Math/sin (* 0.3 (- t dt))))) 0.1)
-               (gv/vec2 (+ 0.5 (* 0.3 (Math/sin (* 0.4 (- t dt))))) 0.9))]))
+     (gl/line2 (gv/vec2 (+ 0.5 (* 0.35 (math/sin (* 0.3 t)))) 0.1)
+               (gv/vec2 (+ 0.5 (* 0.3 (math/sin (* 0.4 t)))) 0.9))
+     (gl/line2 (gv/vec2 (+ 0.5 (* 0.35 (math/sin (* 0.3 (- t dt))))) 0.1)
+               (gv/vec2 (+ 0.5 (* 0.3 (math/sin (* 0.4 (- t dt))))) 0.9))]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
