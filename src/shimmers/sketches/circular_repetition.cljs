fa1a5d8e4c35f0967f75cdd63e6b952f687da488
Charles Comstock <dgtized@gmail.com>
2024-05-31 12:03:29 -0500

    tweaks and add debug showing the seed values

--- src/shimmers/sketches/circular_repetition.cljs
+++ src/shimmers/sketches/circular_repetition.cljs
@@ -8,28 +8,34 @@
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
-   [thi.ng.geom.vector :as gv]))
+   [shimmers.common.ui.debug :as debug]
+   [thi.ng.strf.core :as f]))
+
+(defonce defo (debug/state {}))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
   (q/ellipse-mode :radius)
-  (q/stroke-weight 0.5)
-  {:weights (repeatedly 3 #(dr/random -0.1 0.1))
-   :osc (repeatedly 3 #(dr/random -0.2 0.2))})
+  (q/stroke-weight 0.25)
+  (let [params
+        {:weights (repeatedly 3 #(dr/random -1.0 1.0))
+         :osc (repeatedly 3 #(dr/random -0.5 0.5))}]
+    (reset! defo params)
+    params))
 
 (defn draw [{[a b c] :weights [o1 o2 o3] :osc}]
   (q/background 1.0)
-  (q/with-translation [(cq/rel-vec 0.0 0.0)]
-    (let [center (gv/vec2 0 0)
-          r (cq/rel-h 0.4)
-          t (/ (q/millis) 2000.0)]
-      (dotimes [i 360]
-        (q/push-matrix)
-        (q/rotate-z (+ (* c (+ t i)) (* 2 (Math/sin (* o3 t)))))
-        (q/rotate-y (+ (* b (+ t i)) (* 2 (math/sin (* o2 t)))))
-        (q/rotate-x (+ (* a (+ t i)) (* 2 (math/sin (* o1 t)))))
-        (cq/circle center r)
-        (q/pop-matrix)))))
+  (let [r (cq/rel-h 0.4)
+        t (/ (q/millis) 2000.0)]
+    (dotimes [i 360]
+      (q/push-matrix)
+      (let [angle i
+            scale 0.075]
+        (q/rotate-z (+ (* scale c (+ t angle)) (* 2 (math/sin (* o3 t)))))
+        (q/rotate-y (+ (* scale b (+ t angle)) (* 2 (math/sin (* o2 t)))))
+        (q/rotate-x (+ (* scale a (+ t angle)) (* 2 (math/sin (* o1 t))))))
+      (q/ellipse 0 0 r r)
+      (q/pop-matrix))))
 
 (defn page []
   [sketch/with-explanation
@@ -39,12 +45,18 @@
      :setup setup
      :draw draw
      :middleware [m/fun-mode framerate/mode])
-   [:div.readable-width
-    "Inpired by " [:a {:href "https://junkiyoshi.com/openframeworks20240527/"} "Junki Yoshi - overlap circles"]
-    " but using random phase modulation instead of pure noise."]])
+   [:div
+    [:div.readable-width
+     "Inpired by " [:a {:href "https://junkiyoshi.com/openframeworks20240527/"} "Junki Yoshi - overlap circles"]
+     " but using random phase modulation instead of pure noise."]
+    [:div
+     (let [{:keys [weights osc]} @defo]
+       (for [[axis w o] (mapv vector [:x :y :z] weights osc)]
+         [:code {:key axis}
+          (f/format [(f/pad-left 2 " ") " "(f/float 2) " " (f/float 2)] (name axis) w o) [:br]]))]]])
 
 (sketch/definition circular-repetition
-  {:created-at "2024-05-31"
-   :tags #{}
-   :type :quil}
+    {:created-at "2024-05-31"
+     :tags #{}
+     :type :quil}
   (ctrl/mount page))
