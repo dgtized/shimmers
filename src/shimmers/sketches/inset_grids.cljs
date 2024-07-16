2cc7e3b883b23e03e818817410df10b001ca45a8
Charles Comstock <dgtized@gmail.com>
2024-07-16 13:58:34 -0500

    thread through the area limit

--- src/shimmers/sketches/inset_grids.cljs
+++ src/shimmers/sketches/inset_grids.cljs
@@ -13,7 +13,7 @@
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
-(def min-area (* 0.002 width height))
+(def min-area (* 0.0015 width height))
 
 (defn splits [{[w h] :size}]
   (if (> w h)
@@ -22,16 +22,16 @@
     {:rows (dr/random-int 2 11)
      :cols (dr/random-int 1 5)}))
 
-(defn division [bounds]
+(defn division [limit bounds]
   (let [rect (g/scale-size bounds 0.975)]
-    (if (< (g/area bounds) min-area)
+    (if (< (g/area rect) limit)
       [rect]
       (mapcat (fn [r]
-                (into [r] (division (g/scale-size r 0.98))))
+                (into [r] (division limit (g/scale-size r 0.975))))
               (g/subdivide rect (splits rect))))))
 
 (defn shapes [bounds]
-  (division bounds))
+  (division min-area bounds))
 
 (defn scene [{:keys [scene-id]}]
   (csvg/svg-timed
@@ -39,8 +39,8 @@
      :width width
      :height height
      :stroke "black"
-     :fill "white"
-     :stroke-width 0.5}
+     :fill "none"
+     :stroke-width 0.66}
     (shapes (csvg/screen width height))))
 
 (sketch/definition inset-grids
