a3cacfd6e8e7013ef0224c11dae1a3c7135b4235
Charles Comstock <dgtized@gmail.com>
2024-07-29 20:06:01 -0500

    more tweaks and parameter sizes

--- src/shimmers/sketches/designed_imperfections.cljs
+++ src/shimmers/sketches/designed_imperfections.cljs
@@ -1,5 +1,6 @@
 (ns shimmers.sketches.designed-imperfections
   (:require
+   [clojure.math :as math]
    [shimmers.common.svg :as csvg :include-macros true]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.common.ui.svg :as usvg]
@@ -20,13 +21,13 @@
 (defn shapes []
   (let [seed (dr/noise-seed)]
     (for [box (g/subdivide (csvg/screen width height)
-                           {:cols (/ width 10) :rows (/ height 10)})]
+                           {:cols (/ width 6) :rows (/ height 6)})]
       (let [pos (g/centroid box)
-            n (dr/noise-at-point seed 0.015 pos)
-            damp (/ 1.0 (Math/pow 1.004 (g/dist pos (rv 0.5 0.5))))]
+            n (dr/noise-at-point seed 0.011 pos)
+            damp (/ 1.0 (Math/pow 1.006 (g/dist pos (rv 0.5 0.5))))]
         (gc/circle (tm/+ pos
-                         (v/polar (* 8.0 damp) (* eq/TAU n)))
-                   (* 2.0 (+ 1.0 (* 0.5 (Math/sin (* eq/TAU n)) damp))))))))
+                         (v/polar (* 10.0 damp) (* eq/TAU n)))
+                   (+ 2.0 (* 0.8 damp (math/sin (* eq/TAU (- 1.0 n))))))))))
 
 (defn scene [{:keys [scene-id]}]
   (csvg/svg-timed {:id scene-id
