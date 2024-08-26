7c7203d4be96e1da8ca4e5887697f2a78c649a8e
Charles Comstock <dgtized@gmail.com>
2024-08-25 20:27:14 -0500

    and a larger range

--- src/shimmers/scratch/modulus.cljs
+++ src/shimmers/scratch/modulus.cljs
@@ -4,8 +4,8 @@
   (for [x (range 10)]
     [x (mod x 5) (mod (- 5 x) 5)])
 
-  (for [s (range 3)
-        p (range 2 5)]
+  (for [s (range 4)
+        p (range 2 6)]
     [[s p]
-     (mapv (fn [x] (mod (+ s (mod x p)) 5)) (range 5))
-     (mapv (fn [x] (mod (+ s (mod (- p x) p)) 5)) (range 5))]))
+     (mapv (fn [x] (mod (+ s (mod x p)) 8)) (range 8))
+     (mapv (fn [x] (mod (+ s (mod (- p 1 x) p)) 8)) (range 8))]))
