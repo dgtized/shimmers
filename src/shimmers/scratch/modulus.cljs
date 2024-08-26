daa8432f8b68a71b6494ce24718bb377c78aec2a
Charles Comstock <dgtized@gmail.com>
2024-08-25 20:24:29 -0500

    experiments with reverse

--- src/shimmers/scratch/modulus.cljs
+++ src/shimmers/scratch/modulus.cljs
@@ -2,4 +2,10 @@
 
 (comment
   (for [x (range 10)]
-    [x (mod x 5) (mod (- 5 x) 5)]))
+    [x (mod x 5) (mod (- 5 x) 5)])
+
+  (for [s (range 3)
+        p (range 2 5)]
+    [[s p]
+     (mapv (fn [x] (mod (+ s (mod x p)) 5)) (range 5))
+     (mapv (fn [x] (mod (+ s (mod (- p x) p)) 5)) (range 5))]))
