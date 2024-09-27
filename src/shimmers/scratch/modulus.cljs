dcd676648976050e7a6f3cf0ebc73659e607e9c1
Charles Comstock <dgtized@gmail.com>
2024-09-27 18:38:01 -0500

    check math on updown cyclic modulation

--- src/shimmers/scratch/modulus.cljs
+++ src/shimmers/scratch/modulus.cljs
@@ -8,4 +8,11 @@
         p (range 2 6)]
     [[s p]
      (mapv (fn [x] (mod (+ s (mod x p)) 8)) (range 8))
-     (mapv (fn [x] (mod (+ s (mod (- p 1 x) p)) 8)) (range 8))]))
+     (mapv (fn [x] (mod (+ s (mod (- p 1 x) p)) 8)) (range 8))])
+
+  ;; updown cyclic
+  (for [n [3 4 5]]
+    (for [x (range 16)]
+      [(mod x n)
+       (let [m (mod x (- (* 2 n) 2))]
+         (if (< m n) m (- (* 2 n) 2 m)))])))
