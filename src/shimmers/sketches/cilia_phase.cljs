2b71824e74a2c60fc0c79b3a0ee423485b538f1e
Charles Comstock <dgtized@gmail.com>
2024-10-25 17:19:09 -0500

    adjust amplitude modulation for outer paths

--- src/shimmers/sketches/cilia_phase.cljs
+++ src/shimmers/sketches/cilia_phase.cljs
@@ -37,24 +37,24 @@
     (screen-space fx x)))
 
 ;; How to avoid intersecting cilia?
-(defn cilias [screen-space fx spx]
-  (for [x (range 0 1 0.005)]
+(defn cilias [screen-space fx spx c-amp]
+  (for [x (range 0 1 0.004)]
     (let [pt (screen-space fx x)
           pt' (screen-space fx (+ x 0.0001))
-          len (+ 12 (* 8 (spx x)))
+          len (* height (+ c-amp (* 0.75 c-amp (spx x))))
           offset (tm/normalize (g/rotate (tm/- pt' pt) (* eq/TAU 0.25)) len)]
       (gl/line2 (tm/+ pt offset) (tm/- pt offset)))))
 
 (defn shapes []
   (let [fx (spline-fx)
         spx (spline-fx)]
-    (mapcat (fn [[y amp]]
+    (mapcat (fn [[y amp c-amp]]
               (let [screen (partial screen-space y amp)
                     spline-pts (base-spline screen fx)
-                    cilia (cilias screen fx spx)]
+                    cilia (cilias screen fx spx c-amp)]
                 (concat [(csvg/path (csvg/segmented-path spline-pts))]
                         cilia)))
-            [[0.33 0.2] [0.5 0.25] [0.66 0.2]])))
+            [[0.33 0.2 0.015] [0.5 0.25 0.025] [0.66 0.2 0.015]])))
 
 (defn scene [{:keys [scene-id]}]
   (csvg/svg-timed
