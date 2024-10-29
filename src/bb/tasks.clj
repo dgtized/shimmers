089a53096b3111c6bad93adffbe1cd941ce842cb
Charles Comstock <dgtized@gmail.com>
2024-10-29 00:30:46 -0500

    calculate filename in original release-base

--- src/bb/tasks.clj
+++ src/bb/tasks.clj
@@ -38,8 +38,8 @@
 (defn build-static-site [& opts]
   (let [{:keys [build-dir from to release]} (default-opts opts)
         js-dir (str to "/js")
-        release-base (fs/strip-ext release)
-        release-glob (str "/" (fs/file-name release-base) "*")]
+        release-base (fs/filename (fs/strip-ext release))
+        release-glob (str "/" release-base "*")]
     (fs/delete-tree to)
     (println "Creating" to "from" from "with javascript")
     (fs/copy-tree from to)
