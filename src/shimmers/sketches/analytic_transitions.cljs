234ebe86140c3de459ed8465c82616d9a5b06d65
Charles Comstock <dgtized@gmail.com>
2025-01-26 23:00:58 -0600

    add fancy single field transitions to the animation

--- src/shimmers/sketches/analytic_transitions.cljs
+++ src/shimmers/sketches/analytic_transitions.cljs
@@ -40,7 +40,7 @@
     (for [theta (range 0 revolutions (/ revolutions samples))]
       (reduce (fn [p f]
                 (tm/+ p (f (+ (math/exp (* 0.05 (+ base theta)))
-                              (mod (* 0.33 t) eq/TAU)))))
+                              (mod (* 0.1 t) eq/TAU)))))
               (gv/vec2)
               plot-fs))))
 
@@ -55,19 +55,58 @@
                              (+ (* (dr/random-int 1 6) a) (dr/gaussian 0.0 0.0075))
                              (+ (* (dr/random-int 1 6) b) (dr/gaussian 0.0 0.0075))
                              (dr/random-tau) (dr/random-tau)
-                             0.005 [])]}))
+                             0.005 [])]
+     :t (/ (q/millis) 1000.0)}))
 
-(defn update-state [state]
-  state)
+(defn remove-ended? [pendulums t]
+  (mapv (fn [{:keys [transitions] :as pendulum}]
+          (let [to-remove (filter (fn [{:keys [t1]}] (> t t1)) transitions)]
+            (-> (reduce (fn [pendulum {:keys [field fx]}]
+                          (update pendulum field fx 1.0))
+                        pendulum
+                        to-remove)
+                (update :transitions
+                        (fn [xs] (remove (fn [{:keys [t1]}] (> t t1)) xs))))))
+        pendulums))
 
-(defn draw [{:keys [pendulums]}]
+(defn new-transition [pendulum t]
+  (let [duration (dr/random 1.5 9.0)
+        field (dr/rand-nth [:px :py])
+        curr (get pendulum field)
+        target (dr/random-tau)
+        transition {:t0 t :t1 (+ t duration)
+                    :field field
+                    :fx (fn [_value pct-t]
+                          (tm/mix* curr target pct-t))}]
+    (update pendulum :transitions conj transition)))
+
+(defn run [pendulum {:keys [t0 t1 fx field]} t]
+  (let [pct-t (/ (- t t0) (- t1 t0))]
+    (update pendulum field fx pct-t)))
+
+(defn run-transitions [pendulums t]
+  (for [{:keys [transitions] :as pendulum} pendulums]
+    (reduce (fn [pendulum tx] (run pendulum tx t)) pendulum transitions)))
+
+(defn update-state [{:keys [pendulums] :as state}]
+  (let [t (/ (q/millis) 1000.0)
+        state' (update state :pendulums remove-ended? t)]
+    (-> (if (and (every? (fn [{:keys [transitions]}] (empty? transitions))
+                         pendulums)
+                 (dr/chance 0.1))
+          (let [n (count pendulums)
+                i (dr/random-int n)]
+            (update-in state' [:pendulums i] new-transition t))
+          state')
+        (assoc :t t))))
+
+(defn draw [{:keys [pendulums t] :as state}]
   (q/background 1.0)
   (q/stroke-weight 2.0)
-  (reset! defo pendulums)
-  (let [time (/ (q/millis) 3000.0)
-        size (cq/rel-h 0.45)
+  (reset! defo state)
+  (let [size (cq/rel-h 0.45)
         center (cq/rel-vec 0.5 0.5)]
-    (doseq [p (plot pendulums 6000 time)]
+    (doseq [p (plot (run-transitions pendulums t) 5000 t)]
       (let [[x y] (tm/+ center (tm/* p size))]
         (q/point x y)))))
 
@@ -79,7 +118,7 @@
      :update update-state
      :draw draw
      :middleware [m/fun-mode framerate/mode])
-   #_(debug/display defo)])
+   (debug/display defo)])
 
 (sketch/definition analytic-transitions
   {:created-at "2025-01-26"
