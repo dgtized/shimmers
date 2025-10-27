901fe0cbf42bb7c421da3e6bb50cc80449c7e1a4
Charles Comstock <dgtized@gmail.com>
2025-10-27 15:34:32 -0500

    Add lines of surrounding values

--- src/shimmers/sketches/bias_gain.cljs
+++ src/shimmers/sketches/bias_gain.cljs
@@ -7,18 +7,29 @@
    [shimmers.math.bias-gain :as mbg]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.line :as gl]
-   [thi.ng.geom.vector :as gv]))
+   [thi.ng.geom.vector :as gv]
+   [thi.ng.math.core :as tm]))
 
 (def width 800)
 (def height 600)
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
+(defn plot [s t]
+  (mapv (fn [x] (rv x (- 1.0 (mbg/bias-gain x s t))))
+        (range 0 1 0.0025)))
+
 (defn shapes [s t]
-  (let [w 0.0075
-        points (mapv (fn [x] (rv x (- 1.0 (mbg/bias-gain x s t))))
-                     (range 0 1 0.0025))]
-    [(csvg/path (csvg/segmented-path points) {})
+  (let [w 0.0075]
+    [(csvg/path (csvg/segmented-path (plot (+ s 0.25) t))
+                {:stroke "#0000ff" :stroke-width 0.5})
+     (csvg/path (csvg/segmented-path (plot (max 0.01 (- s 0.25)) t))
+                {:stroke "#0000ff" :stroke-width 0.5})
+     (csvg/path (csvg/segmented-path (plot s (tm/clamp01 (- t 0.125))))
+                {:stroke "#ff0000" :stroke-width 0.5})
+     (csvg/path (csvg/segmented-path (plot s (tm/clamp01 (+ t 0.125))))
+                {:stroke "#ff0000" :stroke-width 0.5})
+     (csvg/path (csvg/segmented-path (plot s t)) {:stroke-width 1.5})
      (csvg/group {:stroke-width 0.66}
        (cs/midsection
         (for [x (range 0 1 0.1)]
@@ -36,8 +47,8 @@
                      :width width
                      :height height
                      :stroke "black"
-                     :fill "white"
-                     :stroke-width 1.5}
+                     :fill "none"
+                     :stroke-width 0.5}
       (shapes s t))))
 
 (defn ui-controls []
