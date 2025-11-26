f2018a59ee66bcc1f541944b3e16376dc4a561bb
Charles Comstock <dgtized@gmail.com>
2025-11-26 17:34:22 -0600

    Extract routing ns from shimmers.core

--- src/shimmers/core.cljs
+++ src/shimmers/core.cljs
@@ -8,64 +8,17 @@
    [reitit.frontend :as rf]
    [reitit.frontend.controllers :as rfc]
    [reitit.frontend.easy :as rfe]
+   [shimmers.routing :as routing]
    [shimmers.sketches :as sketches]
    [shimmers.view.favicon :as favicon]
-   [shimmers.view.index :as view-index]
-   [shimmers.view.sketch :as view-sketch]
-   [spec-tools.data-spec :as ds]))
+   [shimmers.view.sketch :as view-sketch]))
 
 ;; Uncomment to see javascript source of functions at repl
 ;; (set! cljs.core/*print-fn-bodies* true)
 ;; Or just (str the-function)
 
-(defn request->sketch
-  "Lookup a sketch by name from request and annotate it with seed if available."
-  [{:keys [path query]}]
-  (-> path
-      :name
-      sketches/by-name
-      (assoc :seed (:seed query))))
-
-(defn on-event [f action]
-  (fn [request]
-    (let [sketch (request->sketch request)]
-      (when action
-        (println action "sketch" (:sketch-id sketch)))
-      (f sketch))))
-
-;; FIXME: moving this to a different namespace seems to break routing
-;; index does not load and p5 is not required somehow
-;; FIXME: handle invalid paths, re-route to index by-alphabetical
-(def routes
-  (let [on-index
-        [{:start #(favicon/start 333)
-          :stop #(favicon/stop)}]]
-    [["/" ::root]
-     ["/sketches"
-      {:name :shimmers.view.index/by-alphabetical
-       :view #(view-index/by-alphabetical (sketches/all))
-       :controllers on-index}]
-     ["/sketches-by-date"
-      {:name :shimmers.view.index/by-date
-       :view #(view-index/by-date (sketches/all))
-       :controllers on-index}]
-     ["/sketches-by-tag"
-      {:name :shimmers.view.index/by-tag
-       :view #(view-index/by-tag (sketches/all))
-       :controllers on-index}]
-     ["/sketches/:name"
-      {:name :shimmers.view.sketch/sketch-by-name
-       :view (on-event #(view-sketch/sketch-by-name % (sketches/known-names)) nil)
-       :parameters
-       {:path {:name (every-pred string? (set (sketches/known-names)))}
-        :query {(ds/opt :seed) int?}}
-       :controllers
-       [{:parameters {:path [:name] :query [:seed]}
-         :start (on-event view-sketch/start-sketch "start")
-         :stop (on-event view-sketch/stop-sketch "stop")}]}]]))
-
 (defn on-navigate [page-match new-match]
-  (if (or (nil? new-match) (= (:name (:data new-match)) ::root))
+  (if (or (nil? new-match) (= (:name (:data new-match)) :shimmers.routing/root))
     ;; default route, not sure on reitit for frontend routing
     (rfe/replace-state :shimmers.view.index/by-alphabetical)
     (swap! page-match
@@ -88,11 +41,12 @@
 
 (defonce shimmer-root (rdomc/create-root (dom/getElement "shimmer-mount")))
 (defonce !active-route (r/atom nil))
+(comment @!active-route)
 
 (defn init []
   (rfe/start!
    ;; coercion here will cause missing sketches to explode
-   (rf/router routes {:data {:coercion rss/coercion}})
+   (rf/router routing/routes {:data {:coercion rss/coercion}})
    (partial on-navigate !active-route)
    {:use-fragment true})
 
