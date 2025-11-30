fcb8d4262a5cce9dafd157aaecfc4cb734f39005
Charles Comstock <dgtized@gmail.com>
2025-11-29 21:33:43 -0600

    bunch more variation on row count and frequency

--- src/shimmers/sketches/diagonal_spaces.cljs
+++ src/shimmers/sketches/diagonal_spaces.cljs
@@ -15,21 +15,24 @@
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
-(defn row [a b slant]
-  (for [t (tm/norm-range 100)]
+(defn row [a b n slant]
+  (for [t (tm/norm-range n)]
     (if (and (<= 0.1 t 0.9) (dr/chance 0.05))
       (gl/line2 (rv (- t slant) a) (rv (+ t slant) b))
       (gl/line2 (rv t a) (rv t b)))))
 
 (defn shapes []
-  (let [gap 0.015]
-    (for [[a b] (partition 2 1 (tm/norm-range 5))]
-      (let [ga (+ a gap)
-            gb (- b gap)]
+  (let [rows (dr/weighted {5 1 7 1 9 1 11 1})]
+    (for [[a b] (partition 2 1 (tm/norm-range rows))]
+      (let [gap (* 0.05 (- b a))
+            ga (+ a gap)
+            gb (- b gap)
+            slant (dr/weighted {0.025 1 -0.025 1})
+            n (dr/weighted {100 1 125 0.5 150 1 175 0.5 200 0.5 250 0.5})]
         (svg/group {}
                    (gl/line2 (rv 0 ga) (rv 1 ga))
                    (gl/line2 (rv 0 gb) (rv 1 gb))
-                   (svg/group {} (row ga gb (dr/weighted {0.025 1 -0.025 1}))))))))
+                   (svg/group {} (row ga gb n slant)))))))
 
 (defn scene [{:keys [scene-id]}]
   (csvg/svg-timed {:id scene-id
