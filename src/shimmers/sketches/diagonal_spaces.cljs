aa5ab47943e0128162fb0d7c1839f943017f875d
Charles Comstock <dgtized@gmail.com>
2025-12-13 12:56:37 -0600

    allow priority slant over vertical

--- src/shimmers/sketches/diagonal_spaces.cljs
+++ src/shimmers/sketches/diagonal_spaces.cljs
@@ -20,15 +20,15 @@
 (defn row [a b n slant p-slant]
   (mapcat
    (fn [t]
-     (if (and (<= 0.05 t 0.95) (dr/chance (/ p-slant n)))
+     (if (and (<= 0.05 t 0.95) (dr/chance p-slant))
        [(gl/line2 (rv (- t slant) a) (rv (+ t slant) b))]
        [(gl/line2 (rv t a) (rv t b))]))
    (mapv (let [s (dr/weighted {0.5 1.0 1.5 1.0 0.66 1.0 1.33 1.0})
                t (dr/weighted {0.0 1.0 0.25 0.5 0.33 0.5
                                0.5 1.0 0.66 0.5 0.75 0.5 1.0 1.0})]
-           (dr/weighted {identity 10.0
-                         (partial ms/staircase (dr/random-int 3 15)) 1.0
-                         (fn [x] (mbg/bias-gain x s t)) 3.0}))
+           (dr/weighted {identity 20.0
+                         (partial ms/staircase (dr/random-int 3 13)) 1.0
+                         (fn [x] (mbg/bias-gain x s t)) 4.0}))
          (dr/weighted {(tm/norm-range n) 6.0
                        (dr/gaussian-range (/ 1.0 n) (/ 0.2 n) true) 1.0
                        (dr/density-range (/ 0.5 n) (/ 1.5 n) true) 1.0}))))
@@ -41,11 +41,14 @@
             ga (+ a gap)
             gb (- b gap)
             slant (* (dr/random-sign) (dr/weighted {0.025 4 0.03 1 0.015 1}))
-            n (dr/weighted {100 1 125 0.5 150 1 175 0.5 200 0.5 250 0.5})]
+            n (dr/weighted {100 1 125 0.5 150 1 175 0.5 200 0.5 250 0.5})
+            [vert diag] (dr/weighted {[6 1] 4.0 [1 4] 1.0})
+            p-slant (dr/weighted [[(/ (dr/random 1.0 16.0) n) vert]
+                                  [(- 1.0 (/ (dr/random 1.0 16.0) n)) diag]])]
         (svg/group {}
                    (gl/line2 (rv 0 ga) (rv 1 ga))
                    (gl/line2 (rv 0 gb) (rv 1 gb))
-                   (svg/group {} (row ga gb n slant (dr/random 1.0 16.0))))))))
+                   (svg/group {} (row ga gb n slant p-slant)))))))
 
 (defn scene [{:keys [scene-id]}]
   (csvg/svg-timed {:id scene-id
