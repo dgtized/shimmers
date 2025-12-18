a6cdabaebc9f625e1b8f4c65b86e0fe917a7aa26
Charles Comstock <dgtized@gmail.com>
2025-12-17 20:46:56 -0600

    split out path generator

--- src/shimmers/sketches/follow_the_path.cljs
+++ src/shimmers/sketches/follow_the_path.cljs
@@ -43,17 +43,28 @@
                      (* 0.25 eq/TAU))]
     (gl/line2 (tm/- p dp) (tm/+ p dp))))
 
+(defn gen-path [f1 f2]
+  (let [functions
+        [{:id "ramp"
+          :f (fn [x] (+ 0.5 (* (+ 0.1 (* 0.2 x))
+                              (math/sin (+ (* f1 eq/TAU x) (math/sin (* f2 eq/TAU x)))))))
+          :weight 1.0}
+         {:id "osc"
+          :f (fn [x] (+ 0.5 (* 0.3 (math/sin (+ (* f1 eq/TAU x) (math/sin (* f2 eq/TAU x)))))))
+          :weight 1.0}]]
+    (dr/weighted-by :weight functions)))
+
 (defn shapes []
   (let [s (gen-s)
         t (gen-t)
         f1 (dr/random 0.5 3)
         f2 (dr/random 0.5 4)
         invert (dr/weighted [[identity 2.0] [(fn [x] (- 1.0 x)) 1.0]])
-        path (fn [x] (rv x (+ 0.5 (* (+ 0.1 (* 0.2 x))
-                                    (math/sin (+ (* f1 eq/TAU x) (math/sin (* f2 eq/TAU x))))))))
+        {:keys [id f]} (gen-path f1 f2)
+        path (fn [x] (rv x (f x)))
         magnitude (fn [x] (* (max width height)
                             (math/sin (+ (* 0.33 f2 eq/TAU x) (math/sin (* 0.33 f1 eq/TAU x))))))]
-    (swap! defo assoc :s s :t t :f1 f1 :f2 f2)
+    (swap! defo assoc :id id :s s :t t :f1 f1 :f2 f2)
     (into [#_(gl/linestrip2 (mapv path (tm/norm-range 200)))]
           (mapv (partial perp (comp path invert) magnitude)
                 (map (bias-sweep s t)
