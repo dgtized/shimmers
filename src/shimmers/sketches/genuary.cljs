4ded91d4d194df19328e4595493e35658450771d
Charles Comstock <dgtized@gmail.com>
2026-01-05 23:40:23 -0600

    cut the space into a more reasonable region

--- src/shimmers/sketches/genuary.cljs
+++ src/shimmers/sketches/genuary.cljs
@@ -5,21 +5,27 @@
    [shimmers.common.ui.svg :as usvg]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.core :as g]
+   [thi.ng.geom.rect :as rect]
    [thi.ng.geom.svg.core :as svg]
-   [thi.ng.geom.vector :as gv]
-   [thi.ng.math.core :as tm]))
+   [thi.ng.geom.vector :as gv]))
 
 (def width 800)
 (def height 600)
 (defn rv [x y]
   (gv/vec2 (* width x) (* height y)))
 
-(defn letter [box character]
-  (csvg/group {}
-    (svg/text (tm/+ (:p box) (rv 0.1 0.1)) character)))
+(defn letter [region character]
+  (let [box (g/scale-size region 0.9)]
+    (csvg/group {}
+      box
+      (svg/text (rect/bottom-left box) character)
+      (svg/text (rect/top-right box) character))))
 
 (defn shapes [word bounds]
-  (let [letter-boxes (g/subdivide bounds {:rows 1 :cols (count word)}) ]
+  (let [cgroup (g/center (first (g/subdivide (g/scale-size bounds 0.99) {:rows 3 :cols 1}))
+                         (rv 0.5 0.5))
+        letter-boxes
+        (g/subdivide cgroup {:rows 1 :cols (count word)}) ]
     (mapv letter letter-boxes (seq word))))
 
 (defn scene [{:keys [scene-id]}]
@@ -28,7 +34,7 @@
      :width width
      :height height
      :stroke "black"
-     :fill "white"
+     :fill "none"
      :stroke-width 0.5}
     (shapes "Genuary"
             (csvg/screen width height))))
