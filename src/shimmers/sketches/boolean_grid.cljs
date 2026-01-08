af4df1bd750753e54e0632835c8e82b2f2413e16
Charles Comstock <dgtized@gmail.com>
2026-01-08 00:44:51 -0600

    add a weird energy simulation thing

--- src/shimmers/sketches/boolean_grid.cljs
+++ src/shimmers/sketches/boolean_grid.cljs
@@ -17,19 +17,69 @@
    [thi.ng.geom.rect :as rect]
    [thi.ng.math.core :as tm]))
 
+(defn tree-annotate [{:keys [circles] :as state}]
+  (let [tree (saq/add-to-circletree (saq/circletree (cq/screen-rect)) circles)]
+    (assoc state
+           :tree tree
+           :traversal (saq/simple-traversal-tree tree))))
+
 (defn setup []
   (q/color-mode :hsl 1.0)
-  (let [circles (mapv (fn [p] (let [r (/ (tm/clamp (dr/gaussian 1.0 0.4) 0.1 2.0) 14.0)]
-                               (vary-meta (gc/circle p (* 0.5 r (q/height)))
-                                          assoc :state (dr/chance 0.3))))
-                      (rp/poisson-disc-sampling (cq/screen-rect 0.875) 150))]
-    {:circles circles
-     :tree (saq/add-to-circletree (saq/circletree (cq/screen-rect)) circles)}))
+  (let [circles (map-indexed (fn [i p]
+                               (let [r (/ (tm/clamp (dr/gaussian 1.0 0.4) 0.1 2.0) 14.0)
+                                     on (dr/chance 0.3)]
+                                 (vary-meta (gc/circle p (* 0.5 r (q/height)))
+                                            assoc
+                                            :id i
+                                            :state on
+                                            :energy (if on 1.0 0.0))))
+                             (rp/poisson-disc-sampling (cq/screen-rect 0.875) 150))]
+    (tree-annotate {:circles circles})))
+
+(defn xor [a b]
+  (cond (and a b) false
+        a true
+        b true
+        :else false))
+
+(defn force-push
+  [{:keys [p r] :as circle} bounds neighborhood]
+  (let [close (g/closest-point bounds p)
+        pressure (tm/normalize (tm/- p close) (/ (* 1.33 r) (g/dist p close)))
+        forces (reduce (fn [f n]
+                         (let [c (g/get-point-data n)]
+                           (tm/+ f (tm/normalize (tm/- p (:p c))
+                                                 (/ (* 1.1 r) (g/dist p (:p c)))))))
+                       pressure neighborhood)]
+    (g/translate circle forces)))
+
+(defn update-circle-state [circles tree]
+  (for [c circles]
+    (let [neighborhood (rest (saq/k-nearest-neighbors tree 3 (:p c)))
+          mc (meta c)
+          on (:state mc)
+          energy (:energy mc)
+          neighbor-states
+          (for [neighbor neighborhood]
+            (:state (meta (g/get-point-data neighbor))))
+          next-on (if (< 0.02 energy 0.98)
+                    on
+                    (xor on (reduce xor neighbor-states)))]
+      (vary-meta (if on
+                   (force-push c (g/bounds tree) neighborhood)
+                   c)
+                 assoc
+                 :state next-on
+                 :energy (tm/mix* energy (if next-on 1.0 0.0)
+                                  (dr/random 0.01 0.05))))))
+
+(defn update-neighborhood [{:keys [tree] :as state}]
+  (update state :circles update-circle-state tree))
 
 (defn update-state [state]
-  state)
+  (tree-annotate (update-neighborhood state)))
 
-(defn draw [{:keys [circles tree]}]
+(defn draw [{:keys [circles tree traversal]}]
   (q/ellipse-mode :radius)
   (q/background 1.0)
   (q/stroke 0.0 1.0)
@@ -37,8 +87,9 @@
     (qdg/draw circle))
 
   (q/stroke 0.0 0.2)
-  (doseq [{:keys [r] :as node} (saq/simple-traversal-tree tree)
+  (doseq [{:keys [r] :as node} traversal
           :let [circle (:d node)]]
+    (q/no-fill)
     (let [neighbors (saq/k-nearest-neighbors tree 4 (:p circle))]
       (comment (qdg/draw (apply rect/rect r)))
       (doseq [overlap (keep (fn [neighbor]
@@ -47,9 +98,8 @@
                                 (g/get-point-data neighbor)))
                             neighbors)]
         (qdg/draw (gl/line2 (:p circle) (:p overlap))))
-      (if (:state (meta circle))
-        (q/fill 0.0 0.25)
-        (q/no-fill))
+      (when (:state (meta circle))
+        (q/fill 0.0 (* (:energy (meta circle)) 0.25)))
       (qdg/draw (g/scale-size circle 0.9)))))
 
 (defn page []
