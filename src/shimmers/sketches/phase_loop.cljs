544d14542fcf7a7967fcd1ed4e7cecad2251cbaf
Charles Comstock <dgtized@gmail.com>
2026-02-01 01:29:37 -0600

    randomly decide if vertical/horizontal or both

--- src/shimmers/sketches/phase_loop.cljs
+++ src/shimmers/sketches/phase_loop.cljs
@@ -1,17 +1,23 @@
 (ns shimmers.sketches.phase-loop
   (:require
+   [clojure.math :as math]
    [quil.core :as q :include-macros true]
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.math.equations :as eq]
    [shimmers.sketch :as sketch :include-macros true]
-   [thi.ng.math.core :as tm]
-   [clojure.math :as math]))
+   [thi.ng.math.core :as tm]))
 
 (defn setup []
-  (q/color-mode :hsl 1.0))
+  (q/color-mode :hsl 1.0)
+  (let [[h v] (dr/weighted {[true true] 1.0
+                            [true false] 1.0
+                            [false true] 1.0})]
+    {:horizontal h
+     :vertical v}))
 
 (defn update-state [state]
   state)
@@ -24,12 +30,18 @@
   (eq/unit-cos (* eq/TAU (+ x (* 0.3 t)
                        (math/sin (+ x t))))))
 
-(defn draw [_]
+;; TODO: rotation?
+(defn draw [{:keys [horizontal vertical]}]
   (q/background 1.0)
   (let [t (* 0.001 (q/millis))]
-    (doseq [x (tm/norm-range 90)]
-      (q/line (cq/rel-vec x (f1 x t))
-              (cq/rel-vec x (f2 x t))))))
+    (when horizontal
+      (doseq [x (tm/norm-range 90)]
+        (q/line (cq/rel-vec x (f1 x t))
+                (cq/rel-vec x (f2 x t)))))
+    (when vertical
+      (doseq [y (tm/norm-range 90)]
+        (q/line (cq/rel-vec (f2 y t) y)
+                (cq/rel-vec (f1 y t) y))))))
 
 (defn page []
   [:div
