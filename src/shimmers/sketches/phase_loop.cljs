377791e5ce515723003bed3aa5a68aa77a7eafc4
Charles Comstock <dgtized@gmail.com>
2026-02-01 20:11:44 -0600

    tweak some parameters for more generalization

--- src/shimmers/sketches/phase_loop.cljs
+++ src/shimmers/sketches/phase_loop.cljs
@@ -17,31 +17,35 @@
                             [true false] 1.0
                             [false true] 1.0})]
     {:horizontal h
-     :vertical v}))
+     :vertical v
+     :r1 (dr/random 0.1 1.0)
+     :r2 (dr/random 0.1 1.0)
+     :p1 (dr/random)
+     :p2 (dr/random)}))
 
 (defn update-state [state]
   state)
 
 (defn f1 [x t]
   (eq/unit-sin (* eq/TAU (+ x (* 0.1 t)
-                       (* 0.2 (math/cos (+ x t)))))))
+                       (* 0.2 (math/cos (+ x (* 0.4 t))))))))
 
 (defn f2 [x t]
-  (eq/unit-cos (* eq/TAU (+ x (* 0.3 t)
-                       (math/sin (+ x t))))))
+  (eq/unit-cos (* eq/TAU (+ x (* 0.2 t)
+                       (math/sin (+ x (* 0.55 t)))))))
 
 ;; TODO: rotation?
-(defn draw [{:keys [horizontal vertical]}]
+(defn draw [{:keys [horizontal vertical r1 r2 p1 p2]}]
   (q/background 1.0)
   (let [t (* 0.001 (q/millis))]
     (when horizontal
-      (doseq [x (tm/norm-range 90)]
-        (q/line (cq/rel-vec x (f1 x t))
-                (cq/rel-vec x (f2 x t)))))
+      (doseq [x (tm/norm-range 80)]
+        (q/line (cq/rel-vec x (f1 (+ (* r1 x) p1) t))
+                (cq/rel-vec x (f2 (+ (* r2 x) p2) t)))))
     (when vertical
-      (doseq [y (tm/norm-range 90)]
-        (q/line (cq/rel-vec (f2 y t) y)
-                (cq/rel-vec (f1 y t) y))))))
+      (doseq [y (tm/norm-range 80)]
+        (q/line (cq/rel-vec (f2 (+ (* r1 y) p1) t) y)
+                (cq/rel-vec (f1 (+ (* r2 y) p2) t) y))))))
 
 (defn page []
   [:div
