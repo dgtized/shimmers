f04197b7d9c85350f8030cc04a39a2ac7a99088b
Charles Comstock <dgtized@gmail.com>
2026-02-02 21:11:18 -0600

    meh change with added debug info

--- src/shimmers/sketches/phase_loop.cljs
+++ src/shimmers/sketches/phase_loop.cljs
@@ -5,6 +5,7 @@
    [quil.middleware :as m]
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
+   [shimmers.common.string :as scs]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.math.equations :as eq]
@@ -35,24 +36,27 @@
   (eq/unit-cos (* eq/TAU (+ x (* 0.2 t)
                        (math/sin (+ x (* 0.55 t)))))))
 
-(defn rotate [p t]
+(defn rotate [p r]
   (let [c (cq/rel-vec 0.5 0.5)]
-    (tm/+ c (g/rotate (tm/- p c) (* 0.01 t)))))
+    (tm/+ c (g/rotate (tm/- p c) r))))
 
 ;; TODO: more rotation or axis-rotation?
 (defn draw [{:keys [horizontal vertical r1 r2 p1 p2]}]
   (q/background 1.0)
   (let [t (* 0.001 (q/millis))
-        rx (* 2 eq/TAU (tm/smoothstep* 0.5 1.0 (math/cos (+ (* 0.17 t) p1))))
-        ry (* 2 eq/TAU (tm/smoothstep* 0.5 1.0 (math/sin (+ (* 0.21 t) p2))))]
+        rx (tm/smoothstep* 0.95 1.0 (eq/unit-sin (+ (* 0.00011 t) (* 0.5 p1))))
+        ry (tm/smoothstep* 0.95 1.0 (eq/unit-sin (+ (* 0.00013 t) (* 0.5 p2))))]
+    (q/fill 0.0)
+    (q/text (scs/cl-format "~0,6f ~0,6f" rx ry) 5 10)
+    (q/no-fill)
     (when horizontal
       (doseq [x (tm/norm-range 80)]
-        (q/line (rotate (cq/rel-vec x (f1 (+ (* r1 x) p1) t)) rx)
-                (rotate (cq/rel-vec x (f2 (+ (* r2 x) p2) t)) ry))))
+        (q/line (rotate (cq/rel-vec x (f1 (+ (* r1 x) p1) t)) (+ rx p2))
+                (rotate (cq/rel-vec x (f2 (+ (* r2 x) p2) t)) (+ ry p1)))))
     (when vertical
       (doseq [y (tm/norm-range 80)]
-        (q/line (rotate (cq/rel-vec (f2 (+ (* r1 y) p1) t) y) (- rx))
-                (rotate (cq/rel-vec (f1 (+ (* r2 y) p2) t) y) (- ry)))))))
+        (q/line (rotate (cq/rel-vec (f2 (+ (* r1 y) p1) t) y) (- p1 rx))
+                (rotate (cq/rel-vec (f1 (+ (* r2 y) p2) t) y) (- p2 ry)))))))
 
 (defn page []
   [:div
