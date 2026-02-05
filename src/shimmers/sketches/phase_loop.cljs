331754b1351da6b92723fb7624a0e073ba2d43ac
Charles Comstock <dgtized@gmail.com>
2026-02-04 18:53:59 -0600

    push the spacing up and down a little

--- src/shimmers/sketches/phase_loop.cljs
+++ src/shimmers/sketches/phase_loop.cljs
@@ -53,7 +53,8 @@
 (defn spacing [t]
   (map (fn [s]
          (let [k (math/sin (+ (* 0.11 t) (math/cos (+ (* 0.17 t) s))))]
-           (ms/staircase (* 2.0 (bismooth 0.6 1.0 k)) s)))
+           (mod (+ (bismooth 0.5 1.0 (math/sin (+ 1.1 (* 0.13 t) (* 0.2 (math/sin (+ 0.5 t))))))
+                   (ms/staircase (* 2.0 (bismooth 0.6 1.0 k)) s)) 1.0)))
        (tm/norm-range 80)))
 
 ;; FIXME: need to reproject x onto the diagonal lines during rotation
