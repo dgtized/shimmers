e8e14de38fd3148c06fd9726c329ae0ebcf3f7f6
Charles Comstock <dgtized@gmail.com>
2026-02-26 17:53:13 -0600

    more randomness per each starting seed

--- src/shimmers/sketches/bidirectional_growth.cljs
+++ src/shimmers/sketches/bidirectional_growth.cljs
@@ -6,18 +6,19 @@
    [shimmers.common.framerate :as framerate]
    [shimmers.common.quil :as cq]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.math.deterministic-random :as dr]
    [shimmers.math.equations :as eq]
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]))
 
 (defn setup []
   (q/color-mode :hsl 1.0)
-  {})
+  {:seeds (repeatedly 2 dr/random-tau)})
 
 (defn update-state [state]
   state)
 
-(defn draw [_]
+(defn draw [{:keys [seeds]}]
   (q/background 1.0)
   (q/stroke-weight 4.0)
   (let [t (* (q/millis) 0.0005)
@@ -28,10 +29,10 @@
         base (* eq/TAU (math/sin (+ a (* 0.08 t))))]
     (doseq [s (range (- w1) w1 0.05)]
       (let [w2 (* 0.05 (math/sin (+ w1 (math/sin (+ (* 0.66 t) (* 2.81 s) base)))))
-            v1 (* 0.15 (math/sin (+ s (* 0.2 t) base)))
+            v1 (* 0.15 (math/sin (+ s (* 0.2 t) base (nth seeds 0))))
             b (* 0.5 (math/sin (+ s a (* 0.25 t))))]
         (doseq [v (range (- w2) w2 0.02)]
-          (let [vw (+ 0.6 (eq/unit-cos (+ (* 0.21 eq/TAU s) (* 0.35 t))))
+          (let [vw (+ 0.6 (eq/unit-cos (+ (* 0.21 eq/TAU s) (* 0.35 t) (nth seeds 1))))
                 p1 (-> center
                        (v/+polar (cq/rel-h (+ radius v)) (+ (* 1.07 s) (* 0.66 t) a))
                        (v/+polar (cq/rel-h (* vw (+ v v1))) (+ (* 2.19 s) (* 0.1 t) b)))
