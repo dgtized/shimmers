4169f9b4d842a064a9cb2a711d1fbd30b43cb782
Charles Comstock <dgtized@gmail.com>
2026-03-22 12:25:18 -0500

    tweak the wording a bit

--- src/shimmers/view/about.cljs
+++ src/shimmers/view/about.cljs
@@ -8,8 +8,8 @@
    Some of visualize algorithms or are graphical tests, while others explore
    generative constraints of a particular visual idea."]
    [:p "Some are interactive, some are static, some process video from a webcam."]
-   [:p "My philosophy with this project was to publish whatever ideas I had as
-   they came to me, as such none of these sketches are necessarily complete, and
-   I avoid self-curating as I found that stifled my ability to explore freely.
-   Some of them I have revisited and tuned years later, and some I never even
-   made it far enough to explore whatever concept I had."]])
+   [:p "My philosophy with this project was to publish whatever ideas I had as I
+   explored them, as such none of these sketches are necessarily complete. I
+   avoid self-curating as I found that stifled my ability to explore freely.
+   Some of them I have revisited and tuned years later, and some never made it
+   far enough to explore whatever concept I had."]])
