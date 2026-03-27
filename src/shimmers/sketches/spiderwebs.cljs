4a6992c35741f63373ff33edafcdc56b9b9f6069
Charles Comstock <dgtized@gmail.com>
2026-03-27 01:49:16 -0500

    convert spiderwebs to use usvg/page

--- src/shimmers/sketches/spiderwebs.cljs
+++ src/shimmers/sketches/spiderwebs.cljs
@@ -2,9 +2,9 @@
   (:require
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.common.ui.svg :as usvg]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
-   [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
@@ -55,4 +55,4 @@
   {:created-at "2022-11-03"
    :type :svg
    :tags #{}}
-  (ctrl/mount (view-sketch/static-page scene :spiderwebs)))
+  (ctrl/mount (usvg/page sketch-args scene)))
