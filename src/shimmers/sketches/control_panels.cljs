e6ce115a4530a48ddb3b6d571fce04530b066014
Charles Comstock <dgtized@gmail.com>
2026-03-27 01:50:17 -0500

    convert control-panels to usvg/page

--- src/shimmers/sketches/control_panels.cljs
+++ src/shimmers/sketches/control_panels.cljs
@@ -4,12 +4,12 @@
    [shimmers.algorithm.square-packing :as square]
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.common.ui.svg :as usvg]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.math.equations :as eq]
    [shimmers.math.geometry :as geometry]
    [shimmers.math.vector :as v]
    [shimmers.sketch :as sketch :include-macros true]
-   [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.circle :as gc]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
@@ -433,4 +433,4 @@
   {:created-at "2022-02-07"
    :type :svg
    :tags #{}}
-  (ctrl/mount (view-sketch/static-page scene :control-panels)))
+  (ctrl/mount (usvg/page sketch-args scene)))
