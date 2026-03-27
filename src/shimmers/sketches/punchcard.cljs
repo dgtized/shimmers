041a2018f03aba600ad6d887d94c41d898f0705f
Charles Comstock <dgtized@gmail.com>
2026-03-27 01:48:20 -0500

    convert punchcards to usvg/page

--- src/shimmers/sketches/punchcard.cljs
+++ src/shimmers/sketches/punchcard.cljs
@@ -2,9 +2,9 @@
   (:require
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.common.ui.svg :as usvg]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
-   [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.rect :as rect]
    [thi.ng.geom.vector :as gv]
@@ -38,4 +38,4 @@
   {:created-at "2022-01-22"
    :type :svg
    :tags #{}}
-  (ctrl/mount (view-sketch/static-page scene :punchcard)))
+  (ctrl/mount (usvg/page sketch-args scene)))
