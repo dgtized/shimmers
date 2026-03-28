403a639d354d7366672b27d114c7d074fa4e8a1b
Charles Comstock <dgtized@gmail.com>
2026-03-27 19:11:23 -0500

    convert harsh-lines to usvg/page

--- src/shimmers/sketches/harsh_lines.cljs
+++ src/shimmers/sketches/harsh_lines.cljs
@@ -2,9 +2,9 @@
   (:require
    [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
+   [shimmers.common.ui.svg :as usvg]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.sketch :as sketch :include-macros true]
-   [shimmers.view.sketch :as view-sketch]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
    [thi.ng.geom.vector :as gv]
@@ -60,4 +60,4 @@
   {:created-at "2021-04-07"
    :type :svg
    :tags #{:static :deterministic}}
-  (ctrl/mount (view-sketch/static-page scene :harsh-lines)))
+  (ctrl/mount (usvg/page sketch-args scene)))
