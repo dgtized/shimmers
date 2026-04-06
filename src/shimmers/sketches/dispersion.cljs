639802487e03bd1c61cb4fbc940be59d97c6de61
Charles Comstock <dgtized@gmail.com>
2026-04-06 14:04:49 -0500

    convert dispersion to svg as it's static

--- src/shimmers/sketches/dispersion.cljs
+++ src/shimmers/sketches/dispersion.cljs
@@ -1,27 +1,26 @@
 (ns shimmers.sketches.dispersion
   (:require
-   [quil.core :as q :include-macros true]
-   [quil.middleware :as m]
-   [shimmers.common.quil :as cq]
+   [shimmers.common.svg :as csvg]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.common.ui.svg :as usvg]
    [shimmers.math.deterministic-random :as dr]
    [shimmers.math.geometry :as geometry]
    [shimmers.sketch :as sketch :include-macros true]
+   [thi.ng.color.core :as col]
    [thi.ng.geom.core :as g]
    [thi.ng.geom.rect :as rect]
    [thi.ng.geom.vector :as gv]
    [thi.ng.math.core :as tm]))
 
-(defn setup []
-  (q/no-loop)
-  (q/color-mode :hsl 1.0)
-  {})
+(def width 900)
+(def height 600)
+(defn rv [x y]
+  (gv/vec2 (* width x) (* height y)))
 
 (defn rdirection []
   (tm/* (gv/vec2 (* 0.6 (abs (dr/gaussian 0 3.5)))
                  (* -0.4 (abs (dr/gaussian 0 3.5))))
-        (cq/rel-w 0.09)))
+        (* width 0.09)))
 
 (defn epicenter-distance [epicenter max-dist s]
   (/ (g/dist epicenter (g/centroid s)) max-dist))
@@ -40,10 +39,8 @@
            s))
        shapes))
 
-(defn draw [_]
-  (q/background 1.0)
-  (q/stroke-weight 0.2)
-  (let [building (rect/rect (cq/rel-pos 0.1 0.4) (cq/rel-w 0.3) (cq/rel-h 0.6))
+(defn shapes []
+  (let [building (rect/rect (rv 0.1 0.4) (* width 0.3) (* height 0.6))
         [_ ne _ sw] (g/vertices building)
         max-dist (g/dist ne sw)
         tessellated (g/tessellate building {:num 48})
@@ -51,26 +48,26 @@
         shapes (possibly-disperse (fn [s] (- 2.0 (epicenter-distance ne (/ max-dist 2.8) s)))
                                   (fn [_] (rdirection))
                                   divided)]
-    (doseq [shape shapes]
-      (q/stroke-weight 0.02)
-      (q/fill (+ (* 0.005 (dr/gaussian 0 3.5)) 0.55)
-              (+ (* 0.01 (dr/gaussian 0 3.5)) 0.6)
-              (-> (epicenter-distance sw (q/width) shape)
-                  (tm/clamp 0.25 0.75))
-              0.9)
-      (cq/draw-polygon shape))))
+    (for [shape shapes]
+      (vary-meta shape assoc
+                 :fill
+                 (col/as-css (col/hsla (+ (* 0.005 (dr/gaussian 0 3.5)) 0.55)
+                                       (+ (* 0.01 (dr/gaussian 0 3.5)) 0.6)
+                                       (-> (epicenter-distance sw width shape)
+                                           (tm/clamp 0.25 0.75))
+                                       0.9))))))
 
-(defn page [sketch-args]
-  [sketch/with-explanation
-   (sketch/component
-     :size [900 600]
-     :setup setup
-     :draw draw
-     :middleware [m/fun-mode])
-   [:p.center [usvg/generate-link sketch-args]]])
+(defn scene [{:keys [scene-id]}]
+  (csvg/svg-timed {:id scene-id
+                   :width width
+                   :height height
+                   :stroke "white"
+                   :fill "none"
+                   :stroke-width 0.02}
+    (shapes)))
 
 (sketch/definition dispersion
   {:created-at "2021-03-10"
    :tags #{:static :deterministic}
    :type :quil}
-  (ctrl/mount (partial page sketch-args)))
+  (ctrl/mount (usvg/page sketch-args scene)))
