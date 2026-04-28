bbd03ce2e84aaadc128cc500766d0f085b3baa8d
Charles Comstock <dgtized@gmail.com>
2026-04-28 02:26:28 -0500

    generate more interesting triangles

--- src/shimmers/sketches/boundary_testing.cljs
+++ src/shimmers/sketches/boundary_testing.cljs
@@ -7,20 +7,26 @@
    [shimmers.common.quil-draws-geom :as qdg]
    [shimmers.common.ui.controls :as ctrl]
    [shimmers.math.deterministic-random :as dr]
+   [shimmers.math.equations :as eq]
    [shimmers.sketch :as sketch :include-macros true]
    [thi.ng.geom.circle :as gc]
+   [thi.ng.geom.core :as g]
    [thi.ng.geom.line :as gl]
    [thi.ng.geom.polygon :as gp]
    [thi.ng.geom.rect :as rect]
    [thi.ng.geom.triangle :as gt]
-   [thi.ng.math.core :as tm]
-   [thi.ng.geom.core :as g]))
+   [thi.ng.math.core :as tm]))
+
+(defn interesting-triangle [a b]
+  (let [m (tm/mix a b (dr/random))
+        perp (g/scale (g/rotate (tm/- b a) (* eq/TAU 0.25 (dr/rand-nth [-1 1]))) (dr/random 0.2 1.0))]
+    (gt/triangle2 a (tm/+ m perp) b)))
 
 (defn random-shape []
   (let [point-gen (fn [] (cq/rel-vec (dr/random 0.3 0.7) (dr/random 0.3 0.7)))]
     (dr/weighted
      [[(gl/line2 (point-gen) (point-gen)) 1.0]
-      [(gt/triangle2 (point-gen) (point-gen) (point-gen)) 1.0]
+      [(interesting-triangle (point-gen) (point-gen)) 1.0]
       [(rect/rect (point-gen) (point-gen)) 1.0]
       [(gp/polygon2 (repeatedly (dr/random-int 3 9) point-gen)) 1.0]
       [(gc/circle (point-gen) (cq/rel-h (dr/random 0.1 0.25)))
